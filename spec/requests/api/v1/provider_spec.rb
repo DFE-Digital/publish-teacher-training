5c31fb5d96af385e438b2c7c9de79c3a733ecf06
Jake Benilov <benilov@gmail.com>
2018-12-21 10:52:33 +0000

    Add more detail to the provider requester spec
    This makes the spec more verbose but provides more explicit regression coverage
    and hopefully documents the API in the process.

--- spec/requests/api/v1/provider_spec.rb
+++ spec/requests/api/v1/provider_spec.rb
@@ -3,7 +3,16 @@ require "rails_helper"
 RSpec.describe "Providers API", type: :request do
   describe 'GET index' do
     before do
-      FactoryBot.create_list(:provider, 10)
+      provider = FactoryBot.create(:provider,
+        provider_name: "ACME SCITT",
+        provider_code: "A123",
+        provider_type: 'Y',
+        site_count: 0)
+      FactoryBot.create(:site,
+        location_name: "Main site",
+        code: "-",
+        provider: provider)
+
       get "/api/v1/providers", headers: { 'HTTP_AUTHORIZATION' => ActionController::HttpAuthentication::Basic.encode_credentials("bat", "beta") }
     end
 
@@ -13,7 +22,22 @@ RSpec.describe "Providers API", type: :request do
 
     it "JSON body response contains expected provider attributes" do
       json = JSON.parse(response.body)
-      expect(json[0].keys).to match_array(%w[accrediting_provider campuses institution_code institution_name institution_type])
+      expect(json). to eq(
+        [
+          {
+            "accrediting_provider" => nil,
+            "campuses" => [
+              {
+                "campus_code" => "-",
+                "name" => "Main site",
+              }
+            ],
+            "institution_code" => "A123",
+            "institution_name" => "ACME SCITT",
+            "institution_type" => "Y",
+          },
+        ]
+      )
     end
   end
 end
