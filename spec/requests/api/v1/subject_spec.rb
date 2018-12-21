d1bca5f953a598ad4c7917948a8de088fb0feb40
Jake Benilov <benilov@gmail.com>
2018-12-21 11:48:54 +0000

    More explicit request spec

--- spec/requests/api/v1/subject_spec.rb
+++ spec/requests/api/v1/subject_spec.rb
@@ -3,7 +3,13 @@ require "rails_helper"
 RSpec.describe "Subjecs API", type: :request do
   describe 'GET index' do
     before do
-      FactoryBot.create_list(:subject, 10)
+      FactoryBot.create(:subject,
+        subject_name: "Mathematics",
+        subject_code: "B1")
+      FactoryBot.create(:subject,
+        subject_name: "Biology",
+        subject_code: "M4")
+
       get "/api/v1/subjects", headers: { 'HTTP_AUTHORIZATION' => ActionController::HttpAuthentication::Basic.encode_credentials("bat", "beta") }
     end
 
@@ -13,7 +19,16 @@ RSpec.describe "Subjecs API", type: :request do
 
     it "JSON body response contains expected provider attributes" do
       json = JSON.parse(response.body)
-      expect(json[0].keys).to match_array(%w[subject_name subject_code])
+      expect(json).to eq([
+        {
+          "subject_name" => "Mathematics",
+          "subject_code" => "B1",
+        },
+        {
+          "subject_name" => "Biology",
+          "subject_code" => "M4",
+        },
+      ])
     end
   end
 end
