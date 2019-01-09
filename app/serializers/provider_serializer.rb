05474132e75b66c462b24d26fa1ad54006d529c2
Dan Mitchell <dan@danmitchell.co.uk>
2019-01-09 17:40:10 +0000

    Refactor contact addresses

--- app/serializers/provider_serializer.rb
+++ app/serializers/provider_serializer.rb
@@ -19,44 +19,4 @@ class ProviderSerializer < ActiveModel::Serializer
   def accrediting_provider
     # TODO: pull this thru from UCAS import
   end
-
-  def address1
-    if object.enrichments.present?
-      object.enrichments.last.json_data["Address1"]
-    else
-      object.address1
-    end
-  end
-
-  def address2
-    if object.enrichments.present?
-      object.enrichments.last.json_data["Address2"]
-    else
-      object.address2
-    end
-  end
-
-  def address3
-    if object.enrichments.present?
-      object.enrichments.last.json_data["Address3"]
-    else
-      object.address3
-    end
-  end
-
-  def address4
-    if object.enrichments.present?
-      object.enrichments.last.json_data["Address4"]
-    else
-      object.address4
-    end
-  end
-
-  def postcode
-    if object.enrichments.present?
-      object.enrichments.last.json_data["Postcode"]
-    else
-      object.postcode
-    end
-  end
 end
