b94e6989223816097a4fe32c2644473a490975b6
defong <de_fong2k@yahoo.com>
2019-01-31 13:44:53 +0000

    Added code for bad connection

---
+++ spec/controllers/api/v1/providers_controller_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe Api::V1::ProvidersController, type: :controller do
+  before do
+    controller.request.env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Token.encode_credentials('bats')
+    def controller.index
+      raise PG::ConnectionBad
+    end
+  end
+
+  describe "index" do
+    it "render service unavailable" do
+      get :index
+      expect(response).to have_http_status(:service_unavailable)
+      json = JSON.parse(response.body)
+      expect(json). to eq(
+        'code' => 503, 'status' => 'Service Unavailable'
+      )
+    end
+  end
+end
