ce62f17d80049a71d6af804d575ef15e55203656
De Fong <de_fong2k@yahoo.com>
2019-02-01 10:48:33 +0000

    503 for bad db connection (#79)
    * Added code for bad connection
    * Fixed code review issues

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,17 @@ class ApplicationController < ActionController::API
   include ActionController::HttpAuthentication::Token::ControllerMethods
 
   before_action :authenticate
+  rescue_from PG::ConnectionBad, with: :render_service_unavailable
 
   def authenticate
     authenticate_or_request_with_http_token do |token|
       ActiveSupport::SecurityUtils.secure_compare(token, Rails.application.config.authentication_token)
     end
+  end
+
+private
+
+  def render_service_unavailable
+    render json: { code: 503, status: 'Service Unavailable' }.to_json, status: 503
   end
 end
---
+++ spec/controllers/api/v1/providers_controller_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe Api::V1::ProvidersController, type: :controller do
+  describe "index" do
+    it "render service unavailable" do
+      allow(controller).to receive(:index).and_raise(PG::ConnectionBad)
+      allow(controller).to receive(:authenticate)
+
+      get :index
+      expect(response).to have_http_status(:service_unavailable)
+      json = JSON.parse(response.body)
+      expect(json). to eq(
+        'code' => 503, 'status' => 'Service Unavailable'
+      )
+    end
+  end
+end
