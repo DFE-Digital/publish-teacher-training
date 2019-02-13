19a1463a436836d68880bd8d1a54989b31a012ba
Misha Gorodnitzky <misaka@pobox.com>
2019-02-13 18:32:53 +0000

    [914] Add implementation of authenticate that uses JWT

--- Gemfile
+++ Gemfile
@@ -38,6 +38,9 @@ gem 'sentry-raven'
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: %i[mingw mswin x64_mingw jruby]
 
+# For encoding/decoding web token used for authentication
+gem 'jwt'
+
 # Formalise config settings with support for env vars
 gem 'config'
 
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,17 @@ class ApplicationController < ActionController::API
   before_action :authenticate
   rescue_from PG::ConnectionBad, with: :render_service_unavailable
 
+  # TODO: move the JWT token authentication to a V2 controller, and restrict
+  #       the current authentication to V1
   def authenticate
     authenticate_or_request_with_http_token do |token|
-      ActiveSupport::SecurityUtils.secure_compare(token, Rails.application.config.authentication_token)
+      # ActiveSupport::SecurityUtils.secure_compare(token, Rails.application.config.authentication_token)
+      (json_payload, options) = JWT.decode(token,
+                                           Settings.authentication.secret,
+                                           Settings.authentication.encoding)
+      payload = JSON.parse(json_payload)
+      @user = User.find_by(email: payload['email'])
+      @user.present?
     end
   end
 
