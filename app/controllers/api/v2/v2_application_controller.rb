7552353a5e44a96939c2a9bf74fb60b2febdb06d
defong <de_fong2k@yahoo.com>
2019-02-14 12:34:37 +0000

    [914] Fixed minor issues, forgot to add file to commit

---
+++ app/controllers/api/v2/v2_application_controller.rb
@@ -0,0 +1,20 @@
+module Api
+  module V2
+    class V2ApplicationController < ApplicationController
+      include ActionController::HttpAuthentication::Token::ControllerMethods
+
+      before_action :authenticate
+
+      def authenticate
+        authenticate_or_request_with_http_token do |token|
+          (json_payload, _options) = JWT.decode(token,
+                                               Settings.authentication.secret,
+                                               Settings.authentication.encoding)
+          payload = JSON.parse(json_payload)
+          @user = User.find_by(email: payload['email'])
+          @user.present?
+        end
+      end
+    end
+  end
+end
---
+++ spec/controllers/api/v2/v2_application_controller_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+describe Api::V2::V2ApplicationController, type: :controller do
+  describe '#authenticate' do
+    let(:user) { create(:user) }
+    let(:payload) { { email: user.email } }
+    let(:encoded_token) do
+      JWT.encode payload.to_json,
+                 Settings.authentication.secret,
+                 Settings.authentication.encoding
+    end
+
+    before do
+      controller.response = response
+      request.headers['Authorization'] = "Bearer #{encoded_token}"
+    end
+
+    subject { controller.authenticate }
+
+    it { should be true }
+
+    context 'user is not valid' do
+      let(:payload) { { email: 'foobar' } }
+
+      it { should eq "HTTP Token: Access denied.\n" }
+
+      it 'requests authentication via the http header' do
+        controller.authenticate
+
+        expect(response.headers['WWW-Authenticate'])
+          .to eq 'Token realm="Application"'
+      end
+    end
+  end
+end
