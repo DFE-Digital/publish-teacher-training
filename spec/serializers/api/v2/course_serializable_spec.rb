28d41d6a9dba0b64e1dc6dd19fc36fb45e65844b
Jake Benilov <benilov@gmail.com>
2019-02-18 14:12:59 +0000

    [950] Add Courses API v2 with specs and serializer

---
+++ spec/serializers/api/v2/course_serializable_spec.rb
@@ -0,0 +1,15 @@
+require "rails_helper"
+
+RSpec.describe describe API::V2::CourseSerializable do
+  let(:course) { create(:course, start_date: Time.now.utc) }
+  let(:resource) { API::V2::CourseSerializable.new object: course }
+
+  it 'sets type to courses' do
+    expect(resource.jsonapi_type).to eq :courses
+  end
+
+  subject { resource.as_jsonapi.to_json }
+
+  it { should be_json.with_content(type: 'courses') }
+  it { should be_json.with_content(course.start_date.iso8601).at_path("attributes.start_date") }
+end
