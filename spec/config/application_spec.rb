f84ac5377d7775028cee9e0edf8cc0f4de838d64
Misha Gorodnitzky <misaka@pobox.com>
2019-02-19 12:03:45 +0000

    [875] handle PG connection errors in ActionDispatcher
    This was originally implemented in the controller, but we realised that this
    will be hiding stack traces in development and is better implemented in
    ActionDispatch (which is the way Sau oroginally wanted to do it1). We've
    reverted to this implemention and added a super-simple (naive) configuration
    spec to test it.

--- spec/config/application_spec.rb
+++ spec/config/application_spec.rb
@@ -5,4 +5,9 @@ RSpec.describe 'Application config' do
     expect(Rails.application.config.action_dispatch.rescue_responses['Pundit::NotAuthorizedError'])
       .to eq :forbidden
   end
+
+  it 'configures Pg::ConnectionBad to return service_unavailable' do
+    expect(Rails.application.config.action_dispatch.rescue_responses['PG::ConnectionBad'])
+      .to eq :service_unavailable
+  end
 end
