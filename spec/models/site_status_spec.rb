ecb7688ba532bdc87fd885bb85fbade3cee19075
Jake Benilov <benilov@gmail.com>
2019-02-19 18:27:05 +0000

    [995] Inline custom matchers into spec that uses them

--- spec/models/site_status_spec.rb
+++ spec/models/site_status_spec.rb
@@ -14,6 +14,24 @@ require 'rails_helper'
 
 
 RSpec.describe SiteStatus, type: :model do
+  RSpec::Matchers.define :be_findable do
+    match do |actual|
+      SiteStatus.findable.include?(actual)
+    end
+  end
+
+  RSpec::Matchers.define :be_open_for_applications do
+    match do |actual|
+      SiteStatus.open_for_applications.include?(actual)
+    end
+  end
+
+  RSpec::Matchers.define :have_vacancies do
+    match do |actual|
+      SiteStatus.with_vacancies.include?(actual)
+    end
+  end
+
   describe 'associations' do
     it { should belong_to(:site) }
     it { should belong_to(:course) }
