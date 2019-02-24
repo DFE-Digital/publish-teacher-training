b2350329fd77a30e26788c53e5de7585200289b4
Jake Benilov <benilov@gmail.com>
2019-02-24 13:18:57 +0000

    [1014] Document what the WithQualifications.profpost_flag values mean
    This is taken from the UCAS Teacher Training Set-up Guide 2018.

--- app/models/concerns/with_qualifications.rb
+++ app/models/concerns/with_qualifications.rb
@@ -2,10 +2,34 @@ module WithQualifications
   extend ActiveSupport::Concern
 
   included do
+    # The training programme outcome that originated in the UCAS NetUpdate system.
+    #
+    # See [UCAS Teacher Training Set-up Guide](https://www.ucas.com/file/115581/download?token=mv-G6P53),
+    # page 32
     enum profpost_flag: {
+
+      # Recommendation for QTS: the student will not receive an academic teacher
+      # training qualification on successful completion of the
+      # programme.
       recommendation_for_qts: "",
+
+      # Professional: the student will receive a Professional Graduate
+      # Certificate of Education (offered at Level 6) or Professional
+      # Graduate Diploma in Education (PGDE), with no credits or
+      # modules at postgraduate (master’s) Level 7 on successful
+      # completion of the programme.
       professional: "PF",
+
+      # Postgraduate: the student will receive a Postgraduate Certificate
+      # of Education (PGCE) or other qualification which includes at
+      # least one module or some credits at postgraduate
+      # (master’s) Level 7 on successful completion the
+      # programme.
       postgraduate: "PG",
+
+      # Both professional and postgraduate: the student has the option of taking at least one
+      # postgraduate (master’s) Level 7 module or obtaining some
+      # postgraduate-level credits as part of the programme.
       professional_postgraduate: "BO",
     }
 
