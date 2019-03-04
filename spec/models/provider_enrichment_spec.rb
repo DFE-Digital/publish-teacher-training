d78aab3f451189091782dd493b8b130ceca422a6
David Gisbey <davidgisbey@gmail.com>
2019-03-04 16:15:43 +0000

    [1083] remove tests for #touch_provider in provider enrichment spec

--- spec/models/provider_enrichment_spec.rb
+++ spec/models/provider_enrichment_spec.rb
@@ -16,21 +16,4 @@
 require 'rails_helper'
 
 describe ProviderEnrichment, type: :model do
-  describe '#touch_provider' do
-    let(:provider) { create(:provider) }
-
-    it 'sets changed_at to the current time' do
-      Timecop.freeze do
-        provider.enrichments.update(email: "test@email")
-        expect(provider.changed_at).to eq Time.now.utc
-      end
-    end
-
-    it 'leaves updated_at unchanged' do
-      timestamp = 1.hour.ago
-      provider.update updated_at: timestamp
-      provider.enrichments.update(email: "test@email")
-      expect(provider.updated_at).to eq timestamp
-    end
-  end
 end
