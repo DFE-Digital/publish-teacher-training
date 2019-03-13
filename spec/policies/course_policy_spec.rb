7deba7847c4fd3a13e7523e5857bf70cd3942e5e
defong <de_fong2k@yahoo.com>
2019-03-13 17:05:59 +0000

    [1137] Added policy tests for show

--- spec/policies/course_policy_spec.rb
+++ spec/policies/course_policy_spec.rb
@@ -9,4 +9,32 @@ describe CoursePolicy do
       expect(subject.index?).to be_truthy
     end
   end
+
+  describe 'show?' do
+    let(:user) { create(:user) }
+    let(:organisation) { create(:organisation, users: [user]) }
+    let(:course) { create(:course) }
+    let!(:provider) {
+      create(:provider,
+              course_count: 0,
+              courses: [course],
+              organisations: [organisation])
+    }
+
+    context 'when accessing a course of a provider that the user belongs provider' do
+      subject { CoursePolicy.new(user, course) }
+
+      it 'allows the :show action' do
+        expect(subject.show?).to be_truthy
+      end
+    end
+
+    context 'when accessing a course of a provider that the user does not have' do
+      subject { CoursePolicy.new(create(:user), course) }
+
+      it 'allows the :show action' do
+        expect(subject.show?).to be_falsey
+      end
+    end
+  end
 end
