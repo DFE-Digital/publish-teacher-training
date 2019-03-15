ac371554eacfb7986fece67a7c056a319521416e
Jake Benilov <benilov@gmail.com>
2019-03-15 22:10:48 +0000

    [1131] Add vac_status validation
    vac_status needs to be compatible with the course's study mode

--- app/models/site_status.rb
+++ app/models/site_status.rb
@@ -14,6 +14,9 @@
 class SiteStatus < ApplicationRecord
   self.table_name = "course_site"
 
+  validate :vac_status_must_be_consistent_with_course_study_mode,
+    if: Proc.new { |s| s.course&.study_mode.present? }
+
   enum vac_status: {
     both_full_time_and_part_time_vacancies: "B",
     part_time_vacancies: "P",
@@ -43,4 +46,27 @@ class SiteStatus < ApplicationRecord
   }
   scope :with_vacancies, -> { where.not(vac_status: :no_vacancies) }
   scope :open_for_applications, -> { findable.applications_being_accepted_now.with_vacancies }
+
+private
+
+  def vac_status_must_be_consistent_with_course_study_mode
+    unless vac_status_consistent_with_course_study_mode?
+      errors.add(:vac_status, "(#{vac_status}) must be consistent with course study mode #{course.study_mode}")
+    end
+  end
+
+  def vac_status_consistent_with_course_study_mode?
+    case vac_status
+    when 'no_vacancies'
+      true
+    when 'full_time_vacancies'
+      course.full_time? || course.full_time_or_part_time?
+    when 'part_time_vacancies'
+      course.part_time? || course.full_time_or_part_time?
+    when 'both_full_time_and_part_time_vacancies'
+      course.full_time_or_part_time?
+    else
+      false
+    end
+  end
 end
