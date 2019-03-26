628bbab0fe2c960c1a5a270d6b317c1c25f03c9f
Theodor Vararu <theo@vararu.org>
2019-03-26 12:38:42 +0000

    [1133] Add faraday and webmock

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -114,6 +114,8 @@ RSpec.configure do |config|
   # as the one that triggered the failure.
   Kernel.srand config.seed
 =end
+  require 'webmock/rspec'
+
   config.include JSONAPI::RSpec
 
   # Load shared examples
