66915a01134b074649915d796105f79d4bae1120
Heidar Bernhardsson <heidarbn@gmail.com>
2019-03-27 22:37:34 +0000

    Add missing annotation

--- spec/serializers/contact_serializer_spec.rb
+++ spec/serializers/contact_serializer_spec.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: contact
+#
+#  id          :bigint(8)        not null, primary key
+#  provider_id :integer          not null
+#  type        :text             not null
+#  name        :text
+#  email       :text
+#  telephone   :text
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 require "rails_helper"
 
 describe ContactSerializer do
