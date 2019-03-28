73c31f89a70428eccd83716e6b5515fb2fd64a5d
Jake Benilov <benilov@gmail.com>
2019-03-28 23:12:50 +0000

    [1160] Extract before_action in courses controller

--- app/controllers/api/v2/courses_controller.rb
+++ app/controllers/api/v2/courses_controller.rb
@@ -2,6 +2,7 @@ module API
   module V2
     class CoursesController < API::V2::ApplicationController
       before_action :build_provider
+      before_action :build_course, except: :index
 
       def index
         authorize @provider, :can_list_courses?
@@ -11,14 +12,18 @@ module API
       end
 
       def show
-        course = authorize Course.where(provider: @provider).find_by!(course_code: params[:code].upcase)
-        render jsonapi: course, include: [site_statuses: [:site]]
+        render jsonapi: @course, include: [site_statuses: [:site]]
       end
 
     private
 
       def build_provider
         @provider = Provider.find_by!(provider_code: params[:provider_code].upcase)
+      end
+
+      def build_course
+        @course = Course.where(provider: @provider).find_by!(course_code: params[:code].upcase)
+        authorize @course
       end
     end
   end
