d8e7edfde04ecdc2a115db6178dd65912f2356c8
Jake Benilov <benilov@gmail.com>
2019-04-01 18:23:18 +0100

    [1216] Add CourseEnrichment model

---
+++ app/models/course_enrichment.rb
@@ -0,0 +1,18 @@
+# == Schema Information
+#
+# Table name: course_enrichment
+#
+#  id                           :integer          not null, primary key
+#  created_by_user_id           :integer
+#  created_at                   :datetime         not null
+#  provider_code                :text             not null
+#  json_data                    :jsonb
+#  last_published_timestamp_utc :datetime
+#  status                       :integer          not null
+#  ucas_course_code             :text             not null
+#  updated_by_user_id           :integer
+#  updated_at                   :datetime         not null
+#
+
+class CourseEnrichment < ApplicationRecord
+end
