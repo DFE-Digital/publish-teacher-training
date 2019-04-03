487809c959853e4157f0d4aab58b5173cef3f7c5
Theodor Vararu <theo@vararu.org>
2019-04-03 15:19:11 +0100

    [1210] Expose ucas_status on course serializer

--- spec/serializers/api/v2/serializable_course_spec.rb
+++ spec/serializers/api/v2/serializable_course_spec.rb
@@ -13,4 +13,5 @@ describe API::V2::SerializableCourse do
   it { should be_json.with_content(type: 'courses') }
   it { should be_json.with_content(course.start_date.iso8601).at_path("attributes.start_date") }
   it { should be_json.with_content(course.content_status.to_s).at_path("attributes.content_status") }
+  it { should be_json.with_content(course.ucas_status.to_s).at_path("attributes.ucas_status") }
 end
