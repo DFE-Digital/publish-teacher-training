90e39e9d08bd2dbe76e8e8b2982bdab23a11398a
Heidar Bernhardsson <heidarbn@gmail.com>
2019-04-04 15:10:48 +0100

    [1259] Add test for including accrediting provider

--- spec/serializers/api/v2/serializable_course_spec.rb
+++ spec/serializers/api/v2/serializable_course_spec.rb
@@ -3,29 +3,61 @@ require "rails_helper"
 describe API::V2::SerializableCourse do
   let(:jsonapi_renderer) { JSONAPI::Serializable::Renderer.new }
   let(:course)           { create(:course, start_date: Time.now.utc) }
-  let(:provider)         { course.provider }
   let(:course_json) do
     jsonapi_renderer.render(
       course,
       class: {
-        Course: API::V2::SerializableCourse,
-        Provider: API::V2::SerializableProvider
-      },
-      include: [
-        :provider
-      ]
+        Course: API::V2::SerializableCourse
+      }
     ).to_json
   end
   let(:parsed_json) { JSON.parse(course_json) }
 
-  subject { parsed_json['included'] }
+  subject { parsed_json['data'] }
 
-  it { should include(have_type('providers').and(have_id(provider.id.to_s))) }
+  it { should have_type('courses') }
+  it { should have_attributes(:start_date, :content_status, :ucas_status) }
 
-  describe 'data' do
-    subject { parsed_json['data'] }
+  context 'with a provider' do
+    let(:provider) { course.provider }
+    let(:course_json) do
+      jsonapi_renderer.render(
+        course,
+        class: {
+          Course:   API::V2::SerializableCourse,
+          Provider: API::V2::SerializableProvider
+        },
+        include: [
+          :provider
+        ]
+      ).to_json
+    end
 
-    it { should have_type('courses') }
-    it { should have_attributes(:start_date, :content_status, :ucas_status) }
+    it { should have_relationship(:provider) }
+
+    it 'includes the provider' do
+      expect(parsed_json['included'])
+        .to(include(have_type('providers')
+          .and(have_id(provider.id.to_s))))
+    end
+  end
+
+  context 'with an accrediting_provider' do
+    let(:course) { create(:course, :with_accrediting_provider) }
+    let(:accrediting_provider) { course.accrediting_provider }
+    let(:course_json) do
+      jsonapi_renderer.render(
+        course,
+        class: {
+          Course:   API::V2::SerializableCourse,
+          Provider: API::V2::SerializableProvider
+        },
+        include: [
+          :accrediting_provider
+        ]
+      ).to_json
+    end
+
+    it { should have_relationship(:accrediting_provider) }
   end
 end
