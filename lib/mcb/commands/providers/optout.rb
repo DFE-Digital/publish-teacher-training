d0dfdf51ca29c0bec1b8c1ec6bb58f036ea68b50
Misha Gorodnitzky <misaka@pobox.com>
2019-04-10 18:08:27 +0100

    [1281] allow optin and optout to perform on multiple providers

--- lib/mcb/commands/providers/optout.rb
+++ lib/mcb/commands/providers/optout.rb
@@ -1,13 +1,18 @@
 summary 'Opt-out the provider'
-param :code
-usage 'optout <provider_code>'
+usage 'optin <provider_code1 [provider_code2 ...]>'
 
 run do |opts, args, _cmd|
   MCB.init_rails(opts)
 
-  provider_code = args[:code]
-
-  provider = Provider.find_by!(provider_code: provider_code)
-  provider.update(opted_in: false)
-  provider.courses.each { |c| c.touch(:changed_at) }
+  Provider.connection.transaction do
+    args.each do |provider_code|
+      provider = Provider.find_by!(provider_code: provider_code)
+      verbose "updating provider #{provider_code}"
+      provider.update(opted_in: false)
+      provider.courses.each do |c|
+        verbose "  updating course #{c.course_code}"
+        c.touch(:changed_at)
+      end
+    end
+  end
 end
