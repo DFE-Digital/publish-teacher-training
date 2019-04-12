475c7ac661ba0a0d8d505e586e4ef0b0d4ccd682
defong <de_fong2k@yahoo.com>
2019-04-12 20:40:42 +0100

    [1269] Added get subject list and related items

--- app/services/subject_mapper.rb
+++ app/services/subject_mapper.rb
@@ -28,7 +28,7 @@ class SubjectMapper
                         mandarin
                         punjabi]
 
-  @ucas_wfl_welsh = %w[welsh]
+  @ucas_mfl_welsh = %w[welsh]
 
   @ucas_design_and_tech = ["design and technology",
                            "design and technology (food)",
@@ -63,7 +63,7 @@ class SubjectMapper
                    "primary",
                    "lower primary"]
 
-  @ucas_language_cat = ["language",
+  @ucas_language_cat = ["languages",
                         "languages (african)",
                         "languages (asian)",
                         "languages (european)"]
@@ -90,26 +90,19 @@ class SubjectMapper
                       "environmental science",
                       "law"]
 
-  @ucas_rename = {
-    "chinese" => "mandarin",
+  @ucas_rename =     { "chinese" => "mandarin",
     "art / art & design" => "art and design",
     "business education" => "business studies",
     "computer studies" => "computing",
     "science" => "balanced science",
     "dance and performance" => "dance",
     "drama and theatre studies" => "drama",
-    "social science" => "social sciences"
-  }
-
-  @ucas_needs_mention_in_title = {
-      "humanities" => /humanities/,
+    "social science" => "social sciences" }
+  @ucas_needs_mention_in_title = { "humanities" => /humanities/,
       "science" => /(?<!social |computer )science/,
-      "modern studies" => /modern studies/
-  }
-
+      "modern studies" => /modern studies/ }
   def self.is_further_education(subjects)
     subjects = subjects.map { |subject| (subject.strip! || subject).downcase }
-
     (subjects & @ucas_further_education).any?
   end
 
@@ -117,5 +110,153 @@ class SubjectMapper
     res = (@ucas_rename[ucas_subject] || ucas_subject).capitalize
 
     (res.sub "english", "English" || res)
+  end
+
+  def self.map_to_secondary_subjects(course_title, ucas_subjects)
+    secondary_subjects = []
+
+      # Does the subject list mention maths?
+    if (ucas_subjects & @ucas_mathemtics).any?
+      secondary_subjects.push("Mathematics")
+    end
+
+      # Does the subject list mention physics?
+    if (ucas_subjects & @ucas_physics).any?
+      secondary_subjects.push("Physics")
+    end
+
+      # Does the subject list mention D&T?
+    if (ucas_subjects & @ucas_design_and_tech).any?
+      secondary_subjects.push("Design and technology")
+    end
+
+      # Does the subject list mention Classics?
+    if (ucas_subjects & @ucas_classics).any?
+      secondary_subjects.push("Classics")
+    end
+
+
+      # Does the subject list mention Mandarin Chinese
+    if (ucas_subjects & @ucas_mfl_mandarin).any?
+      secondary_subjects.push("Mandarin")
+    end
+
+      #  Does the subject list mention a mainstream foreign language
+    (ucas_subjects & @ucas_mfl_main).each do |ucas_subject|
+      secondary_subjects.push(map_to_subject_name(ucas_subject))
+    end
+
+      #  Does the subject list mention languages but hasn't already been covered?
+      pp ucas_subjects
+      pp @ucas_language_cat
+      pp @ucas_mfl_mandarin
+      pp @ucas_mfl_main
+
+    if (ucas_subjects & @ucas_language_cat).any? && (ucas_subjects & @ucas_mfl_mandarin).none? && (ucas_subjects & @ucas_mfl_main).none?
+      secondary_subjects.push("Modern languages (other)")
+    end
+
+      # Does the subject list mention one or more sciences?
+    (ucas_subjects & @ucas_science_fields).each do |ucas_subject|
+      secondary_subjects.push(map_to_subject_name(ucas_subject))
+    end
+      # Does the subject list mention a subject we are happy to translate directly?
+    (ucas_subjects & @ucas_direct_translation_secondary).each do |ucas_subject|
+      secondary_subjects.push(map_to_subject_name(ucas_subject))
+    end
+      # Does the subject list mention a subject we are happy to translate if the course title contains a mention?
+    (ucas_subjects & @ucas_needs_mention_in_title.keys).each do |ucas_subject|
+      if course_title.match?(@ucas_needs_mention_in_title[ucas_subject])
+        secondary_subjects.push(map_to_subject_name(ucas_subject))
+      end
+    end
+
+      # Does the subject list mention english, and it's mentioned in the title (or it's the only subject we know for this course)?
+    if (ucas_subjects & @ucas_english).any?
+      if secondary_subjects.none? || course_title.index("english") != nil
+        secondary_subjects.push("English")
+      end
+    end
+
+      # if nothing else yet, try welsh
+    if secondary_subjects.none? && (ucas_subjects & @ucas_mfl_welsh).any?
+      secondary_subjects.push("Welsh")
+    end
+
+    secondary_subjects
+  end
+
+
+        # /// <summary>
+        # /// This maps a list of of UCAS subjects to our interpretation of subjects.
+        # /// UCAS subjects are a pretty loose tagging system where individual tags don't always
+        # /// represent the subjects you will be able to teach but also categories (such as "secundary", "foreign languages" etc)
+        # /// there is also duplication ("chinese" vs "mandarin") and ambiguity
+        # /// (does "science" = Balanced science, a category, or Primary with science?)
+        # ///
+        # /// This takes this list of tags and the course title and applies heuristics to determine
+        # /// which subjects you will be allowed to teach when you graduate, making the subjects more suitable for searching.
+        # /// </summary>
+        # /// <param name="course_title">The name of the course</param>
+        # /// <param name="ucas_subjects">The subject tags from UCAS</param>
+        # /// <returns>An enumerable of all the subjects the course should be findable by.</returns>
+
+
+  def self.map_to_primary_subjects(ucas_subjects)
+    primary_subjects = %w[Primary]
+    ucas_primary_language_specialisation = @ucas_language_cat + @ucas_mfl_main + @ucas_mfl_other
+
+    ucas_primary_science_specialisation = %w[science] + @ucas_physics + @ucas_science_fields
+
+    ucas_primary_geo_hist_specialisation = %w[geography history]
+      # Does the subject list mention English?
+    if((ucas_subjects & @ucas_english).any?)
+      primary_subjects.push("Primary with English")
+    end
+      # Does the subject list mention geography or history?
+    if((ucas_subjects & ucas_primary_geo_hist_specialisation).any?)
+      primary_subjects.push("Primary with geography and history")
+    end
+      # Does the subject list mention maths?
+    if((ucas_subjects & @ucas_mathemtics).any?)
+      primary_subjects.push("Primary with mathematics")
+    end
+      # Does the subject list mention any mfl subject?
+    if((ucas_subjects & ucas_primary_language_specialisation).any?)
+      primary_subjects.push("Primary with modern languages")
+    end
+      # Does the subject list mention PE?
+    if(ucas_subjects.index("physical education") != nil)
+      primary_subjects.push("Primary with physical education")
+    end
+      # Does the subject list mention science?
+
+    if((ucas_subjects & ucas_primary_science_specialisation).any?)
+      primary_subjects.push("Primary with science")
+    end
+    primary_subjects
+  end
+
+  def self.get_subject_list(course_title, ucas_subjects)
+    ucas_subjects = ucas_subjects.map { |subject| (subject.strip! || subject).downcase }
+    course_title = (course_title.strip! || course_title).downcase
+    # if unexpected throw.
+    if (ucas_subjects & @ucas_unexpected).any?
+      raise "found unsupported subject name(s): #{(ucas_subjects & @ucas_unexpected) * ', '}"
+    # If the subject indicates that it's primary, do not associate it with any
+    # Secondary subjects (that happens a lot in UCAS data). Instead, mark it as primary
+    # and additionally test for specialisations (e.g. Pimary with mathematics)
+    # note a course can cover multiple specialisations, e.g. Primary with geography and Primary with history
+    elsif (ucas_subjects & @ucas_primary).any?
+      return map_to_primary_subjects(ucas_subjects)
+    # If the subject indicates that it's in the Further Education space,
+    # just assign Further education to it and do not associate it with any
+    # secondary subjects
+    elsif (ucas_subjects & @ucas_further_education).any?
+      return ["Further education"]
+    # The most common case is when the course is teaching secondary subjects.
+    else
+      return map_to_secondary_subjects(course_title, ucas_subjects)
+    end
   end
 end
--- spec/services/subject_mapper_spec.rb
+++ spec/services/subject_mapper_spec.rb
@@ -61,4 +61,149 @@ describe SubjectMapper do
       it { should eq "Bad English" }
     end
   end
+
+  describe "#get_subject_list" do
+
+  specs = [
+    {
+      course_title: "",
+      ucas_subjects: %w[primary english],
+      expected_subjects: ["Primary", "Primary with English"],
+      test_case: "an example of a primary specialisation"
+    },
+    {
+      course_title: "",
+      ucas_subjects: %w[primary physics],
+      expected_subjects: ["Primary", "Primary with science"],
+      test_case: "another example of a primary specialisation"
+    },
+    {
+      course_title: "Early Years",
+      ucas_subjects: ["primary", "early years"],
+      expected_subjects: %w[Primary],
+
+test_case: "an example of early years (which is absorbed into primary)"    },
+    {
+      course_title: "Physics (Welsh medium)",
+      ucas_subjects: ["physics (abridged)", "welsh", "secondary", "science"],
+      expected_subjects: %w[Physics],
+test_case: "an example where science should be excluded because it's used as a category"    },
+
+
+    {
+      course_title: "Physics",
+      ucas_subjects: %w[physics secondary science english],
+      expected_subjects: %w[Physics],
+      test_case: "examples of how the title is considered when adding additional subjects"
+    },
+    {
+      course_title: "Physics with English",
+      ucas_subjects: %w[physics secondary science english],
+      expected_subjects: %w[Physics English],
+      test_case: "examples of how the title is considered when adding additional subjects"
+    },
+    {
+      course_title: "Physics with Science",
+      ucas_subjects: %w[physics secondary science english],
+      expected_subjects: ["Physics", "Balanced science"],
+      test_case: "examples of how the title is considered when adding additional subjects"
+    },
+    {
+      course_title: "Physics with Science and English",
+      ucas_subjects: %w[physics secondary science english],
+      expected_subjects: ["Physics", "Balanced science", "English"],
+      test_case: "examples of how the title is considered when adding additional subjects"
+    },
+    {
+      course_title: "Further ed",
+      ucas_subjects: ["further education", "numeracy"],
+      expected_subjects: ["Further education"],
+      test_case: "further education examplpe"
+    },
+    {
+      course_title: "MFL (Chinese)",
+      ucas_subjects: ["secondary", "languages", "languages (asian)", "chinese"],
+      expected_subjects: %w[Mandarin],
+test_case: "a rename"
+    },
+    {
+      course_title: "",
+      ucas_subjects: %w[secondary welsh],
+      expected_subjects: %w[Welsh],
+test_case: "an example of welsh, which only triggers if nothing else goes"    },
+
+    {
+      course_title: "Computer science",
+      ucas_subjects: ["computer studies", "science"],
+      expected_subjects: %w[Computing],
+test_case: "here science is used as a category"    },
+    {
+      course_title: "Computer science with Science",
+      ucas_subjects: ["computer studies", "science"],
+      expected_subjects: ["Computing", "Balanced science"],
+      test_case: "here it is explicit"
+    },
+
+    {
+      course_title: "Primary with Mathematics",
+      ucas_subjects: %w[primary mathematics],
+      expected_subjects: ["Primary", "Primary with mathematics"],
+      test_case: "bug fix test:  accidentally included maths in the list of sciences"
+    },
+
+    {
+      course_title: "Mfl",
+      ucas_subjects: %w[languages],
+      expected_subjects: ["Modern languages (other)"],
+      test_case: "mfl"
+    },
+    {
+      course_title: "Latin",
+      ucas_subjects: %w[latin],
+      expected_subjects: %w[Classics],
+test_case: "latin and classics have been merged"    },
+    {
+      course_title: "Primary (geo)",
+      ucas_subjects: %w[primary geography],
+      expected_subjects: ["Primary", "Primary with geography and history"],
+      test_case: "Primary with hist/geo have beeen merged"
+    },
+    {
+      course_title: "Primary (history)",
+      ucas_subjects: %w[primary history],
+      expected_subjects: ["Primary", "Primary with geography and history"],
+      test_case: "Primary with hist/geo have beeen merged"
+    },
+
+    {
+      course_title: "Computing",
+      ucas_subjects: ["secondary", "computer studies", "information communication technology"],
+      expected_subjects: %w[Computing],
+test_case: "no ICT"    },
+
+    {
+      course_title: "Mandarin and ESOL",
+      ucas_subjects: ["mandarin", "english as a second or other language"],
+      expected_subjects: ["Mandarin", "English as a second or other language"],
+      test_case: "secondary ESOL"
+    },
+    {
+      course_title: "PCET ESOL",
+      ucas_subjects: ["further education", "english as a second or other language"],
+      expected_subjects: ["Further education"],
+      test_case: "secondary ESOL"
+    },
+  ]
+        # public void MapToSearchAndCompareCourse(string courseTitle","string commaSeparatedUcasSubjects","string commaSeparatedExpectedSubjects)
+
+
+
+        specs.each do |spec|
+            describe "Test case '#{spec[:test_case]}''" do
+              subject { SubjectMapper.get_subject_list(spec[:course_title], spec[:ucas_subjects]) }
+
+              it { should match_array spec[:expected_subjects] }
+            end
+          end
+        end
 end
