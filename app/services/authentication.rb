7fae3a8f2f6dbd9c8f161a047965c465c3b96218
Heidar Bernhardsson <heidarbn@gmail.com>
2019-04-24 14:39:43 +0100

    [1337] Add authentication service class
    This is identical from the original, except it looks up by
    sign_in_user_id first, then falls back to email.
    
    It also has an additional guard against matching with an empty string
    email.

---
+++ app/services/authentication.rb
@@ -0,0 +1,61 @@
+class Authentication
+  attr_accessor :encoded_token, :user
+
+  def self.call(encoded_token)
+    new(encoded_token).call
+  end
+
+  def initialize(encoded_token)
+    @encoded_token = encoded_token
+  end
+
+  def call
+    @user = user_by_sign_in_user_id || user_by_email
+    update_user_email if user_email_does_not_match_token?
+
+    user
+  end
+
+private
+
+  def decoded_token
+    @decoded_token ||= JWT.decode(
+      encoded_token,
+      Settings.authentication.secret,
+      Settings.authentication.algorithm
+    )
+    (decoded_token_payload, _algorithm) = @decoded_token
+
+    decoded_token_payload
+  end
+
+  def email_from_token
+    decoded_token['email']&.downcase
+  end
+
+  def sign_in_user_id_from_token
+    decoded_token['sign_in_user_id']
+  end
+
+  def user_by_email
+    return unless email_from_token.present?
+
+    User.find_by("lower(email) = ?", email_from_token)
+  end
+
+  def user_by_sign_in_user_id
+    return unless sign_in_user_id_from_token.present?
+
+    User.find_by(sign_in_user_id: sign_in_user_id_from_token)
+  end
+
+  def user_email_does_not_match_token?
+    return unless user
+
+    user.email&.downcase != email_from_token
+  end
+
+  def update_user_email
+    user.update(email: email_from_token)
+  end
+end
---
+++ spec/services/authentication_spec.rb
@@ -0,0 +1,93 @@
+describe Authentication do
+  describe '.call' do
+    let(:user) { create(:user) }
+
+    subject { described_class.call(encode_token(payload)) }
+
+    def encode_token(payload)
+      JWT.encode(
+        payload,
+        Settings.authentication.secret,
+        Settings.authentication.algorithm
+      )
+    end
+
+    context 'with a valid DfE-SignIn ID and email' do
+      let(:payload) do
+        {
+          email:           user.email,
+          sign_in_user_id: user.sign_in_user_id
+        }
+      end
+
+      it { should eq user }
+    end
+
+    context 'with a valid DfE-SignIn ID but invalid email' do
+      let(:email) { Faker::Internet.email }
+      let(:payload) do
+        {
+          email:           email,
+          sign_in_user_id: user.sign_in_user_id
+        }
+      end
+
+      it { should eq user }
+      it "update's the user's email" do
+        expect { subject }.to(change { user.reload.email }.to(email))
+      end
+    end
+
+    context 'with a valid email but an invalid DfE-SignIn ID' do
+      let(:payload) do
+        {
+          email:           user.email,
+          sign_in_user_id: SecureRandom.uuid
+        }
+      end
+
+      it { should eq user }
+    end
+
+    context 'with an email that has different case from the database' do
+      let(:payload) { { email: user.email.upcase } }
+
+      before do
+        user.update(email: user.email.capitalize)
+      end
+
+      it { should eq user }
+    end
+
+    context 'when the email is an empty string' do
+      before do
+        user.update_attribute(:email, '')
+      end
+
+      let(:payload) do
+        {
+          email:           '',
+          sign_in_user_id: SecureRandom.uuid
+        }
+      end
+
+      it 'does not authenticate the user based on an empty string match' do
+        expect(subject).not_to eq user
+      end
+    end
+
+    context 'when the sign_in_user_id is nil' do
+      let!(:user) { create(:user, sign_in_user_id: nil) }
+      let(:payload) do
+        {
+          email:           Faker::Internet.email,
+          sign_in_user_id: nil
+        }
+      end
+
+      it 'does not authenticate the user based on a nil match' do
+        expect(subject).not_to eq user
+      end
+    end
+  end
+end
