d20d10e8e79b29674b45471145f5b5e8237e92c5
David Gisbey <davidgisbey@gmail.com>
2019-05-04 22:31:14 +0100

    [1364] Add traits for status to access request factory

--- spec/factories/access_requests.rb
+++ spec/factories/access_requests.rb
@@ -22,8 +22,24 @@ FactoryBot.define do
     email_address { Faker::Internet.email }
     first_name { Faker::Name.first_name }
     last_name { Faker::Name.last_name }
-    organisation { create(:organisation) }
+    organisation { requester.organisations.first }
     reason { Faker::Lorem.sentence.to_s }
     requester_email { requester.email }
+  end
+
+  trait :requested do
+    status { :requested }
+  end
+
+  trait :approved do
+    status { :approved }
+  end
+
+  trait :completed do
+    status { :completed }
+  end
+
+  trait :declined do
+    status { :declined }
   end
 end
