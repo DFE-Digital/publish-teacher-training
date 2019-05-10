f5f2e5d19457bd824e043a023f0148fb12b1dcc1
Theodor Vararu <theo@vararu.org>
2019-05-10 12:06:53 +0100

    [XXXX] Allow course edit_published to edit multiple courses

--- lib/mcb/commands/courses/edit_published.rb
+++ lib/mcb/commands/courses/edit_published.rb
@@ -1,7 +1,5 @@
 name 'edit_published'
 summary 'Edit publisheds course directly in the DB'
-param :provider_code
-param :course_code
 
 ENTRY_REQUIREMENT_OPTIONS = {
   must_have_qualification_at_application_time: 1,
@@ -16,8 +14,14 @@ run do |opts, args, _cmd|
 
   cli = HighLine.new
 
-  provider = Provider.find_by!(provider_code: args[:provider_code])
-  course = provider.courses.find_by!(course_code: args[:course_code])
+  multi_course_mode = args.size > 2
+  if multi_course_mode
+    provider = Provider.find_by!(provider_code: args[0])
+    courses = provider.courses.filter{ |course| args.include?(course.course_code) }
+  else
+    provider = Provider.find_by!(provider_code: args[0])
+    courses = [provider.courses.find_by!(course_code: args[1])]
+  end
 
   flow = :root
   finished = false
@@ -25,12 +29,15 @@ run do |opts, args, _cmd|
     cli.choose do |menu|
       case flow
       when :root
-        puts Terminal::Table.new rows: MCB::CourseShow.new(course).to_h
-        puts "Course status: #{course.ucas_status}"
+        courses.each { |c| puts Terminal::Table.new rows: MCB::CourseShow.new(c).to_h }
 
-        menu.prompt = "Editing course"
+        if multi_course_mode
+          menu.prompt = "Editing multiple courses"
+        else
+          menu.prompt = "Editing course"
+        end
         menu.choice(:exit) { finished = true }
-        menu.choice(:toggle_sites) { flow = :toggle_sites }
+        menu.choice(:toggle_sites) { flow = :toggle_sites } unless multi_course_mode
         menu.choice(:edit_route) { flow = :edit_route }
         menu.choice(:edit_qualifications) { flow = :edit_qualifications }
         menu.choice(:edit_english) { flow = :edit_english }
@@ -39,34 +46,34 @@ run do |opts, args, _cmd|
       when :toggle_sites
         menu.prompt = "Toggling course sites"
         menu.choice(:done) { flow = :root }
-        menu.choices(*(course.actual_and_potential_site_statuses.map(&:description))) do |site_str|
+        menu.choices(*(courses.first.actual_and_potential_site_statuses.map(&:description))) do |site_str|
           site_code = site_str.match(/\(code: (.*)\)/)[1]
-          course.toggle_site(provider.sites.find_by!(code: site_code))
+          courses.first.toggle_site(provider.sites.find_by!(code: site_code))
         end
       when :edit_route
         menu.prompt = "Editing course route"
-        menu.choices(*Course.program_types.keys) { |value| course.program_type = value }
+        menu.choices(*Course.program_types.keys) { |value| courses.each{ |c| c.program_type = value } }
         flow = :root
       when :edit_qualifications
         menu.prompt = "Editing course qualifications"
-        menu.choices(*Course.qualifications.keys) { |value| course.qualification = value }
+        menu.choices(*Course.qualifications.keys) { |value| courses.each{ |c| c.qualification = value } }
         flow = :root
       when :edit_english
         menu.prompt = "Editing course english"
-        menu.choices(*ENTRY_REQUIREMENT_OPTIONS.keys) { |value| course.english = value }
+        menu.choices(*ENTRY_REQUIREMENT_OPTIONS.keys) { |value| courses.each{ |c| c.english = value } }
         flow = :root
       when :edit_maths
         menu.prompt = "Editing course maths"
-        menu.choices(*ENTRY_REQUIREMENT_OPTIONS.keys) { |value| course.maths = value }
+        menu.choices(*ENTRY_REQUIREMENT_OPTIONS.keys) { |value| courses.each{ |c| c.maths = value } }
         flow = :root
       when :edit_science
         menu.prompt = "Editing course science"
-        menu.choices(*ENTRY_REQUIREMENT_OPTIONS.keys) { |value| course.science = value }
+        menu.choices(*ENTRY_REQUIREMENT_OPTIONS.keys) { |value| courses.each{ |c| c.science = value } }
         flow = :root
       end
     end
-    course.save!
-    course.reload
-    course.site_statuses.reload
+    courses.each(&:save!)
+    courses.each(&:reload)
+    courses.first.site_statuses.reload unless multi_course_mode
   end
 end
