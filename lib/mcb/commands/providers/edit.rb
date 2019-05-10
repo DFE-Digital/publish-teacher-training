cc54ece40461807fc3ee969f85b29e9d4b03f9cd
Theodor Vararu <theo@vararu.org>
2019-05-10 12:18:07 +0100

    [XXXX] Add multi-course editing to mcb provider edit

--- lib/mcb/commands/providers/edit.rb
+++ lib/mcb/commands/providers/edit.rb
@@ -11,13 +11,27 @@ run do |opts, args, _cmd|
   courses = provider.courses
 
   finished = false
+  chosen_course_codes = []
   until finished do
     cli.choose do |menu|
-      menu.prompt = "Choose a course"
+      if chosen_course_codes.empty?
+        menu.prompt = "Choose one or multiple courses to edit."
+      else
+        menu.prompt = "Chosen courses: #{chosen_course_codes.join(", ")}"
+      end
+
       menu.choice(:exit) { finished = true }
+
+      unless chosen_course_codes.empty?
+        menu.choice(:edit_selected_courses) do
+          command_params = ['courses', 'edit_published', args[:code], *chosen_course_codes] + (opts[:env].present? ? ['-E', opts[:env]] : [])
+          $mcb.run(command_params)
+          chosen_course_codes = []
+        end
+      end
+
       menu.choices(*courses.map(&:course_code)) do |course_code|
-        command_params = ['courses', 'edit_published', args[:code], course_code] + (opts[:env].present? ? ['-E', opts[:env]] : [])
-        $mcb.run(command_params)
+        chosen_course_codes.push(course_code)
       end
     end
   end
