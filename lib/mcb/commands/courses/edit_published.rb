510ad639aaeaeb432b1738751c1df22cd04af7fc
Jake Benilov <benilov@gmail.com>
2019-05-14 17:29:51 +0100

    Move logic out of mcb command into models

--- lib/mcb/commands/courses/edit_published.rb
+++ lib/mcb/commands/courses/edit_published.rb
@@ -11,49 +11,6 @@ ENTRY_REQUIREMENT_OPTIONS = {
   not_set: nil,
 }.freeze
 
-def vacancy_status(course)
-  case course.study_mode
-  when "full_time"
-    :full_time_vacancies
-  when "part_time"
-    :part_time_vacancies
-  when "full_time_or_part_time"
-    :both_full_time_and_part_time_vacancies
-  else
-    raise "Unexpected study mode #{course.study_mode}"
-  end
-end
-
-def site_choices(course, provider)
-  existing_sites = course.site_statuses.map(&:site)
-  new_sites_to_add = provider.sites - existing_sites
-  site_status_choices = course.site_statuses.map {|ss| "#{ss.site.location_name} (code: #{ss.site.code}) – #{ss.status}/#{ss.publish}" }
-  new_site_choices = new_sites_to_add.map {|s| "#{s.location_name} (code: #{s.code})" }
-
-  site_status_choices + new_site_choices
-end
-
-def toggle_site(course, provider, site_str)
-  existing_sites = course.site_statuses.map(&:site)
-  new_sites_to_add = provider.sites - existing_sites
-  site_code = site_str.match(/\(code: (.*)\)/)[1]
-  if site_status = course.site_statuses.detect { |ss| ss.site.code == site_code }
-    puts "Toggling #{site_status}"
-    site_status.toggle!
-  else
-    is_course_new = course.new? # persist this before we create the site status
-    site_status = SiteStatus.create!(
-      course: course,
-      site: new_sites_to_add.detect { |s| s.code == site_code },
-      vac_status: vacancy_status(course),
-      status: :new_status,
-      applications_accepted_from: Date.today,
-      publish: :unpublished,
-    )
-    site_status.start! unless is_course_new
-  end
-end
-
 run do |opts, args, _cmd|
   MCB.init_rails(opts)
 
@@ -82,8 +39,9 @@ run do |opts, args, _cmd|
       when :toggle_sites
         menu.prompt = "Toggling course sites"
         menu.choice(:done) { flow = :root }
-        menu.choices(*(site_choices(course, provider))) do |site_str|
-          toggle_site(course, provider, site_str)
+        menu.choices(*(course.actual_and_potential_site_statuses.map(&:description))) do |site_str|
+          site_code = site_str.match(/\(code: (.*)\)/)[1]
+          course.toggle_site(provider.sites.find_by!(code: site_code))
         end
       when :edit_route
         menu.prompt = "Editing course route"
