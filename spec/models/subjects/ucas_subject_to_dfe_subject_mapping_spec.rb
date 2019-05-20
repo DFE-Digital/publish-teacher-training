d30dfab3efd96aa6f5950dd1f7dd41ab536f6728
Jake Benilov <benilov@gmail.com>
2019-05-20 23:40:13 +0100

    [1516] Add model spec for UCASSubjectToDFESubjectMapping

---
+++ spec/models/subjects/ucas_subject_to_dfe_subject_mapping_spec.rb
@@ -0,0 +1,48 @@
+module Subjects
+  describe UCASSubjectToDFESubjectMapping do
+    context "when passed a static array of UCAS subjects" do
+      subject { UCASSubjectToDFESubjectMapping.new(["maths", "maths (abridged)"], "Mathematics") }
+
+      it { should be_applicable_to(%w[maths], :anything) }
+      it { should be_applicable_to(["maths (abridged)", "something else"], :anything) }
+      it { should_not be_applicable_to(%w[english], :anything) }
+
+      its(:to_dfe_subject) { should eq("Mathematics") }
+    end
+
+    context "when passed a lambda that matches on the course title" do
+      subject {
+        UCASSubjectToDFESubjectMapping.new(
+          {
+            ucas_subjects: ["english", "english literature"],
+            course_title_matches: ->(course_title) { course_title =~ /english/ }
+          },
+          "English"
+        )
+      }
+
+      it { should be_applicable_to(["english", "english literature", "something else"], "english language") }
+      it { should_not be_applicable_to(["something else"], "english language") }
+      it { should_not be_applicable_to(%w[english], "some other course title") }
+
+      its(:to_dfe_subject) { should eq("English") }
+    end
+
+    context "when passed a lambda that matches on the UCAS subjects list" do
+      subject {
+        UCASSubjectToDFESubjectMapping.new(
+          {
+            ucas_subjects_match: ->(ucas_subjects) { ucas_subjects.size == 2 },
+          },
+          "English"
+        )
+      }
+
+      it { should be_applicable_to(%w[a b], :anything) }
+      it { should_not be_applicable_to(%w[a], :anything) }
+      it { should_not be_applicable_to(%[a b c], :anything) }
+
+      its(:to_dfe_subject) { should eq("English") }
+    end
+  end
+end
