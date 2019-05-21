d417af6fc701da393eb2b576fb600e5a3446bd23
dankmitchell <dan@danmitchell.co.uk>
2019-05-21 22:17:55 +0100

    [1503] Add enrichments to provider endpoint

--- app/serializers/api/v2/serializable_provider.rb
+++ app/serializers/api/v2/serializable_provider.rb
@@ -13,6 +13,43 @@ module API
 
       attributes :provider_code, :provider_name, :accredited_body?, :can_add_more_sites?
 
+
+      attribute :address1 do
+        @object.external_contact_info['address1']
+      end
+
+      attribute :address2 do
+        @object.external_contact_info['address2']
+      end
+
+      attribute :address3 do
+        @object.external_contact_info['address3']
+      end
+
+      attribute :address4 do
+        @object.external_contact_info['address4']
+      end
+
+      attribute :postcode do
+        @object.external_contact_info['postcode']
+      end
+
+      attribute :region_code do
+        @object.external_contact_info['region_code']
+      end
+
+      attribute :telephone do
+        @object.external_contact_info['telephone']
+      end
+
+      attribute :email do
+        @object.external_contact_info['email']
+      end
+
+      attribute :website do
+        @object.external_contact_info['website']
+      end
+
       enrichment_attribute :train_with_us
       enrichment_attribute :train_with_disability
 
