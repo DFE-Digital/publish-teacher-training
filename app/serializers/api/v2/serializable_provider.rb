866ef64d628c838548a047d9f29988f219c19838
dankmitchell <dan@danmitchell.co.uk>
2019-05-21 12:11:59 +0100

    [1503] Expose provider enrichments

--- app/serializers/api/v2/serializable_provider.rb
+++ app/serializers/api/v2/serializable_provider.rb
@@ -1,9 +1,20 @@
 module API
   module V2
     class SerializableProvider < JSONAPI::Serializable::Resource
+      class << self
+        def enrichment_attribute(name, enrichment_name = name)
+          attribute name do
+            @object.enrichments.last&.__send__(enrichment_name)
+          end
+        end
+      end
+
       type 'providers'
 
       attributes :provider_code, :provider_name, :accredited_body?, :can_add_more_sites?
+
+      enrichment_attribute :train_with_us
+      enrichment_attribute :train_with_disability
 
       has_many :sites
 
