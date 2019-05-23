90f2524af03bd76699d927f6103dbe0a8173e0b7
De Fong <de_fong2k@yahoo.com>
2019-05-23 15:03:45 +0100

    [1429] snc serializer part 5 (#403)
    * [1429] Added serialization for the course variant
    
    * [1429] Fixed code reviews

--- app/serializers/search_and_compare/course_serializer.rb
+++ app/serializers/search_and_compare/course_serializer.rb
@@ -5,6 +5,22 @@ module SearchAndCompare
     has_one :provider, key: :Provider, serializer: SearchAndCompare::ProviderSerializer
     has_one :accrediting_provider, key: :AccreditingProvider, serializer: SearchAndCompare::ProviderSerializer
 
+    # TODO: After completion
+    # TASK: Anything that return (ie. default_xxx_value, or attribute(:xxx))
+    #         (int)             0
+    #         (reference type)  nil
+    #         (bool)            false
+    #         (date)            '0001-01-01T00:00:00'
+    #       applies to SearchAndCompare::ProviderSerializer
+    #
+    #       snc should just hydrate default values when omitted
+    #
+    # TASK: strftime('%Y-%m-%dT%H:%M:%S')
+    #       double check that this can be removed
+    #       as long as its a valid date format it should work in snc
+    # TASK: see attribute(:Route)
+    # TASK: see attribute(:Salary)
+
     # Course_default_value_Mapping
     attribute(:Id)                                    { 0 }
     attribute(:ProviderCodeName)                      { nil }
@@ -25,20 +41,40 @@ module SearchAndCompare
 
 
     # Salary_nested_default_value_Mapping
-    attribute(:Salary)                                { default_salary }
+    # TODO: After completion
+    # TASK: Double check is Salary actual in use in snc else drop it
+    attribute(:Salary)                                { default_salary_value }
 
-    def default_salary
+    # Subjects_related_Mapping
+    attribute(:IsSen)                                 { object.is_send? }
+    attribute(:CourseSubjects)                        { course_subjects }
+
+    # Course_variant_Mapping
+    # TODO: After completion
+    # TASK: Route.Name can be blank, snc needs to relax blank rule
+    #       Route.Name can be dropped, snc don't use it
+    #       Course.Route.IsSalaried should become Course.IsSalaried
+    #       Then Route
+    #       Route can be dropped altogether in snc
+    attribute(:Route)                                 { route }
+
+    attribute(:IsSalaried)                            { is_salaried? }
+    attribute(:Mod)                                   { object.description }
+    attribute(:IncludesPgce)                          { include_pgce }
+
+    attribute(:FullTime)                              { object.part_time? ? 3 : 1 }
+    attribute(:PartTime)                              { object.full_time? ? 3 : 1 }
+
+  private
+
+    def default_salary_value
       {
         Minimum: nil,
         Maximum: nil,
       }
     end
 
-    # Subjects_related_Mapping
-    attribute(:IsSen)                                 { object.is_send? }
-    attribute(:CourseSubjects)                        { get_subjects }
-
-    def get_subjects
+    def course_subjects
       # CourseSubject_Mapping
       object.dfe_subjects.map do |subject_name|
         {
@@ -62,6 +98,41 @@ module SearchAndCompare
             }
         }
       end
+    end
+
+    def route
+      route_names = {
+        higher_education_programme: "Higher education programme",
+        school_direct_training_programme: "School Direct training programme",
+        school_direct_salaried_training_programme: "School Direct (salaried) training programme",
+        scitt_programme: "SCITT programme",
+        pg_teaching_apprenticeship: "PG Teaching Apprenticeship",
+      }
+
+      {
+        # Route_default_value_Mapping
+        Id: 0,
+        Courses: nil,
+        # Route_Complex_value_Mapping
+        Name: route_names[object.program_type.to_sym],
+        IsSalaried: is_salaried?
+      }
+    end
+
+    def is_salaried?
+      !object.is_fee_based?
+    end
+
+    def include_pgce
+      include_pgces = {
+        qts: 0,
+        pgce_with_qts: 1,
+        pgde_with_qts: 3,
+        pgce: 5,
+        pgde: 6,
+      }
+
+      include_pgces[object.qualification.to_sym]
     end
   end
 end
