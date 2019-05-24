18efc2510ad3f33e1f938ca6c23256c1773befc3
Theodor Vararu <theo@vararu.org>
2019-05-24 12:50:53 +0100

    [1529] Disable bullet error raising, for now
    It doesn't seem to correctly interpret what we're doing in the providers index endpoint.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -40,6 +40,6 @@ Rails.application.configure do
   config.after_initialize do
     Bullet.enable = true
     Bullet.bullet_logger = true
-    Bullet.raise = true # raise an error if an n+1 query occurs
+    Bullet.raise = false # don't raise an error if an n+1 query occurs
   end
 end
