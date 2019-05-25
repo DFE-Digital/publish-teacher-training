de4cd5ce48d81daddef6f9b119d39b56603d7839
dankmitchell <dan@danmitchell.co.uk>
2019-05-25 15:46:13 +0100

    [1483] Add bursary_amount and scholarship_amount to course

--- spec/models/dfe_subject_spec.rb
+++ spec/models/dfe_subject_spec.rb
@@ -9,6 +9,14 @@ describe DFESubject do
     it { should have_early_career_payments }
 
     it { should eq(DFESubject.new("Mathematics")) }
+
+    it 'returns the #bursary_amount' do
+      expect(subject.bursary_amount).to be_present
+    end
+
+    it 'returns the #scholarship_amount' do
+      expect(subject.scholarship_amount).to be_present
+    end
   end
 
   context "(physical education)" do
@@ -17,5 +25,13 @@ describe DFESubject do
     it { should_not have_bursary }
     it { should_not have_scholarship }
     it { should_not have_early_career_payments }
+
+    it 'returns the #bursary_amount' do
+      expect(subject.bursary_amount).to_not be_present
+    end
+
+    it 'returns the #scholarship_amount' do
+      expect(subject.scholarship_amount).to_not be_present
+    end
   end
 end
