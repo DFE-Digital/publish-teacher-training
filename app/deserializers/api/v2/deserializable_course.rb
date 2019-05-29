572c30ceccdf4d4fb3c74406d398d1168f4d703c
Tim Abell <tim@timwise.co.uk>
2019-05-29 16:23:57 +0100

    [1464] Remove calculated fields from deserializer
    PR feedback - were copied from serializer but don't make sense here as
    you can't update these values.

--- app/deserializers/api/v2/deserializable_course.rb
+++ app/deserializers/api/v2/deserializable_course.rb
@@ -13,19 +13,9 @@ module API
                  :personal_qualities,
                  :qualifications,
                  :salary_details,
-                 :findable?,
-                 :open_for_applications?,
-                 :has_vacancies?,
                  :course_code,
                  :name,
-                 :study_mode,
-                 :description,
-                 :content_status,
-                 :ucas_status,
-                 :funding,
-                 :applications_open_from,
-                 :level,
-                 :is_send?
+                 :study_mode
 
       attribute :required_qualifications do |value|
         { qualifications: value }
