64bc08588decafbb453eaeda1ce7006af09d033a
Misha Gorodnitzky <misaka@pobox.com>
2019-05-31 16:47:53 +0100

    [1566] add mcb provider touch command

---
+++ lib/mcb/commands/providers/touch.rb
@@ -0,0 +1,10 @@
+summary 'Update a provider so that it will be at the top of the apiv1 results'
+usage 'touch <provider_code>'
+param :provider_code, transform: ->(code) { code.upcase }
+
+run do |opts, args, _cmd|
+  MCB.init_rails(opts)
+
+  provider = Provider.find_by(provider_code: args[:provider_code])
+  provider.touch
+end
---
+++ spec/lib/mcb/commands/providers/touch_spec.rb
@@ -0,0 +1,25 @@
+require 'mcb_helper'
+
+describe '"mcb providers touch"' do
+  let(:provider) { create :provider, updated_at: 1.day.ago, changed_at: 1.day.ago }
+
+  it 'updates the providers updated_at' do
+    Timecop.freeze do
+      with_stubbed_stdout do
+        $mcb.run(%W[provider touch #{provider.provider_code}])
+      end
+
+      expect(provider.reload.updated_at).to eq Time.now
+    end
+  end
+
+  it 'updates the providers changed_at' do
+    Timecop.freeze do
+      with_stubbed_stdout do
+        $mcb.run(%W[provider touch #{provider.provider_code}])
+      end
+
+      expect(provider.reload.changed_at).to eq Time.now
+    end
+  end
+end
