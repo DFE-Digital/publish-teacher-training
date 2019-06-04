1d9a20aed94782282f133b1941643243ec2fe249
Jake Benilov <benilov@gmail.com>
2019-06-04 10:04:00 +0100

    [1520] Move course financial incentives logic to a concern
    This makes it a bit clearer that this logic is separate from everything else.

---
+++ app/models/concerns/with_financial_incentives.rb
@@ -0,0 +1,31 @@
+module WithFinancialIncentives
+  extend ActiveSupport::Concern
+
+  included do
+    def has_bursary?
+      dfe_subjects.any?(&:has_bursary?)
+    end
+
+    def has_scholarship_and_bursary?
+      dfe_subjects.any?(&:has_scholarship_and_bursary?)
+    end
+
+    def has_early_career_payments?
+      dfe_subjects.any?(&:has_early_career_payments?)
+    end
+
+    def bursary_amount
+      dfe_subject_which_defines_financial_incentives.bursary_amount
+    end
+
+    def scholarship_amount
+      dfe_subject_which_defines_financial_incentives.scholarship_amount
+    end
+
+  private
+
+    def dfe_subject_which_defines_financial_incentives
+      dfe_subjects.max_by(&:total_bursary_and_early_career_payments_amount)
+    end
+  end
+end
--- app/models/course.rb
+++ app/models/course.rb
@@ -24,6 +24,7 @@
 
 class Course < ApplicationRecord
   include WithQualifications
+  include WithFinancialIncentives
   include ChangedAt
 
   has_associated_audits
@@ -271,31 +272,5 @@ class Course < ApplicationRecord
     to_remove.each { |site| remove_site!(site: site) }
 
     sites.reload
-  end
-
-  def has_bursary?
-    dfe_subjects.any?(&:has_bursary?)
-  end
-
-  def has_scholarship_and_bursary?
-    dfe_subjects.any?(&:has_scholarship_and_bursary?)
-  end
-
-  def has_early_career_payments?
-    dfe_subjects.any?(&:has_early_career_payments?)
-  end
-
-  def bursary_amount
-    dfe_subject_which_defines_financial_incentives.bursary_amount
-  end
-
-  def scholarship_amount
-    dfe_subject_which_defines_financial_incentives.scholarship_amount
-  end
-
-private
-
-  def dfe_subject_which_defines_financial_incentives
-    dfe_subjects.max_by(&:total_bursary_and_early_career_payments_amount)
   end
 end
