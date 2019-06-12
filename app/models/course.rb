715be0590f2e9980a2241e45e91c7b0a34030c3d
Theodor Vararu <theo@vararu.org>
2019-06-12 15:22:10 +0100

    [1571] Use guard clause for course#validate_enrichment

--- app/models/course.rb
+++ app/models/course.rb
@@ -118,18 +118,18 @@ class Course < ApplicationRecord
 
   def validate_enrichment
     latest_enrichment = enrichments.latest_first.first
-    if latest_enrichment.present?
-      latest_enrichment.valid? :publish
-      latest_enrichment.errors.messages.each do |field, _error|
-        # Compute a key of `latest_enrichment__FIELD` to allow the frontend to determine
-        # which field should be linked to from the error title.
-        key = "latest_enrichment__#{field}".to_sym
-        # `full_messages_for` here will remove any `^`s defined in the validator or en.yml.
-        # We still need it for later, so re-add it.
-        # jsonapi_errors will throw if it's given an array, so we call `.first`.
-        message = "^" + latest_enrichment.errors.full_messages_for(field).first.to_s
-        errors.add key, message
-      end
+    return unless latest_enrichment.present?
+
+    latest_enrichment.valid? :publish
+    latest_enrichment.errors.messages.each do |field, _error|
+      # Compute a key of `latest_enrichment__FIELD` to allow the frontend to determine
+      # which field should be linked to from the error title.
+      key = "latest_enrichment__#{field}".to_sym
+      # `full_messages_for` here will remove any `^`s defined in the validator or en.yml.
+      # We still need it for later, so re-add it.
+      # jsonapi_errors will throw if it's given an array, so we call `.first`.
+      message = "^" + latest_enrichment.errors.full_messages_for(field).first.to_s
+      errors.add key, message
     end
   end
 
