e9fba8074abc4527eb89186b7f089e45674abd3d
Theodor Vararu <theo@vararu.org>
2019-06-21 11:10:52 +0100

    [1624] Match word count validator to frontend
    The word count component in the frontend isn't doing anything as sophisticated as the word count library, which causes a mismatch between the frontend and the backend. This changes to the same RegExp used in the frontend, and removes the words_counted library.

--- app/validators/words_count_validator.rb
+++ app/validators/words_count_validator.rb
@@ -2,7 +2,7 @@ class WordsCountValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     return if value.blank?
 
-    if WordsCounted.count(value).token_count > options[:maximum]
+    if value.scan(/\S+/).size > options[:maximum]
       record.errors[attribute] << (options[:message] || "^Reduce the word count for #{attribute.to_s.humanize(capitalize: false)}")
     end
   end
