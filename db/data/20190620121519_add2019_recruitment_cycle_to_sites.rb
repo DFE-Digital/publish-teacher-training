d02d835638150fcff424d1fe4f3e68ad245701f7
Misha Gorodnitzky <misaka@pobox.com>
2019-06-21 12:44:00 +0100

    [1558] use raw SQL when updating recruitment_cycle on sites
    This make the update succeed in <1s (versus ~60s when using AR updates)

--- db/data/20190620121519_add2019_recruitment_cycle_to_sites.rb
+++ db/data/20190620121519_add2019_recruitment_cycle_to_sites.rb
@@ -1,10 +1,14 @@
 class Add2019RecruitmentCycleToSites < ActiveRecord::Migration[5.2]
   def up
     current_recruitment_cycle = RecruitmentCycle.where(year: '2019').first
-    Site.update(recruitment_cycle: current_recruitment_cycle)
+    Site.connection.update <<~EOSQL
+      UPDATE site SET recruitment_cycle_id=#{current_recruitment_cycle.id}
+    EOSQL
   end
 
   def down
-    Site.update(recruitment_cycle: nil)
+    Site.connection.update <<~EOSQL
+      UPDATE site SET recruitment_cycle_id=NULL
+    EOSQL
   end
 end
