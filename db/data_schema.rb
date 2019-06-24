7c7ef6b4271163a93c6b979100a2cadb6f0d3c15
defong <de_fong2k@yahoo.com>
2019-06-24 12:49:25 +0100

    [1673] Fixed broken forward motion due to required mandatory historical data changes

--- db/data_schema.rb
+++ db/data_schema.rb
@@ -1,3 +1,3 @@
 # encoding: UTF-8
 
-DataMigrate::Data.define(version: 20190620121519)
+DataMigrate::Data.define(version: 20190619144634)
--- db/migrate/20190620121519_add_2019_recruitment_cycle_to_sites.rb
+++ db/migrate/20190620121519_add_2019_recruitment_cycle_to_sites.rb
@@ -1,3 +1,10 @@
+# Note: These migrations need to run in the below order
+# 1) AddRecruitmentCycleToSite
+#     Add schema change the foreign id as `null`
+# 2) Add2019RecruitmentCycleToSites
+#     Amend the data (backfill)
+# 3) SetSiteRecruitmentCycleToNotNull
+#     Amend schema the foreign id as `not null`
 class Add2019RecruitmentCycleToSites < ActiveRecord::Migration[5.2]
   def up
     current_recruitment_cycle = RecruitmentCycle.where(year: '2019').first
