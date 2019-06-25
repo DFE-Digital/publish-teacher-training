7a0f497a0d2e4774f3114f81912292851b7c83da
Jake Benilov <benilov@gmail.com>
2019-06-25 22:48:40 +0100

    mcb nctl generate_allocations_reports can generate all the reports

--- lib/mcb/commands/nctl/generate_allocations_reports.rb
+++ lib/mcb/commands/nctl/generate_allocations_reports.rb
@@ -1,19 +1,17 @@
 summary 'Generate allocation reports for a given NCTL ID'
-param :template_path
-param :nctl_id
 usage 'generate_allocations_report <template path> <nctl ID>'
 
 run do |opts, args, _cmd|
   MCB.init_rails(opts)
 
-  nctl_id = args[:nctl_id]
-  verbose "processing NCTL ID: #{nctl_id}"
-
-  template_path = File.expand_path(args[:template_path])
+  template_path = File.expand_path(args[0])
   verbose "template: #{template_path}"
 
-  nctl_organisation = NCTLOrganisation.find_by!(nctl_id: nctl_id)
+  nctl_id = args[1]
+  nctl_organisations = nctl_id.present? ? NCTLOrganisation.where(nctl_id: nctl_id) : NCTLOrganisation.all
 
-  puts "Generating allocations report for #{nctl_organisation.name}"
-  nctl_organisation.save_allocations_report(template_path)
+  nctl_organisations.each do |nctl_organisation|
+    puts "Generating allocations report for #{nctl_organisation}"
+    nctl_organisation.save_allocations_report(template_path)
+  end
 end
