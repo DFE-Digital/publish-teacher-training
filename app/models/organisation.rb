fd8d9a19120bc7b4a27bfd38166ffcd0953d9e60
Jake Benilov <benilov@gmail.com>
2019-06-25 23:46:17 +0100

    Put back Organisation#nctl_organisation_for as it's needed for migration

--- app/models/organisation.rb
+++ app/models/organisation.rb
@@ -11,4 +11,19 @@ class Organisation < ApplicationRecord
   has_and_belongs_to_many :users
   has_and_belongs_to_many :providers
   has_many :nctl_organisations
+
+  # This isn't used by the code anywhere but is needed by the AddNCTLOrganisationIdToProvider migration
+  def nctl_organisation_for(provider)
+    potential_organisations = if provider.accredited_body?
+                                nctl_organisations.accredited_body
+                              else
+                                nctl_organisations.school
+                              end
+
+    if potential_organisations.size <= 1
+      potential_organisations.first
+    else
+      raise "Multiple potential NCTL orgs found: #{potential_organisations.pluck(:nctl_id).join(', ')} for provider #{provider.provider_code}"
+    end
+  end
 end
