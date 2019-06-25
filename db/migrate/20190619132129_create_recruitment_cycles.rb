01817e19f0b1eee672912d4d09b50e61e42e29b2
De Fong <de_fong2k@yahoo.com>
2019-06-25 16:56:35 +0100

    [1673] Fixed broken forward motion due to required mandatory historical data changes (#534)
    * [1673] Fixed broken forward motion due to required mandatory historical data changes
    
    * [1673] Fixed more migration issues

--- db/migrate/20190619132129_create_recruitment_cycles.rb
+++ db/migrate/20190619132129_create_recruitment_cycles.rb
@@ -1,3 +1,9 @@
+# Note: These migrations need to run in the below order
+# 1) CreateRecruitmentCycle
+#     Add schema change the foreign id as `null`
+# 2) CreateRecruitmentCycles
+#     Amend the data (backfill)
+# 3) Then the relevant course and site migration chains
 class CreateRecruitmentCycles < ActiveRecord::Migration[5.2]
   def up
     RecruitmentCycle.create(year: '2019', application_start_date: Date.new(2018, 10, 9))
--- db/migrate/20190619144634_add_2019_recruitment_cycle_to_courses.rb
+++ db/migrate/20190619144634_add_2019_recruitment_cycle_to_courses.rb
@@ -1,3 +1,10 @@
+# Note: These migrations need to run in the below order
+# 1) AddRecruitmentCycleToCourse
+#     Add schema change the foreign id as `null`
+# 2) Add2019RecruitmentCycleToCourses
+#     Amend the data (backfill)
+# 3) SetRecruitmentCycleToNotNull
+#     Amend schema the foreign id as `not null`
 class Add2019RecruitmentCycleToCourses < ActiveRecord::Migration[5.2]
   def up
     current_recruitment_cycle = RecruitmentCycle.where(year: '2019').first
