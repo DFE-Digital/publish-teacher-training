9f2a286383c2fdac0c224ad51733af619aa3e919
Tim Abell <tim@timwise.co.uk>
2019-06-27 16:23:34 +0100

    [1645] mcb - add input file to psql command
    Part of being able to reset qa/staging, but will also be generally
    useful

--- lib/mcb/commands/az/apps/psql.rb
+++ lib/mcb/commands/az/apps/psql.rb
@@ -1,5 +1,7 @@
 name 'psql'
 summary 'connect to the psql server for an app'
+option :f, 'source_file', 'source sql file to pass to psql to run',
+       argument: :optional
 
 instance_eval(&MCB.remote_connect_options)
 
@@ -11,6 +13,8 @@ run do |opts, _args, _cmd|
 
   ENV['PGPASSWORD'] = ENV['DB_PASSWORD']
   psql = "psql -h #{ENV['DB_HOSTNAME']} -U #{ENV['DB_USERNAME']} -d #{ENV['DB_DATABASE']}"
+  source_file = opts[:source_file]
+  psql = "#{psql} --file '#{source_file}'" if source_file
 
   # could have used verbose() here, but it's worth being certain which psql server you are connected to
   puts psql
