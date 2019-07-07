e24c35f3d39ed14de36d76fb72ee46f3e1bd31af
Jake Benilov <benilov@gmail.com>
2019-07-07 15:33:57 +0100

    [1737] mcb providers edit: select and launch multi-course editing

--- lib/mcb/provider_editor.rb
+++ lib/mcb/provider_editor.rb
@@ -2,10 +2,12 @@ module MCB
   class ProviderEditor
     attr_reader :provider
 
-    def initialize(provider:, requester:)
+    def initialize(provider:, requester:, environment: nil)
       @cli = ProviderCLI.new(provider)
       @provider = provider
       @requester = requester
+      @environment = environment
+      @selected_courses = []
 
       check_authorisation
     end
@@ -29,6 +31,7 @@ module MCB
     def main_loop
       choices = [
         "edit provider name",
+        "edit courses",
       ]
       @cli.ask_multiple_choice(prompt: "What would you like to do?", choices: choices)
     end
@@ -36,6 +39,8 @@ module MCB
     def perform_action(choice)
       if choice == "edit provider name"
         edit_provider_name
+      elsif choice == "edit courses"
+        select_courses_to_edit_and_launch_editor
       end
     end
 
@@ -44,12 +49,29 @@ module MCB
       update(provider_name: @cli.ask_name)
     end
 
+    def select_courses_to_edit_and_launch_editor
+      @selected_courses = @cli.multiselect(
+        initial_items: @selected_courses,
+        possible_items: @provider.courses.order(:name),
+      )
+      mcb_courses_edit(@selected_courses.map(&:course_code)) unless @selected_courses.empty?
+    end
+
+    def mcb_courses_edit(course_codes)
+      command_params = ['courses', 'edit', provider.provider_code] + course_codes + environment_options
+      $mcb.run(command_params)
+    end
+
     def update(attrs)
       @provider.update(attrs)
     end
 
     def check_authorisation
       raise Pundit::NotAuthorizedError unless ProviderPolicy.new(@requester, @provider).update?
+    end
+
+    def environment_options
+      @environment.present? ? ['-E', @environment] : []
     end
   end
 end
--- spec/lib/mcb/provider_editor_spec.rb
+++ spec/lib/mcb/provider_editor_spec.rb
@@ -29,6 +29,44 @@ describe MCB::ProviderEditor do
           .from("Original name").to("ACME SCITT")
       end
 
+      describe "(course editing)" do
+        let!(:courses) { create(:course, course_code: 'A01X', name: 'Biology', provider: provider) }
+        let!(:course2) { create(:course, course_code: 'A02X', name: 'History', provider: provider) }
+        let!(:course3) { create(:course, course_code: 'A03X', name: 'Economics', provider: provider) }
+
+        it 'lists the courses for the given provider' do
+          output, = run_editor("edit courses", "continue", "exit")
+          expect(output).to include("[ ] Biology (A01X)", "[ ] History (A02X)", "[ ] Economics (A03X)")
+        end
+
+        it 'invokes course editing on the selected courses' do
+          allow($mcb).to receive(:run)
+
+          run_editor(
+            "edit courses", # choose the option
+            "[ ] Biology (A01X)", # pick the first course
+            "[ ] Economics (A03X)", # pick the second course
+            "continue", # finish selecting courses
+            "exit" # from the command
+          )
+
+          expect($mcb).to have_received(:run).with(%w[courses edit X12 A01X A03X])
+        end
+
+        context "(run against an Azure environment)" do
+          let(:environment) { 'qa' }
+          subject { described_class.new(provider: provider, requester: requester, environment: environment) }
+
+          it 'invokes course editing in the environment that the "providers edit" command was invoked' do
+            allow($mcb).to receive(:run)
+
+            run_editor("edit courses", "[ ] Biology (A01X)", "continue", "exit")
+
+            expect($mcb).to have_received(:run).with(%w[courses edit X12 A01X -E qa])
+          end
+        end
+      end
+
       it 'does nothing upon an immediate exit' do
         expect { run_editor("exit") }.to_not change { provider.reload.provider_name }.
           from("Original name")
