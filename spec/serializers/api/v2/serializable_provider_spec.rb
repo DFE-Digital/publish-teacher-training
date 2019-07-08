4ae40870fa0d38a7131a2348ebf899e065a2cd8c
Misha Gorodnitzky <misaka@pobox.com>
2019-07-08 16:17:34 +0100

    [1564] add recruitment_cycle_year to provider serialiser

--- spec/serializers/api/v2/serializable_provider_spec.rb
+++ spec/serializers/api/v2/serializable_provider_spec.rb
@@ -11,10 +11,9 @@ describe API::V2::SerializableProvider do
   subject { JSON.parse(resource.as_jsonapi.to_json) }
 
   it { should have_type 'providers' }
-  it {
-    should have_attribute(:provider_code).with_value(provider.provider_code)
-    should have_attribute(:provider_name).with_value(provider.provider_name)
-    should have_attribute(:accredited_body?).with_value(true)
-    should have_attribute(:can_add_more_sites?).with_value(true)
-  }
+  it { should have_attribute(:provider_code).with_value(provider.provider_code) }
+  it { should have_attribute(:provider_name).with_value(provider.provider_name) }
+  it { should have_attribute(:accredited_body?).with_value(true) }
+  it { should have_attribute(:can_add_more_sites?).with_value(true) }
+  it { should have_attribute(:recruitment_cycle_year).with_value(provider.recruitment_cycle.year) }
 end
