afe91d323dec6bb3dd02017c461b62835442ba85
Misha Gorodnitzky <misaka@pobox.com>
2019-07-08 16:17:34 +0100

    [1564] add recruitment_cycle_year to provider serialiser

--- spec/serializers/api/v2/serializable_site_spec.rb
+++ spec/serializers/api/v2/serializable_site_spec.rb
@@ -11,13 +11,12 @@ describe API::V2::SerializableSite do
   subject { JSON.parse(resource.as_jsonapi.to_json) }
 
   it { should have_type 'sites' }
-  it {
-    should have_attribute(:location_name).with_value(site.location_name)
-    should have_attribute(:address1).with_value(site.address1)
-    should have_attribute(:address2).with_value(site.address2)
-    should have_attribute(:address3).with_value(site.address3)
-    should have_attribute(:address4).with_value(site.address4)
-    should have_attribute(:postcode).with_value(site.postcode)
-    should have_attribute(:region_code).with_value(site.region_code)
-  }
+  it { should have_attribute(:location_name).with_value(site.location_name) }
+  it { should have_attribute(:address1).with_value(site.address1) }
+  it { should have_attribute(:address2).with_value(site.address2) }
+  it { should have_attribute(:address3).with_value(site.address3) }
+  it { should have_attribute(:address4).with_value(site.address4) }
+  it { should have_attribute(:postcode).with_value(site.postcode) }
+  it { should have_attribute(:region_code).with_value(site.region_code) }
+  it { should have_attribute(:recruitment_cycle_year).with_value(site.recruitment_cycle.year) }
 end
