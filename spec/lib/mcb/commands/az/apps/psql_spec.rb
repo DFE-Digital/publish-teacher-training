0528582ab94b193643829cc65460555ba31498fa
Tim Abell <tim@timwise.co.uk>
2019-07-09 09:42:54 +0100

    spec wip

--- spec/lib/mcb/commands/az/apps/psql_spec.rb
+++ spec/lib/mcb/commands/az/apps/psql_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 require 'mcb_helper'
 
 describe 'mcb az apps psql' do
-  it 'returns the psql of apps' do
+  it 'runs psql for localhost' do
     allow(MCB).to receive(:exec_command).with(
       "psql",
       "-h", "localhost",
@@ -12,6 +12,28 @@ describe 'mcb az apps psql' do
 
     with_stubbed_stdout do
       $mcb.run(%w[az apps psql])
+    end
+  end
+
+  it 'runs psql for azure server' do
+    app_config = {
+      'MANAGE_COURSES_POSTGRESQL_SERVICE_HOST' => 'azhost',
+      'PG_DATABASE'                            => 'pgdb',
+      'PG_USERNAME'                            => 'azuser',
+      'PG_PASSWORD'                            => 'azpass',
+      'RAILS_ENV'                              => 'qa',
+    }
+    allow(MCB::Azure).to(receive(:get_config).and_return(app_config))
+
+    allow(MCB).to receive(:exec_command).with(
+      "psql",
+      "-h", "azhost",
+      "-U", "azuser",
+      "-d", "pgdb"
+    )
+
+    with_stubbed_stdout(stdin: "qa") do
+      $mcb.run(%w[az apps psql -E qa])
     end
   end
 end
