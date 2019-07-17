fe1889cbce2237e4fd053b2a0ce900dd73911076
Faissal Bensefia <faissal@madetech.com>
2019-07-17 11:55:52 +0100

    [1731] Decouple audit tests from terminal size to fix Travis

--- spec/lib/mcb/commands/providers/audit_spec.rb
+++ spec/lib/mcb/commands/providers/audit_spec.rb
@@ -22,20 +22,73 @@ describe 'mcb providers audit' do
 
   context 'with an unspecified recruitment year' do
     it 'displays audit for provider for default recruitment year' do
-      output = audit(rolled_over_provider.provider_code)[:stdout]
-      expect(output).to have_text_table_row('user', 'user email', 'action', 'associated', 'associated', 'changes', 'created_at')
-      expect(output).to have_text_table_row('id', '', '', 'id', 'type', '')
-      expect(output).to include("\"recruitment_cycle_id\"=>#{recruitment_year2.id}")
+      output = parse_text_table(audit(rolled_over_provider.provider_code)[:stdout])
+
+      expect(output[0]).to eq(%w[userid useremail action associatedid associatedtype changes created_at])
+      expect(output[1][5]).to include("\"recruitment_cycle_id\"=>#{recruitment_year2.id}")
     end
   end
 
   context 'with a specified recruitment year' do
     it 'displays audit for provider for specified recruitment year' do
-      output = audit(rolled_over_provider.provider_code, '-r', recruitment_year1.year)[:stdout]
+      output = parse_text_table(audit(rolled_over_provider.provider_code, '-r', recruitment_year1.year)[:stdout])
 
-      expect(output).to have_text_table_row('user', 'user email', 'action', 'associated', 'associated', 'changes', 'created_at')
-      expect(output).to have_text_table_row('id', '', '', 'id', 'type', '')
-      expect(output).to include("\"recruitment_cycle_id\"=>#{recruitment_year1.id}")
+      expect(output[0]).to eq(%w[userid useremail action associatedid associatedtype changes created_at])
+      expect(output[1][5]).to include("\"recruitment_cycle_id\"=>#{recruitment_year1.id}")
     end
   end
+end
+
+#Issues to be mindful of when using this function
+# It is coupled to the border styling
+# All whitespace is discarded
+# Rows containing entirely dashes will be interpretted as the beginning of a new row
+def parse_text_table(text_table)
+  cells = parse_cells(text_table)
+
+  output_rows = [[]]
+  cells.each do |cell|
+    if cell == "\n"
+      output_rows.append([])
+    else
+      output_rows.last.append(cell)
+    end
+  end
+
+  grouped_rows = []
+  output_rows.each do |row|
+    if is_a_row_delimeter?(row)
+      grouped_rows.append([])
+    else
+      grouped_rows.last.append(row)
+    end
+  end
+
+  real_rows = []
+  grouped_rows.each do |group|
+    real_rows.append([])
+
+    group.each do |row|
+      row.each_with_index do |cell, index|
+        real_rows.last[index] = "" if real_rows.last[index].nil?
+        real_rows.last[index] += cell
+      end
+
+      real_rows.last.map! do |cell|
+        cell.gsub(/\s/, '')
+      end
+    end
+  end
+
+  real_rows
+end
+
+def parse_cells(text_table)
+  cell_delimeters = /\||\+/
+  text_table.split(cell_delimeters).reject(&:empty?)
+end
+
+def is_a_row_delimeter?(row)
+  contains_only_dashes = /^\-+$/
+  row.join("").match?(contains_only_dashes)
 end
