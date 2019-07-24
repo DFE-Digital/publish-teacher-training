d661dee7162ee9e337713355740822c045b5d8a8
Paul Hayes <fofr@users.noreply.github.com>
2019-07-24 12:39:46 +0100

    [1804] Fix nil entry requirements for required subjects
    - New validations mean some courses are now invalid, preventing saving
    - Maths and English are always required, science is required for primary courses
    - When nil but required, set them to the most permissive option, 'equivalence_test'
    - This affects 22 courses in each cycle

---
+++ db/migrate/20190724112016_fix_nil_entry_requirements.rb
@@ -0,0 +1,15 @@
+class FixNilEntryRequirements < ActiveRecord::Migration[5.2]
+  def up
+    say_with_time 'fixing courses with a nil subject requirement for required subjects' do
+      Course
+        .includes(:subjects)
+        .select { |c| c.maths.nil? || c.english.nil? || (c.gcse_science_required? && c.science.nil?) }
+        .each do |course|
+        course.maths = 'equivalence_test' if course.maths.nil?
+        course.english = 'equivalence_test' if course.english.nil?
+        course.science = 'equivalence_test' if course.science.nil?
+        course.save
+      end
+    end
+  end
+end
