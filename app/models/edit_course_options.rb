0deb317df8e873e84c96b3e42974384b07932fa3
Paul Hayes <fofr@users.noreply.github.com>
2019-07-25 16:51:22 +0100

    [1711] Expose qualification options
    - Expose to send to frontend
    - Show different options for FE courses

--- app/models/edit_course_options.rb
+++ app/models/edit_course_options.rb
@@ -8,4 +8,16 @@ class EditCourseOptions
       .reject { |k, _v| %i[not_set not_required].include?(k) }
       .keys
   end
+
+  def qualifications
+    qualifications_with_qts, qualifications_without_qts = Course::qualifications.keys.partition { |q| q.include?('qts') }
+    @course.level == :further_education ? qualifications_without_qts : qualifications_with_qts
+  end
+
+  def all
+    {
+      entry_requirements: entry_requirements,
+      qualifications: qualifications
+    }
+  end
 end
--- spec/models/edit_course_options_spec.rb
+++ spec/models/edit_course_options_spec.rb
@@ -1,12 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe EditCourseOptions, type: :model do
-  let(:course) { create(:course, name: 'Biology', course_code: '3X9F') }
+  let(:course) { create(:course) }
   let(:edit_course_options) { EditCourseOptions.new(course) }
 
   context 'entry_requirements' do
     it 'returns the entry requirements that users can choose between' do
       expect(edit_course_options.entry_requirements).to eq(%i[must_have_qualification_at_application_time expect_to_achieve_before_training_begins equivalence_test])
+    end
+  end
+
+  context 'qualifications' do
+    context 'for a course that’s not further education' do
+      it 'returns only QTS options for users to choose between' do
+        expect(edit_course_options.qualifications).to eq(%w[qts pgce_with_qts pgde_with_qts])
+        edit_course_options.qualifications.each do |q|
+          expect(q.include?('qts')).to be_truthy
+        end
+      end
+    end
+
+    context 'for a further education course' do
+      let(:course) { create(:course, subjects: [build(:further_education_subject)]) }
+      it 'returns only QTS options for users to choose between' do
+        expect(edit_course_options.qualifications).to eq(%w[pgce pgde])
+        edit_course_options.qualifications.each do |q|
+          expect(q.include?('qts')).to be_falsy
+        end
+      end
     end
   end
 end
