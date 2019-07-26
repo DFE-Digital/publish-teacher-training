2f39785ea088350cfe90f5d3c4e20dac0e2b0a19
Jake Benilov <benilov@gmail.com>
2019-07-26 19:14:39 +0100

    [1798] Add the 'mcb providers create' command

---
+++ lib/mcb/commands/providers/create.rb
@@ -0,0 +1,15 @@
+summary 'Create a new provider via the DB'
+
+run do |opts, _args, _cmd|
+  MCB.init_rails(opts)
+
+  Provider.connection.transaction do
+    provider = MCB.get_recruitment_cycle(opts).providers.build
+    requester = User.find_by!(email: MCB.config[:email])
+
+    MCB::ProviderEditor.new(
+      provider: provider,
+      requester: requester
+    ).new_provider_wizard
+  end
+end
