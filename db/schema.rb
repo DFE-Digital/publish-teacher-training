8b0635fa11404a354651e0d81a1fb4705a22f33b
Misha Gorodnitzky <misaka@pobox.com>
2019-07-30 15:15:32 +0100

    update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_26_104939) do
+ActiveRecord::Schema.define(version: 2019_07_30_133727) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_buffercache"
