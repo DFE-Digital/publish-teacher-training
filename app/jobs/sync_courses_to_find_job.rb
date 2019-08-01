d5b77ee5ab44b726e066a0262dab88449b553db4
Misha Gorodnitzky <misaka@pobox.com>
2019-08-01 17:35:56 +0100

    [1837] allow SearchAndCompareAPIService to expose the status code
    This allows better error reporting in case the request to sac fails.

--- app/jobs/sync_courses_to_find_job.rb
+++ app/jobs/sync_courses_to_find_job.rb
@@ -2,6 +2,12 @@ class SyncCoursesToFindJob < ApplicationJob
   queue_as :find_sync
 
   def perform(*courses)
-    SearchAndCompareAPIService::Request.sync(courses)
+    request = SearchAndCompareAPIService::Request.new
+    unless request.sync(courses)
+      raise(RuntimeError.new(
+              "Error #{request.response.status} received syncing courses: " \
+              + courses.join('; ')
+            ))
+    end
   end
 end
