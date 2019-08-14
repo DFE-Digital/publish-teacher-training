3a18c16d3eba8b89edff05e16037bdc2f68d7cb5
David Gisbey <davidgisbey@gmail.com>
2019-08-14 16:19:12 +0100

    [1972] Allow non-admin users to create access requests

--- spec/policies/access_request_policy_spec.rb
+++ spec/policies/access_request_policy_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 describe AccessRequestPolicy do
   subject { described_class }
 
-  permissions :approve?, :index?, :create?, :show? do
+  permissions :approve?, :index?, :show? do
     let(:access_request) { build(:access_request) }
 
     context 'non-admin user' do
@@ -14,6 +14,14 @@ describe AccessRequestPolicy do
 
     context 'admin user' do
       let(:user) { build(:user, :admin) }
+
+      it { should permit(user) }
+    end
+  end
+
+  permissions :create? do
+    context 'non-admin user' do
+      let(:user) { build(:user) }
 
       it { should permit(user) }
     end
