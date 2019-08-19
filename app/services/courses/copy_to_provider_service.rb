f5a27222bc61ab7af6c0949758e88b97210858b6
Richard Race <richard.race@madetech.com>
2019-08-19 09:44:07 +0100

    Refactor to not rely on ActiveRecord Relation to create new Course

--- app/services/courses/copy_to_provider_service.rb
+++ app/services/courses/copy_to_provider_service.rb
@@ -9,17 +9,22 @@ module Courses
 
       return nil if new_course.present?
 
-      new_course = @course.dup
-      new_course.subjects << @course.subjects
+      new_course = nil
 
-      new_provider.courses << new_course
+      Course.transaction do
+        new_course = @course.dup
+        new_course.provider = new_provider
+        new_course.save!
 
-      last_enrichment = @course.enrichments.latest_first.first
-      last_enrichment.copy_to_course(new_course) if last_enrichment.present?
+        new_course.subjects << @course.subjects
 
-      @course.sites.each do |site|
-        new_site = new_provider.sites.find_by(code: site.code)
-        new_site&.copy_to_course(new_course)
+        last_enrichment = @course.enrichments.latest_first.first
+        last_enrichment.copy_to_course(new_course) if last_enrichment.present?
+
+        @course.sites.each do |site|
+          new_site = new_provider.sites.find_by(code: site.code)
+          new_site&.copy_to_course(new_course)
+        end
       end
 
       new_course
