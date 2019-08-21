e8ba4d637d202f524f85c8b83fb2a8a37630fb78
Daniel Burnley <dan@danops.uk>
2019-08-21 16:43:22 +0100

    [1947] Ensure welcome email date sent doesn't get updated early

--- spec/services/send_welcome_email_service_spec.rb
+++ spec/services/send_welcome_email_service_spec.rb
@@ -10,7 +10,8 @@ describe SendWelcomeEmailService do
       spy(
         first_name: 'Meowington',
         email: 'meowington@cat.net',
-        first_login_date_utc: nil
+        first_login_date_utc: nil,
+        welcome_email_date_utc: nil
       )
     end
 
@@ -41,7 +42,8 @@ describe SendWelcomeEmailService do
     let(:current_user_spy) do
       spy(
         first_name: 'Meowington',
-        first_login_date_utc: Time.local(2018, 1, 1).utc
+        first_login_date_utc: Time.local(2018, 1, 1).utc,
+        welcome_email_date_utc: Time.local(2018, 1, 1).utc
       )
     end
 
@@ -51,12 +53,41 @@ describe SendWelcomeEmailService do
       expect(current_user_spy).not_to have_received(:update).with(hash_including(first_login_date_utc: Time.now.utc))
     end
 
-    it 'does not update their welcome email date' do
-      expect(current_user_spy).not_to have_received(:update).with(hash_including(welcome_email_date_utc: Time.now.utc))
+    context 'And has received a welcome email' do
+      it 'does not update their welcome email date' do
+        expect(current_user_spy).not_to have_received(:update).with(hash_including(welcome_email_date_utc: Time.now.utc))
+      end
+
+      it 'does not send the welcome email' do
+        expect(mailer_spy).not_to have_received(:send_welcome_email)
+      end
     end
 
-    it 'does not send the welcome email' do
-      expect(mailer_spy).not_to have_received(:send_welcome_email)
+    context 'And has not received a welcome email' do
+      let(:current_user_spy) do
+        spy(
+          email: 'meowington@cat.net',
+          first_name: 'Meowington',
+          first_login_date_utc: Time.local(2018, 1, 1).utc,
+          welcome_email_date_utc: nil
+        )
+      end
+
+      it 'sets their welcome email date to now' do
+        expect(current_user_spy).to have_received(:update).with(hash_including(welcome_email_date_utc: Time.now.utc))
+      end
+
+      it 'sends the welcome email' do
+        expect(mailer_spy).to have_received(:send_welcome_email)
+      end
+
+      it 'sends the email to the user' do
+        expect(mailer_spy).to have_received(:send_welcome_email).with(hash_including(email: 'meowington@cat.net'))
+      end
+
+      it 'sends the users first name in the email' do
+        expect(mailer_spy).to have_received(:send_welcome_email).with(hash_including(first_name: 'Meowington'))
+      end
     end
   end
 end
