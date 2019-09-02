3ef5050742296f82873f06423cc38c2e459bd618
Misha Gorodnitzky <misaka@pobox.com>
2019-09-02 15:44:32 +0100

    [2055] add using params in build_new

--- spec/requests/api/v2/providers/courses/build_new_spec.rb
+++ spec/requests/api/v2/providers/courses/build_new_spec.rb
@@ -16,6 +16,18 @@ describe 'GET /providers/:provider_code/courses/build_new' do
   end
   let(:jsonapi_renderer) { JSONAPI::Serializable::Renderer.new }
 
+  def course_to_jsonapi(course)
+    rendered_course = jsonapi_renderer.render(
+      course,
+      class: {
+        Course: API::V2::SerializableCourse
+      }
+    )
+    # Converting the rendered course to JSON and back normalises any symbols
+    # into strings. Better #deep_symbolize_keys because it also does values.
+    JSON.parse(rendered_course.to_json)
+  end
+
   describe '#build_new' do
     it 'returns a new course resource' do
       get "/api/v2/recruitment_cycles/#{recruitment_cycle.year}" \
@@ -23,18 +35,24 @@ describe 'GET /providers/:provider_code/courses/build_new' do
           "/courses/build_new",
           headers: { 'HTTP_AUTHORIZATION' => credentials }
 
-      course = provider.courses.new
-      rendered_course = jsonapi_renderer.render(
-        course,
-        class: {
-          Course: API::V2::SerializableCourse
-        }
-      )
-      # Converting the rendered course to JSON and back normalises any symbols
-      # into strings. Better #deep_symbolize_keys because it also does values.
-      jsonapi_course = JSON.parse(rendered_course.to_json)
+      expected_course_jsonapi = course_to_jsonapi(provider.courses.new)
       jsonapi_response = JSON.parse(response.body)
-      expect(jsonapi_response['data']).to eq jsonapi_course['data']
+      expect(jsonapi_response['data']).to eq expected_course_jsonapi['data']
+    end
+
+    context 'with course attributes set in query parameters' do
+      it 'intialises the course with the provider attributes' do
+        get "/api/v2/recruitment_cycles/#{recruitment_cycle.year}" \
+            "/providers/#{provider.provider_code}" \
+            "/courses/build_new",
+            headers: { 'HTTP_AUTHORIZATION' => credentials },
+            params: { name: 'Foo Bar Course' }
+
+        course = provider.courses.new(name: 'Foo Bar Course')
+        expected_course_jsonapi = course_to_jsonapi(course)
+        jsonapi_response = JSON.parse(response.body)
+        expect(jsonapi_response['data']).to eq expected_course_jsonapi['data']
+      end
     end
   end
 end
