786d51a1bfb239a8de011e59e548351cc73e99fe
David Gisbey <davidgisbey@gmail.com>
2019-09-12 16:50:44 +0100

    [2127] Add migration to create subject table
    Created the table
    Populated the table
    Created the necessary classes due to STI

---
+++ app/models/subject.rb
@@ -0,0 +1,18 @@
+# == Schema Information
+#
+# Table name: subject
+#
+#  id           :bigint           not null, primary key
+#  type         :text
+#  subject_code :text
+#  subject_name :text
+#
+
+class Subject < ApplicationRecord
+  has_many :course_subjects
+  has_many :courses, through: :course_subjects
+
+  def to_sym
+    subject_name.parameterize.underscore.to_sym
+  end
+end
