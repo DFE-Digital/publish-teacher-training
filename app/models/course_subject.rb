3b1330aefe4547dfcb5214304a2f4d092bd2ae91
defong <de_fong2k@yahoo.com>
2019-09-13 18:07:46 +0100

    [2123] Added course_subject table

--- app/models/course_subject.rb
+++ app/models/course_subject.rb
@@ -1,3 +1,12 @@
+# == Schema Information
+#
+# Table name: course_subject
+#
+#  id         :integer          not null, primary key
+#  course_id  :integer
+#  subject_id :integer
+#
+
 class CourseSubject < ApplicationRecord
   self.table_name = 'course_subject'
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_12_162820) do
+ActiveRecord::Schema.define(version: 2019_09_13_112820) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_buffercache"
@@ -124,6 +124,13 @@ ActiveRecord::Schema.define(version: 2019_09_12_162820) do
     t.text "vac_status"
     t.index ["course_id"], name: "IX_course_site_course_id"
     t.index ["site_id"], name: "IX_course_site_site_id"
+  end
+
+  create_table "course_subject", id: :serial, force: :cascade do |t|
+    t.integer "course_id"
+    t.integer "subject_id"
+    t.index ["course_id"], name: "index_course_subject_on_course_id"
+    t.index ["subject_id"], name: "index_course_subject_on_subject_id"
   end
 
   create_table "course_ucas_subject", id: :serial, force: :cascade do |t|
