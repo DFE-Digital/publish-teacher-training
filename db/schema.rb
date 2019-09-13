3ba2b345401a1302e856f71f4ff8207444901a4d
defong <de_fong2k@yahoo.com>
2019-09-13 12:34:48 +0100

    [2127] Fixed realigned migrations ordering

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_12_131116) do
+ActiveRecord::Schema.define(version: 2019_09_12_150048) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_buffercache"
