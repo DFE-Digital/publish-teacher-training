acc51ce8e5ef2b52df1abf49b8afb2e24243e998
defong <de_fong2k@yahoo.com>
2019-09-13 18:07:46 +0100

    [2123] Added course_subject table

---
+++ db/migrate/20190913112820_create_course_subject_table.rb
@@ -0,0 +1,10 @@
+class CreateCourseSubjectTable < ActiveRecord::Migration[5.2]
+  def change
+    create_table "course_subject", id: :serial, force: :cascade do |t|
+      t.integer "course_id"
+      t.integer "subject_id"
+      t.index %w[course_id], name: "index_course_subject_on_course_id"
+      t.index %w[subject_id], name: "index_course_subject_on_subject_id"
+    end
+  end
+end
