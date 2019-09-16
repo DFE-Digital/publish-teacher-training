259422cac384a048e21bcff59a81c571a4a0154d
defong <de_fong2k@yahoo.com>
2019-09-16 12:42:24 +0100

    [2123] Fixed slow querying via tuning

--- db/migrate/20190913125905_migrate_subjects.rb
+++ db/migrate/20190913125905_migrate_subjects.rb
@@ -1,8 +1,8 @@
 class MigrateSubjects < ActiveRecord::Migration[5.2]
   def change
-    say_with_time 'populating migrating course subjects' do
+    say_with_time 'populating/migrating course subjects' do
       all_subject = Subject.all
-      all_course_includes_ucas_subjects = Course.includes(:ucas_subjects)
+      all_course_includes_ucas_subjects = Course.includes(:ucas_subjects, :provider)
       all_course_includes_ucas_subjects.each do |course|
         course.level = course.ucas_level
         course.subjects = all_subject.where subject_name: course.dfe_subjects.map(&:to_s)
