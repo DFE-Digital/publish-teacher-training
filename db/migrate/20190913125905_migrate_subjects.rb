5e9a6605714e5b4ade5c2f8320a8c65b631117ac
defong <de_fong2k@yahoo.com>
2019-09-16 13:30:04 +0100

    [2123] Added

--- db/migrate/20190913125905_migrate_subjects.rb
+++ db/migrate/20190913125905_migrate_subjects.rb
@@ -2,7 +2,7 @@ class MigrateSubjects < ActiveRecord::Migration[5.2]
   def change
     say_with_time 'populating/migrating course subjects' do
       all_subject = Subject.all
-      all_course_includes_ucas_subjects = Course.includes(:ucas_subjects, :provider)
+      all_course_includes_ucas_subjects = Course.includes(:ucas_subjects, :provider, :subjects)
       all_course_includes_ucas_subjects.each do |course|
         course.level = course.ucas_level
         course.subjects = all_subject.where subject_name: course.dfe_subjects.map(&:to_s)
