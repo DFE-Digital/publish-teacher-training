06ac329e3104018201212280ea7bb2e2432342cd
defong <de_fong2k@yahoo.com>
2019-09-16 18:00:05 +0100

    [2123] Amended to exclude validation

--- db/migrate/20190913125905_migrate_subjects.rb
+++ db/migrate/20190913125905_migrate_subjects.rb
@@ -1,13 +1,21 @@
 class MigrateSubjects < ActiveRecord::Migration[5.2]
-  def change
+  def up
     say_with_time 'populating/migrating course subjects' do
       all_subject = Subject.all
-      all_course_includes_ucas_subjects = Course.includes(:ucas_subjects, :provider, :subjects)
+      all_course_includes_ucas_subjects = Course.includes(
+        :ucas_subjects,
+        :subjects,
+        provider: :recruitment_cycle
+      )
       all_course_includes_ucas_subjects.each do |course|
-        course.level = course.ucas_level
-        course.subjects = all_subject.where subject_name: course.dfe_subjects.map(&:to_s)
-        course.save
+        course.update_column(:level, course.ucas_level)
+        course.subjects = all_subject.select { |s| s.subject_name.in? course.dfe_subjects.map(&:to_s) }
       end
     end
+  end
+
+  def down
+    CourseSubject.connection.truncate :course_subject
+    Course.all.each { |c| c.update_column(:level, nil) }
   end
 end
