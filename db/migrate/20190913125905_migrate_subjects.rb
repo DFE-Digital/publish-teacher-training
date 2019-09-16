1b289a95f88ec2f996dd51fe9cde5812866ad01a
defong <de_fong2k@yahoo.com>
2019-09-16 18:42:57 +0100

    [2123] Fixed casing issues and loop tuning

--- db/migrate/20190913125905_migrate_subjects.rb
+++ db/migrate/20190913125905_migrate_subjects.rb
@@ -1,15 +1,22 @@
 class MigrateSubjects < ActiveRecord::Migration[5.2]
   def up
     say_with_time 'populating/migrating course subjects' do
-      all_subject = Subject.all
-      all_course_includes_ucas_subjects = Course.includes(
+      all_subjects = Subject.all
+      all_courses_includes_ucas_subjects = Course.includes(
         :ucas_subjects,
         :subjects,
         provider: :recruitment_cycle
       )
-      all_course_includes_ucas_subjects.each do |course|
+      all_courses_includes_ucas_subjects.each do |course|
         course.update_column(:level, course.ucas_level)
-        course.subjects = all_subject.select { |s| s.subject_name.in? course.dfe_subjects.map(&:to_s) }
+
+        dfe_subjects = course.dfe_subjects.map do |dfe_subject|
+          dfe_subject.to_s.downcase
+        end
+
+        course.subjects = all_subjects.select do |subject|
+          subject.subject_name.downcase.in? dfe_subjects
+        end
       end
     end
   end
