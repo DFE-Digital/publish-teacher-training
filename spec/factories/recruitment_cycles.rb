b5fd34ed8553f3344ffde0bfcb379b4cd57618f1
Misha Gorodnitzky <misaka@pobox.com>
2019-09-28 01:09:42 +0100

    [2070] don't hardcode year in recruitment_cycle factory

--- spec/factories/recruitment_cycles.rb
+++ spec/factories/recruitment_cycles.rb
@@ -12,12 +12,16 @@
 
 FactoryBot.define do
   factory :recruitment_cycle do
-    year { "2019" }
+    year { Settings.current_recruitment_cycle_year }
     application_start_date { DateTime.new(year.to_i - 1, 10, 1) }
     application_end_date { DateTime.new(year.to_i, 9, 30) }
 
+    trait :previous do
+      year { (Settings.current_recruitment_cycle_year.to_i - 1).to_s }
+    end
+
     trait :next do
-      year { "2020" }
+      year { (Settings.current_recruitment_cycle_year.to_i + 1).to_s }
     end
   end
 end
