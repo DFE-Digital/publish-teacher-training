0243b6bc4dc929eb63c1eb27f7d81ea836b4d3d5
David Gisbey <davidgisbey@gmail.com>
2019-10-07 15:22:47 +0100

    [2229] Allow command to take provider codes as arguments

--- lib/mcb/commands/providers/withdraw_courses.rb
+++ lib/mcb/commands/providers/withdraw_courses.rb
@@ -1,25 +1,11 @@
 summary "Withdraw courses for a pre determined list of providers"
-usage "withdraw_courses"
+usage "withdraw_courses <provider code1> [provider code2] ...""
 
 run do |opts, args, _cmd|
   MCB.init_rails(opts)
-  providers = RecruitmentCycle.find_by(year: '2020').providers.where(provider_code: ['1XN',
-                                                                      '1KM',
-                                                                      '156',
-                                                                      '1PG',
-                                                                      '2BG',
-                                                                      '1OZ',
-                                                                      '2JS',
-                                                                      '1SC',
-                                                                      '169',
-                                                                      '19W',
-                                                                      '1DF',
-                                                                      '2LQ',
-                                                                      '2LL'])
-
-  providers.each do |provider|
-    provider.courses.each do |course|
-      course.withdraw
-    end
+  current_cycle = RecruitmentCycle.current
+  args.each do |provider_code|
+    provider = current_cycle.providers.find_by!(provider_code: provider_code.upcase)
+    provider.courses.each(&:withdraw)
   end
 end
--- spec/lib/mcb/commands/providers/withdraw_courses_spec.rb
+++ spec/lib/mcb/commands/providers/withdraw_courses_spec.rb
@@ -1,18 +1,19 @@
 require "mcb_helper"
 
-describe "mcb providers touch" do
-  def execute_withdraw_courses
-    $mcb.run(["providers", "withdraw_courses"])
+describe "mcb providers withdraw_courses" do
+  def execute_withdraw_courses(arguments: [])
+    $mcb.run(["providers", "withdraw_courses", *arguments])
   end
+
   let(:current_cycle) { find_or_create :recruitment_cycle }
-  let(:provider) { create(:provider, provider_code: '1XN', courses: [course, course2], sites: [site], recruitment_cycle: current_cycle) }
-  let(:course)  { build(:course, site_statuses: [site_status]) }
-  let(:course2)  { build(:course, site_statuses: [site_status2]) }
+  let(:provider) { create(:provider, courses: [course, course2], sites: [site], recruitment_cycle: current_cycle) }
+  let(:course) { build(:course, site_statuses: [site_status]) }
+  let(:course2) { build(:course, site_statuses: [site_status2]) }
   let(:site) { build(:site) }
   let(:site_status) { build(:site_status, :published, :full_time_vacancies, site: site) }
   let(:site_status2) { build(:site_status, :published, :full_time_vacancies, site: site) }
 
-  let(:provider2) { create(:provider, provider_code: '1KM', courses: [course3, course4], sites: [site2], recruitment_cycle: current_cycle) }
+  let(:provider2) { create(:provider, courses: [course3, course4], sites: [site2], recruitment_cycle: current_cycle) }
   let(:course3)  { build(:course, site_statuses: [site_status3]) }
   let(:course4)  { build(:course, site_statuses: [site_status4]) }
   let(:site2) { build(:site) }
@@ -20,11 +21,10 @@ describe "mcb providers touch" do
   let(:site_status4) { build(:site_status, :published, :full_time_vacancies, site: site2) }
 
 
-  it "makes the providers coursesfor the current cycle non-findable" do
+  it "withdraws the providers courses for the current cycle" do
     provider
     provider2
-
-    execute_withdraw_courses
+    execute_withdraw_courses(arguments: [provider.provider_code, provider2.provider_code])
 
     expect(course.reload.findable?).to eq false
     expect(course2.reload.findable?).to eq false
