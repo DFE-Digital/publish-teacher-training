e7c92f0a56ebd5d55f1c7b6061ec3506ea55ee07
Paul Hayes <fofr@users.noreply.github.com>
2019-10-07 13:40:27 +0100

    Make update faster
    Update all at once, as suggested by @danielburnley

--- db/migrate/20191007103707_fix_secondary_science_course_requirements.rb
+++ db/migrate/20191007103707_fix_secondary_science_course_requirements.rb
@@ -2,9 +2,7 @@ class FixSecondaryScienceCourseRequirements < ActiveRecord::Migration[6.0]
   def up
     # There is no requirement for Science for secondary courses
     say_with_time "setting secondary science course requirements to not_set" do
-      Course.select { |course| course.level == "secondary" && !course.science.nil? }.each do |course|
-        course.update_attribute("science", nil)
-      end
+      Course.where(level: "secondary").where.not(science: nil).update_all(science: nil)
     end
   end
 end
