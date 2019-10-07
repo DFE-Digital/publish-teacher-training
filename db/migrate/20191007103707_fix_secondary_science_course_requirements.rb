c0098117ce4cbecf82cbb6b481821884a1548e84
Paul Hayes <fofr@users.noreply.github.com>
2019-10-07 12:16:09 +0100

    Use update_attribute to avoid validations
    Some courses couldn't be saved using `update` because they're in an invalid state.

--- db/migrate/20191007103707_fix_secondary_science_course_requirements.rb
+++ db/migrate/20191007103707_fix_secondary_science_course_requirements.rb
@@ -3,7 +3,7 @@ class FixSecondaryScienceCourseRequirements < ActiveRecord::Migration[6.0]
     # There is no requirement for Science for secondary courses
     say_with_time "setting secondary science course requirements to not_set" do
       Course.select { |course| course.level == "secondary" && !course.science.nil? }.each do |course|
-        course.update(science: nil)
+        course.update_attribute("science", nil)
       end
     end
   end
