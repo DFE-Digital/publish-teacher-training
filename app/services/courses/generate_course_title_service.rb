6257f4078b1ffbb799ff16fa5820f050c093db0a
Daniel Burnley <dan@danops.uk>
2019-10-11 11:13:57 +0100

    [2327] Add edge cases for bursary support in Find

--- app/services/courses/generate_course_title_service.rb
+++ app/services/courses/generate_course_title_service.rb
@@ -28,20 +28,24 @@ module Courses
 
       return title if languages.empty? || languages.length >= 4
 
+      languages = languages.map { |language| format_language_name(language) }
+
       if languages.length == 1
         title + " (#{languages[0]})"
       elsif languages.length == 2
-        title + " (#{languages[0]} and #{languages[1]})"
+        title + " (#{languages.join(' and ')})"
       elsif languages.length == 3
-        title + " (#{languages[0]}, #{languages[1]}, #{languages[2]})"
+        title + " (#{languages.join(', ')})"
       end
     end
 
     def generated_title(subjects)
       return "" if subjects.empty?
 
+      subjects = subjects.map { |s| format_subject_name(s) }
+
       if subjects.length == 1
-        subjects[0].to_s
+        subjects[0]
       else
         "#{subjects[0]} with #{subjects[1]}"
       end
@@ -53,6 +57,24 @@ module Courses
 
     def append_send_info(title)
       title + " with Special educational needs and disability"
+    end
+
+    def format_subject_name(subject)
+      if subject.subject_name == "Communication and media studies"
+        "Media studies"
+      else
+        subject.to_s
+      end
+    end
+
+    def format_language_name(language)
+      if language.subject_name == "English as a second or other language"
+        "English"
+      elsif language.subject_name == "Modern languages (other)"
+        "Other"
+      else
+        language.to_s
+      end
     end
   end
 end
--- spec/services/courses/generate_course_title_service_spec.rb
+++ spec/services/courses/generate_course_title_service_spec.rb
@@ -41,9 +41,9 @@ describe Courses::GenerateCourseTitleService do
     let(:level) { "primary" }
 
     context "With a single subject" do
-      let(:subjects) { [Subject.new(subject_name: "Physics")] }
+      let(:subjects) { [Subject.new(subject_name: "Physical education")] }
       it "Returns the subject name" do
-        expect(generated_title).to eq("Physics")
+        expect(generated_title).to eq("Physical education")
       end
 
       include_examples "with SEND"
@@ -119,6 +119,116 @@ describe Courses::GenerateCourseTitleService do
         end
 
         include_examples "with SEND"
+      end
+    end
+
+    context "Names which require altering" do
+      context "Communications and media studies" do
+        context "With single subject" do
+          let(:subjects) do
+            [create(:subject, :communication_and_media_studies)]
+          end
+
+          it "Returns the title Media studies" do
+            expect(generated_title).to eq("Media studies")
+          end
+        end
+
+        context "With multiple subjects" do
+          let(:subjects) do
+            [
+              create(:subject, :communication_and_media_studies),
+              create(:subject, :mathematics),
+            ]
+          end
+
+          it "Returns the title Media studies" do
+            expect(generated_title).to eq("Media studies with Mathematics")
+          end
+        end
+      end
+
+
+      context "English as a second language" do
+        context "With a single language" do
+          let(:subjects) do
+            [modern_languages, create(:subject, :english_as_a_second_language)]
+          end
+
+          it "Returns the title Modern Languages (English)" do
+            expect(generated_title).to eq("Modern Languages (English)")
+          end
+        end
+
+        context "With two languages" do
+          let(:subjects) do
+            [
+              modern_languages,
+              create(:subject, :english_as_a_second_language),
+              create(:subject, :spanish),
+            ]
+          end
+
+          it "Returns the title Modern Languages (English and Spanish)" do
+            expect(generated_title).to eq("Modern Languages (English and Spanish)")
+          end
+        end
+
+        context "With three languages" do
+          let(:subjects) do
+            [
+              modern_languages,
+              create(:subject, :english_as_a_second_language),
+              create(:subject, :french),
+              create(:subject, :spanish),
+            ]
+          end
+
+          it "Returns the title Modern Languages (English, French, Spanish)" do
+            expect(generated_title).to eq("Modern Languages (English, French, Spanish)")
+          end
+        end
+      end
+
+      context "Modern Languages (Other)" do
+        context "With a single language" do
+          let(:subjects) do
+            [modern_languages, create(:subject, :modern_languages_other)]
+          end
+
+          it "Returns the title Modern Languages (Other)" do
+            expect(generated_title).to eq("Modern Languages (Other)")
+          end
+        end
+
+        context "With two languages" do
+          let(:subjects) do
+            [
+              modern_languages,
+              create(:subject, :modern_languages_other),
+              create(:subject, :spanish),
+            ]
+          end
+
+          it "Returns the title Modern Languages (Other and Spanish)" do
+            expect(generated_title).to eq("Modern Languages (Other and Spanish)")
+          end
+        end
+
+        context "With three languages" do
+          let(:subjects) do
+            [
+              modern_languages,
+              create(:subject, :modern_languages_other),
+              create(:subject, :french),
+              create(:subject, :spanish),
+            ]
+          end
+
+          it "Returns the title Modern Languages (Other, French, Spanish)" do
+            expect(generated_title).to eq("Modern Languages (Other, French, Spanish)")
+          end
+        end
       end
     end
   end
