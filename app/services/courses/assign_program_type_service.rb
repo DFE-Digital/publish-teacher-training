d3d4cf2cca91c1c24ed4cd83c44938da89cd4add
David Gisbey <davidgisbey@gmail.com>
2019-11-11 16:49:21 +0000

    [2476] Refactor to reduce cognitive complexity

--- app/services/courses/assign_program_type_service.rb
+++ app/services/courses/assign_program_type_service.rb
@@ -3,21 +3,31 @@ module Courses
     def execute(funding_type, course)
       case funding_type
       when "salary"
-        if !course.self_accredited?
-          course.program_type = :school_direct_salaried_training_programme
-        else
-          course.errors.add(:program_type, "Salary is not valid for a self accredited course")
-        end
+        update_salaried_program(course)
       when "apprenticeship"
         course.program_type = :pg_teaching_apprenticeship
       when "fee"
-        course.program_type = if !course.self_accredited?
-                                :school_direct_training_programme
-                              elsif course.provider.scitt?
-                                :scitt_programme
-                              else
-                                :higher_education_programme
-                              end
+        course.program_type = update_fee_program(course)
+      end
+    end
+
+  private
+
+    def update_salaried_program(course)
+      if !course.self_accredited?
+        course.program_type = :school_direct_salaried_training_programme
+      else
+        course.errors.add(:program_type, "Salary is not valid for a self accredited course")
+      end
+    end
+
+    def update_fee_program(course)
+      if !course.self_accredited?
+        :school_direct_training_programme
+      elsif course.provider.scitt?
+        :scitt_programme
+      else
+        :higher_education_programme
       end
     end
   end
