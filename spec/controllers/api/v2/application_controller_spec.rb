471f35a2c7a0e14450517387129664875b19917a
Toby Retallick <toby.retallick@gmail.com>
2019-11-15 09:48:33 +0000

    [2511] - Store the request ID
    - Store the request ID in RequestStore
    - If the incoming request has no request ID (UUID) then generate one

--- spec/controllers/api/v2/application_controller_spec.rb
+++ spec/controllers/api/v2/application_controller_spec.rb
@@ -72,12 +72,25 @@ describe API::V2::ApplicationController, type: :controller do
     end
   end
 
+  describe "#store_request_id" do
+    it "stores the request id" do
+      request_uuid = SecureRandom.uuid
+
+      allow(request).to receive(:uuid).and_return(request_uuid)
+      allow(RequestStore).to receive(:store).and_return({})
+
+      controller.__send__(:store_request_id)
+
+      expect(RequestStore.store).to eq(request_id: request_uuid)
+    end
+  end
+
   describe "#append_info_to_payload" do
     it "sets the request_id in the payload to the request uuid" do
       payload = {}
       request_uuid = SecureRandom.uuid
 
-      allow(request).to receive(:uuid).and_return(request_uuid)
+      allow(RequestStore).to receive(:store).and_return(request_id: request_uuid)
 
       controller.__send__(:append_info_to_payload, payload)
 
