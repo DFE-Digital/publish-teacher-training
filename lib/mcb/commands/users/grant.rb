ceb10f141b63cea2170e2cee6b29b7292347f626
David Gisbey <davidgisbey@gmail.com>
2019-11-20 17:12:58 +0000

    [2541] Remove admin functionality

--- lib/mcb/commands/users/grant.rb
+++ lib/mcb/commands/users/grant.rb
@@ -2,15 +2,13 @@ summary "Attach a user to an organisation/provider in the DB. Will prompt to cre
 param :id_or_email_or_sign_in_id
 option :p, "provider_code", "provider code",
        argument: :optional, transform: ->(code) { code.upcase }
-flag nil, "admin", "admin"
 usage "grant --admin <user_id/email/sign_in_user_id> -p <provider_code>"
 
 run do |opts, args, _cmd|
   MCB.init_rails(opts)
 
   provider = nil
-  admin = opts[:admin]
   provider = Provider.find_by!(provider_code: opts[:provider_code]) if opts[:provider_code]
 
-  MCB::Editor::GrantAccessWizard.new(provider, args[:id_or_email_or_sign_in_id], admin).run
+  MCB::Editor::GrantAccessWizard.new(provider, args[:id_or_email_or_sign_in_id]).run
 end
