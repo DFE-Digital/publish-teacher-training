cafdff3cc79fd97ccfbf534b9a949854afb77298
Tim Abell <tim@timwise.co.uk>
2019-11-30 08:54:12 +0000

    [2571] Include accredited body in new course
    So that the frontend remembers the current accredited body selection

--- spec/requests/api/v2/build_new_course_spec.rb
+++ spec/requests/api/v2/build_new_course_spec.rb
@@ -31,7 +31,7 @@ describe "/api/v2/build_new_course", type: :request do
         PrimarySubject: API::V2::SerializableSubject,
         Provider: API::V2::SerializableProvider,
       },
-      include: %i[subjects sites provider],
+      include: %i[subjects sites accrediting_provider],
     ).to_json)
   end
 
