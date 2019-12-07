48745d661305b510a005ee3dbcbcff6d0c68b24d
Toby Retallick <toby.retallick@gmail.com>
2019-12-07 16:35:38 +0000

    [2647] - Handle when empty results returned from Geocoder

--- spec/jobs/geocode_job_spec.rb
+++ spec/jobs/geocode_job_spec.rb
@@ -27,20 +27,37 @@ describe GeocodeJob, type: :job do
     expect(described_class.new.queue_name).to eq("geocoding")
   end
 
-  it "executes perform" do
-    results = [
-      OpenStruct.new(latitude: 50.345676, longitude: -1.345676),
-    ]
+  context "executing the job" do
+    it "updates the record when there are results" do
+      results = [
+        OpenStruct.new(latitude: 50.345676, longitude: -1.345676),
+      ]
 
-    allow(Geocoder).to receive(:search).and_return(results)
-    expect(Geocoder).to receive(:search).with(site.full_address)
+      allow(Geocoder).to receive(:search).and_return(results)
+      expect(Geocoder).to receive(:search).with(site.full_address)
 
-    site.save!
-    perform_enqueued_jobs { job }
+      site.save!
+      perform_enqueued_jobs { job }
 
-    updated_site = Site.find(site.id)
+      updated_site = Site.find(site.id)
 
-    expect(updated_site.latitude).to eq(50.345676)
-    expect(updated_site.longitude).to eq(-1.345676)
+      expect(updated_site.latitude).to eq(50.345676)
+      expect(updated_site.longitude).to eq(-1.345676)
+    end
+
+    it "does not attempt to update the record when there are no results" do
+      results = []
+
+      allow(Geocoder).to receive(:search).and_return(results)
+      expect(Geocoder).to receive(:search).with(site.full_address)
+
+      site.save!
+      perform_enqueued_jobs { job }
+
+      updated_site = Site.find(site.id)
+
+      expect(updated_site.latitude).to eq(nil)
+      expect(updated_site.longitude).to eq(nil)
+    end
   end
 end
