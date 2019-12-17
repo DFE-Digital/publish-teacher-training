0313dc67b25a3b9bef66379008db9bf6395e3bdd
Faissal Bensefia <faissal@madetech.com>
2019-12-17 11:53:08 +0000

    [2697] Include relations that are being lazy loaded to speed up query

--- app/controllers/api/v2/organisations_controller.rb
+++ app/controllers/api/v2/organisations_controller.rb
@@ -6,8 +6,9 @@ module API
         @organisations = Organisation.all
         if params_includes_provider?
           current_recruitment_cycle = RecruitmentCycle.current
-          @organisations = @organisations.includes(:providers)
+          @organisations = @organisations.includes(:providers, :users, :nctl_organisations, providers: %i[contacts sites courses recruitment_cycle ucas_preferences])
                              .where(provider: { recruitment_cycle_id: current_recruitment_cycle.id })
+
         end
 
         render jsonapi: @organisations, include: params[:include]
--- app/models/provider.rb
+++ app/models/provider.rb
@@ -14,7 +14,6 @@
 #  discarded_at                     :datetime
 #  email                            :text
 #  id                               :integer          not null, primary key
-#  last_published_at                :datetime
 #  latitude                         :float
 #  longitude                        :float
 #  postcode                         :text
@@ -33,7 +32,6 @@
 #
 # Indexes
 #
-#  IX_provider_last_published_at                             (last_published_at)
 #  index_provider_on_changed_at                              (changed_at) UNIQUE
 #  index_provider_on_discarded_at                            (discarded_at)
 #  index_provider_on_latitude_and_longitude                  (latitude,longitude)
