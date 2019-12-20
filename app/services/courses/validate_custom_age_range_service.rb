78901e4ca541a78b91c47d7f44412a3e524db0b4
David Gisbey <davidgisbey@gmail.com>
2019-12-20 14:05:55 +0000

    [2704] Refactor to use named captures

--- app/services/courses/validate_custom_age_range_service.rb
+++ app/services/courses/validate_custom_age_range_service.rb
@@ -1,13 +1,13 @@
 module Courses
   class ValidateCustomAgeRangeService
     def execute(age_range_in_years, course)
-      # This regex checks that a 1 or 2 digit number is selected for the 'to' and 'from' values.
-      # It also checks that the age range is correctly formatted with _to_ in between these values
-      valid_regex_pattern = /^(\d{1,2})_to_(\d{1,2})$/
-      #The below grabs the 'from' and 'to' ages and puts them into variables if the regex is valid
-      from_age, to_age = valid_regex_pattern.match(age_range_in_years).captures if valid_regex_pattern.match(age_range_in_years)
-
       error_message = "#{age_range_in_years} is invalid. You must enter a valid age range."
+      valid_regex_pattern = Regexp.new(/^(?<from>\d{1,2})_to_(?<to>\d{1,2})$/)
+
+      if valid_regex_pattern.match(age_range_in_years)
+        from_age = get_ages(age_range_in_years, valid_regex_pattern)["from"]
+        to_age = get_ages(age_range_in_years, valid_regex_pattern)["to"]
+      end
 
       if !valid_regex_pattern.match(age_range_in_years)
         course.errors.add(:age_range_in_years, error_message)
@@ -15,19 +15,23 @@ module Courses
         course.errors.add(:age_range_in_years, error_message)
       elsif to_age_invalid?(to_age)
         course.errors.add(:age_range_in_years, error_message)
-      elsif to_age.to_i - from_age.to_i < 4
+      elsif to_age - from_age < 4
         course.errors.add(:age_range_in_years, "#{age_range_in_years} is invalid. Your age range must cover at least 4 years.")
       end
     end
 
   private
 
+    def get_ages(age_range_in_years, valid_regex_pattern)
+      valid_regex_pattern.match(age_range_in_years)&.named_captures&.transform_values { |v| v.to_i }
+    end
+
     def from_age_invalid?(from_age)
-      from_age.to_i < 3 || from_age.to_i > 14
+      from_age < 3 || from_age > 14
     end
 
     def to_age_invalid?(to_age)
-      to_age.to_i < 7 || to_age.to_i > 18
+      to_age < 7 || to_age > 18
     end
   end
 end
