551349195c7f70d7a5f6400a39fc6bb255231428
Graham Pengelly <graham@three-tier.com>
2020-01-14 15:45:53 +0000

    Add CoursePolicy::Scope
    Restrict course access to users that belong either to a provider that is
    delivering the course or an accrediting body that provides the
    qualification.

--- app/policies/course_policy.rb
+++ app/policies/course_policy.rb
@@ -1,6 +1,25 @@
 class CoursePolicy
   attr_reader :user, :course
 
+  class Scope
+    attr_reader :user, :scope
+
+    def initialize(user, scope)
+      @user = user
+      @scope = scope
+    end
+
+    def resolve
+      if user.admin?
+        scope.all
+      else
+        scope
+          .where(provider_id: user.providers.pluck(:id))
+          .or(Course.where(accrediting_provider_code: user.providers.pluck(:provider_code)))
+      end
+    end
+  end
+
   def initialize(user, course)
     @user = user
     @course = course
--- spec/policies/course_policy_spec.rb
+++ spec/policies/course_policy_spec.rb
@@ -2,6 +2,7 @@ require "rails_helper"
 
 describe CoursePolicy do
   let(:user) { create(:user) }
+  let(:organisation) { create(:organisation, users: [user]) }
 
   subject { described_class }
 
@@ -10,7 +11,6 @@ describe CoursePolicy do
   end
 
   permissions :show?, :update?, :withdraw? do
-    let(:organisation) { create(:organisation, users: [user]) }
     let(:course) { create(:course) }
     let!(:provider) {
       create(:provider,
@@ -23,6 +23,39 @@ describe CoursePolicy do
     context "with a user outside the organisation" do
       let(:other_user) { create(:user) }
       it { should_not permit(other_user, course) }
+    end
+  end
+
+  describe CoursePolicy::Scope do
+    let(:accredited_body) { create(:provider, :accredited_body, organisations: [organisation]) }
+    let(:training_provider) { create(:provider) }
+    let!(:course) { create(:course, provider: training_provider, accrediting_provider: accredited_body) }
+    let!(:other_course) { create(:course) }
+
+    subject { described_class.new(user, Course).resolve }
+
+    before { organisation }
+
+    context "user from the accredited_body" do
+      it { is_expected.to contain_exactly(course) }
+    end
+
+    context "user not from the accredited body" do
+      let(:organisation) { create(:organisation) }
+
+      it { is_expected.to be_empty }
+    end
+
+    context "a user from the provider" do
+      let(:organisation) { training_provider.organisations.first }
+      let(:user) { create(:user, organisations: [organisation]) }
+
+      it { is_expected.to contain_exactly(course) }
+    end
+
+    context "an admin" do
+      let(:user) { create(:user, :admin) }
+      it { is_expected.to contain_exactly(course, other_course) }
     end
   end
 end
