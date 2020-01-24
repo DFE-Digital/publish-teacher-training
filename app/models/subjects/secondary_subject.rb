c1a8fa215f21319e2899bbf92d4271f486e907ba
Tim Abell <tim@timwise.co.uk>
2020-01-24 18:21:42 +0000

    [2851] Bust modern languages cache to fix seed test
    When run with other tests the modern languages subject pointer went
    stale because the data was removed and re-added in the seeding

--- app/models/subjects/secondary_subject.rb
+++ app/models/subjects/secondary_subject.rb
@@ -20,4 +20,8 @@ class SecondarySubject < Subject
   def self.modern_languages
     @modern_languages ||= find_by(subject_name: "Modern Languages")
   end
+
+  def self.clear_modern_languages_cache
+    @modern_languages = nil
+  end
 end
