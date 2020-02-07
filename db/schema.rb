2faa396a67cf19f49a4beaa3dee052e15a2e6f08
Aga Dufrat <aga.dufrat@digital.education.gov.uk>
2020-02-07 10:23:12 +0000

    [2906] Add a migration to fix subject areas naming
    so it confirms to the style guide.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_31_104849) do
+ActiveRecord::Schema.define(version: 2020_02_07_094915) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_buffercache"
