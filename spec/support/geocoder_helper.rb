54f85f63d0f9b9d010a29e8da5793c98eb2f590e
Theodor Vararu <theo@vararu.org>
2020-02-27 18:31:59 +0000

    Simplify GeocoderHelper
    One of the whens was the same as the else, so remove it.

--- spec/support/geocoder_helper.rb
+++ spec/support/geocoder_helper.rb
@@ -9,29 +9,20 @@ module GeocoderHelper
           full_address: "Long Lane, Holbury, Southampton, SO45 2PA",
           zip: "SO45 2PA",
           state: "England",
+          state_code: "England",
           country: "United Kingdom",
           country_code: "gb",
-        )
-      when "Academies Enterprise Trust: Aylward Academy, Windmill Road, London, N18 1NB"
-        Geokit::GeoLoc.new(
-          lat: 51.4524877,
-          lng: -0.1204749,
-          full_address: "Academies Enterprise Trust: Aylward Academy, Windmill Road, London, N18 1NB",
-          zip: "N18 1NB",
-          state: "London",
-          state_code: "London",
-          country: "United Kingdom",
-          country_code: "UK",
         )
       else
         Geokit::GeoLoc.new(
           lat: 51.4524877,
           lng: -0.1204749,
           full_address: "Academies Enterprise Trust: Aylward Academy, Windmill Road, London, N18 1NB",
-          zip: "HX4 8JB",
-          state: "England",
+          zip: "N18 1NB",
+          state: "London",
+          state_code: "England",
           country: "United Kingdom",
-          country_code: "gb",
+          country_code: "UK",
         )
       end
     end
