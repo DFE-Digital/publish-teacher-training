d1ba6b8283f600f418e1418c2b300b5cd09d294b
Faissal Bensefia <faissal@madetech.com>
2020-03-10 14:39:41 +0000

    [3087] Tag background jobs with job id, queue and name, and format logs to be easier to use
    [3087] Tag background jobs with correct job id, queue and name when logging
    
    [3087] Only include job_queue and job_id in logs if they have values
    
    [3087] Use semantic logging for Sidekiq
    
    [3087] WIP: make Sidekiq context dumps much shorter
    
    [3087] WIP: make exceptions clearer and more concise
    
    [3087] Correct tagging placement
    
    [3087] Refactor formatting and add formatting for backtraces
    
    [3087] Refactor formatting and add formatting for backtraces
    
    [3087] Remove dead code from production.rb
    
    [3087] Refactor logstash formatting
    
    [3087] Fix warning when logstash settings are not specified
    
    Co-authored-by: Misha Gorodnitzky <misaka@pobox.com>

--- app/jobs/sync_courses_to_find_job.rb
+++ app/jobs/sync_courses_to_find_job.rb
@@ -2,6 +2,9 @@ class SyncCoursesToFindJob < ApplicationJob
   queue_as :find_sync
 
   def perform(*courses)
+    RequestStore.store[:job_id] = provider_job_id
+    RequestStore.store[:job_queue] = queue_name
+
     request = SearchAndCompareAPIService::Request.new
     unless request.sync(courses)
       raise(
