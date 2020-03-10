908f492e37dd3ef076948c808ed4c8c59a5658f8
Phil Lee <asmega@gmail.com>
2020-03-10 15:37:17 +0000

    [3108] Improve parallel specs
    - This uses a feature in parallel_tests to even tests across cores
    - This is done by logging test run times
    - For me this has shaved 2 minutes off the test suite

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -86,4 +86,14 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  # Report N+1 queries
+  if Bullet.enable?
+    config.before(:each) { Bullet.start_request }
+    config.after(:each)  { Bullet.end_request }
+  end
+
+  config.include ActiveJob::TestHelper, type: :request
+
+  ActiveJob::Base.queue_adapter = :test
 end
