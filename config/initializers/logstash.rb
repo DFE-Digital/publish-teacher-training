ed05bddde0b7326d371e9c93b18457286c3b6e03
Daniel Burnley <dan@danops.uk>
2020-03-31 11:05:10 +0100

    [3195] Add the commit sha to logging and sentry

--- config/initializers/logstash.rb
+++ config/initializers/logstash.rb
@@ -1,6 +1,8 @@
 LogStashLogger.configure do |config|
+  commit_sha = File.read(Rails.root.join("COMMIT_SHA")).strip
   config.customize_event do |event|
     event["application"] = Settings.application
     event["environment"] = Rails.env
+    event["release"] = commit_sha
   end
 end
