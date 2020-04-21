5e99e8f11c49e3216a12e06b60ecd20e388b5f46
Graham Pengelly <graham@three-tier.com>
2020-04-21 14:59:39 +0100

    [3257] Update annotations

--- spec/factories/allocations.rb
+++ spec/factories/allocations.rb
@@ -1,3 +1,19 @@
+# == Schema Information
+#
+# Table name: allocation
+#
+#  accredited_body_id :bigint
+#  created_at         :datetime         not null
+#  id                 :bigint           not null, primary key
+#  number_of_places   :integer
+#  provider_id        :bigint
+#  updated_at         :datetime         not null
+#
+# Indexes
+#
+#  index_allocation_on_accredited_body_id  (accredited_body_id)
+#  index_allocation_on_provider_id         (provider_id)
+#
 FactoryBot.define do
   factory :allocation do
     association :accredited_body, factory: %i(provider accredited_body)
--- spec/models/allocation_spec.rb
+++ spec/models/allocation_spec.rb
@@ -1,3 +1,19 @@
+# == Schema Information
+#
+# Table name: allocation
+#
+#  accredited_body_id :bigint
+#  created_at         :datetime         not null
+#  id                 :bigint           not null, primary key
+#  number_of_places   :integer
+#  provider_id        :bigint
+#  updated_at         :datetime         not null
+#
+# Indexes
+#
+#  index_allocation_on_accredited_body_id  (accredited_body_id)
+#  index_allocation_on_provider_id         (provider_id)
+#
 require "rails_helper"
 
 RSpec.describe Allocation do
