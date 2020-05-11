159a717abb3812b1afcedebe376d0b2ed5d24736
Misha Gorodnitzky <misaka@pobox.com>
2020-05-11 11:44:22 +0100

    [3345] fix magic link token migration, add columns correctly

--- db/migrate/20200501133532_add_magic_link_tokens_to_users.rb
+++ db/migrate/20200501133532_add_magic_link_tokens_to_users.rb
@@ -1,8 +1,8 @@
 class AddMagicLinkTokensToUsers < ActiveRecord::Migration[6.0]
   def change
     change_table :user, bulk: true do |table|
-      table.add_column :magic_link_token, :string, unique: true
-      table.add_column :magic_link_token_sent_at, :datetime
+      table.string :magic_link_token, unique: true
+      table.datetime :magic_link_token_sent_at
     end
   end
 end
