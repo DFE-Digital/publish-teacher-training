084c2e35af53feff27d6dddea5fe03f7479f7172
Phil Lee <asmega@gmail.com>
2020-06-09 11:01:11 +0100

    [3511] Eager load associations on subjects page

--- app/controllers/api/v3/subject_areas_controller.rb
+++ app/controllers/api/v3/subject_areas_controller.rb
@@ -2,7 +2,7 @@ module API
   module V3
     class SubjectAreasController < API::V3::ApplicationController
       def index
-        render jsonapi: SubjectArea.active, fields: fields_param, include: params[:include], class: CourseSerializersService.new.execute[:v3]
+        render jsonapi: SubjectArea.active.includes(subjects: [:financial_incentive]), fields: fields_param, include: params[:include], class: CourseSerializersService.new.execute[:v3]
       end
     end
   end
