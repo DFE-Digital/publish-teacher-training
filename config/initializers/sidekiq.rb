af14c3fd1f2dc3615bb6e3ba851a68a64fd3f708
Graham Pengelly <graham@three-tier.com>
2020-06-26 15:46:49 +0100

    [3477] Add redis credentials for PaaS
    The redis credentials are provided by the binding via the VCAP_SERVICES
    environment variable. We add this option in the Sidekiq initialisation
    code. The password environment variable option is still there and can be
    removed once we have migrated to paas completely.

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,15 +1,40 @@
-Sidekiq.configure_client do |config|
-  config.redis = {
-    password: Settings.mcbg.redis_password,
-  }
-end
+if ENV.key?("VCAP_SERVICES")
+  service_config = JSON.parse(ENV["VCAP_SERVICES"])
+  redis_config = service_config["redis"].first
+  redis_credentials = redis_config["credentials"]
 
-Sidekiq.configure_server do |config|
-  config.redis = {
-    password: Settings.mcbg.redis_password,
-  }
+  Sidekiq.configure_server do |config|
+    config.redis = {
+      url: redis_credentials["uri"],
+    }
 
-  if Settings.bg_jobs
-    Sidekiq::Cron::Job.load_from_hash Settings.bg_jobs
+    if Settings.bg_jobs
+      Sidekiq::Cron::Job.load_from_hash Settings.bg_jobs
+    end
   end
+
+  Sidekiq.configure_client do |config|
+    config.redis = {
+      url: redis_credentials["uri"],
+    }
+  end
+
+else
+
+  Sidekiq.configure_client do |config|
+    config.redis = {
+      password: Settings.mcbg.redis_password,
+    }
+  end
+
+  Sidekiq.configure_server do |config|
+    config.redis = {
+      password: Settings.mcbg.redis_password,
+    }
+
+    if Settings.bg_jobs
+      Sidekiq::Cron::Job.load_from_hash Settings.bg_jobs
+    end
+  end
+
 end
