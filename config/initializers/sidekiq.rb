1f9cc45d3be3dd8a211e156bac1ca5da4143bf2d
Graham Pengelly <graham@three-tier.com>
2020-06-26 15:46:49 +0100

    Add redis credentials for PAAS
    Initial proof of concept for retrieving redis credentials from PAAS.
    This will need tidying up if it works.

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,3 +1,21 @@
+if Rails.env.qa_paas?
+  service_config = JSON.parse(ENV["VCAP_SERVICES"])
+  redis_config = service_config["redis"].first
+  redis_credentials = redis_config["credentials"]
+
+  Sidekiq.configure_server do |config|
+    config.redis = {
+      url: redis_credentials["uri"],
+    }
+  end
+
+  Sidekiq.configure_client do |config|
+    config.redis = {
+      url: redis_credentials["uri"],
+    }
+  end
+end
+
 Sidekiq.configure_client do |config|
   config.redis = {
     password: Settings.mcbg.redis_password,
