9f59717e39ec654db4a0f893281985a20fe5d5d2
De Fong <de_fong2k@yahoo.com>
2020-11-16 20:54:59 +0000

    [373] Amended migrations

--- db/migrate/20201113114332_add_uuid_to_course.rb
+++ db/migrate/20201113114332_add_uuid_to_course.rb
@@ -1,13 +1,7 @@
 class AddUuidToCourse < ActiveRecord::Migration[6.0]
-  def up
-    enable_extension "uuid-ossp" unless extension_enabled?("uuid-ossp")
-
+  def change
     add_column :course, :uuid, :uuid, default: "uuid_generate_v4()", null: false
 
     add_index :course, :uuid, unique: true, name: "index_courses_unique_uuid"
-  end
-
-  def down
-    raise ActiveRecord::IrreversibleMigration
   end
 end
