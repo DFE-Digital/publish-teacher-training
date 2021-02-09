1b1e21b3100d2395297a7551cd563b6b32ac3f71
David Gisbey <davidgisbey@gmail.com>
2021-02-09 00:00:08 +0000

    Add service to encapsulate shared logic

--- lib/mcb/commands/sites/add_ttwa_and_london_borough.rb
+++ lib/mcb/commands/sites/add_ttwa_and_london_borough.rb
@@ -6,10 +6,6 @@ option :s, "sleep", "time to sleep between each request", argument: :optional, d
 
 instance_eval(&MCB.remote_connect_options)
 
-# TODO: Shared logic in service
-# model hooks to call on create/update sites postcode
-# expose on V3
-
 run do |opts, _args, _cmd|
   MCB.init_rails(opts)
 
@@ -20,20 +16,7 @@ run do |opts, _args, _cmd|
   .not(travel_to_work_area: nil)
   .find_each(batch_size: opts[:batch_size]) do |site|
     verbose "adding travel to work area for '#{site.provider_code}' course '#{site.course_code} course '#{site.code}'"
-
-    url = URI("https://mapit.mysociety.org/point/4326/#{site.longitude},#{site.latitude}?type=TTW,LBO&api_key=#{Settings.mapit_api_key}")
-    response = Net::HTTP.get(url)
-    json = JSON.parse(response)
-
-    ttw_area = json.dig(json.keys.first, "name")
-
-    site.update!(travel_to_work_area: ttw_area)
-
-    if ttw_area == "London"
-      london_borough = json.dig(json.keys.second, "name")
-      site.update!(london_borough: london_borough)
-    end
-
+    TravelToWorkAreaAndLondonBoroughService.add_travel_to_work_area_and_london_borough(site: site)
     sleep(opts[:sleep])
   end
 end
