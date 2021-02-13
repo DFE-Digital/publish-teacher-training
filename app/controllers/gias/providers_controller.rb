19348c0fd34f297542dfa2ee094ee7ef4255e851
Misha Gorodnitzky <misaka@pobox.com>
2021-02-13 10:29:06 +0000

    change dashboad to use provider filters

--- app/controllers/gias/providers_controller.rb
+++ app/controllers/gias/providers_controller.rb
@@ -21,48 +21,6 @@ module GIAS
       @pagy, @providers = pagy(providers)
     end
 
-    def index_of_providers_with_any_postcode_match
-      recruitment_cycle = RecruitmentCycle.current
-      @pagy, @providers = pagy recruitment_cycle.providers.with_establishments_that_match_any_postcode
-
-      render :index
-    end
-
-    def index_of_providers_that_match_by_postcode
-      recruitment_cycle = RecruitmentCycle.current
-      @pagy, @providers = pagy recruitment_cycle.providers.that_match_establishments_by_postcode
-
-      render :index
-    end
-
-    def index_of_providers_with_sites_that_match_by_postcode
-      recruitment_cycle = RecruitmentCycle.current
-      @pagy, @providers = pagy recruitment_cycle.providers.with_sites_that_match_establishments_by_postcode
-
-      render :index
-    end
-
-    def index_of_providers_with_any_name_match
-      recruitment_cycle = RecruitmentCycle.current
-      @pagy, @providers = pagy recruitment_cycle.providers.with_establishments_that_match_any_name
-
-      render :index
-    end
-
-    def index_of_providers_that_match_by_name
-      recruitment_cycle = RecruitmentCycle.current
-      @pagy, @providers = pagy recruitment_cycle.providers.that_match_establishments_by_name
-
-      render :index
-    end
-
-    def index_of_providers_with_sites_that_match_by_name
-      recruitment_cycle = RecruitmentCycle.current
-      @pagy, @providers = pagy recruitment_cycle.providers.with_sites_that_match_establishments_by_name
-
-      render :index
-    end
-
     def show
       @provider = Provider.find(params[:id])
 
--- config/routes.rb
+++ config/routes.rb
@@ -16,18 +16,12 @@ Rails.application.routes.draw do
   get "/auth/dfe/signout", to: "sessions#destroy"
 
   namespace :gias do
-    get "/", to: "/gias#dashboard"
+    get "/", to: "/gias#dashboard", as: :dashboard
     get "/import_establishments", to: "/gias#import_establishments"
 
     resources :establishments, param: :urn, only: [:index, :show]
 
     resources :postcodes, param: :postcode, only: :show
-    get "providers_with_any_name_match", to: "providers#index_of_providers_with_any_name_match"
-    get "providers_that_match_by_name", to: "providers#index_of_providers_that_match_by_name"
-    get "providers_with_sites_that_match_by_name", to: "providers#index_of_providers_with_sites_that_match_by_name"
-    get "providers_with_any_postcode_match", to: "providers#index_of_providers_with_any_postcode_match"
-    get "providers_that_match_by_postcode", to: "providers#index_of_providers_that_match_by_postcode"
-    get "providers_with_sites_that_match_by_postcode", to: "providers#index_of_providers_with_sites_that_match_by_postcode"
     resources :providers, only: [:index, :show]
     resources :sites, only: :show
   end
