5bd3909f2d0df81681672bafbb1be79318084621
Misha Gorodnitzky <misaka@pobox.com>
2021-02-13 10:11:39 +0000

    add filtering to providers index

--- app/webpacker/packs/application.js
+++ app/webpacker/packs/application.js
@@ -3,8 +3,5 @@ import '../scripts/govuk_assets_import'
 import '../styles/application.scss'
 import '../scripts/components'
 import { initAll } from 'govuk-frontend'
-import Rails from "@rails/ujs";
-
-Rails.start();
 
 initAll();
