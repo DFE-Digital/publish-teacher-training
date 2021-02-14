c080bce07c8aa56e0066dd2fd6b06aa12492bb57
Misha Gorodnitzky <misaka@pobox.com>
2021-02-14 23:58:24 +0000

    add establishments counts to dashboard

--- app/controllers/gias_controller.rb
+++ app/controllers/gias_controller.rb
@@ -1,14 +1,21 @@
 class GIASController < GIAS::ApplicationController
   def dashboard
     @current_recruitment_cycle = RecruitmentCycle.current
+    @establishments = GIASEstablishment
+    @establishments_that_match_any_name = @establishments.that_match_providers_or_sites_by_name
+    @establishments_that_match_provider_name = @establishments.that_match_providers_by_name
+    @establishments_that_match_site_name = @establishments.that_match_sites_by_name
+    @establishments_that_match_any_postcode = @establishments.that_match_providers_or_sites_by_postcode
+    @establishments_that_match_provider_postcode = @establishments.that_match_providers_by_postcode
+    @establishments_that_match_site_postcode = @establishments.that_match_sites_by_postcode
+
     @providers = @current_recruitment_cycle.providers
-    @establishments = GIASEstablishment.all
-    @providers_that_match_by_postcode = @providers.that_match_establishments_by_postcode
-    @providers_with_sites_that_match_by_postcode = @providers.with_sites_that_match_establishments_by_postcode
+    @providers_with_any_name_match = @providers.with_establishments_that_match_any_name
     @providers_that_match_by_name = @providers.that_match_establishments_by_name
     @providers_with_sites_that_match_by_name = @providers.with_sites_that_match_establishments_by_name
-    @providers_with_any_name_match = @providers.with_establishments_that_match_any_name
     @providers_with_any_postcode_match = @providers.with_establishments_that_match_any_postcode
+    @providers_that_match_by_postcode = @providers.that_match_establishments_by_postcode
+    @providers_with_sites_that_match_by_postcode = @providers.with_sites_that_match_establishments_by_postcode
   end
 
   def import_establishments
--- app/models/gias_establishment.rb
+++ app/models/gias_establishment.rb
@@ -4,4 +4,47 @@ class GIASEstablishment < ApplicationRecord
                           foreign_key: "establishment_id",
                           inverse_of: :establishments
 
+  has_and_belongs_to_many :providers_matched_by_postcode,
+                          join_table: :gias_establishment_provider_postcode_matches,
+                          class_name: "Provider",
+                          foreign_key: "establishment_id"
+
+  has_and_belongs_to_many :sites_matched_by_postcode,
+                          join_table: :gias_establishment_site_postcode_matches,
+                          class_name: "Site",
+                          foreign_key: "establishment_id"
+
+  has_and_belongs_to_many :providers_matched_by_name,
+                          join_table: :gias_establishment_provider_name_matches,
+                          class_name: "Provider",
+                          foreign_key: "establishment_id"
+
+  has_and_belongs_to_many :sites_matched_by_name,
+                          join_table: :gias_establishment_site_name_matches,
+                          class_name: "Site",
+                          foreign_key: "establishment_id"
+
+  scope :that_match_providers_by_postcode, -> do
+    joins(:providers_matched_by_postcode).distinct
+  end
+
+  scope :that_match_sites_by_postcode, -> do
+    joins(:sites_matched_by_postcode).distinct
+  end
+
+  scope :that_match_providers_or_sites_by_postcode, -> do
+    where(id: (that_match_providers_by_postcode.pluck(:id) + that_match_sites_by_postcode.pluck(:id))).distinct
+  end
+
+  scope :that_match_providers_by_name, -> do
+    joins(:providers_matched_by_name).distinct
+  end
+
+  scope :that_match_sites_by_name, -> do
+    joins(:sites_matched_by_name).distinct
+  end
+
+  scope :that_match_providers_or_sites_by_name, -> do
+    where(id: (that_match_providers_by_name.pluck(:id) + that_match_sites_by_name.pluck(:id)))
+  end
 end
