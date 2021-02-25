44040b8401f53e786422ff09fb3dc9fd8bc4afd0
Toby Retallick <toby.retallick@gmail.com>
2021-02-25 16:08:52 +0000

    Update the default sleep and batchsize to conform with Mapit API rate limits

--- lib/mcb/commands/sites/add_travel_to_work_data.rb
+++ lib/mcb/commands/sites/add_travel_to_work_data.rb
@@ -1,8 +1,11 @@
 name "add travel to work data"
 summary "Batch add travel to work area or London Borough if located in London to Sites in the current cycle."
 usage "add_travel_to_work_data [options]"
-option :b, "batch_size", "batch size", argument: :optional, default: 100, transform: method(:Integer)
-option :s, "sleep", "time to sleep between each request", argument: :optional, default: 0.25, transform: method(:Float)
+option :b, "batch_size", "batch size", argument: :optional, default: 10, transform: method(:Integer)
+option :s, "sleep", "time to sleep between each request", argument: :optional, default: 1.25, transform: method(:Float)
+
+# rate limit is an average of 1 call per second in a rolling 3 minute period
+# https://mapit.mysociety.org/legal/#usage
 
 instance_eval(&MCB.remote_connect_options)
 
