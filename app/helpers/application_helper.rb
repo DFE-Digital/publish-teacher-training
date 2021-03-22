747a0e54dec29761b2bcfa3a43f9faedb71ce1d2
De Fong <de_fong2k@yahoo.com>
2021-03-22 11:03:58 +0000

    [1114] Added header

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,10 @@
 module ApplicationHelper
   include Pagy::Frontend
+  
+  def header_items(current_user)
+    return unless current_user
+
+    items = [{ name: t("header.items.sign_out"), url: sign_out_path }]
+    items
+  end
 end
---
+++ spec/components/header/view_spec.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+module Header
+  describe View do
+    alias_method :component, :page
+
+    let(:items) { nil }
+
+    before do
+      render_inline(described_class.new(service_name: "Test Service", items: items))
+    end
+
+    it "renders Service's name" do
+      expect(component.find(".govuk-header__product-name")).to have_text("Test Service")
+    end
+
+    it "does not render links" do
+      expect(component).not_to have_selector(".app-header__content")
+    end
+
+    context "with one menu item" do
+      let(:items) { [{ name: "Link", url: "www.google.com" }] }
+
+      it "renders the link and adds the correct class" do
+        expect(component.find_all(".app-header__content .govuk-header__link").length).to eq(1)
+        expect(page).to have_css(".app-header__content--single-item")
+      end
+    end
+
+    context "with multiple menu items" do
+      let(:items) { [{ name: "Link1", url: "www.google.com" }, { name: "Link2", url: "www.google.com" }] }
+
+      it "renders the links" do
+        expect(component.find_all(".app-header__content .govuk-header__link").length).to eq(2)
+        expect(page).to_not have_css(".app-header__content--single-item")
+      end
+    end
+  end
+end
