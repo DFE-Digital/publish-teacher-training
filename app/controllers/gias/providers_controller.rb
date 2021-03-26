7c9c5980e8593e2aefba93d577e31dfa4b56a6e3
Misha Gorodnitzky <misaka@pobox.com>
2021-03-26 13:45:44 +0000

    [452] add provider name and postcode filter
    Allow filtering on providers that match an establishment by name and
    postcode (not including sites).

--- app/controllers/gias/providers_controller.rb
+++ app/controllers/gias/providers_controller.rb
@@ -8,6 +8,12 @@ module GIAS
 
       providers = providers.search_by_code_or_name(@filters.search) if @filters.search.present?
 
+      if @filters.name_and_postcode&.include? "provider"
+        providers = providers
+                      .joins(:establishments_matched_by_postcode)
+                      .joins(:establishments_matched_by_name)
+                      .where('"gias_establishment_provider_postcode_matches"."establishment_id" = "gias_establishment_provider_name_matches"."establishment_id"')
+      end
       providers = providers.that_match_establishments_by_postcode            if @filters.postcode.include? "provider"
       providers = providers.with_sites_that_match_establishments_by_postcode if @filters.postcode.include? "sites"
       providers = providers.with_establishments_that_match_any_postcode      if @filters.postcode.include? "provider_or_sites"
@@ -42,12 +48,14 @@ module GIAS
 
     def build_filters
       @filters = OpenStruct.new(
-        name:     params.dig(:filters, :name) || [],
-        postcode: params.dig(:filters, :postcode) || [],
-        search:   params.dig(:filters, :search),
+        name_and_postcode: params.dig(:filters, :name_and_postcode) || [],
+        name:              params.dig(:filters, :name) || [],
+        postcode:          params.dig(:filters, :postcode) || [],
+        search:            params.dig(:filters, :search),
       )
 
       @filter_object = OpenStruct.new(
+        name_and_postcode: @filters.name_and_postcode.reject(&:blank?),
         name: @filters.name.reject(&:blank?),
         postcode: @filters.postcode.reject(&:blank?),
         search: @filters.search,
