a147da45dbfba79e3f2149b612843847e3565514
Arlston Fernandes <gpg@arlston.com>
2021-06-24 14:21:57 +0100

    Add EmailAddressValidator, from earlier EmailValidator
    Previously we used a validator defined as EmailValidator,
    which had to be discarded when we switched to usng DfE sign in,
    which has a dependency on validates_email gem via omniauth_openid_connect gem.
    
    However, validates_email relies on Mail::Address to validate addresses,
    which isn't optimal for our use, as 'Person Example <person@example.com>' would be a
    valid email address.
    
    Hence to get around the clashing implementation, this commit renames
    EmailValidator to EmailAddressValidator.
    
    Additionally, it picks up the implementation directly from the rails
    guides at https://guides.rubyonrails.org/active_record_validations.html#custom-validators
    and adds a few test cases matching malformed data found among our
    providers.

---
+++ app/validators/email_address_validator.rb
@@ -0,0 +1,9 @@
+class EmailAddressValidator < ActiveModel::EachValidator
+  EMAIL_VALIDATION_ERROR_MESSAGE = "^Enter an email address in the correct format, like name@example.com".freeze
+
+  def validate_each(record, attribute, value)
+    unless value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+      record.errors.add attribute, (options[:message] || EMAIL_VALIDATION_ERROR_MESSAGE)
+    end
+  end
+end
