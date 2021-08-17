207b22702e8341bdb9b5291385427ac0d88af4dc
Graham Pengelly <graham@three-tier.com>
2021-08-17 17:39:47 +0100

    [xxx] BigQuery key is JSON
    In secrets we store the big query secret as a JSON string so there's no
    need to `to_json` which breaks everything.

--- config/initializers/bigquery.rb
+++ config/initializers/bigquery.rb
@@ -3,7 +3,7 @@ require "google/cloud/bigquery"
 if FeatureService.enabled?(:send_request_data_to_bigquery)
   Google::Cloud::Bigquery.configure do |config|
     config.project_id  = Settings.google.bigquery.project_id
-    config.credentials = JSON.parse(Settings.google.bigquery.api_json_key.to_json)
+    config.credentials = JSON.parse(Settings.google.bigquery.api_json_key)
   end
 
   Google::Cloud::Bigquery.new
