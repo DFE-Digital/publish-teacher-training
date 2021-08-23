c1139ec764868ded11eb3e445aeaf2402d1916a2
Graham Pengelly <graham@three-tier.com>
2021-08-23 18:17:10 +0100

    [2256] Add and hook up EmitsEntityEvent
    * Add analytics config
    * Add EmitsEntityEvents concern
    * include it in ApplicationModel
    
    This will cause creation and update of configured models to be sent to
    BigQuery for analytics.

---
+++ app/models/concerns/emits_entity_events.rb
@@ -0,0 +1,41 @@
+module EmitsEntityEvents
+  extend ActiveSupport::Concern
+
+  included do
+    attr_accessor :event_tags
+
+    after_create do
+      data = entity_data(attributes)
+      send_event("create_entity", data) if data.any?
+    end
+
+    after_update do
+      # in this after_update hook we don’t have access to the new fields via
+      # #attributes — we need to dig them out of saved_changes which stores
+      # them in the format { attr: ['old', 'new'] }
+
+      interesting_changes = entity_data(saved_changes.transform_values(&:last))
+
+      if interesting_changes.any?
+        send_event("update_entity", entity_data(attributes).merge(interesting_changes))
+      end
+    end
+  end
+
+  def send_event(type, data)
+    return unless FeatureService.enabled?(:send_request_data_to_bigquery)
+
+    event = BigQuery::EntityEvent.new do |ee|
+      ee.with_type(type)
+      ee.with_entity_table_name(self.class.table_name)
+      ee.with_data(data)
+    end
+
+    SendEventToBigQueryJob.perform_later(event.as_json)
+  end
+
+  def entity_data(changeset)
+    exportable_attrs = Rails.configuration.analytics[self.class.table_name.to_sym]
+    changeset.slice(*exportable_attrs&.map(&:to_s))
+  end
+end
---
+++ spec/models/concerns/emits_entity_events_spec.rb
@@ -0,0 +1,59 @@
+require "rails_helper"
+require "sidekiq/testing"
+
+# This spec is coupled to the Candidate model. I thought
+# this was preferable to making an elaborate mock which
+# didn't depend on the db
+RSpec.describe EmitsEntityEvents do
+  include ActiveJob::TestHelper
+  let(:include_fields) { [] }
+
+  before do
+    allow(FeatureService).to receive(:enabled?).with(:send_request_data_to_bigquery).and_return(true)
+    allow(Rails.configuration).to receive(:analytics).and_return({
+      provider: include_fields,
+    })
+  end
+
+  describe "create" do
+    context "when an entity has configured fields to include" do
+      let(:include_fields) { %w(provider_name provider_code) }
+
+      before do
+        create(:provider)
+      end
+
+      it "sends the event" do
+        expect(SendEventToBigQueryJob).to have_been_enqueued
+      end
+    end
+
+    context "when an entity does not have configured fields to include" do
+      it "doesn't send an event" do
+        expect(SendEventToBigQueryJob).not_to have_been_enqueued
+      end
+    end
+  end
+
+  describe "update" do
+    before do
+      provider = create(:provider)
+      clear_enqueued_jobs
+      provider.update(provider_name: "Dave", provider_code: "M1X")
+    end
+
+    context "when an entity has configured fields to include" do
+      let(:include_fields) { %w(provider_name provider_code) }
+
+      it "sends the event" do
+        expect(SendEventToBigQueryJob).to have_been_enqueued
+      end
+    end
+
+    context "when an entity does not have configured fields to include" do
+      it "doesn't send an event" do
+        expect(SendEventToBigQueryJob).not_to have_been_enqueued
+      end
+    end
+  end
+end
