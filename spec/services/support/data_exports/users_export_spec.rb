dc7e3369cb55d073073123c43003111e4cff8c4c
Graham Pengelly <graham@three-tier.com>
2021-09-23 10:01:08 +0100

    [xxx] Add extra fields to users export
    Add user ID, first and last login dates.
    
    If we add user ID BAs can link this list with the anonymised data in
    staging for analysis without having to do said analysis on production.
    
    First and last login dates are useful for UR to determine active users.

--- spec/services/support/data_exports/users_export_spec.rb
+++ spec/services/support/data_exports/users_export_spec.rb
@@ -8,19 +8,22 @@ RSpec.describe Support::DataExports::UsersExport do
   end
 
   describe ".user_data" do
-    let(:user) { build(:user) }
+    let(:user) { build(:user, first_login_date_utc: 2.days.ago, last_login_date_utc: 1.day.ago) }
     let(:provider) { build(:provider) }
 
     it "returns hash of user data" do
       res = subject.send(:user_data, user, provider)
       expect(res).to eql(
         {
+          user_id: user.id,
           provider_code: provider.provider_code,
           provider_name: provider.provider_name,
           provider_type: provider.provider_type,
           first_name: user.first_name,
           last_name: user.last_name,
           email_address: user.email,
+          first_login_at: user.first_login_date_utc,
+          last_login_at: user.last_login_date_utc,
         },
       )
     end
@@ -41,20 +44,26 @@ RSpec.describe Support::DataExports::UsersExport do
       res = subject.data
       expect(res).to eql([
         {
+          user_id: user1.id,
           provider_code: user1.providers.first.provider_code,
           provider_name: user1.providers.first.provider_name,
           provider_type: user1.providers.first.provider_type,
           first_name: user1.first_name,
           last_name: user1.last_name,
           email_address: user1.email,
+          first_login_at: user1.first_login_date_utc,
+          last_login_at: user1.last_login_date_utc,
         },
         {
+          user_id: user2.id,
           provider_code: user2.providers.first.provider_code,
           provider_name: user2.providers.first.provider_name,
           provider_type: user2.providers.first.provider_type,
           first_name: user2.first_name,
           last_name: user2.last_name,
           email_address: user2.email,
+          first_login_at: user2.first_login_date_utc,
+          last_login_at: user2.last_login_date_utc,
         },
       ])
     end
