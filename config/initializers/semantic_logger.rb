8c889a1b1c0958a474fef4414203c25d2e1d5630
Malcolm Baig <git@baig.org>
2021-10-09 10:17:33 +0100

    Don't use json format logs in dev and test
    Much easier to read the default Rails format.

--- config/initializers/semantic_logger.rb
+++ config/initializers/semantic_logger.rb
@@ -1,4 +1,6 @@
 Rails.application.config.semantic_logger.application = Settings.application_name
 Rails.application.config.rails_semantic_logger.format = :json
-SemanticLogger.add_appender(io: STDOUT, level: Rails.application.config.log_level, formatter: Rails.application.config.rails_semantic_logger.format)
-Rails.application.config.logger.info("Application logging to STDOUT")
+unless Rails.env.development? || Rails.env.test?
+  SemanticLogger.add_appender(io: STDOUT, level: Rails.application.config.log_level, formatter: Rails.application.config.rails_semantic_logger.format)
+  Rails.application.config.logger.info("Application logging to STDOUT")
+end
