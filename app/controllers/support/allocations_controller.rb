bd653006515a095687a295aedb001137cad1ec50
Stephan Ellenberger <stephan.ellenberger@digital.education.gov.uk>
2021-10-29 14:35:51 +0100

    [320] Added filter to page

--- app/controllers/support/allocations_controller.rb
+++ app/controllers/support/allocations_controller.rb
@@ -7,11 +7,19 @@ module Support
   private
 
     def filtered_providers_with_allocations
-      Support::Providers::Filter.call(providers: find_providers_with_allocations, filters: nil)
+      Support::Providers::Filter.call(providers: find_providers_with_allocations, filters: filters)
     end
 
     def find_providers_with_allocations
       Provider.where(id: Allocation.current_allocations.select(:provider_id))
+    end
+
+    def filters
+      @filters ||= ProviderFilter.new(params: filter_params).filters
+    end
+
+    def filter_params
+      params.permit(:text_search, :page, :commit)
     end
   end
 end
