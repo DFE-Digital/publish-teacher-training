0e3d16089f5eb27df189681dcec65c2ce79c3b6f
Stephan Ellenberger <stephan.ellenberger@digital.education.gov.uk>
2021-11-08 11:01:10 +0000

    [320] Follow apply guideline for feature specs

--- spec/features/support/allocations/allocation_uplift_edit_spec.rb
+++ spec/features/support/allocations/allocation_uplift_edit_spec.rb
@@ -3,13 +3,8 @@
 require "rails_helper"
 
 feature "Edit an Allocation Uplift" do
-  let(:user) { create(:user, :admin) }
-  let(:provider) { create(:provider) }
-  let(:allocation) { create(:allocation, :with_allocation_uplift, provider: provider, number_of_places: 5) }
-  let(:new_allocation_uplift) { 500 }
-
   before do
-    given_i_am_authenticated(user: user)
+    given_i_am_authenticated(user: create(:user, :admin))
     and_there_are_providers_with_allocations_and_uplifts
     when_i_visit_the_allocation_uplift_edit_page
   end
@@ -25,15 +20,17 @@ feature "Edit an Allocation Uplift" do
 private
 
   def and_there_are_providers_with_allocations_and_uplifts
-    allocation
+    @provider = create(:provider)
+    @allocation = create(:allocation, :with_allocation_uplift, provider: @provider, number_of_places: 5)
   end
 
   def when_i_visit_the_allocation_uplift_edit_page
-    allocation_uplift_edit_page.load(uplift_id: allocation.allocation_uplift.id, allocation_id: allocation.id)
+    allocation_uplift_edit_page.load(uplift_id: @allocation.allocation_uplift.id, allocation_id: @allocation.id)
   end
 
   def then_i_can_edit_their_allocation_uplift_amount
-    allocation_uplift_edit_page.allocation_uplift_amount.set(new_allocation_uplift)
+    @new_allocation_uplift = 500
+    allocation_uplift_edit_page.allocation_uplift_amount.set(@new_allocation_uplift)
   end
 
   def and_i_click_continue
@@ -41,7 +38,7 @@ private
   end
 
   def and_i_get_redirected_to_allocation_show_page
-    expect(allocations_show_page).to be_displayed(id: allocation.id)
+    expect(allocations_show_page).to be_displayed(id: @allocation.id)
   end
 
   def with_a_success_message
@@ -49,7 +46,7 @@ private
   end
 
   def and_the_allocation_has_updated
-    allocation.reload
-    expect(allocation.allocation_uplift.uplifts).to eq new_allocation_uplift
+    @allocation.reload
+    expect(@allocation.allocation_uplift.uplifts).to eq @new_allocation_uplift
   end
 end
