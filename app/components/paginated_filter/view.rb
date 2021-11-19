241d42b74f9a9cfc77eef4d3476eb0ae63803252
Stephan Ellenberger <stephan.ellenberger@digital.education.gov.uk>
2021-11-19 16:15:04 +0000

    [603] Include filter label in paginated filter

--- app/components/paginated_filter/view.rb
+++ app/components/paginated_filter/view.rb
@@ -2,13 +2,14 @@
 
 module PaginatedFilter
   class View < ViewComponent::Base
-    attr_reader :filters, :collection
+    attr_reader :filters, :collection, :filter_label
 
     renders_one :filter_actions
 
-    def initialize(filters:, collection:)
+    def initialize(filters:, collection:, filter_label:)
       @filters = filters
       @collection = collection
+      @filter_label = filter_label
     end
   end
 end
