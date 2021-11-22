1eed18d1d61e94407453fd3e07218fcac53d901a
Stephan Ellenberger <stephan.ellenberger@digital.education.gov.uk>
2021-11-22 17:34:04 +0000

    [406] Change allocation table to use card-list

---
+++ app/components/allocation_record_card/view.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+module AllocationRecordCard
+  class View < GovukComponent::Base
+    with_collection_parameter :allocation
+    delegate :provider, :accredited_body, to: :allocation
+    delegate :provider_name, to: :provider
+
+    attr_reader :allocation, :heading_level
+
+    def initialize(heading_level = 3, allocation:)
+      @allocation = allocation
+      @heading_level = heading_level
+    end
+
+    def provider_code
+      tag.p("Provider code: #{provider.provider_code}", class: "govuk-caption-m govuk-!-font-size-16 allocation-record-card__id govuk-!-margin-bottom-0")
+    end
+
+    def accredited_body_code
+      tag.p("Accrediting Body code: #{accredited_body&.provider_code}", class: "govuk-caption-m govuk-!-font-size-16 allocation-record-card__id govuk-!-margin-bottom-0")
+    end
+
+    def accredited_body_name
+      tag.p("Accredited by #{accredited_body&.provider_name}", class: "govuk-caption-m govuk-!-font-size-16 allocation-record-card__id govuk-!-margin-bottom-0")
+    end
+
+    def number_of_places
+      tag.p("Allocated places: #{allocation.confirmed_number_of_places}", class: "govuk-caption-m govuk-!-font-size-16 allocation-record-card__id govuk-!-margin-bottom-0")
+    end
+
+    def number_of_uplifts
+      tag.p("Allocation Uplifts: #{allocation.allocation_uplift&.uplifts.to_i}", class: "govuk-caption-m govuk-!-font-size-16 allocation-record-card__id govuk-!-margin-bottom-0")
+    end
+  end
+end
