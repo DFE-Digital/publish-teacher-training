cb1a58a79bb31f61d5a961df46139348884e2922
Stephan Ellenberger <stephan.ellenberger@digital.education.gov.uk>
2021-11-25 13:24:11 +0000

    [406] Use openstruct instead of factorybot for view previews

--- spec/components/allocation_record_card/view_preview.rb
+++ spec/components/allocation_record_card/view_preview.rb
@@ -9,7 +9,42 @@ module AllocationRecordCard
   private
 
     def multiple_allocations
-      @multiple_allocations ||= FactoryBot.create_list(:allocation, 3, number_of_places: 5)
+      [
+        OpenStruct.new(
+          id: 1,
+          confirmed_number_of_places: 3,
+          provider: provider,
+          accredited_body: accredited_body,
+          allocation_uplift: allocation_uplift,
+        ),
+        OpenStruct.new(
+          id: 2,
+          confirmed_number_of_places: 5,
+          provider: provider,
+          accredited_body: accredited_body,
+          allocation_uplift: allocation_uplift,
+        ),
+      ]
+    end
+
+    def provider
+      @provider ||= OpenStruct.new(
+        provider_code: "HC7",
+        provider_name: "Hogwarts School of Witchcraft and Wizardry",
+      )
+    end
+
+    def accredited_body
+      @accredited_body ||= OpenStruct.new(
+        provider_code: "SP1",
+        provider_name: "South Park Elementary",
+      )
+    end
+
+    def allocation_uplift
+      @allocation_uplift ||= OpenStruct.new(
+        uplifts: 4,
+      )
     end
   end
 end
