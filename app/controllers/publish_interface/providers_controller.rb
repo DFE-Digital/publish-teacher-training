ed411ec6c2070b9e28d09f4614d76bbecc3459cd
Malcolm Baig <git@baig.org>
2021-12-06 11:40:20 +0000

    Migrate Publish - start moving About Your Organisation page
    Port over the controller action, routes, and view template.

--- app/controllers/publish_interface/providers_controller.rb
+++ app/controllers/publish_interface/providers_controller.rb
@@ -1,4 +1,33 @@
 module PublishInterface
   class ProvidersController < PublishInterfaceController
+    rescue_from ActiveRecord::RecordNotFound, with: :not_found
+    decorates_assigned :provider
+    before_action :build_recruitment_cycle
+    before_action :build_provider
+
+    def details
+      redirect_to_contact_page_with_ukprn_error if @provider.ukprn.blank?
+      @errors = flash[:error_summary]
+      flash.delete(:error_summary)
+    end
+
+  private
+
+    def build_recruitment_cycle
+      cycle_year = params[:recruitment_cycle_year] || params[:year] || Settings.current_cycle
+      @recruitment_cycle = RecruitmentCycle.find_by(year: cycle_year)
+    end
+
+    def build_provider
+      @provider = Provider
+        .where(recruitment_cycle: @recruitment_cycle, provider_code: params[:provider_code])
+        .first
+    end
+
+    def redirect_to_contact_page_with_ukprn_error
+      flash[:error] = { id: "provider-error", message: "Please enter a UKPRN before continuing" }
+
+      redirect_to contact_provider_recruitment_cycle_path(@provider.provider_code, @provider.recruitment_cycle_year)
+    end
   end
 end
