91a06d9526a74241169f4a1aec28a972ecf2371b
George Holborn <george.holborn@digital.education.gov.uk>
2021-12-07 18:00:16 +0000

    Update provider filter model
    Configure to work with provider and course params

--- app/models/provider_filter.rb
+++ app/models/provider_filter.rb
@@ -16,12 +16,23 @@ private
   attr_reader :params
 
   def merged_filters
-    @merged_filters ||= text_search.with_indifferent_access
+    @merged_filters ||= params.include?("text_search") ? text_search.with_indifferent_access : provider_and_course_search.with_indifferent_access
   end
 
   def text_search
     return {} if params[:text_search].blank?
 
-    { "text_search" => params[:text_search] }
+    {
+      "text_search" => params[:text_search],
+    }
+  end
+
+  def provider_and_course_search
+    return {} if params[:provider_search].blank? && params[:course_search].blank?
+
+    {
+      "provider_search" => params[:provider_search],
+      "course_search" => params[:course_search],
+    }
   end
 end
