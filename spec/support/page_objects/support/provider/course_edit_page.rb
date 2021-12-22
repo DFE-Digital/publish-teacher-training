145e7c27aa198c9b6fb11da418338ff367b4eaa0
George Holborn <george.holborn@digital.education.gov.uk>
2021-12-22 13:59:16 +0000

    Update feature spec to use new form

--- spec/support/page_objects/support/provider/course_edit_page.rb
+++ spec/support/page_objects/support/provider/course_edit_page.rb
@@ -6,11 +6,11 @@ module PageObjects
       class CourseEdit < PageObjects::Base
         set_url "/support/providers/{provider_id}/courses/{course_id}/edit"
 
-        element :course_code, "#course-course-code-field"
-        element :name, "#course-name-field"
-        element :start_date_day, "#course_start_date_3i"
-        element :start_date_month, "#course_start_date_2i"
-        element :start_date_year, "#course_start_date_1i"
+        element :course_code, "#support-edit-course-form-course-code-field"
+        element :name, "#support-edit-course-form-name-field"
+        element :start_date_day, "#support_edit_course_form_start_date_3i"
+        element :start_date_month, "#support_edit_course_form_start_date_2i"
+        element :start_date_year, "#support_edit_course_form_start_date_1i"
         element :error_summary, ".govuk-error-summary"
         element :continue, ".govuk-button"
       end
