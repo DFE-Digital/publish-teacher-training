b59036f65b0867a35e5b3cf31c2ff8422dede264
Stephan Ellenberger <stephan.ellenberger@digital.education.gov.uk>
2022-01-06 16:13:07 +0000

    [500] Edit course form date to be dynamic

--- app/forms/support/edit_course_form.rb
+++ app/forms/support/edit_course_form.rb
@@ -8,8 +8,9 @@ module Support
     ].freeze
 
     attr_accessor(*FIELDS)
-    attr_accessor :start_date_day, :start_date_month, :start_date_year, :course
+    attr_accessor :start_date_day, :start_date_month, :start_date_year, :course, :applications_open_from_date_day, :applications_open_from_date_month, :applications_open_from_date_year
     validate :validate_start_date_format
+    validate :validate_applications_open_from_date_format
 
     def initialize(course)
       @course = course
@@ -20,6 +21,9 @@ module Support
         start_date_day: @course.start_date&.day,
         start_date_month: @course.start_date&.month,
         start_date_year: @course.start_date&.year,
+        applications_open_from_date_day: @course.applications_open_from&.day,
+        applications_open_from_date_month: @course.applications_open_from&.month,
+        applications_open_from_date_year: @course.applications_open_from&.year,
       )
     end
 
@@ -38,24 +42,44 @@ module Support
     end
 
     def start_date
-      @start_date ||= check_start_date
+      @start_date ||= check_date(:start_date)
+    end
+
+    def applications_open_from_date
+      @applications_open_from_date ||= check_date(:applications_open_from_date)
     end
 
   private
 
-    def check_start_date
-      date_args = [start_date_year, start_date_month, start_date_day].map(&:to_i)
+    def check_date(date_type)
+      date_args = date_array(date_type).map(&:to_i)
 
-      Date.valid_date?(*date_args) ? Date.new(*date_args) : Struct.new(:day, :month, :year).new(start_date_day, start_date_month, start_date_year)
+      if Date.valid_date?(*date_args)
+        Date.new(*date_args)
+      elsif date_args_blank?(date_type)
+        nil
+      else
+        OpenStruct.new(
+          day: send("#{date_type}_day"),
+          month: send("#{date_type}_month"),
+          year: send("#{date_type}_year"),
+        )
+      end
+    end
+
+    def date_array(date_type)
+      [send("#{date_type}_year"), send("#{date_type}_month"), send("#{date_type}_day")]
     end
 
     def assign_attributes_to_course
       attributes = {
         course_code: course_code,
         name: name,
+        start_date: start_date,
+        applications_open_from: applications_open_from_date,
       }
 
-      attributes[:start_date] = start_date if valid_date? || date_args_blank?
+      attributes[:applications_open_from] = applications_open_from_date if valid_date?(:applications_open_from_date)
 
       course.assign_attributes(attributes)
     end
@@ -65,17 +89,25 @@ module Support
     end
 
     def validate_start_date_format
-      return if date_args_blank?
-
-      errors.add(:start_date, "Start date format is invalid") unless valid_date?
+      validate_date(:start_date)
     end
 
-    def valid_date?
-      start_date.is_a?(Date)
+    def validate_applications_open_from_date_format
+      validate_date(:applications_open_from_date)
     end
 
-    def date_args_blank?
-      [start_date_year, start_date_month, start_date_day].any?(&:blank?)
+    def validate_date(date_type)
+      return if date_args_blank?(date_type)
+
+      errors.add(date_type, "#{date_type.to_s.humanize.capitalize} format is invalid") unless valid_date?(date_type)
+    end
+
+    def valid_date?(date_type)
+      send(date_type).is_a?(Date)
+    end
+
+    def date_args_blank?(date_type)
+      date_array(date_type).any?(&:blank?)
     end
   end
 end
