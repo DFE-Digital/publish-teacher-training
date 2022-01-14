cb49572c7a924cdff3fcd9b3ae37ffaf6fe32385
De Fong <de_fong2k@yahoo.com>
2022-01-14 09:55:36 +0000

    [0509] Ported validation from Provider

--- app/forms/publish_interface/about_your_organisation_form.rb
+++ app/forms/publish_interface/about_your_organisation_form.rb
@@ -1,8 +1,12 @@
 module PublishInterface
   class AboutYourOrganisationForm < BaseProviderForm
-  class AboutYourOrganisationForm
-    include ActiveModel::Model
-    include ActiveModel::Validations
+    validates :train_with_us, presence: { message: "Enter details about training with you" }, if: :train_with_us_changed?
+    validates :train_with_disability, presence: { message: "Enter details about training with a disability" }, if: :train_with_disability_changed?
+
+    validates :train_with_us, words_count: { maximum: 250, message: "Reduce the word count for training with you" }
+    validates :train_with_disability, words_count: { maximum: 250, message: "Reduce the word count for training with disabilities and other needs" }
+
+    validate :add_enrichment_errors
 
     FIELDS = %i[
       train_with_us
@@ -19,6 +23,18 @@ module PublishInterface
     end
 
   private
+
+    def train_with_us_changed?
+      changed?(:train_with_us)
+    end
+
+    def train_with_disability_changed?
+      changed?(:train_with_disability)
+    end
+
+    def changed?(attribute)
+      public_send(attribute) != @provider.public_send(attribute)
+    end
 
     def accredited_body(provider_name:, provider_code:, description:)
       AccreditedBody.new(
@@ -59,6 +75,9 @@ module PublishInterface
 
     class AccreditedBody
       include ActiveModel::Model
+      validates :description, words_count: { maximum: 100, message: lambda do |object, _data|
+        "Reduce the word count for #{object.provider_name}"
+      end }
 
       attr_accessor :provider_name, :provider_code, :description
     end
