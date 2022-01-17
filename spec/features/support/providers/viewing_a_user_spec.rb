4bbc041c715b1a41e332841720428886e9ec4716
Stephan Ellenberger <stephan.ellenberger@digital.education.gov.uk>
2022-01-17 12:35:10 +0000

    [510] Add specs to user page

--- spec/features/support/providers/viewing_a_user_spec.rb
+++ spec/features/support/providers/viewing_a_user_spec.rb
@@ -3,30 +3,37 @@
 require "rails_helper"
 
 feature "Viewing a user" do
-  let(:admin) { create(:user, :admin) }
-  let(:user) { create(:user, :with_provider) }
-
   before do
-    given_i_am_authenticated(user: admin)
+    given_i_am_authenticated(user: create(:user, :admin))
+    and_there_is_a_user
     when_i_visit_the_provider_show_page
-    # and_click_on_the_users_tab
-    # and_i_click_on_the_user
+    and_click_on_the_users_tab
+    i_can_see_a_list_of_users
+    and_i_click_on_the_user
   end
 
   scenario "i can view providers that belong to a user" do
-    # then_i_should_see_a_table_of_providers
+    then_i_should_see_a_table_of_providers
   end
 
   def when_i_visit_the_provider_show_page
-    provider_show_page.load(id: user.providers.first.id)
+    provider_show_page.load(id: @user.providers.first.id)
+  end
+
+  def and_there_is_a_user
+    @user = create(:user, :with_provider)
   end
 
   def and_click_on_the_users_tab
     provider_show_page.users_tab.click
   end
 
+  def i_can_see_a_list_of_users
+    expect(provider_users_index_page.users.first.full_name.text).to eq(@user.full_name)
+  end
+
   def and_i_click_on_the_user
-    provider_users_index_page.users.first.email.click
+    provider_users_index_page.users.first.full_name.click
   end
 
   def then_i_should_see_a_table_of_providers
