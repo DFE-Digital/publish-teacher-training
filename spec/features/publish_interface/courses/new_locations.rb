215c362ca896ce1f7521a7f6aabb437d473e7295
Stephan Ellenberger <stephan.ellenberger@digital.education.gov.uk>
2022-02-03 16:37:26 +0000

    [563] Add new locations spec

---
+++ spec/features/publish_interface/courses/new_locations.rb
@@ -0,0 +1,65 @@
+require "rails_helper"
+
+feature "selection locations" do
+  before do
+    given_i_am_authenticated_as_a_provider_user
+    and_that_sites_exist
+    when_i_visit_the_new_locations_page
+  end
+
+  scenario "selecting multiple locations" do
+    when_i_select_a_location
+    and_i_click_continue
+    then_i_am_met_with_the_accredited_body_page
+  end
+
+  # scenario "invalid entries" do
+  #   and_i_select_nothing
+  #   and_i_click_continue
+  #   then_i_am_met_with_errors
+  # end
+
+private
+
+  def given_i_am_authenticated_as_a_provider_user
+    @user = create(:user, :with_provider)
+    given_i_am_authenticated(user: @user)
+  end
+
+  def and_that_sites_exist
+    provider.sites << create_list(:site, 3)
+  end
+
+  def when_i_visit_the_new_locations_page
+    new_locations_page.load(provider_code: provider.provider_code, recruitment_cycle_year: Settings.current_recruitment_cycle_year, query: params)
+  end
+
+  def params
+    { "course[age_range_in_years]" => ["3_to_7"], "course[funding_type]" => ["fee"], "course[is_send]" => ["0"], "course[level]" => "primary", "course[qualification]" => "qts", "course[study_mode]" => "full_time", "course[subjects][]" => "2" }
+  end
+
+  def when_i_select_a_location
+    new_locations_page.check("sites-#{provider.sites.first.id}")
+    new_locations_page.check("sites-#{provider.sites.second.id}")
+  end
+
+  def and_i_select_nothing; end
+
+  def and_i_click_continue
+    new_locations_page.continue.click
+  end
+
+  def provider
+    @provider ||= @user.providers.first
+  end
+
+  def then_i_am_met_with_the_accredited_body_page
+    expect(page.current_path).to match("/publish/organisations/#{provider.provider_code}/#{Settings.current_recruitment_cycle_year}/courses/accredited-body/new")
+    expect(page).to have_content("Who is the accredited body?")
+  end
+
+  def then_i_am_met_with_errors
+    expect(page).to have_content("There is a problem")
+    expect(page).to have_content("Pick full time, part time or full time and part time")
+  end
+end
