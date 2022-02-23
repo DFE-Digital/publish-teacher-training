e5b3ef825ca617f15e353c5847b634733146e923
De Fong <de_fong2k@yahoo.com>
2022-02-23 14:30:18 +0000

    [0629] Added markdown renderer

--- app/helpers/publish_helper.rb
+++ app/helpers/publish_helper.rb
@@ -4,4 +4,42 @@ module PublishHelper
   def old_publish_link_for(path)
     "#{Settings.publish_url}#{path.sub(/\/publish\//, '/')}"
   end
+
+  def markdown(source)
+    render = Govuk::MarkdownRenderer
+    # Options: https://github.com/vmg/redcarpet#and-its-like-really-simple-to-use
+    # lax_spacing: HTML blocks do not require to be surrounded by an empty line as in the Markdown standard.
+    # autolink: parse links even when they are not enclosed in <> characters
+    options = { autolink: true, lax_spacing: true }
+    markdown = Redcarpet::Markdown.new(render, options)
+    markdown.render(source).html_safe
+
+    # Convert quotes to smart quotes
+    source_with_smart_quotes = smart_quotes(source)
+    markdown.render(source_with_smart_quotes).html_safe
+  end
+
+  def smart_quotes(string)
+    return "" if string.blank?
+
+    RubyPants.new(string, [2, :dashes], ruby_pants_options).to_html
+  end
+
+private
+
+  # Use characters rather than HTML entities for smart quotes this matches how
+  # we write smart quotes in templates and allows us to use them in <title>
+  # elements
+  # https://github.com/jmcnevin/rubypants/blob/master/lib/rubypants.rb
+  def ruby_pants_options
+    {
+      double_left_quote: "“",
+      double_right_quote: "”",
+      single_left_quote: "‘",
+      single_right_quote: "’",
+      ellipsis: "…",
+      em_dash: "—",
+      en_dash: "–",
+    }
+  end
 end
