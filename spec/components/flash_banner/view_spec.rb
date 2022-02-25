078c4a5362fc8efde9fbdfa6d34181a8fffc3575
De Fong <de_fong2k@yahoo.com>
2022-02-25 11:08:10 +0000

    Fixed flakey banner specs

--- spec/components/flash_banner/view_spec.rb
+++ spec/components/flash_banner/view_spec.rb
@@ -6,19 +6,24 @@ module FlashBanner
   describe View do
     alias_method :component, :page
 
-    let(:referer) { nil }
-    let(:message) { "Provider #{type}" }
-    let(:type) { View::FLASH_TYPES.sample }
-    let(:flash) { ActionDispatch::Flash::FlashHash.new(type => message) }
-    let(:expected_title) { type == :success ? "Success" : "Important" }
+    View::FLASH_TYPES.each do |type|
+      context "when flash type is set to: #{type}" do
+        let(:referer) { nil }
+        let(:message) { "Provider #{type}" }
+        let(:flash) { ActionDispatch::Flash::FlashHash.new(type => message) }
+        let(:expected_title) do
+          { success: "Success", warning: "Important", info: "Important" }[type]
+        end
 
-    before do
-      render_inline(described_class.new(flash: flash))
-    end
+        before do
+          render_inline(described_class.new(flash: flash))
+        end
 
-    it "renders flash message" do
-      expect(component).to have_text(expected_title)
-      expect(component).to have_text(message)
+        it "renders flash message" do
+          expect(component).to have_text(expected_title)
+          expect(component).to have_text(message)
+        end
+      end
     end
   end
 end
