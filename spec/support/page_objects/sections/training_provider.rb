65a3ae666e96d2a164d359c7b7c7f7f0d8905195
David Kewal <davidrk@hey.com>
2022-04-04 15:41:57 +0100

    [836] Update spec element target name

--- spec/support/page_objects/sections/training_provider.rb
+++ spec/support/page_objects/sections/training_provider.rb
@@ -5,7 +5,7 @@ require_relative "base"
 module PageObjects
   module Sections
     class TrainingProvider < PageObjects::Sections::Base
-      element :name, '[data-qa="link"]'
+      element :name, '[data-qa="training_provider_name"]'
       element :course_count, '[data-qa="course_count"]'
     end
   end
