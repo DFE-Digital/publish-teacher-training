59cbaa8a41d587a732755b6492160fca284af794
maxmills <8balldigitalsolutions@gmail.com>
2022-04-06 18:07:02 +0100

    [622]Add recruitment_cycles_controller

---
+++ app/controllers/publish/recruitment_cycles_controller.rb
@@ -0,0 +1,21 @@
+module Publish
+  class RecruitmentCyclesController < PublishController
+    def show
+      @recruitment_cycle = RecruitmentCycle.find_by(year: params[:year])
+      #@provider = Provider.where(recruitment_cycle_year: params[:year])
+      #  .find(params[:provider_code])
+      #  .first
+      @provider ||= recruitment_cycle.providers.find_by!(provider_code:  params[:provider_code] || params[:code])
+authorize provider, :show?
+      unless @provider.rolled_over?
+        redirect_to provider_path(@provider.provider_code)
+      end
+    end
+
+    private
+
+      #def provider
+      #  @provider ||= recruitment_cycle.providers.find_by!(provider_code:  params[:provider_code] || #params[:code])
+      #end
+  end
+end
