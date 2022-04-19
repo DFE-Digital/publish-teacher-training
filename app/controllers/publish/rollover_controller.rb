6e7e4762e9b80f3a9f0bc7dfef50ac84ad313ccc
David Kewal <davidrk@hey.com>
2022-04-19 16:44:42 +0100

    Update app/controllers/publish/rollover_controller.rb
    Co-authored-by: De Fong <de_fong2k@yahoo.com>

--- app/controllers/publish/rollover_controller.rb
+++ app/controllers/publish/rollover_controller.rb
@@ -5,7 +5,7 @@ module Publish
     def create
       InterruptPageAcknowledgement.find_or_create_by!(
         user: current_user,
-        recruitment_cycle: RecruitmentCycle.find_by!(year: Settings.current_recruitment_cycle_year),
+        recruitment_cycle: RecruitmentCycle.current
         page: "rollover",
       )
 
