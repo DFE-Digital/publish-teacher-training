c16eec9bab34045a6efd6c4539ce8faf0a14b083
maxmills <8balldigitalsolutions@gmail.com>
2022-04-21 10:07:36 +0100

    [622] Rm comments and fix backlink

--- app/controllers/publish/recruitment_cycles_controller.rb
+++ app/controllers/publish/recruitment_cycles_controller.rb
@@ -4,18 +4,11 @@ module Publish
       authorize provider, :show?
 
       @recruitment_cycle = RecruitmentCycle.find_by(year: params[:year])
-      # @provider = Provider.where(recruitment_cycle_year: params[:year])
-      #  .find(params[:provider_code])
-      #  .first
       @provider ||= recruitment_cycle.providers.find_by!(provider_code: params[:provider_code] || params[:code])
 
       unless @provider.rolled_over?
         redirect_to publish_provider_path(@provider.provider_code)
       end
     end
-
-    # def provider
-    #  @provider ||= recruitment_cycle.providers.find_by!(provider_code:  params[:provider_code] ||#  params[:code])
-    # end
   end
 end
