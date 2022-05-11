97a55c8174a8f7b28152b19cf7b1f70e750984bf
maxmills <8balldigitalsolutions@gmail.com>
2022-05-11 12:34:35 +0100

    restore v2 serialisers

--- app/deserializers/api/v2/deserializable_contact.rb
+++
@@ -1,7 +0,0 @@
-module API
-  module V2
-    class DeserializableContact < JSONAPI::Deserializable::Resource
-      attributes :name, :email, :telephone, :permission_given, :type
-    end
-  end
-end
