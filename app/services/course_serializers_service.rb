e9ef02fadb576b967ba9e6ca2601e21af4135718
maxmills <8balldigitalsolutions@gmail.com>
2022-05-16 15:22:51 +0100

    update course seializer service again

--- app/services/course_serializers_service.rb
+++ app/services/course_serializers_service.rb
@@ -11,7 +11,7 @@ class CourseSerializersService
     provider_serializer: API::V3::SerializableProvider,
     # provider_enrichment_serializer: API::V2::SerializableProviderEnrichment,
     recruitment_cycle_serializer: API::V3::SerializableRecruitmentCycle,
-    v3_subject_area_serializer: API::V3::SerializableSubjectArea
+    subject_area_serializer: API::V3::SerializableSubjectArea
   )
     @course_serializer = course_serializer
     @subject_serializer = subject_serializer
@@ -22,10 +22,9 @@ class CourseSerializersService
     @site_serializer = site_serializer
     @site_status_serializer = site_status_serializer
     @provider_serializer = provider_serializer
-    # @provider_enrichment_serializer = provider_enrichment_serializer
     @recruitment_cycle_serializer = recruitment_cycle_serializer
 
-    @v3_subject_area_serializer = v3_subject_area_serializer
+    @subject_area_serializer = v3_subject_area_serializer
   end
 
   def execute
@@ -39,17 +38,17 @@ class CourseSerializersService
       SiteStatus: @site_status_serializer,
       Site: @site_serializer,
       Provider: @provider_serializer,
-      # ProviderEnrichment: @provider_enrichment_serializer,
       RecruitmentCycle: @recruitment_cycle_serializer,
-      v3: {
-        SubjectArea: @v3_subject_area_serializer,
-        # this is ok until the serializers for subjects need to diverge from V2
-        Subject: @subject_serializer,
-        PrimarySubject: @primary_subject_serializer,
-        SecondarySubject: @secondary_subject_serializer,
-        ModernLanguagesSubject: @modern_languages_subject_serializer,
-        FurtherEducationSubject: @further_education_subject_serializer,
-      },
+      SubjectArea: @subject_area_serializer
+      # v3: {
+      #   SubjectArea: @v3_subject_area_serializer,
+      #   # this is ok until the serializers for subjects need to diverge from V2
+      #   Subject: @subject_serializer,
+      #   PrimarySubject: @primary_subject_serializer,
+      #   SecondarySubject: @secondary_subject_serializer,
+      #   ModernLanguagesSubject: @modern_languages_subject_serializer,
+      #   FurtherEducationSubject: @further_education_subject_serializer,
+      # },
     }
   end
 end
--- app/services/course_serializers_service_v3.rb
+++ app/services/course_serializers_service_v3.rb
@@ -9,7 +9,6 @@ class CourseSerializersServiceV3
     site_status_serializer: API::V3::SerializableSiteStatus,
     site_serializer: API::V3::SerializableSite,
     provider_serializer: API::V3::SerializableProvider,
-    # provider_enrichment_serializer: API::V2::SerializableProviderEnrichment,
     recruitment_cycle_serializer: API::V3::SerializableRecruitmentCycle,
     subject_area_serializer: API::V3::SerializableSubjectArea
   )
@@ -22,7 +21,6 @@ class CourseSerializersServiceV3
     @site_serializer = site_serializer
     @site_status_serializer = site_status_serializer
     @provider_serializer = provider_serializer
-    # @provider_enrichment_serializer = provider_enrichment_serializer
     @recruitment_cycle_serializer = recruitment_cycle_serializer
     @subject_area_serializer = subject_area_serializer
   end
@@ -38,7 +36,6 @@ class CourseSerializersServiceV3
       SiteStatus: @site_status_serializer,
       Site: @site_serializer,
       Provider: @provider_serializer,
-      # ProviderEnrichment: @provider_enrichment_serializer,
       RecruitmentCycle: @recruitment_cycle_serializer,
       SubjectArea: @subject_area_serializer,
     }
