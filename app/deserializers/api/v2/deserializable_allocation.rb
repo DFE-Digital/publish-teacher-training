164bd71a7b76cdeb17f08306f02c4345ced68fc4
maxmills <8balldigitalsolutions@gmail.com>
2022-05-17 13:56:55 +0100

    rm v2 deserializers

--- app/deserializers/api/v2/deserializable_allocation.rb
+++
@@ -1,9 +0,0 @@
-module API
-  module V2
-    class DeserializableAllocation < JSONAPI::Deserializable::Resource
-      attributes :provider_id,
-                 :number_of_places,
-                 :request_type
-    end
-  end
-end
