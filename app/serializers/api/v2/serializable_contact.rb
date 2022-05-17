1c24d8442c230224b73b1f7189a5db343f7194d5
maxmills <8balldigitalsolutions@gmail.com>
2022-05-17 13:25:03 +0100

    rm v2 serializers

--- app/serializers/api/v2/serializable_contact.rb
+++
@@ -1,9 +0,0 @@
-module API
-  module V2
-    class SerializableContact < JSONAPI::Serializable::Resource
-      type "contacts"
-
-      attributes :name, :email, :telephone, :type
-    end
-  end
-end
--- app/serializers/api/v2/serializable_provider_suggestion.rb
+++
@@ -1,9 +0,0 @@
-module API
-  module V2
-    class SerializableProviderSuggestion < JSONAPI::Serializable::Resource
-      type "provider"
-
-      attributes :provider_name, :provider_code
-    end
-  end
-end
