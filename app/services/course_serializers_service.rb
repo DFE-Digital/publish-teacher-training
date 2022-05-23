246845a7b2d5ac6e33589d9b64b11d0e3218cb18
maxmills <8balldigitalsolutions@gmail.com>
2022-05-23 17:07:22 +0100

    update course serializer service

--- app/services/course_serializers_service.rb
+++ app/services/course_serializers_service.rb
@@ -1,17 +1,16 @@
 class CourseSerializersService
   def initialize(
-    course_serializer: API::V2::SerializableCourse,
-    subject_serializer: API::V2::SerializableSubject,
-    primary_subject_serializer: API::V2::SerializableSubject,
-    secondary_subject_serializer: API::V2::SerializableSubject,
-    modern_languages_subject_serializer: API::V2::SerializableSubject,
-    further_education_subject_serializer: API::V2::SerializableSubject,
-    site_status_serializer: API::V2::SerializableSiteStatus,
-    site_serializer: API::V2::SerializableSite,
-    provider_serializer: API::V2::SerializableProvider,
-    provider_enrichment_serializer: API::V2::SerializableProviderEnrichment,
-    recruitment_cycle_serializer: API::V2::SerializableRecruitmentCycle,
-    v3_subject_area_serializer: API::V3::SerializableSubjectArea
+    course_serializer: API::V3::SerializableCourse,
+    subject_serializer: API::V3::SerializableSubject,
+    primary_subject_serializer: API::V3::SerializableSubject,
+    secondary_subject_serializer: API::V3::SerializableSubject,
+    modern_languages_subject_serializer: API::V3::SerializableSubject,
+    further_education_subject_serializer: API::V3::SerializableSubject,
+    site_status_serializer: API::V3::SerializableSiteStatus,
+    site_serializer: API::V3::SerializableSite,
+    provider_serializer: API::V3::SerializableProvider,
+    recruitment_cycle_serializer: API::V3::SerializableRecruitmentCycle,
+    subject_area_serializer: API::V3::SerializableSubjectArea
   )
     @course_serializer = course_serializer
     @subject_serializer = subject_serializer
@@ -22,10 +21,8 @@ class CourseSerializersService
     @site_serializer = site_serializer
     @site_status_serializer = site_status_serializer
     @provider_serializer = provider_serializer
-    @provider_enrichment_serializer = provider_enrichment_serializer
     @recruitment_cycle_serializer = recruitment_cycle_serializer
-
-    @v3_subject_area_serializer = v3_subject_area_serializer
+    @subject_area_serializer = subject_area_serializer
   end
 
   def execute
@@ -39,17 +36,8 @@ class CourseSerializersService
       SiteStatus: @site_status_serializer,
       Site: @site_serializer,
       Provider: @provider_serializer,
-      ProviderEnrichment: @provider_enrichment_serializer,
       RecruitmentCycle: @recruitment_cycle_serializer,
-      v3: {
-        SubjectArea: @v3_subject_area_serializer,
-        # this is ok until the serializers for subjects need to diverge from V2
-        Subject: @subject_serializer,
-        PrimarySubject: @primary_subject_serializer,
-        SecondarySubject: @secondary_subject_serializer,
-        ModernLanguagesSubject: @modern_languages_subject_serializer,
-        FurtherEducationSubject: @further_education_subject_serializer,
-      },
+      SubjectArea: @subject_area_serializer,
     }
   end
 end
