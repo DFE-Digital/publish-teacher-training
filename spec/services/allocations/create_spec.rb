2fe541e01503cfd370d16aa10c5d7f57b93f0811
maxmills <8balldigitalsolutions@gmail.com>
2022-05-25 16:59:15 +0100

    fix test syntax

--- spec/services/allocations/create_spec.rb
+++ spec/services/allocations/create_spec.rb
@@ -76,7 +76,9 @@ RSpec.describe Allocations::Create do
     context 'testing rand' do
       let(:random_number) { rand(1..99) }
 
-      expect(random_number).to eq 0
+      it 'tests rand for 0' do
+        expect(random_number).to eq 0
+      end
     end
   end
 end
