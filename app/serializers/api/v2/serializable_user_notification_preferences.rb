66bd50df8cfdb1bc657285ffdd13b0319fd0f3ed
maxmills <8balldigitalsolutions@gmail.com>
2022-05-26 12:32:14 +0100

    delete v2 serializers

--- app/serializers/api/v2/serializable_user_notification_preferences.rb
+++
@@ -1,9 +0,0 @@
-module API
-  module V2
-    class SerializableUserNotificationPreferences < JSONAPI::Serializable::Resource
-      type "user_notification_preferences"
-
-      attributes :enabled, :updated_at
-    end
-  end
-end
