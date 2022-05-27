7e1dd0556b1799932b659691b13cec45449b26a5
avinhurry <avin.hurry@digital.education.gov.uk>
2022-05-27 14:26:15 +0100

    More little tweaks

--- spec/features/publish/primary_nav_spec.rb
+++ spec/features/publish/primary_nav_spec.rb
@@ -6,7 +6,7 @@ feature "Primary nav" do
   scenario "view page as Anne - user with single provider" do
     given_the_new_publish_flow_feature_flag_is_enabled
     and_i_am_authenticated_as_a_provider_user
-    when_i_visit_the_primary_nav
+    when_i_visit_the_providers_show_page
     then_i_should_see_the_organisation_details_link
     and_i_should_see_the_locations_link
     and_i_should_see_the_courses_link
@@ -18,7 +18,7 @@ feature "Primary nav" do
   scenario "view page as Susy - user with accredited body" do
     given_the_new_publish_flow_feature_flag_is_enabled
     given_i_am_authenticated_as_an_accredited_body_user
-    when_i_visit_the_primary_nav
+    when_i_visit_the_providers_show_page
     then_i_should_see_the_organisation_details_link
     and_i_should_see_the_locations_link
     and_i_should_see_the_courses_link
@@ -38,8 +38,8 @@ feature "Primary nav" do
     given_i_am_authenticated(user: create(:user, providers: [create(:provider, :accredited_body)]))
   end
 
-  def when_i_visit_the_primary_nav
-    primary_nav.load
+  def when_i_visit_the_providers_show_page
+    publish_providers_show_page.load
   end
 
   def then_i_should_see_the_organisation_details_link
--- spec/support/page_objects/publish/primary_nav.rb
+++ spec/support/page_objects/publish/primary_nav.rb
@@ -3,8 +3,6 @@
 module PageObjects
   module Publish
     class PrimaryNav < PageObjects::Base
-      set_url "/publish/organisations/{provider_code}"
-
       element :organisation_details, :link, "Organisation details"
       element :locations, :link, "Locations"
       element :courses, :link, "Courses"
