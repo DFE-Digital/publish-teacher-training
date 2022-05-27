81069f25325b33c2007dc1e4dff66d71e03004ae
avinhurry <avin.hurry@digital.education.gov.uk>
2022-05-27 14:05:20 +0100

    Change feature specs wording to then, and

--- spec/features/publish/primary_nav_spec.rb
+++ spec/features/publish/primary_nav_spec.rb
@@ -7,23 +7,23 @@ feature "Primary nav" do
     given_the_new_publish_flow_feature_flag_is_enabled
     and_i_am_authenticated_as_a_provider_user
     when_i_visit_the_primary_nav
-    i_should_see_the_organisation_details_link
-    i_should_see_the_locations_link
-    i_should_see_the_courses_link
-    i_should_see_the_users_link
-    i_should_not_see_the_training_partners_link
-    i_should_not_see_the_change_organisation_link
+    then_i_should_see_the_organisation_details_link
+    and_i_should_see_the_locations_link
+    and_i_should_see_the_courses_link
+    and_i_should_see_the_users_link
+    and_i_should_not_see_the_training_partners_link
+    and_i_should_not_see_the_training_partners_link
   end
 
   scenario "view page as Susy - user with accredited body" do
     given_the_new_publish_flow_feature_flag_is_enabled
     given_i_am_authenticated_as_an_accredited_body_user
     when_i_visit_the_primary_nav
-    i_should_see_the_organisation_details_link
-    i_should_see_the_locations_link
-    i_should_see_the_courses_link
-    i_should_see_the_users_link
-    i_should_see_the_training_partners_link
+    then_i_should_see_the_organisation_details_link
+    and_i_should_see_the_locations_link
+    and_i_should_see_the_courses_link
+    and_i_should_see_the_users_link
+    and_i_should_see_the_training_partners_link
   end
 
   def given_the_new_publish_flow_feature_flag_is_enabled
@@ -42,31 +42,31 @@ feature "Primary nav" do
     primary_nav.load
   end
 
-  def i_should_see_the_organisation_details_link
+  def then_i_should_see_the_organisation_details_link
     expect(primary_nav).to have_organisation_details
   end
 
-  def i_should_see_the_locations_link
+  def and_i_should_see_the_locations_link
     expect(primary_nav).to have_locations
   end
 
-  def i_should_see_the_courses_link
+  def and_i_should_see_the_courses_link
     expect(primary_nav).to have_courses
   end
 
-  def i_should_see_the_users_link
+  def and_i_should_see_the_users_link
     expect(primary_nav).to have_users
   end
 
-  def i_should_see_the_training_partners_link
+  def and_i_should_see_the_training_partners_link
     expect(primary_nav).to have_training_partners
   end
 
-  def i_should_not_see_the_training_partners_link
+  def and_i_should_not_see_the_training_partners_link
     expect(primary_nav).not_to have_training_partners
   end
 
-  def i_should_not_see_the_change_organisation_link
+  def and_i_should_not_see_the_training_partners_link
     expect(page).not_to have_text "Change organisation"
   end
 end
