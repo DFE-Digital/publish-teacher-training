73028950f173ee9be8f84b193d433ebe16685ad5
maxmills <8balldigitalsolutions@gmail.com>
2022-06-01 00:41:39 +0100

    test for copy list

--- spec/support/page_objects/sections/copy_content.rb
+++ spec/support/page_objects/sections/copy_content.rb
@@ -7,6 +7,10 @@ module PageObjects
     class CopyContent < PageObjects::Sections::Base
       set_default_search_arguments '[data-qa="course__copy-content-form"]'
 
+      def copy_options
+        find('select#copy-from').all('option').collect(&:text)
+      end
+
       def copy(course)
         select("#{course.name} (#{course.course_code})", from: "Copy from")
         click_on("Copy content")
