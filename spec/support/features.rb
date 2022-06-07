91bffa35dfad65a972e672f40c2b05f0b34ef11c
avinhurry <avin.hurry@digital.education.gov.uk>
2022-06-07 09:38:22 +0100

    Refactor into features.rb

--- spec/support/features.rb
+++ spec/support/features.rb
@@ -14,4 +14,22 @@ RSpec.configure do |config|
   config.include FeatureHelpers::PublishPages, type: :feature
   config.include FeatureHelpers::CourseSteps, type: :feature
   config.include DfESignInUserHelper, type: :feature
+
+  # This is required as a workaround for failing tests when the `can_edit_current_and_next_cycles` feature flag is active
+  config.around do |example|
+    can_edit_current_and_next_cycles = example.metadata[:can_edit_current_and_next_cycles]
+
+    if can_edit_current_and_next_cycles == false
+      old_value = Settings.features.rollover.can_edit_current_and_next_cycles
+      Settings.features.rollover.can_edit_current_and_next_cycles = false
+      Rails.application.reload_routes!
+
+      example.run
+
+      Settings.features.rollover.can_edit_current_and_next_cycles = old_value
+      Rails.application.reload_routes!
+    else
+      example.run
+    end
+  end
 end
