483dae46ff8046964c354ad30bc6111244cf36c6
avinhurry <avin.hurry@digital.education.gov.uk>
2022-06-14 14:58:28 +0100

    Add dynamic method creator to support_pages

--- spec/support/feature_helpers/support_pages.rb
+++ spec/support/feature_helpers/support_pages.rb
@@ -2,92 +2,107 @@
 
 module FeatureHelpers
   module SupportPages
-    def support_provider_index_page
-      @provider_index_page ||= PageObjects::Support::ProviderIndex.new
-    end
+    raw_file_names = Dir["spec/support/page_objects/support/**/*.rb"]
 
-    def support_provider_show_page
-      @provider_show_page ||= PageObjects::Support::ProviderShow.new
-    end
+    processed_file_names = raw_file_names.map { |raw_file_name| raw_file_name.chomp(".rb").gsub("spec/support/", "") }
 
-    def support_provider_new_page
-      @provider_new_page ||= PageObjects::Support::ProviderNew.new
-    end
+    processed_file_names.each do |processed_file_name|
+      file_name = processed_file_name.split("/").last
+      method_name = "support_#{file_name}_page"
+      define_method method_name do
+        return instance_variable_get("@#{file_name}") if instance_variable_get("@#{file_name}").present?
 
-    def support_provider_edit_page
-      @provider_edit_page ||= PageObjects::Support::ProviderEdit.new
-    end
+        page_object = processed_file_name.camelize.constantize
 
-    def support_provider_users_index_page
-      @provider_users_index_page ||= PageObjects::Support::ProviderUsersIndex.new
+        instance_variable_set("@#{file_name}", page_object.new)
+      end
     end
-
-    def support_provider_courses_index_page
-      @provider_courses_index_page ||= PageObjects::Support::Provider::CoursesIndex.new
-    end
-
-    def support_provider_locations_index_page
-      @provider_locations_index_page ||= PageObjects::Support::Provider::LocationsIndex.new
-    end
-
-    def support_provider_location_create_page
-      @provider_location_create_page ||= PageObjects::Support::Provider::LocationCreate.new
-    end
-
-    def support_provider_location_edit_page
-      @provider_location_edit_page ||= PageObjects::Support::Provider::LocationEdit.new
-    end
-
-    def support_course_edit_page
-      @course_edit_page ||= PageObjects::Support::Provider::CourseEdit.new
-    end
-
-    def support_users_show_page
-      @users_show_page ||= PageObjects::Support::UserShow.new
-    end
-
-    def support_users_show_providers_page
-      @users_show_providers_page ||= PageObjects::Support::UserShowProviders.new
-    end
-
-    def support_users_index_page
-      @users_index_page ||= PageObjects::Support::UsersIndex.new
-    end
-
-    def support_user_new_page
-      @user_new_page ||= PageObjects::Support::UserNew.new
-    end
-
-    def support_user_edit_page
-      @user_edit_page ||= PageObjects::Support::UserEdit.new
-    end
-
-    def support_allocations_index_page
-      @allocations_index_page ||= PageObjects::Support::AllocationsIndex.new
-    end
-
-    def support_allocations_show_page
-      @allocations_show_page ||= PageObjects::Support::AllocationsShow.new
-    end
-
-    def support_allocation_uplift_edit_page
-      @allocation_uplift_edit_page ||= PageObjects::Support::AllocationUpliftEdit.new
-    end
-
-    def support_allocation_uplift_new_page
-      @allocation_uplift_new_page ||= PageObjects::Support::AllocationUpliftNew.new
-    end
-
-    def support_access_requests_page
-      @access_requests_page ||= PageObjects::Support::AccessRequests::Index.new
-    end
-
-    def support_access_requests_confirm_page
-      @access_requests_confirm_page ||= PageObjects::Support::AccessRequests::Confirm.new
-    end
-
-    def support_sign_in_page
-      @sign_in_page ||= PageObjects::SignIn.new
-    end
+  #   def support_provider_index_page
+  #     @provider_index_page ||= PageObjects::Support::ProviderIndex.new
+  #   end
+  #
+  #   def support_provider_show_page
+  #     @provider_show_page ||= PageObjects::Support::ProviderShow.new
+  #   end
+  #
+  #   def support_provider_new_page
+  #     @provider_new_page ||= PageObjects::Support::ProviderNew.new
+  #   end
+  #
+  #   def support_provider_edit_page
+  #     @provider_edit_page ||= PageObjects::Support::ProviderEdit.new
+  #   end
+  #
+  #   def support_provider_users_index_page
+  #     @provider_users_index_page ||= PageObjects::Support::ProviderUsersIndex.new
+  #   end
+  #
+  #   def support_provider_courses_index_page
+  #     @provider_courses_index_page ||= PageObjects::Support::Provider::CoursesIndex.new
+  #   end
+  #
+  #   def support_provider_locations_index_page
+  #     @provider_locations_index_page ||= PageObjects::Support::Provider::LocationsIndex.new
+  #   end
+  #
+  #   def support_provider_location_create_page
+  #     @provider_location_create_page ||= PageObjects::Support::Provider::LocationCreate.new
+  #   end
+  #
+  #   def support_provider_location_edit_page
+  #     @provider_location_edit_page ||= PageObjects::Support::Provider::LocationEdit.new
+  #   end
+  #
+  #   def support_course_edit_page
+  #     @course_edit_page ||= PageObjects::Support::Provider::CourseEdit.new
+  #   end
+  #
+  #   def support_users_show_page
+  #     @users_show_page ||= PageObjects::Support::UserShow.new
+  #   end
+  #
+  #   def support_users_show_providers_page
+  #     @users_show_providers_page ||= PageObjects::Support::UserShowProviders.new
+  #   end
+  #
+  #   def support_users_index_page
+  #     @users_index_page ||= PageObjects::Support::UsersIndex.new
+  #   end
+  #
+  #   def support_user_new_page
+  #     @user_new_page ||= PageObjects::Support::UserNew.new
+  #   end
+  #
+  #   def support_user_edit_page
+  #     @user_edit_page ||= PageObjects::Support::UserEdit.new
+  #   end
+  #
+  #   def support_allocations_index_page
+  #     @allocations_index_page ||= PageObjects::Support::AllocationsIndex.new
+  #   end
+  #
+  #   def support_allocations_show_page
+  #     @allocations_show_page ||= PageObjects::Support::AllocationsShow.new
+  #   end
+  #
+  #   def support_allocation_uplift_edit_page
+  #     @allocation_uplift_edit_page ||= PageObjects::Support::AllocationUpliftEdit.new
+  #   end
+  #
+  #   def support_allocation_uplift_new_page
+  #     @allocation_uplift_new_page ||= PageObjects::Support::AllocationUpliftNew.new
+  #   end
+  #
+  #   def support_access_requests_page
+  #     @access_requests_page ||= PageObjects::Support::AccessRequests::Index.new
+  #   end
+  #
+  #   def support_access_requests_confirm_page
+  #     @access_requests_confirm_page ||= PageObjects::Support::AccessRequests::Confirm.new
+  #   end
+  #
+  #   def support_sign_in_page
+  #     @sign_in_page ||= PageObjects::SignIn.new
+  #   end
   end
 end
