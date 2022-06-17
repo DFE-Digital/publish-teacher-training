de51212e9116a727f3ee9d06f2eaec59ba9ce415
avinhurry <avin.hurry@digital.education.gov.uk>
2022-06-17 19:38:26 +0100

    Remove the nav bar from the cycle switcher page

--- app/helpers/navigation_bar_helper.rb
+++ app/helpers/navigation_bar_helper.rb
@@ -1,5 +1,9 @@
 module NavigationBarHelper
   def render_navigation_bar?(provider)
-    provider && !current_page?(root_path) && provider.recruitment_cycle && !request.path.include?("support")
+    provider &&
+      !current_page?(root_path) &&
+      provider.recruitment_cycle &&
+      !request.path.include?("support") &&
+      !request.path.match?("/publish/organisations/#{provider_code}")
   end
 end
