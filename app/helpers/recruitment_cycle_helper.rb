8d9c06cdffa32fb422cec775f83be0f1c24c21e2
avinhurry <avin.hurry@digital.education.gov.uk>
2022-06-17 19:28:02 +0100

    Rework the cycle switcher page

--- app/helpers/recruitment_cycle_helper.rb
+++ app/helpers/recruitment_cycle_helper.rb
@@ -2,11 +2,11 @@
 
 module RecruitmentCycleHelper
   def current_recruitment_cycle_period_text
-    "#{Settings.current_recruitment_cycle_year} to #{Settings.current_recruitment_cycle_year + 1}"
+    "#{Settings.current_recruitment_cycle_year - 1} to #{Settings.current_recruitment_cycle_year }"
   end
 
   def next_recruitment_cycle_period_text
-    "#{Settings.current_recruitment_cycle_year + 1} to #{Settings.current_recruitment_cycle_year + 2}"
+    "#{Settings.current_recruitment_cycle_year } to #{Settings.current_recruitment_cycle_year + 1}"
   end
 
   def previous_recruitment_cycle_period_text
