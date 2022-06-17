4bab2f49f05dfdd71c582ba16e79a62aa9705536
avinhurry <avin.hurry@digital.education.gov.uk>
2022-06-17 20:04:30 +0100

    Use current page instead of .match

--- app/helpers/navigation_bar_helper.rb
+++ app/helpers/navigation_bar_helper.rb
@@ -1,9 +1,7 @@
 module NavigationBarHelper
   def render_navigation_bar?(provider)
-    provider &&
-      !current_page?(root_path) &&
-      provider.recruitment_cycle &&
-      !request.path.include?("support") &&
-      !request.path.match?("/publish/organisations/#{provider_code}")
+    provider && !current_page?(root_path) && !current_page?(publish_provider_path(provider.provider_code)) &&
+    provider.recruitment_cycle &&
+    !request.path.include?("support")
   end
 end
