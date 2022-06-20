777fa351c9c7ebdac07f3cbacb2458f51d8a2a1d
avinhurry <avin.hurry@digital.education.gov.uk>
2022-06-20 11:23:48 +0100

    Add rollover text to title bar
    - Add logic to display current cycle if current cycle

--- app/components/title_bar/view.rb
+++ app/components/title_bar/view.rb
@@ -12,5 +12,21 @@ module TitleBar
     def link
       govuk_link_to t("change_organisation"), root_path, class: "title-bar-link inline govuk-link--no-visited-state"
     end
+
+    def current_recruitment_cycle?
+      params[:recruitment_cycle_year].to_i == Settings.current_recruitment_cycle_year
+    end
+
+    def rollover_active?
+      Settings.features.rollover.can_edit_current_and_next_cycles == true
+    end
+
+    def current_recruitment_cycle_year
+      Settings.current_recruitment_cycle_year - 1
+    end
+
+    def next_recruitment_cycle_year
+      Settings.current_recruitment_cycle_year
+    end
   end
 end
