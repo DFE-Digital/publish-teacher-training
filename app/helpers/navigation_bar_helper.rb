de5b038f4873538952b6fa9b24cdc41e55321310
avinhurry <avin.hurry@digital.education.gov.uk>
2022-06-20 15:30:01 +0100

    Re-order method
    To allow the add provider support page to work

--- app/helpers/navigation_bar_helper.rb
+++ app/helpers/navigation_bar_helper.rb
@@ -1,7 +1,7 @@
 module NavigationBarHelper
   def render_navigation_bar?(provider)
+    !request.path.include?("support") &&
     provider && !current_page?(root_path) && !current_page?(publish_provider_path(provider.provider_code)) &&
-    provider.recruitment_cycle &&
-    !request.path.include?("support")
+    provider.recruitment_cycle
   end
 end
