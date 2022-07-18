16cc95f375087381edd390e96624b0043688a00e
Max Mills <8balldigitalsolutions@gmail.com>
2022-07-18 15:15:40 +0100

    rubocop

--- app/forms/publish/course_rollover_form.rb
+++ app/forms/publish/course_rollover_form.rb
@@ -13,7 +13,7 @@ module Publish
   private
 
     def course_is_rollable
-      return if course.content_status == :draft || :empty || :rolled_over
+      return if (course.content_status == :draft) || (course.content_status == :empty) || (course.content_status == :rolled_over)
 
       errors.add(:course_is_draft, message: I18n.t("activemodel.errors.models.publish/course_rollover_form.course_is_rollable"))
     end
