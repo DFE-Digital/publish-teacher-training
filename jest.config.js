0e7962a5543c8a74d1444b1788071726a2369587
David Kewal <davidrk@hey.com>
2022-07-22 11:34:56 +0100

    [291] Fix package.json

--- jest.config.js
+++ jest.config.js
@@ -1,27 +1,29 @@
 module.exports = {
-  moduleDirectories: ['node_modules', 'app/javascript'],
-  moduleFileExtensions: ['js', 'ts'],
+  moduleDirectories: ["node_modules", "app/javascript"],
+  moduleFileExtensions: ["js", "ts"],
   automock: false,
   resetMocks: true,
-  transform: { '^.+\\.(js)?$': 'babel-jest' },
-  cacheDirectory: '<rootDir>/.jest-cache',
+  transform: { "^.+\\.(js)?$": "babel-jest" },
+  cacheDirectory: "<rootDir>/.jest-cache",
   collectCoverage: true,
-  coverageDirectory: 'coverage/frontend',
-  coverageReporters: ['text', 'lcov'],
+  coverageDirectory: "coverage/frontend",
+  coverageReporters: ["text", "lcov"],
   collectCoverageFrom: [
-    '<rootDir>/app/javascript/**/*.js',
-    '!<rootDir>/app/javascript/**/index.js',
-    '!<rootDir>/app/javascript/**/application.js',
-    '!<rootDir>/app/javascript/utils/test.js'
+    "<rootDir>/app/javascript/**/*.js",
+    "!<rootDir>/app/javascript/**/index.js",
+    "!<rootDir>/app/javascript/**/application.js",
+    "!<rootDir>/app/javascript/utils/test.js",
   ],
-  reporters: ['default'],
-  transformIgnorePatterns: ['node_modules/*'],
+  reporters: ["default"],
+  transformIgnorePatterns: ["node_modules/*"],
   moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/app/javascript/$1'
+    "^@/(.*)$": "<rootDir>/app/javascript/$1",
   },
-  roots: ['app/javascript'],
-  testEnvironment: 'jsdom',
-  testMatch: ['**/app/javascript/**/*.spec.(js|ts)'],
-  testURL: 'http://localhost/',
-  testPathIgnorePatterns: []
-}
+  roots: ["app/javascript"],
+  testEnvironment: "jsdom",
+  testMatch: ["**/app/javascript/**/*.spec.(js|ts)"],
+  testPathIgnorePatterns: [],
+  testEnvironmentOptions: {
+    "url": "http://localhost",
+  }
+};
