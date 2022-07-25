5d5f5a6c891b248243dee32aa963a9e3f7bc827c
defong <de_fong2k@yahoo.com>
2022-07-25 12:35:45 +0100

    [0333] Added standardjs

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,31 +1,31 @@
 // Entry point for the build script in your package.json
 
-import "babel-polyfill";
-import jQuery from "jquery";
-import { initAll } from "govuk-frontend";
+import 'babel-polyfill'
+import jQuery from 'jquery'
+import { initAll } from 'govuk-frontend'
 
-import { initAutocomplete } from "./autocomplete";
-import initLocationsMap from "./locations-map";
-import FilterToggle from "./filters";
-import CookieBanner from "./cookie_banner";
+import { initAutocomplete } from './autocomplete'
+import initLocationsMap from './locations-map'
+import FilterToggle from './filters'
+import CookieBanner from './cookie_banner'
 
-window.jQuery = jQuery;
-window.$ = jQuery;
-window.initLocationsMap = initLocationsMap;
+window.jQuery = jQuery
+window.$ = jQuery
+window.initLocationsMap = initLocationsMap
 
-initAll();
-FilterToggle.init();
-CookieBanner.init();
+initAll()
+FilterToggle.init()
+CookieBanner.init()
 
 try {
-  const $autocomplete = document.getElementById("provider-autocomplete");
-  const $provider_input = document.getElementById("provider");
-  const provider_template = (result) =>
-    result && `${result.name} (${result.code})`;
+  const autocomplete = document.getElementById('provider-autocomplete')
+  const providerInput = document.getElementById('provider')
+  const providerTemplate = (result) =>
+    result && `${result.name} (${result.code})`
 
-  if ($autocomplete && $provider_input) {
-    initAutocomplete($autocomplete, $provider_input, provider_template);
+  if (autocomplete && providerInput) {
+    initAutocomplete(autocomplete, providerInput, providerTemplate)
   }
 } catch (err) {
-  console.error("Failed to initialise provider autocomplete:", err);
+  console.error('Failed to initialise provider autocomplete:', err)
 }
