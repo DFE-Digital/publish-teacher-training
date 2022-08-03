a83da3d77ab00fbd3cb20df77d19fd5a1d021b8e
avinhurry <avin.hurry@digital.education.gov.uk>
2022-08-03 14:09:24 +0100

    Remove all logic associated with the `google_analytics` feature flag

--- app/helpers/cookies_helper.rb
+++ app/helpers/cookies_helper.rb
@@ -2,7 +2,7 @@
 
 module CookiesHelper
   def hide_cookie_banner?
-    request.cookie_jar[Settings.cookies.consent.name] =~ /accepted|rejected/ || !FeatureService.enabled?(:google_analytics)
+    request.cookie_jar[Settings.cookies.consent.name] =~ /accepted|rejected/
   end
 
   def google_analytics_allowed?
