4914f94290b8a9bc68665fc0c7b7c5ce1b301898
avinhurry <avin.hurry@digital.education.gov.uk>
2022-08-11 16:11:28 +0100

    Use module, not ::

--- app/components/find_interface/header/view.rb
+++ app/components/find_interface/header/view.rb
@@ -1,16 +1,18 @@
 # frozen_string_literal: true
 
-class FindInterface::Header::View < ApplicationComponent
-  attr_reader :service_name
+module FindInterface
+  class Header::View < ApplicationComponent
+    attr_reader :service_name
 
-  include ActiveModel
+    include ActiveModel
 
-  def initialize(service_name:, classes: [], html_attributes: {})
-    super(classes:, html_attributes:)
-    @service_name = service_name
-  end
+    def initialize(service_name:, classes: [], html_attributes: {})
+      super(classes:, html_attributes:)
+      @service_name = service_name
+    end
 
-  def environment_header_class
-    "app-header--#{Settings.environment.name}"
+    def environment_header_class
+      "app-header--#{Settings.environment.name}"
+    end
   end
 end
