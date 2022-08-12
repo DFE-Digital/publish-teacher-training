526ee4f94f0f4b01209edd720ad0c90f4f339994
defong <de_fong2k@yahoo.com>
2022-08-12 09:55:59 +0100

    [0400] Amended to having no active state (blue link)

--- spec/support/page_objects/publish/header.rb
+++ spec/support/page_objects/publish/header.rb
@@ -4,7 +4,7 @@ module PageObjects
   module Publish
     class Header < PageObjects::Base
       element :notifications_preference_link, "a.govuk-header__link", text: "Notifications"
-      element :active_notifications_preference_link, "li.govuk-header__navigation-item--active a.govuk-header__link", text: "Notifications"
+      element :active_notifications_preference_link, "li.govuk-header__navigation-item a.govuk-header__link", text: "Notifications"
     end
   end
 end
