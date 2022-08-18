594fdf6c28168464efc3e697a18d45276319c293
avinhurry <avin.hurry@digital.education.gov.uk>
2022-08-18 15:04:52 +0100

    Add summary component component

---
+++ app/components/find_interface/courses/summary_component/view.rb
@@ -0,0 +1,23 @@
+module FindInterface
+  module Courses
+    class SummaryComponent::View < ViewComponent::Base
+      include ApplicationHelper
+      include ViewHelper
+
+      attr_reader :course
+      delegate :accrediting_provider,
+        :provider,
+        :funding_option,
+        :age_range_in_years,
+        :length,
+        :applications_open_from,
+        :outcome,
+        :start_date, to: :course
+
+      def initialize(course)
+        super
+        @course = course
+      end
+    end
+  end
+end
