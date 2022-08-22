7d64f25171bee821198426911e719a5025661718
defong <de_fong2k@yahoo.com>
2022-08-22 15:56:40 +0100

    [0421] Fixed ordering of routes

--- config/routes/find.rb
+++ config/routes/find.rb
@@ -3,13 +3,13 @@ constraints(FindConstraint.new) do
 end
 
 namespace :find, path: "/find" do
-  scope module: :search do
-    resources :age_groups, path: "/age-groups"
-    resources :locations, path: "/"
-  end
-
   get "/accessibility", to: "pages#accessibility", as: :accessibility
   get "/privacy-policy", to: "pages#privacy", as: :privacy
   get "/terms-conditions", to: "pages#terms", as: :terms
   resource :cookie_preferences, only: %i[show update], path: "/cookies", as: :cookies
+
+  scope module: :search do
+    resources :age_groups, path: "/age-groups"
+    resources :locations, path: "/"
+  end
 end
