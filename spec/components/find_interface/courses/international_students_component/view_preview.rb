13d78c45352eb92b68bda1494d88a1bdbe78fdf2
avinhurry <avin.hurry@digital.education.gov.uk>
2022-08-30 09:39:47 +0100

    Add view spec and preview for InternationalStudentsComponent

---
+++ spec/components/find_interface/courses/international_students_component/view_preview.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+module FindInterface::Courses::InternationalStudentsComponent
+  class ViewPreview < ViewComponent::Preview
+    def salaried_course
+      course = Course.new(course_code: "FIND", program_type: "school_direct_salaried_training_programme",
+        provider: Provider.new(provider_code: "DFE")).decorate
+      render FindInterface::Courses::InternationalStudentsComponent::View.new(course:)
+    end
+
+    def fee_paying_course
+      course = Course.new(course_code: "FIND", program_type: "school_direct_training_programme",
+        provider: Provider.new(provider_code: "DFE")).decorate
+      render FindInterface::Courses::InternationalStudentsComponent::View.new(course:)
+    end
+
+    def non_salaried_course_and_provider_can_sponsor_student_visa
+      course = Course.new(course_code: "FIND", program_type: "school_direct_training_programme",
+        provider: Provider.new(provider_code: "DFE", can_sponsor_student_visa: true)).decorate
+      render FindInterface::Courses::InternationalStudentsComponent::View.new(course:)
+    end
+
+    def salaried_course_and_provider_can_sponsor_skilled_worker_visa
+      course = Course.new(course_code: "FIND", program_type: "school_direct_salaried_training_programme",
+        provider: Provider.new(provider_code: "DFE", can_sponsor_skilled_worker_visa: true)).decorate
+      render FindInterface::Courses::InternationalStudentsComponent::View.new(course:)
+    end
+  end
+end
