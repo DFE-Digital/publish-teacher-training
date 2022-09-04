5a7085c26e943b16f7acaa09d7348ea16ddb4844
Samuel Culley <samuel.culley@digital.education.gov.uk>
2022-09-04 21:02:52 +0100

    Add student visa sponsorship edit form

--- app/controllers/publish/providers/visas_controller.rb
+++ app/controllers/publish/providers/visas_controller.rb
@@ -7,6 +7,12 @@ module Publish
         @provider_visa_form = ProviderVisaForm.new(provider)
       end
 
+      def student_edit
+        authorize(provider, :edit?)
+
+        @provider_student_visa_form = ProviderStudentVisaForm.new(provider)
+      end
+
       def update
         authorize(provider, :update?)
 
@@ -24,12 +30,37 @@ module Publish
         end
       end
 
+      def student_update
+        authorize(provider, :update?)
+
+        @provider_student_visa_form = ProviderStudentVisaForm.new(provider, params: provider_student_visa_params)
+
+        if @provider_student_visa_form.save!
+          flash[:success] = I18n.t("success.visa_changes")
+
+          redirect_to details_publish_provider_recruitment_cycle_path(
+                        provider.provider_code,
+                        recruitment_cycle.year,
+                        )
+        else
+          render :student_edit
+        end
+      end
+
     private
 
       def provider_visa_params
         return { can_sponsor_student_visa: nil } if params[:publish_provider_visa_form].blank?
 
         params.require(:publish_provider_visa_form).permit(*ProviderVisaForm::FIELDS).transform_values do |value|
+          ActiveModel::Type::Boolean.new.cast(value)
+        end
+      end
+
+      def provider_student_visa_params
+        return { can_sponsor_student_visa: nil } if params[:publish_provider_student_visa_form].blank?
+
+        params.require(:publish_provider_student_visa_form).permit(*ProviderStudentVisaForm::FIELDS).transform_values do |value|
           ActiveModel::Type::Boolean.new.cast(value)
         end
       end
--- config/routes/publish.rb
+++ config/routes/publish.rb
@@ -189,7 +189,11 @@ namespace :publish, as: :publish do
         get "/contact", on: :member, to: "contacts#edit"
         put "/contact", on: :member, to: "contacts#update"
         get "/visas", on: :member, to: "visas#edit"
+        get "/student-visa", on: :member, to: "visas#student_edit"
+        get "/skilled-worker-visa", on: :member, to: "visas#skilled_worker_edit"
         put "/visas", on: :member, to: "visas#update"
+        put "/student-visa", on: :member, to: "visas#student_update"
+        put "/skilled-worker-visa", on: :member, to: "visas#skilled_worker_update"
 
         resources :allocations, only: %i[index], on: :member, param: :training_provider_code do
           collection do
