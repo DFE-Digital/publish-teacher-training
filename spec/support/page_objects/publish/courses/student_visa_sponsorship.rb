fb4b0fcf249196b711e9b810709e4461a95a6a5f
avinhurry <avin.hurry@digital.education.gov.uk>
2022-09-06 18:21:23 +0100

    Add feature tests for course confirmation page
    - Test that the correct new forms get surfaced when funding type changes in the course creation flow.

---
+++ spec/support/page_objects/publish/courses/student_visa_sponsorship.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+module PageObjects
+  module Publish
+    module Courses
+      class StudentVisaSponsorship < PageObjects::Base
+        set_url "/publish/organisations/{provider_code}/{recruitment_cycle_year}/courses/student-visa-sponsorship/new{?query*}"
+
+        element :continue, '[data-qa="course__save"]'
+
+        element :yes, "#course_can_sponsor_student_visa_true"
+      end
+    end
+  end
+end
