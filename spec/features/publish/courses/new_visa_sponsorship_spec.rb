469416987432e85f4959086cb8684185ec06297c
defong <de_fong2k@yahoo.com>
2022-09-12 12:19:36 +0100

    [433] Added tests

--- spec/features/publish/courses/new_visa_sponsorship_spec.rb
+++ spec/features/publish/courses/new_visa_sponsorship_spec.rb
@@ -1,32 +1,67 @@
 require "rails_helper"
 
-feature "visa sponsorship for lead school (add course summary page)", { can_edit_current_and_next_cycles: false } do
+feature "visa sponsorship (add course summary page)", { can_edit_current_and_next_cycles: false } do
   before do
     given_the_visa_sponsorship_on_course_feature_flag_is_active
-    given_i_am_authenticated_as_a_provider_user
-    when_i_visit_the_course_confirmation_page
   end
 
-  scenario "changing funding_type to fee shows the student question" do
-    when_i_change_fee_or_salary
-    and_i_choose_fee
-    and_i_click_continue
-    then_i_should_see_the_student_visas_title
+  context "for lead school" do
+    before do
+      given_i_am_authenticated_as_a_provider_user
+      when_i_visit_the_course_confirmation_page
+    end
 
-    when_i_choose_yes_for_student_visa
-    and_i_click_continue
-    then_i_should_be_back_on_the_course_confirmation_page
+    scenario "changing funding_type to fee shows the student question" do
+      when_i_change_fee_or_salary
+      and_i_choose_fee
+      and_i_click_continue
+      then_i_should_see_the_student_visas_title
+
+      when_i_choose_yes_for_student_visa
+      and_i_click_continue
+      then_i_should_be_back_on_the_course_confirmation_page
+    end
+
+    scenario "changing funding_type to salaried shows the skilled worker question" do
+      when_i_change_fee_or_salary
+      and_i_choose_salary
+      and_i_click_continue
+      then_i_should_see_the_skilled_worker_visas_title
+
+      when_i_choose_yes_for_skilled_worker_visa
+      and_i_click_continue
+      then_i_should_be_back_on_the_course_confirmation_page
+    end
   end
 
-  scenario "changing funding_type to salaried shows the skilled worker question" do
-    when_i_change_fee_or_salary
-    and_i_choose_salary
-    and_i_click_continue
-    then_i_should_see_the_skilled_worker_visas_title
+  context "for scitt or uni provider" do
+    before do
+      given_the_visa_sponsorship_on_course_feature_flag_is_active
+      given_i_am_authenticated_as_a_provider_user(scitt_or_uni_provider)
+      when_i_visit_the_course_confirmation_page
+    end
 
-    when_i_choose_yes_for_skilled_worker_visa
-    and_i_click_continue
-    then_i_should_be_back_on_the_course_confirmation_page
+    scenario "changing funding_type to fee shows the student question" do
+      when_i_change_apprenticeship
+      and_i_choose_no_for_apprenticeship
+      and_i_click_continue
+      then_i_should_see_the_student_visas_title
+
+      when_i_choose_yes_for_student_visa
+      and_i_click_continue
+      then_i_should_be_back_on_the_course_confirmation_page
+    end
+
+    scenario "changing funding_type to salaried shows the skilled worker question" do
+      when_i_change_apprenticeship
+      and_i_choose_yes_for_apprenticeship
+      and_i_click_continue
+      then_i_should_see_the_skilled_worker_visas_title
+
+      when_i_choose_yes_for_skilled_worker_visa
+      and_i_click_continue
+      then_i_should_be_back_on_the_course_confirmation_page
+    end
   end
 
 private
@@ -35,8 +70,16 @@ private
     allow(Settings.features).to receive(:visa_sponsorship_on_course).and_return(true)
   end
 
-  def given_i_am_authenticated_as_a_provider_user
-    @user = create(:user, providers: [build(:provider, sites: [build(:site)])])
+  def lead_school_provider
+    build(:provider, sites: [build(:site)])
+  end
+
+  def scitt_or_uni_provider
+    build(:provider, :accredited_body, sites: [build(:site)])
+  end
+
+  def given_i_am_authenticated_as_a_provider_user(provider = lead_school_provider)
+    @user = create(:user, providers: [provider])
     @user.providers.first.courses << create(:course, :with_accrediting_provider)
     given_i_am_authenticated(user: @user)
   end
@@ -51,6 +94,18 @@ private
       recruitment_cycle_year: Settings.current_recruitment_cycle_year,
       query: confirmation_params(provider),
     )
+  end
+
+  def when_i_change_apprenticeship
+    course_confirmation_page.details.apprenticeship.change_link.click
+  end
+
+  def and_i_choose_yes_for_apprenticeship
+    new_apprenticeship_page.yes.click
+  end
+
+  def and_i_choose_no_for_apprenticeship
+    new_apprenticeship_page.no.click
   end
 
   def when_i_change_fee_or_salary
