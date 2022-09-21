906f807b897dd245ffe549def99f1bc1fa677b71
maxmills <8balldigitalsolutions@gmail.com>
2022-09-21 11:42:03 +0100

    subjects controller

--- app/controllers/find/search/subjects_controller.rb
+++ app/controllers/find/search/subjects_controller.rb
@@ -4,6 +4,27 @@ module Find
       def index
         @subjects_form = SubjectsForm.new(subject_codes: params[:subject_codes], age_group: params[:age_group])
       end
+
+      def create
+        @subjects_form = SubjectsForm.new(subject_codes: sanitised_subject_codes, age_group: params[:age_group])
+binding.pry
+        if @subjects_form.valid?
+          redirect_to course_results_path # TODO: create this
+        else
+          render :index
+        end
+      end
+
+    private
+
+      def sanitised_subject_codes
+        find_subjects_form_params["subject_codes"].compact_blank!
+      end
+
+      def find_subjects_form_params
+        params.require(:find_subjects_form)
+          .permit(:age_group, subject_codes: [])
+      end
     end
   end
 end
