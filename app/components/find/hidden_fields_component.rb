2d90d4dca4fadd81f144b91df515018f801f0d38
maxmills <8balldigitalsolutions@gmail.com>
2022-09-21 13:43:52 +0100

    disable super

--- app/components/find/hidden_fields_component.rb
+++ app/components/find/hidden_fields_component.rb
@@ -1,13 +1,14 @@
 module Find
   class HiddenFieldsComponent < ViewComponent::Base
     attr_reader :query_params, :form_name, :form, :exclude_keys
-
+    # rubocop:disable Lint/MissingSuper
     def initialize(query_params:, form_name:, form:, exclude_keys: [])
       @query_params = query_params
       @form_name = form_name
       @form = form
       @exclude_keys = exclude_keys
     end
+    # rubocop:enable Lint/MissingSuper
 
     def params
       query_params[form_name] || query_params
