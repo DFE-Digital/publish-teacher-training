af77478a4bd76a34b7dbe02d76ea0bdd662cc62d
David Kewal <davidrk@hey.com>
2022-09-22 10:44:31 +0100

    [521] PR feedback

--- app/helpers/publish/course_helper.rb
+++ app/helpers/publish/course_helper.rb
@@ -1,7 +1,7 @@
 module Publish
   module CourseHelper
     def course_value_provided?(value)
-      value.presence || t("course.value_not_provided")
+      value.presence || tag.span(t("course.value_not_entered"), class: "govuk-hint").html_safe
     end
   end
 end
--- spec/helpers/publish/course_helper_spec.rb
+++ spec/helpers/publish/course_helper_spec.rb
@@ -6,11 +6,11 @@ module Publish
   describe CourseHelper, type: :helper do
     describe "#course_value_provided?" do
       it "returns a default text when value is nil" do
-        expect(course_value_provided?(nil)).to eq(t("course.value_not_provided"))
+        expect(course_value_provided?(nil)).to eq("<span class=\"govuk-hint\">Not entered</span>")
       end
 
       it "returns a default text when value is empty" do
-        expect(course_value_provided?("")).to eq(t("course.value_not_provided"))
+        expect(course_value_provided?("")).to eq("<span class=\"govuk-hint\">Not entered</span>")
       end
     end
   end
