a2665e41626f266c266fcecb312eabf3e82bd7ea
defong <de_fong2k@yahoo.com>
2022-09-23 12:02:23 +0100

    [0503] Amended so the redirect_to is tidier

--- app/controllers/publish/courses/funding_type_controller.rb
+++ app/controllers/publish/courses/funding_type_controller.rb
@@ -15,7 +15,7 @@ module Publish
             redirect_to new_publish_provider_recruitment_cycle_courses_skilled_worker_visa_sponsorship_path(path_params)
           end
         else
-          redirect_to next_step
+          redirect_to(next_step)
         end
       end
 
@@ -34,9 +34,10 @@ module Publish
         if @course_funding_form.valid?
           if @funding_type_updated
             @course_funding_form.stash
-            redirect_to_visa_step
+            redirect_to(visa_step_path)
           else
-            redirect_to_course_page
+
+            redirect_to(course_page_path)
           end
         else
           @errors = @course_funding_form.errors.messages
@@ -58,33 +59,31 @@ module Publish
         @funding_type_updated = params[:course][:funding_type] != @course.funding_type
       end
 
-      def redirect_to_visa_step
+      def visa_step_path_values
+        {
+          provider_code: course.provider_code,
+          recruitment_cycle_year: course.recruitment_cycle_year,
+          course_code: course.course_code,
+          funding_type_updated: @funding_type_updated,
+          origin_step: current_step,
+        }
+      end
+
+      def visa_step_path
         if course.is_fee_based?
-          redirect_to student_visa_sponsorship_publish_provider_recruitment_cycle_course_path(
-            course.provider_code,
-            course.recruitment_cycle_year,
-            course.course_code,
-            funding_type_updated: @funding_type_updated,
-            origin_step: current_step,
-          )
+          student_visa_sponsorship_publish_provider_recruitment_cycle_course_path(visa_step_path_values)
         else
-          redirect_to skilled_worker_visa_sponsorship_publish_provider_recruitment_cycle_course_path(
-            course.provider_code,
-            course.recruitment_cycle_year,
-            course.course_code,
-            funding_type_updated: @funding_type_updated,
-            origin_step: current_step,
+          skilled_worker_visa_sponsorship_publish_provider_recruitment_cycle_course_path(
+            visa_step_path_values,
           )
         end
       end
 
-      def redirect_to_course_page
-        redirect_to(
-          details_publish_provider_recruitment_cycle_course_path(
-            course.provider_code,
-            course.recruitment_cycle_year,
-            course.course_code,
-          ),
+      def course_page_path
+        details_publish_provider_recruitment_cycle_course_path(
+          course.provider_code,
+          course.recruitment_cycle_year,
+          course.course_code,
         )
       end
     end
