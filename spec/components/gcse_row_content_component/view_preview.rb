259417d53ef5d56effcdb147072fda5d53247c28
defong <de_fong2k@yahoo.com>
2022-10-03 14:21:28 +0100

    [0532] Fixed all recruitment cycle years to be independent

--- spec/components/gcse_row_content_component/view_preview.rb
+++ spec/components/gcse_row_content_component/view_preview.rb
@@ -3,7 +3,7 @@
 module GcseRowContentComponent
   class ViewPreview < ViewComponent::Preview
     def incomplete
-      course = Course.new(course_code: "C0D3", provider: Provider.new(provider_code: "E2E", recruitment_cycle: RecruitmentCycle.new(year: "2022")))
+      course = Course.new(course_code: "C0D3", provider: Provider.new(provider_code: "E2E", recruitment_cycle: RecruitmentCycle.new(year: Settings.current_recruitment_cycle_year)))
 
       render(GcseRowContentComponent::View.new(course: course.decorate))
     end
