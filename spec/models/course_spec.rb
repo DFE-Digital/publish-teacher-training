9a86f1b64fc453438c174390c0a2255fa477fb6d
defong <de_fong2k@yahoo.com>
2022-10-05 17:14:32 +0100

    [0570] Added Engineers Teach Physics to the basic details tab

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -163,6 +163,66 @@ describe Course, type: :model do
     end
   end
 
+  describe "#applicable_for_engineers_teach_physics?" do
+    subject { course. applicable_for_engineers_teach_physics? }
+
+    context "primary course" do
+      let(:course) { build(:course, :primary) }
+
+      it "return false" do
+        expect(subject).to be(false)
+      end
+    end
+
+    context "further education course" do
+      let(:course) { build(:course, :further_education) }
+
+      it "return false" do
+        expect(subject).to be(false)
+      end
+    end
+
+    context "secondary course" do
+      context "without physics" do
+        let(:course) { build(:course, :secondary) }
+
+        it "return false" do
+          expect(subject).to be(false)
+        end
+      end
+
+      context "with physics" do
+        let(:physics) { find_or_create(:secondary_subject, :physics) }
+
+        let(:course) { create(:course, :secondary, subjects: [physics]) }
+
+        it "return true" do
+          expect(subject).to be(true)
+        end
+
+        context "with another subject" do
+          let(:maths) { find_or_create(:secondary_subject, :mathematics) }
+
+          context "on position 0" do
+            let(:course) { create(:course, :secondary, subjects: [physics, maths]) }
+
+            it "return true" do
+              expect(subject).to be(true)
+            end
+          end
+
+          context "on position 1" do
+            let(:course) { create(:course, :secondary, subjects: [maths, physics]) }
+
+            it "return true" do
+              expect(subject).to be(false)
+            end
+          end
+        end
+      end
+    end
+  end
+
   context "ordering" do
     describe "canonical" do
       let(:provider_a) { create(:provider, provider_name: "Provider A") }
