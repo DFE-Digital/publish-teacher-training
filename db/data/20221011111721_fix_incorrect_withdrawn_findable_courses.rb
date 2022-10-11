465a0db1f298ad1e94c5520f55c59f10ecd089f2
David Kewal <davidrk@hey.com>
2022-10-11 14:54:30 +0100

    [694] PR feedback

--- db/data/20221011111721_fix_incorrect_withdrawn_findable_courses.rb
+++ db/data/20221011111721_fix_incorrect_withdrawn_findable_courses.rb
@@ -2,7 +2,9 @@
 
 class FixIncorrectWithdrawnFindableCourses < ActiveRecord::Migration[7.0]
   def up
-    Course.joins(:site_statuses, :enrichments).where(enrichments: { status: %w[withdrawn] }, site_statuses: { status: "running" }).find_each do |course|
+    Course.joins(:site_statuses, :enrichments, provider: :recruitment_cycle).where(provider: { recruitment_cycle: { year: "2023" } }, enrichments: { status: %w[withdrawn] }, site_statuses: { status: "running" }).find_each do |course|
+      next if course.is_published?
+
       course.site_statuses.update_all(publish: :unpublished, status: :suspended)
     end
   end
--- db/data/20221011111730_fix_incorrect_draft_findable_courses.rb
+++ db/data/20221011111730_fix_incorrect_draft_findable_courses.rb
@@ -2,7 +2,9 @@
 
 class FixIncorrectDraftFindableCourses < ActiveRecord::Migration[7.0]
   def up
-    Course.joins(:site_statuses, :enrichments).where(enrichments: { status: %w[draft] }, site_statuses: { status: "running" }).find_each do |course|
+    Course.joins(:site_statuses, :enrichments, provider: :recruitment_cycle).where(provider: { recruitment_cycle: { year: "2023" } }, enrichments: { status: %w[draft rolled_over] }, site_statuses: { status: "running" }).find_each do |course|
+      next if course.is_published?
+
       course.site_statuses.update_all(publish: :unpublished, status: :new_status)
     end
   end
