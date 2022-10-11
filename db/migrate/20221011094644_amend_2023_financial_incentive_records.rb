f7dcc1454a3fee5ec527a50a04f9c0fa4c9c0c0e
defong <de_fong2k@yahoo.com>
2022-10-11 11:06:40 +0100

    [0697] Amended 2023 financial incentives

---
+++ db/migrate/20221011094644_amend_2023_financial_incentive_records.rb
@@ -0,0 +1,23 @@
+class Amend2023FinancialIncentiveRecords < ActiveRecord::Migration[7.0]
+  def up
+    say_with_time "populating 2022 finanical incentive" do
+      FinancialIncentive.destroy_all
+
+      year = 2023
+
+      Subjects::FinancialIncentiveCreatorService.new(year:).execute
+      Subjects::FinancialIncentiveSetSubjectKnowledgeEnhancementCourseAvailableService.new(year:).execute
+    end
+  end
+
+  def down
+    say_with_time "populating 2022 finanical incentive" do
+      FinancialIncentive.destroy_all
+
+      [2021, 2022].each do |year|
+        Subjects::FinancialIncentiveCreatorService.new(year:).execute
+        Subjects::FinancialIncentiveSetSubjectKnowledgeEnhancementCourseAvailableService.new(year:).execute
+      end
+    end
+  end
+end
