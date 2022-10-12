8ff315f82165a599fe6f8f413c6a9aa9f562842a
David Kewal <davidrk@hey.com>
2022-10-12 09:35:13 +0100

    Update db/data/20221011111721_fix_incorrect_withdrawn_findable_courses.rb
    Co-authored-by: De Fong <de_fong2k@yahoo.com>

--- db/data/20221011111721_fix_incorrect_withdrawn_findable_courses.rb
+++ db/data/20221011111721_fix_incorrect_withdrawn_findable_courses.rb
@@ -5,7 +5,7 @@ class FixIncorrectWithdrawnFindableCourses < ActiveRecord::Migration[7.0]
     Course.joins(:site_statuses, :enrichments, provider: :recruitment_cycle).where(provider: { recruitment_cycle: { year: "2023" } }, enrichments: { status: %w[withdrawn] }, site_statuses: { status: "running" }).find_each do |course|
       next if course.is_published?
 
-      course.site_statuses.update_all(publish: :unpublished, status: :suspended)
+      course.site_statuses.update_all(vac_status: :no_vacancies, publish: :unpublished, status: :suspended)
     end
   end
 
