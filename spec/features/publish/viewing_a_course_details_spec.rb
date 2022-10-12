f8f99be9237f902517b9918b908a989a06e01b00
maxmills <8balldigitalsolutions@gmail.com>
2022-10-12 16:06:06 +0100

    rm params

--- spec/features/publish/viewing_a_course_details_spec.rb
+++ spec/features/publish/viewing_a_course_details_spec.rb
@@ -12,16 +12,16 @@ feature "Course show" do
   end
 
   context "Engineers Teach Physics course" do
- #   context "the engineers teach physics on course feature flag is active" do
- #     scenario "i can view the course basic details" do
- #       given_the_engineers_teach_physics_on_course_feature_flag_is_active
- #       given_i_am_authenticated_as_a_provider_user
- #       and_there_is_a_published_physics_course
- #       when_i_visit_the_course_details_page
- #       then_i_see_the_secondary_course_basic_details
- #       and_i_see_engineers_teach_physics_row
- #     end
- #   end
+    #   context "the engineers teach physics on course feature flag is active" do
+    #     scenario "i can view the course basic details" do
+    #       given_the_engineers_teach_physics_on_course_feature_flag_is_active
+    #       given_i_am_authenticated_as_a_provider_user
+    #       and_there_is_a_published_physics_course
+    #       when_i_visit_the_course_details_page
+    #       then_i_see_the_secondary_course_basic_details
+    #       and_i_see_engineers_teach_physics_row
+    #     end
+    #   end
 
     scenario "i can view the course basic details" do
       given_i_am_authenticated_as_a_provider_user
