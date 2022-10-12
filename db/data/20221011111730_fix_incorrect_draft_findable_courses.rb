c6ddaf56d4f3445b859cbe6fd333888314afa3d8
David Kewal <davidrk@hey.com>
2022-10-12 09:51:47 +0100

    [694] Determine correct vac status

--- db/data/20221011111730_fix_incorrect_draft_findable_courses.rb
+++ db/data/20221011111730_fix_incorrect_draft_findable_courses.rb
@@ -1,11 +1,17 @@
 # frozen_string_literal: true
 
 class FixIncorrectDraftFindableCourses < ActiveRecord::Migration[7.0]
+  VAC_MAPPING = {
+    full_time_or_part_time: :both_full_time_and_part_time_vacancies,
+    part_time: :part_time_vacancies,
+    full_time: :full_time_vacancies,
+  }.freeze
+
   def up
     Course.joins(:site_statuses, :enrichments, provider: :recruitment_cycle).where(provider: { recruitment_cycle: { year: "2023" } }, enrichments: { status: %w[draft rolled_over] }, site_statuses: { status: "running" }).find_each do |course|
       next if course.is_published?
 
-      course.site_statuses.update_all(publish: :unpublished, status: :new_status)
+      course.site_statuses.update_all(publish: :unpublished, status: :new_status, vac_status: VAC_MAPPING[course.study_mode.to_sym] || :no_vacancies)
     end
   end
 
