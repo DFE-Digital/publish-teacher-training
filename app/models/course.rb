f544cd3cf953be893c67931f2e714f2f010759ec
maxmills <8balldigitalsolutions@gmail.com>
2022-10-12 21:48:21 +0100

    modern languages option

--- app/models/course.rb
+++ app/models/course.rb
@@ -94,7 +94,7 @@ class Course < ApplicationRecord
   delegate :after_2021?, :year, to: :recruitment_cycle, allow_nil: true, prefix: :recruitment_cycle
 
   def applicable_for_engineers_teach_physics?
-    master_subject_id == SecondarySubject.physics.id && !subject_ids.include?(SecondarySubject.modern_languages.id)
+    master_subject_id == SecondarySubject.physics.id # && !subject_ids.include?(SecondarySubject.modern_languages.id)
   end
 
   def set_subject_position(course_subject)
