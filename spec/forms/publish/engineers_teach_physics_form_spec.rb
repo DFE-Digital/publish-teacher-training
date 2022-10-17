a690a2d8703091cf1fa338c14f5d9bdfb47c0657
maxmills <8balldigitalsolutions@gmail.com>
2022-10-17 01:28:57 +0100

    form validation

---
+++ spec/forms/publish/engineers_teach_physics_form_spec.rb
@@ -0,0 +1,28 @@
+require "rails_helper"
+
+module Publish
+  describe EngineersTeachPhysicsForm do
+    let(:params) { { :campaign_name => "" } }
+    let(:subject_ids) { [biology_secondary_subject.id] }
+    let(:course) { create(:course, :secondary, subjects: [physics_secondary_subject]) }
+    let(:physics_secondary_subject) { find_or_create(:secondary_subject, :physics) }
+
+    subject { described_class.new(course, params:) }
+
+    describe "validation" do
+      it "is not valid when campaign_name is not included" do
+        form  =  described_class.new(course, params:)
+        expect(form.valid?).to be(false)
+        expect(form.errors[:campaign_name])
+            .to include("Select an option")
+      end
+
+      it "is valid when campaign_names are included" do
+        Course.campaign_names.keys.each do |campaign|
+          form = described_class.new(course, params: { campaign_name: campaign })
+          expect(form.valid?).to be(true)
+        end
+      end
+    end
+  end
+end
