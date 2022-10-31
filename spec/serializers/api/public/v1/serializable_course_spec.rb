b1e9b16d3207421197e50cfae5978f0d482abe63
avinhurry <avin.hurry@digital.education.gov.uk>
2022-10-31 09:31:34 +0000

    Expose campaign_name field on course to v1 api

--- spec/serializers/api/public/v1/serializable_course_spec.rb
+++ spec/serializers/api/public/v1/serializable_course_spec.rb
@@ -73,6 +73,7 @@ RSpec.describe API::Public::V1::SerializableCourse do
   it { is_expected.to have_attribute(:scholarship_amount).with_value(nil) }
   it { is_expected.to have_attribute(:can_sponsor_skilled_worker_visa) }
   it { is_expected.to have_attribute(:can_sponsor_student_visa) }
+  it { is_expected.to have_attribute(:campaign_name) }
 
   context "when bursary amount is present" do
     let(:course) { create(:course, :with_accrediting_provider, :secondary, enrichments: [enrichment], subjects: [find_or_create(:secondary_subject, :classics)]) }
