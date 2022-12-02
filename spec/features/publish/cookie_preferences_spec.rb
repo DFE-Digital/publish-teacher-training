5634a9be0df4b499533162d6edbf31af9ec7d388
defong <de_fong2k@yahoo.com>
2022-12-02 15:39:59 +0000

    [0872] Added more tests for cookie preferences

--- spec/features/publish/cookie_preferences_spec.rb
+++ spec/features/publish/cookie_preferences_spec.rb
@@ -4,6 +4,9 @@ require "rails_helper"
 
 feature "Updating cookie preferences" do
   scenario "i can update my cookie preferences" do
+    given_i_am_on_the_cookie_preferences_page
+    and_i_can_see_the_heading
+    and_i_can_see_the_the_use_of_cookies_for_service
     when_i_give_consent_and_submit
     then_i_should_see_a_confirmation_message
   end
@@ -35,5 +38,17 @@ feature "Updating cookie preferences" do
     expect(cookie_preferences_page.error_messages).to include(
       "Select yes if you want to accept Google Analytics cookies",
     )
+  end
+
+  def and_i_can_see_the_heading
+    expect(cookie_preferences_page.heading).to have_content("Cookies")
+  end
+
+  def and_i_can_see_the_the_use_of_cookies_for_service
+    expect(cookie_preferences_page).to have_content("We use cookies to make #{service_name} work and collect information about how you use our service.")
+  end
+
+  def service_name
+    "Publish teacher training courses (Publish)"
   end
 end
