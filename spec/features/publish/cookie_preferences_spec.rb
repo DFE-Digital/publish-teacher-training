e142ed287a8e2ba5648af9357668ed2b53c420f1
defong <de_fong2k@yahoo.com>
2022-12-02 15:34:54 +0000

    [0872] Amended cookie preferences spec to be readable

--- spec/features/publish/cookie_preferences_spec.rb
+++ spec/features/publish/cookie_preferences_spec.rb
@@ -4,31 +4,30 @@ require "rails_helper"
 
 feature "Updating cookie preferences" do
   scenario "i can update my cookie preferences" do
-    when_i_visit_the_cookie_preferences_page
-    and_i_give_consent_and_submit
+    when_i_give_consent_and_submit
     then_i_should_see_a_confirmation_message
   end
 
   scenario "i cannot update without selecting a preference" do
-    when_i_visit_the_cookie_preferences_page
-    and_i_submit
+    given_i_am_on_the_cookie_preferences_page
+    when_i_submit
     then_i_should_see_an_error_message
   end
 
-  def when_i_visit_the_cookie_preferences_page
+  def given_i_am_on_the_cookie_preferences_page
     cookie_preferences_page.load
   end
 
-  def and_i_give_consent_and_submit
+  def when_i_give_consent_and_submit
     cookie_preferences_page.yes_option.choose
-    and_i_submit
+    when_i_submit
   end
 
   def then_i_should_see_a_confirmation_message
-    expect(cookie_preferences_page).to have_text("Your cookie preferences have been updated")
+    expect(cookie_preferences_page).to have_content("Your cookie preferences have been updated")
   end
 
-  def and_i_submit
+  def when_i_submit
     cookie_preferences_page.submit.click
   end
 
