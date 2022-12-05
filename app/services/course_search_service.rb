3642db858919666ad424707b82c9e8eea4e8d068
Max Mills <8balldigitalsolutions@gmail.com>
2022-12-05 14:15:19 +0000

    fix previous cm

--- app/services/course_search_service.rb
+++ app/services/course_search_service.rb
@@ -165,11 +165,11 @@ private
   end
 
   def sort_by_provider_ascending?
-    sort == Set["0"] || CourseSearchService::PROVIDER_ASCENDING
+    sort == Set["0"] || sort == PROVIDER_ASCENDING
   end
 
   def sort_by_provider_descending?
-    sort == Set["1"] || CourseSearchService::PROVIDER_DESCENDING
+    sort == Set["1"] || sort == PROVIDER_DESCENDING
   end
 
   def sort_by_distance?
