fe65b405716a0879128f7784f52c780bf9624892
avinhurry <avin.hurry@digital.education.gov.uk>
2022-12-05 16:20:22 +0000

    Add test

--- spec/features/publish/managing_users_spec.rb
+++ spec/features/publish/managing_users_spec.rb
@@ -36,9 +36,18 @@ feature "Adding user to organisation as a provider user", { can_edit_current_and
     end
   end
 
+  describe "Viewing a user in an organisation" do
+    scenario "With an existing user" do
+      given_i_visit_the_users_index_page
+      and_i_click_on_the_user
+      then_i_should_be_on_the_user_show_page
+      and_the_users_name_should_be_displayed
+    end
+  end
+
   def given_i_am_authenticated_as_a_provider_user
     @provider = create(:provider, provider_name: "Batman's Chocolate School")
-    @user = create(:user, providers: [@provider])
+    @user = create(:user, first_name: "Mr", last_name: "User", providers: [@provider])
     given_i_am_authenticated(user: @user)
   end
 
@@ -107,5 +116,17 @@ feature "Adding user to organisation as a provider user", { can_edit_current_and
     expect(users_new_page.error_summary).to have_text("Enter a first name")
     expect(users_new_page.error_summary).to have_text("Enter a last name")
     expect(users_new_page.error_summary).to have_text("Enter an email address")
+  end
+
+  def and_i_click_on_the_user
+    click_link "Mr User"
+  end
+
+  def then_i_should_be_on_the_user_show_page
+    expect(users_show_page).to be_displayed
+  end
+
+  def and_the_users_name_should_be_displayed
+    expect(users_show_page).to have_text("Mr User")
   end
 end
