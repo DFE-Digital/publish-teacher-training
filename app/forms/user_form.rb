1556152f266d9112037f0fed65893571d81e32ab
avinhurry <avin.hurry@digital.education.gov.uk>
2022-12-07 15:15:54 +0000

    Remove lowercase validation
    BobKidney@example.com is a valid email

--- app/forms/user_form.rb
+++ app/forms/user_form.rb
@@ -12,7 +12,6 @@ class UserForm < Form
   validates :last_name, presence: true
   validates :email, presence: true
   validates :email, email_address: true
-  validate :email_is_lowercase
 
   def compute_fields
     model.attributes.symbolize_keys.slice(*FIELDS).merge(new_attributes)
