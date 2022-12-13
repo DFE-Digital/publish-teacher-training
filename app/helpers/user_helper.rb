8832c1daa660326f0da7c32ddc873571340726e5
avinhurry <avin.hurry@digital.education.gov.uk>
2022-12-13 18:08:33 +0000

    Add helper method to check if email has been changed

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -8,4 +8,8 @@ module UserHelper
   def user_details(user)
     "#{user[:first_name]} #{user[:last_name]} <#{user[:email]}>"
   end
+
+  def email_changed?(user_form)
+    user_form.email != User.find(params[:user_id]).email
+  end
 end
