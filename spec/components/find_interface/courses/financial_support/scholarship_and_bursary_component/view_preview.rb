111d974d97a9c671989980359f38632c4d65d232
defong <de_fong2k@yahoo.com>
2022-12-13 14:12:45 +0000

    [0866] Amended to show preview

--- spec/components/find_interface/courses/financial_support/scholarship_and_bursary_component/view_preview.rb
+++ spec/components/find_interface/courses/financial_support/scholarship_and_bursary_component/view_preview.rb
@@ -2,8 +2,56 @@
 
 module FindInterface::Courses::FinancialSupport::ScholarshipAndBursaryComponent
   class ViewPreview < ViewComponent::Preview
-    def with_scholarship_early_career_and_scholarship_body
-      course = Course.new(subjects: [Subject.new(id: 22, type: "SecondarySubject", subject_code: "F8", subject_name: "Physics", financial_incentive: FinancialIncentive.new(scholarship: 2000,
+    def physic_with_scholarship_early_career_and_scholarship_body
+      course = Course.new(subjects: [Subject.new(id: 22, type: "SecondarySubject", subject_code: "F3", subject_name: "Physics", financial_incentive: FinancialIncentive.new(scholarship: 2000,
+        bursary_amount: 3000,
+        early_career_payments: 2000))])
+
+      render FindInterface::Courses::FinancialSupport::ScholarshipAndBursaryComponent::View.new(course.decorate)
+    end
+
+    def chemistry_with_scholarship_early_career_and_scholarship_body
+      course = Course.new(subjects: [Subject.new(id: 22, type: "SecondarySubject", subject_code: "F3", subject_name: "Chemistry", financial_incentive: FinancialIncentive.new(scholarship: 2000,
+        bursary_amount: 3000,
+        early_career_payments: 2000))])
+
+      render FindInterface::Courses::FinancialSupport::ScholarshipAndBursaryComponent::View.new(course.decorate)
+    end
+
+    def computing_with_scholarship_early_career_and_scholarship_body
+      course = Course.new(subjects: [Subject.new(id: 22, type: "SecondarySubject", subject_code: "11", subject_name: "Computing", financial_incentive: FinancialIncentive.new(scholarship: 2000,
+        bursary_amount: 3000,
+        early_career_payments: 2000))])
+
+      render FindInterface::Courses::FinancialSupport::ScholarshipAndBursaryComponent::View.new(course.decorate)
+    end
+
+    def maths_with_scholarship_early_career_and_scholarship_body
+      course = Course.new(subjects: [Subject.new(id: 22, type: "SecondarySubject", subject_code: "G1", subject_name: "Maths", financial_incentive: FinancialIncentive.new(scholarship: 2000,
+        bursary_amount: 3000,
+        early_career_payments: 2000))])
+
+      render FindInterface::Courses::FinancialSupport::ScholarshipAndBursaryComponent::View.new(course.decorate)
+    end
+
+    def french_with_scholarship_early_career_and_scholarship_body
+      course = Course.new(subjects: [Subject.new(id: 22, type: "SecondarySubject", subject_code: "15", subject_name: "French", financial_incentive: FinancialIncentive.new(scholarship: 2000,
+        bursary_amount: 3000,
+        early_career_payments: 2000))])
+
+      render FindInterface::Courses::FinancialSupport::ScholarshipAndBursaryComponent::View.new(course.decorate)
+    end
+
+    def german_with_scholarship_early_career_and_scholarship_body
+      course = Course.new(subjects: [Subject.new(id: 22, type: "SecondarySubject", subject_code: "17", subject_name: "German", financial_incentive: FinancialIncentive.new(scholarship: 2000,
+        bursary_amount: 3000,
+        early_career_payments: 2000))])
+
+      render FindInterface::Courses::FinancialSupport::ScholarshipAndBursaryComponent::View.new(course.decorate)
+    end
+
+    def spanish_with_scholarship_early_career_and_scholarship_body
+      course = Course.new(subjects: [Subject.new(id: 22, type: "SecondarySubject", subject_code: "22", subject_name: "Spanish", financial_incentive: FinancialIncentive.new(scholarship: 2000,
         bursary_amount: 3000,
         early_career_payments: 2000))])
 
