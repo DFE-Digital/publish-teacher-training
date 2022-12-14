9526c77ecd75f91c98ea472079c747cc015f77f3
avinhurry <avin.hurry@digital.education.gov.uk>
2022-12-14 12:37:23 +0000

    Add feature tests

---
+++ spec/support/page_objects/publish/users_edit.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+module PageObjects
+  module Publish
+    class UsersEdit < PageObjects::Base
+      set_url "/publish/organisations/{provider_code}/users/edit"
+
+      element :first_name, "#publish-user-form-first-name-field"
+      element :last_name, "#publish-user-form-last-name-field"
+      element :last_name, "#publish-user-form-last-name-field"
+      element :email, "#publish-user-form-email-field"
+
+      element :error_summary, ".govuk-error-summary"
+    end
+  end
+end
--- spec/support/page_objects/publish/users_show.rb
+++ spec/support/page_objects/publish/users_show.rb
@@ -6,6 +6,10 @@ module PageObjects
       set_url "/publish/organisations/{provider_code}/users/{id}"
 
       element :remove_user_link, ".govuk-link", text: "Remove user"
+
+      element :change_first_name, "a.govuk-link", text: "Change last name"
+      element :change_last_name, "a.govuk-link", text: "Change first name"
+      element :change_email_address, "a.govuk-link", text: "Change email address"
     end
   end
 end
