1014c6a2180f4c91c11bce1e129da885e3b467bf
avinhurry <avin.hurry@digital.education.gov.uk>
2022-12-15 10:47:24 +0000

    Remove redundant authorisation

--- app/controllers/publish/users_edit_check_controller.rb
+++ app/controllers/publish/users_edit_check_controller.rb
@@ -10,7 +10,6 @@ module Publish
       @user_form = UserForm.new(current_user, user)
       if @user_form.save!
         UserAssociationsService::Create.call(user: @user_form.model, provider:) if @user_form.model.providers.exclude?(provider)
-        authorize(provider)
         redirect_to publish_provider_user_path(id: params[:user_id])
         flash[:success] = "User updated"
       end
