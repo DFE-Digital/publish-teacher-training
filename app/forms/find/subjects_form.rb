880fe142767ef59f8a59bdbde2a6d32cbe4be481
Max Mills <8balldigitalsolutions@gmail.com>
2022-12-23 14:16:49 +0000

    refactor form

--- app/forms/find/subjects_form.rb
+++ app/forms/find/subjects_form.rb
@@ -2,15 +2,18 @@ module Find
   class SubjectsForm
     include ActiveModel::Model
 
-    attr_accessor :subjects, :age_group
+    attr_reader :subjects, :age_group
+
+    def initialize(subjects:, age_group:)
+      @subjects = subjects
+      @age_group = age_group
+    end
 
     validate :subjects_have_been_selected
 
     def subjects_have_been_selected
       if subjects.blank? && age_group.present?
         errors.add(:subjects, :"#{age_group}_subject")
-      elsif subjects.blank?
-        errors.add(:subjects, :blank)
       end
     end
 
