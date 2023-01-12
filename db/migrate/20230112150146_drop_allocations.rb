08f1919fb3220eb6a477f61b4dc44c1a0d583619
defong <de_fong2k@yahoo.com>
2023-01-12 15:08:08 +0000

    [0920] Removed allocation related tables

---
+++ db/migrate/20230112150146_drop_allocations.rb
@@ -0,0 +1,10 @@
+class DropAllocations < ActiveRecord::Migration[7.0]
+  def up
+    drop_table :allocation_uplift
+    drop_table :allocation
+  end
+
+  def down
+    raise ActiveRecord::IrreversibleMigration, " The 'allocation' & 'allocation_uplift' tables was only used when PE allocation/recruitment was restricted, this is now not the case."
+  end
+end
