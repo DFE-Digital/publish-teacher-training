52fb97096005139c7b6f6464eddc31f5c7841dbf
Max Mills <8balldigitalsolutions@gmail.com>
2023-01-16 16:59:24 +0000

    typo

--- app/controllers/find/results_controller.rb
+++ app/controllers/find/results_controller.rb
@@ -3,10 +3,10 @@ module Find
     before_action :render_feedback_component
 
     def index
-      matched_parmas = MatchOldParams.call(request.query_parameters)
+      matched_params = MatchOldParams.call(request.query_parameters)
 
-      @results_view = ResultsView.new(query_parameters: matched_parmas)
-      @filters_view = ResultFilters::FiltersView.new(params: matched_parmas)
+      @results_view = ResultsView.new(query_parameters: matched_params)
+      @filters_view = ResultFilters::FiltersView.new(params: matched_params)
       @courses = @results_view.courses.page params[:page]
       @number_of_courses_string = @results_view.number_of_courses_string
     end
