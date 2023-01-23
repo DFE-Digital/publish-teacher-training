1366acebf0affa7dd8e737eaeb96f33d2bb10bcc
Max Mills <8balldigitalsolutions@gmail.com>
2023-01-23 16:27:26 +0000

    subjects array

--- app/controllers/find/search/subjects_controller.rb
+++ app/controllers/find/search/subjects_controller.rb
@@ -25,12 +25,14 @@ module Find
     private
 
       def sanitised_subject_codes
+        return form_params["subjects"].split if form_params["subjects"].is_a? String
+
         form_params["subjects"].compact_blank!
       end
 
       def form_params
         params.require(:find_subjects_form)
-          .permit(:c, :latitude, :longitude, :loc, :lq, :radius, :sortby, :age_group, :has_vacancies, :l, :send_courses, :prev_l, :prev_lat, :prev_lng, :prev_loc, :prev_lq, :prev_query, :prev_rad, "provider.provider_name", :degree_required, :can_sponsor_visa, :funding, qualification: [], subjects: [], study_type: [])
+          .permit(:c, :latitude, :longitude, :loc, :lq, :radius, :sortby, :age_group, :has_vacancies, :l, :send_courses, :prev_l, :prev_lat, :prev_lng, :prev_loc, :prev_lq, :prev_query, :prev_rad, "provider.provider_name", :degree_required, :can_sponsor_visa, :funding, :subjects, qualification: [], subjects: [], study_type: [])
       end
 
       def build_backlink_query_parameters
