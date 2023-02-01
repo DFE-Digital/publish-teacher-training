7953d687c27ba465850e34db392b9f700db396cb
Max Mills <8balldigitalsolutions@gmail.com>
2023-02-01 15:56:51 +0000

    rubocop

--- spec/helpers/publish/subject_helper_spec.rb
+++ spec/helpers/publish/subject_helper_spec.rb
@@ -4,9 +4,9 @@ require 'rails_helper'
 
 describe Publish::SubjectHelper do
   describe '#primary_form_options' do
-    let(:subjects) { [find_or_create(:primary_subject, :primary_with_science)] }
-
     subject { primary_form_options(subjects) }
+
+    let(:subjects) { [find_or_create(:primary_subject, :primary_with_science)] }
 
     it 'returns primary subject id' do
       expect(subject.first.id).to eq subjects.first.id
