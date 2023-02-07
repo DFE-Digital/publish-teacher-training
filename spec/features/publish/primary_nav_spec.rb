3a84791d748f98797ee745e200e708d010236b47
avinhurry <avin.hurry@digital.education.gov.uk>
2023-02-07 09:24:04 +0000

    Rename training partners to accredited bodies

--- spec/features/publish/primary_nav_spec.rb
+++ spec/features/publish/primary_nav_spec.rb
@@ -10,8 +10,8 @@ feature 'Primary nav', { can_edit_current_and_next_cycles: false } do
     and_i_should_see_the_locations_link
     and_i_should_see_the_courses_link
     and_i_should_see_the_users_link
-    and_i_should_not_see_the_training_partners_link
-    and_i_should_not_see_the_training_partners_link
+    and_i_should_not_see_the_accredited_bodies_link
+    and_i_should_not_see_the_accredited_bodies_link
   end
 
   scenario 'view page as Susy - user with accredited body' do
@@ -21,7 +21,7 @@ feature 'Primary nav', { can_edit_current_and_next_cycles: false } do
     and_i_should_see_the_locations_link
     and_i_should_see_the_courses_link
     and_i_should_see_the_users_link
-    and_i_should_see_the_training_partners_link
+    and_i_should_see_the_accredited_bodies_link
   end
 
   def and_i_am_authenticated_as_a_provider_user
@@ -52,15 +52,15 @@ feature 'Primary nav', { can_edit_current_and_next_cycles: false } do
     expect(publish_primary_nav_page).to have_users
   end
 
-  def and_i_should_see_the_training_partners_link
-    expect(publish_primary_nav_page).to have_training_partners
+  def and_i_should_see_the_accredited_bodies_link
+    expect(publish_primary_nav_page).to have_accredited_bodies
   end
 
-  def and_i_should_not_see_the_training_partners_link
-    expect(publish_primary_nav_page).not_to have_training_partners
+  def and_i_should_not_see_the_accredited_bodies_link
+    expect(publish_primary_nav_page).not_to have_accredited_bodies
   end
 
-  def and_i_should_not_see_the_training_partners_link
+  def and_i_should_not_see_the_accredited_bodies_link
     expect(page).not_to have_text 'Change organisation'
   end
 end
--- spec/support/page_objects/publish/primary_nav.rb
+++ spec/support/page_objects/publish/primary_nav.rb
@@ -7,7 +7,7 @@ module PageObjects
       element :locations, :link, 'Locations'
       element :courses, :link, 'Courses'
       element :users, :link, 'Users'
-      element :training_partners, :link, 'Training partners'
+      element :accredited_bodies, :link, 'Training partners'
     end
   end
 end
