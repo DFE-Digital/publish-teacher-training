f156758bb09dfd7cc9e950c1e4177b033bd9bc2c
David Kewal <davidrk@hey.com>
2023-02-07 14:52:38 +0000

    [1035] Scrub out sensitive data from certain exceptions

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,14 +1,27 @@
 # frozen_string_literal: true
 
-Sentry.init do |config|
-  # Let's not exclude ActiveRecord::RecordNotFound from Sentry
-  # https://github.com/DFE-Digital/publish-teacher-training/pull/160
-  config.excluded_exceptions -= ['ActiveRecord::RecordNotFound']
+PG_DETAIL_REGEX = /^DETAIL:.*$/
+PG_DETAIL_FILTERED = '[PG DETAIL FILTERED]'
 
+def filter_record_not_unique_exception_messages!(event, hint)
+  return unless hint[:exception].is_a?(ActiveRecord::RecordNotUnique)
+
+  event.exception.values.each do |single_exception| # rubocop:disable Style/HashEachMethods
+    single_exception.value.gsub!(PG_DETAIL_REGEX, PG_DETAIL_FILTERED)
+  end
+end
+
+Sentry.init do |config|
   filter = ActiveSupport::ParameterFilter.new(Rails.application.config.filter_parameters)
-  config.before_send = lambda do |event, _hint|
+  config.before_send = lambda do |event, hint|
+    filter_record_not_unique_exception_messages!(event, hint)
     filter.filter(event.to_hash)
   end
 
   config.release = ENV.fetch('COMMIT_SHA', nil)
+
+  config.excluded_exceptions += %w[
+    ActiveRecord::RecordNotFound
+    Pundit::NotAuthorizedError
+  ]
 end
