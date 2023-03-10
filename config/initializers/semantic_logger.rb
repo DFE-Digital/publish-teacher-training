1f2a371b5597f1049e3814ac34f9ed54702bfba0
Max Mills <8balldigitalsolutions@gmail.com>
2023-03-10 12:11:15 +0000

    semantic logger in test env

---
+++ config/initializers/semantic_logger.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+# if Rails.env.development? || Rails.env.production?
+Rails.application.configure do
+  config.semantic_logger.application = Settings.application_name
+  config.log_tags = [:request_id]
+  config.log_level = Settings.log_level
+end
+SemanticLogger.add_appender(io: $stdout, level: Rails.application.config.log_level, formatter: Rails.application.config.log_format)
+Rails.application.config.logger.info('Application logging to STDOUT')
+# end
