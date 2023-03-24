eff8bc993cbf9b60700350871215a11af6fea134
avinhurry <avin.hurry@digital.education.gov.uk>
2023-03-24 19:13:08 +0000

    Fix primary flow

--- app/controllers/publish/courses/degrees/start_controller.rb
+++ app/controllers/publish/courses/degrees/start_controller.rb
@@ -16,24 +16,23 @@ module Publish
           authorize(provider)
 
           @start_form = DegreeStartForm.new(degree_grade_required: grade_required_params)
-
-          if course.is_primary? && @start_form.valid? && !goto_preview?
+          if course.is_primary? && @start_form.valid? && !goto_preview? && @start_form.degree_grade_required.blank?
             @start_form.save(course)
             course_updated_message('Minimum degree classification')
             redirect_to publish_provider_recruitment_cycle_course_path
-          elsif course.is_primary? && @start_form.valid? && goto_preview?
+          elsif course.is_primary? && @start_form.valid? && goto_preview? && @start_form.degree_grade_required.blank?
             @start_form.save(course)
             redirect_to preview_publish_provider_recruitment_cycle_course_path(provider.provider_code, course.recruitment_cycle_year, course.course_code)
+          elsif @start_form.valid? && !goto_preview? && @start_form.degree_grade_required.blank?
+            @start_form.save(course)
+            redirect_to degrees_subject_requirements_publish_provider_recruitment_cycle_course_path
+          elsif @start_form.valid? && goto_preview? && @start_form.degree_grade_required.blank?
+            @start_form.save(course)
+            redirect_to degrees_subject_requirements_publish_provider_recruitment_cycle_course_path(goto_preview: true)
           elsif @start_form.degree_grade_required.present? && !goto_preview?
             redirect_to degrees_grade_publish_provider_recruitment_cycle_course_path
           elsif @start_form.degree_grade_required.present? && goto_preview?
             redirect_to degrees_grade_publish_provider_recruitment_cycle_course_path(goto_preview: true)
-          elsif @start_form.valid? && !goto_preview?
-            @start_form.save(course)
-            redirect_to degrees_subject_requirements_publish_provider_recruitment_cycle_course_path
-          elsif @start_form.valid? && goto_preview?
-            @start_form.save(course)
-            redirect_to degrees_subject_requirements_publish_provider_recruitment_cycle_course_path(goto_preview: true)
           else
             @errors = @start_form.errors.messages
             render :edit
