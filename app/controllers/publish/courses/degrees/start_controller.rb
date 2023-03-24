5ad723540ffaf3b1295c8f00e04c0ac63d5e1d88
avinhurry <avin.hurry@digital.education.gov.uk>
2023-03-24 17:07:58 +0000

    Fix ordering

--- app/controllers/publish/courses/degrees/start_controller.rb
+++ app/controllers/publish/courses/degrees/start_controller.rb
@@ -24,16 +24,16 @@ module Publish
           elsif course.is_primary? && @start_form.valid? && goto_preview?
             @start_form.save(course)
             redirect_to preview_publish_provider_recruitment_cycle_course_path(provider.provider_code, course.recruitment_cycle_year, course.course_code)
+          elsif @start_form.degree_grade_required.present? && !goto_preview?
+            redirect_to degrees_grade_publish_provider_recruitment_cycle_course_path
+          elsif @start_form.degree_grade_required.present? && goto_preview?
+            redirect_to degrees_grade_publish_provider_recruitment_cycle_course_path(goto_preview: true)
           elsif @start_form.valid? && !goto_preview?
             @start_form.save(course)
             redirect_to degrees_subject_requirements_publish_provider_recruitment_cycle_course_path
           elsif @start_form.valid? && goto_preview?
             @start_form.save(course)
             redirect_to degrees_subject_requirements_publish_provider_recruitment_cycle_course_path(goto_preview: true)
-          elsif @start_form.degree_grade_required.present? && !goto_preview?
-            redirect_to degrees_grade_publish_provider_recruitment_cycle_course_path
-          elsif @start_form.degree_grade_required.present? && goto_preview?
-            redirect_to degrees_grade_publish_provider_recruitment_cycle_course_path(goto_preview: true)
           else
             @errors = @start_form.errors.messages
             render :edit
