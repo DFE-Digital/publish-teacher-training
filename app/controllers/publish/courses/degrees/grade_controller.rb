bd9a49ad9e91c14af201c24789770ff610a5783a
avinhurry <avin.hurry@digital.education.gov.uk>
2023-03-24 17:26:57 +0000

    Carry across goto_preview logic

--- app/controllers/publish/courses/degrees/grade_controller.rb
+++ app/controllers/publish/courses/degrees/grade_controller.rb
@@ -15,12 +15,21 @@ module Publish
 
           @grade_form = DegreeGradeForm.new(grade: grade_params)
 
-          if course.is_primary? && @grade_form.save(course)
+          if course.is_primary? && @grade_form.valid? && !goto_preview?
+            @grade_form.save(course)
             course_updated_message('Minimum degree classification')
 
             redirect_to publish_provider_recruitment_cycle_course_path
-          elsif @grade_form.save(course)
+
+          elsif course.is_primary? && @grade_form.valid? && goto_preview?
+            @grade_form.save(course)
+            redirect_to preview_publish_provider_recruitment_cycle_course_path(provider.provider_code, course.recruitment_cycle_year, course.course_code)
+          elsif @grade_form.valid? && !goto_preview?
+            @grade_form.save(course)
             redirect_to degrees_subject_requirements_publish_provider_recruitment_cycle_course_path
+          elsif @grade_form.valid? && goto_preview?
+            @grade_form.save(course)
+            redirect_to degrees_subject_requirements_publish_provider_recruitment_cycle_course_path(goto_preview: true)
           else
             @errors = @grade_form.errors.messages
             render :edit
@@ -36,8 +45,12 @@ module Publish
         def grade_params
           return if params[:publish_degree_grade_form].blank?
 
-          params.require(:publish_degree_grade_form).permit(:grade)[:grade]
+          params.require(:publish_degree_grade_form)
+                .except(:goto_preview)
+                .permit(:grade)[:grade]
         end
+
+        def goto_preview? = params.dig(:publish_degree_grade_form, :goto_preview) == 'true'
       end
     end
   end
