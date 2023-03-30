34a8d17dc8001a750c01610e1c6c8c0e0230c0fc
defong <de_fong2k@yahoo.com>
2023-03-30 15:49:20 +0100

    [1153] Added helper for `goto_preview?`

--- app/helpers/find/goto_preview_helper.rb
+++ app/helpers/find/goto_preview_helper.rb
@@ -5,5 +5,9 @@ module Find
     def goto_preview_value(param_form_key:, params:)
       params[:goto_preview] || params.dig(param_form_key, :goto_preview)
     end
+
+    def goto_preview?(param_form_key:, params:)
+      goto_preview_value(param_form_key:, params:) == 'true'
+    end
   end
 end
--- spec/helpers/find/goto_preview_helper_spec.rb
+++ spec/helpers/find/goto_preview_helper_spec.rb
@@ -35,5 +35,35 @@ module Find
         end
       end
     end
+
+    describe '#goto_preview?' do
+      subject do
+        goto_preview?(param_form_key:, params:)
+      end
+
+      context 'params is empty' do
+        let(:params) { {} }
+
+        it 'returns falsey' do
+          expect(subject).to be_falsey
+        end
+      end
+
+      context 'params has goto_preview set to "true"' do
+        let(:params) { { goto_preview: 'true' } }
+
+        it 'returns truthy' do
+          expect(subject).to be_truthy
+        end
+      end
+
+      context 'params has param_form_key with goto_preview set to "true"' do
+        let(:params) { { param_form_key: { goto_preview: 'true' } } }
+
+        it 'returns truthy' do
+          expect(subject).to be_truthy
+        end
+      end
+    end
   end
 end
