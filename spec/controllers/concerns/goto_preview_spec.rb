a27db326fd57d19957e28a3b7acd3616ef81cbb9
defong <de_fong2k@yahoo.com>
2023-03-31 10:49:43 +0100

    [1153] Added concern for `goto_preview?`

---
+++ spec/controllers/concerns/goto_preview_spec.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe GotoPreview do
+  let(:test_class) do
+    Class.new do
+      include GotoPreview
+
+      def params; end
+
+      def param_form_key = :param_form_key
+    end.new
+  end
+
+  before do
+    allow(test_class).to receive(:params).and_return(params)
+  end
+
+  describe '#goto_preview?' do
+    subject do
+      test_class.goto_preview?
+    end
+
+    context 'params is empty' do
+      let(:params) { {} }
+
+      it 'returns falsey' do
+        expect(subject).to be_falsey
+      end
+    end
+
+    context 'params has param_form_key with goto_preview set to "true"' do
+      let(:params) { { param_form_key: { goto_preview: 'true' } } }
+
+      it 'returns truthy' do
+        expect(subject).to be_truthy
+      end
+    end
+  end
+end
