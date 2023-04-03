0c22643c8f720d50a9237101cad3c050d50b938b
Max Mills <8balldigitalsolutions@gmail.com>
2023-04-03 23:15:06 +0100

    rm another comment

--- spec/support/page_objects/publish/school_delete.rb
+++ spec/support/page_objects/publish/school_delete.rb
@@ -6,9 +6,7 @@ module PageObjects
       set_url '/publish/organisations/{provider_code}/{recruitment_cycle_year}/schools/{school_id}/delete'
 
       element :heading, 'h1'
-      # element :warning_text, '.govuk-warning-text'
       element :cancel_link, '.govuk-link', text: 'Cancel'
-
       element :remove_school_button, '.govuk-button', text: 'Remove school'
     end
   end
