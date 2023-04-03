25a4e763033e7fabfe1ead84685c921dab6d3080
Max Mills <8balldigitalsolutions@gmail.com>
2023-04-03 23:37:09 +0100

    rm whitespace

--- spec/support/page_objects/publish/school_show.rb
+++ spec/support/page_objects/publish/school_show.rb
@@ -4,9 +4,7 @@ module PageObjects
   module Publish
     class SchoolShow < PageObjects::Base
       set_url '/publish/organisations/{provider_code}/{recruitment_cycle_year}/schools/{school_id}'
-
       element :change_name, 'a.govuk-link.location_name', text: 'Change'
-
       element :remove_school_link, '.govuk-link', text: 'Remove school'
     end
   end
