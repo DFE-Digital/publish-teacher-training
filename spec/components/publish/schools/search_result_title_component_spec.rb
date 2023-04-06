7466f274ff9171c1d480da21c56b1ced69d6f011
David Kewal <davidrk@hey.com>
2023-04-06 11:49:08 +0100

    [1162] Forgot one more

--- spec/components/publish/schools/search_result_title_component_spec.rb
+++ spec/components/publish/schools/search_result_title_component_spec.rb
@@ -12,32 +12,32 @@ module Publish
       it 'renders many results text when result is more than limit' do
         render_component(10_000)
 
-        expect(page).to have_text("10000 results found for '#{query}'")
+        expect(page).to have_text("10000 results found for ‘#{query}’")
         expect(page).to have_link('Try narrowing down your search', href: '/test')
       end
 
       it 'renders few results text when result is less than limit' do
         render_component(10)
 
-        expect(page).to have_text("10 results found for '#{query}'")
+        expect(page).to have_text("10 results found for ‘#{query}’")
         expect(page).to have_link('Change your search', href: '/test')
-        expect(page).to have_text('if the school you\'re looking for is not listed.')
+        expect(page).to have_text('if the school you’re looking for is not listed.')
       end
 
       it 'renders one result text when there is only one result' do
         render_component(1)
 
-        expect(page).to have_text("1 result found for '#{query}'")
+        expect(page).to have_text("1 result found for ‘#{query}’")
         expect(page).to have_link('Change your search', href: '/test')
-        expect(page).to have_text('if the school you\'re looking for is not listed.')
+        expect(page).to have_text('if the school you’re looking for is not listed.')
       end
 
       it 'renders no results text when there are no results' do
         render_component(0)
 
-        expect(page).to have_text("No results found for '#{query}'")
+        expect(page).to have_text("No results found for ‘#{query}’")
         expect(page).to have_link('Change your search', href: '/test')
-        expect(page).not_to have_text('if the school you\'re looking for is not listed.')
+        expect(page).not_to have_text('if the school you’re looking for is not listed.')
       end
 
       def render_component(results_count)
