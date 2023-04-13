47bdc8867a1b93a6e33ad32101176555af40a623
defong <de_fong2k@yahoo.com>
2023-04-13 15:06:23 +0100

    [1194] Added page for `new-provider`

--- app/controllers/support/providers_controller.rb
+++ app/controllers/support/providers_controller.rb
@@ -17,6 +17,10 @@ module Support
       @provider.organisations.build
     end
 
+    def new_provider
+      @provider = ProviderForm.new(current_user, recruitment_cycle:)
+    end
+
     def edit
       provider
     end
@@ -27,6 +31,16 @@ module Support
         redirect_to support_recruitment_cycle_provider_path(provider.recruitment_cycle_year, provider), flash: { success: 'Provider was successfully created' }
       else
         render :new
+      end
+    end
+
+    def create_provider
+      @provider = ProviderForm.new(current_user, recruitment_cycle:, params: provider_form_params)
+
+      if @provider.valid?
+        redirect_to support_recruitment_cycle_providers_path(recruitment_cycle.year), flash: { success: 'Provider was successfully created' }
+      else
+        render :new_provider
       end
     end
 
@@ -83,6 +97,11 @@ module Support
                                                                         address4
                                                                         postcode],
                                                    organisations_attributes: %i[name]).merge(recruitment_cycle:)
+    end
+
+    def provider_form_params
+      params.require(:support_provider_form)
+            .permit(ProviderForm::FIELDS)
     end
   end
 end
