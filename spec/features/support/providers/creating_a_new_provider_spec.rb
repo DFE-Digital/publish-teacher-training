3eb2237af5fe202eb15c9e29be44738ab6c21c30
defong <de_fong2k@yahoo.com>
2023-04-14 14:30:33 +0100

    [1194] Added spec for the new provider flow

---
+++ spec/features/support/providers/creating_a_new_provider_spec.rb
@@ -0,0 +1,83 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+feature 'Creating a provider' do
+  let(:user) { create(:user, :admin) }
+
+  before do
+    given_i_am_authenticated(user:)
+    when_i_visit_the_new_provider_page
+  end
+
+  %i[university lead_school scitt].each do |provider_type|
+    scenario "add a new #{provider_type} provider" do
+      when_i_fill_in_a_valid_provider_details(provider_type:)
+      and_i_click_the_continue_button
+      then_i_am_redirected_back_to_the_support_providers_index_page
+      and_a_success_message_is_displayed
+    end
+  end
+
+  scenario 'with invalid details' do
+    when_i_click_the_continue_button
+    then_i_see_the_error_summary
+  end
+
+  scenario 'back link return to correct page' do
+    when_i_click_the_back_link
+    then_i_am_redirected_back_to_the_support_providers_index_page
+  end
+
+  private
+
+  def and_a_success_message_is_displayed
+    expect(page).to have_content('Provider was successfully created')
+  end
+
+  def when_i_visit_the_new_provider_page
+    visit "/support/#{Settings.current_recruitment_cycle_year}/providers/new-provider"
+  end
+
+  def when_i_fill_in_a_valid_provider_details(provider_type:)
+    fill_in 'Provider name', with: 'My favourite provider'
+    fill_in 'Provider code', with: 'A32'
+    fill_in 'UK provider reference number (UKPRN)', with: '12341234'
+
+    case provider_type
+    when :lead_school
+      choose 'No'
+
+      choose 'School'
+      fill_in 'Unique reference number (URN)', with: '54321'
+    when :university
+      choose 'Yes'
+      fill_in 'Accredited provider ID', with: '1111'
+
+      choose 'Higher education institution (HEI)'
+    when :scitt
+      choose 'Yes'
+      fill_in 'Accredited provider ID', with: '5555'
+
+      choose 'School centred initial teacher training (SCITT)'
+    end
+  end
+
+  def and_i_click_the_continue_button
+    click_button 'Continue'
+  end
+
+  def when_i_click_the_back_link
+    click_on 'Back'
+  end
+
+  def then_i_am_redirected_back_to_the_support_providers_index_page
+    expect(page).to have_current_path("/support/#{Settings.current_recruitment_cycle_year}/providers")
+  end
+
+  def then_i_see_the_error_summary
+    expect(page.find('.govuk-error-summary')).to be_visible
+  end
+
+  alias_method :when_i_click_the_continue_button, :and_i_click_the_continue_button
+end
--- spec/features/support/providers/providers_list_spec.rb
+++ spec/features/support/providers/providers_list_spec.rb
@@ -4,8 +4,11 @@ require 'rails_helper'
 
 feature 'View providers' do
   let(:user) { create(:user, :admin) }
+  let(:support_new_provider_creation_flow) { false }
 
   before do
+    allow(Settings.features).to receive(:support_new_provider_creation_flow).and_return(support_new_provider_creation_flow)
+
     given_i_am_authenticated(user:)
     and_there_are_providers
     when_i_visit_the_support_provider_index_page
@@ -13,6 +16,20 @@ feature 'View providers' do
 
   scenario 'i can view the providers' do
     then_i_see_the_providers
+  end
+
+  scenario 'nagivate to the new provider page' do
+    and_i_click_on_add_provider
+    then_i_am_on_the_old_new_provider_page
+  end
+
+  context 'support_new_provider_creation_flow is set to true' do
+    let(:support_new_provider_creation_flow) { true }
+
+    scenario 'nagivate to the old new provider page' do
+      and_i_click_on_add_provider
+      then_i_am_on_the_new_provider_page
+    end
   end
 
   def and_there_are_providers
@@ -25,5 +42,17 @@ feature 'View providers' do
 
   def then_i_see_the_providers
     expect(support_provider_index_page.providers.size).to eq(2)
+  end
+
+  def then_i_am_on_the_new_provider_page
+    expect(page).to have_current_path("/support/#{Settings.current_recruitment_cycle_year}/providers/new-provider")
+  end
+
+  def then_i_am_on_the_old_new_provider_page
+    expect(page).to have_current_path("/support/#{Settings.current_recruitment_cycle_year}/providers/new")
+  end
+
+  def and_i_click_on_add_provider
+    click_on('Add provider')
   end
 end
