66ff9092fc0c7021fd84ac75715e46528e68a6fd
avinhurry <avin.hurry@digital.education.gov.uk>
2023-04-18 15:53:52 +0100

    Only need 2 conditions
    We no longer need the extra condition as we are now using one generic error message

--- app/validators/ukprn_format_validator.rb
+++ app/validators/ukprn_format_validator.rb
@@ -6,10 +6,8 @@ class UkprnFormatValidator < ActiveModel::EachValidator
 
     if value.blank?
       record.errors.add(attribute, :blank)
-    elsif value[0] != '1'
-      record.errors.add(attribute, :start_with_one)
-    elsif !value.match?(/\A\d{8}\Z/)
-      record.errors.add(attribute, :contains_eight_numbers)
+    elsif value[0] != '1' || !value.match?(/\A\d{8}\Z/)
+      record.errors.add(attribute, :contains_eight_numbers_starting_with_one)
     end
   end
 end
