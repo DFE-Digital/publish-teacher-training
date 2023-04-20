2fa16ae6184ac2df47c115bfb7aaa0d5159334fb
Max Mills <8balldigitalsolutions@gmail.com>
2023-04-20 15:52:22 +0100

    refactor spec

--- spec/features/publish/searching_for_a_school_spec.rb
+++ spec/features/publish/searching_for_a_school_spec.rb
@@ -15,7 +15,7 @@ feature 'Searching for a school from the GIAS list' do
     then_i_should_see_an_error_message
 
     when_i_search_for_a_school_with_a_valid_query
-    then_i_see_the_school_i_searched_for
+    and_the_school_form_should_be_prefilled(@school)
 
     when_i_visit_the_school_search_page
     when_i_search_for_a_school_with_a_partial_query
@@ -27,7 +27,7 @@ feature 'Searching for a school from the GIAS list' do
 
     when_i_select_the_school
     then_i_should_be_taken_to_the_add_school_page
-    and_the_school_form_should_be_prefilled_with_the_school_details
+    and_the_school_form_should_be_prefilled(@school_two)
   end
 
   private
@@ -70,10 +70,6 @@ feature 'Searching for a school from the GIAS list' do
     click_continue
   end
 
-  def then_i_see_the_school_i_searched_for
-    expect(page.find_by_id('support-school-form-location-name-field').value).to eq(@school.name)
-  end
-
   def when_i_select_the_school
     choose @school_two.name
     click_continue
@@ -83,13 +79,13 @@ feature 'Searching for a school from the GIAS list' do
     expect(page.current_url).to include("school_id=#{@school_two.id}")
   end
 
-  def and_the_school_form_should_be_prefilled_with_the_school_details
-    expect(page).to have_field('School name', with: @school_two.name)
-    expect(page).to have_field('URN', with: @school_two.urn)
+  def and_the_school_form_should_be_prefilled(school)
+    expect(page).to have_field('School name', with: school.name)
+    expect(page).to have_field('URN', with: school.urn)
 
-    expect(page).to have_field('Address line 1', with: @school_two.address1)
-    expect(page).to have_field('Town or city', with: @school_two.town)
-    expect(page).to have_field('Postcode', with: @school_two.postcode)
+    expect(page).to have_field('Address line 1', with: school.address1)
+    expect(page).to have_field('Town or city', with: school.town)
+    expect(page).to have_field('Postcode', with: school.postcode)
   end
 
   def and_i_search_with_an_invalid_query
