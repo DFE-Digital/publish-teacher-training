97a8c4f48350ab43ccba7b26081568139f10c1af
Max Mills <8balldigitalsolutions@gmail.com>
2023-04-20 16:31:02 +0100

    update for 400 status

--- spec/controllers/api/public/v1/school_suggestions_controller_spec.rb
+++ spec/controllers/api/public/v1/school_suggestions_controller_spec.rb
@@ -8,17 +8,33 @@ RSpec.describe API::Public::V1::SchoolSuggestionsController do
     let!(:site2) { create(:gias_school, name: 'claytown high school') }
     let!(:site3) { create(:gias_school, name: 'another school') }
 
-    before do
-      get :index, params: {
-        query: 'cla'
-      }
+    context 'with invalid params' do
+      before do
+        get :index, params: {
+          query: 'cl'
+        }
+      end
+
+      it 'responds with 400 status' do
+        expect(json_response['errors']).to eq(
+          [{ 'status' => 400, 'title' => 'BAD REQUEST', 'detail' => 'Unknown school name, urn or postcode, please check the query string.' }]
+        )
+      end
     end
 
-    it 'responds with a SchoolSuggestionListResponse' do
-      expect(json_response[0]['id']).to eql(site1.id)
-      expect(json_response[0]['name']).to eql(site1.name)
-      expect(json_response[1]['id']).to eql(site2.id)
-      expect(json_response[1]['name']).to eql(site2.name)
+    context 'with valid params' do
+      before do
+        get :index, params: {
+          query: 'cla'
+        }
+      end
+
+      it 'responds with a SchoolSuggestionListResponse' do
+        expect(json_response[0]['id']).to eql(site1.id)
+        expect(json_response[0]['name']).to eql(site1.name)
+        expect(json_response[1]['id']).to eql(site2.id)
+        expect(json_response[1]['name']).to eql(site2.name)
+      end
     end
   end
 end
