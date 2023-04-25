756a72e23f2d324a6a3cee4cce37c58fc741b626
David Kewal <davidrk@hey.com>
2023-04-25 11:58:22 +0100

    [xxx] Fix specs

--- app/javascript/utils/request_helper.spec.js
+++ app/javascript/utils/request_helper.spec.js
@@ -2,20 +2,19 @@
  * @jest-environment jsdom
  */
 
-import { request, getPath } from "./request_helper";
+import { request, getPath } from './request_helper'
 
-describe("Request helpers", () => {
+describe('Request helpers', () => {
+  describe('getPath', () => {
+    it('should return a path', () => {
+      const path = getPath('/endpoint', 'queryString')
+      expect(path).toBe('/endpoint?query=queryString')
+    })
+  })
 
-  describe("getPath", () => {
-    it("should return a path", () => {
-      const path = getPath("/endpoint", "queryString");
-      expect(path).toBe("/endpoint?query=queryString");
-    });
-  });
-
-  describe("request", () => {
-    let requestFn;
-    const abortMock = jest.fn();
+  describe('request', () => {
+    let requestFn
+    const abortMock = jest.fn()
 
     beforeEach(() => {
       global.XMLHttpRequest = jest.fn(() => ({
@@ -23,43 +22,43 @@ describe("Request helpers", () => {
         addEventListener: (_, cb) => cb(),
         open: jest.fn(),
         send: jest.fn(),
-        responseText: "[]",
-        readyState: 2,
-      }));
-      requestFn = request("/endpoint");
-    });
+        responseText: '[]',
+        readyState: 2
+      }))
+      requestFn = request('/endpoint')
+    })
 
-    it("should return a function", () => {
-      expect(typeof requestFn).toBe("function");
-    });
+    it('should return a function', () => {
+      expect(typeof requestFn).toBe('function')
+    })
 
-    describe("when called", () => {
-      const cb = jest.fn();
-
-      beforeEach(() => {
-        requestFn("foo", cb);
-      });
-
-      it("should perform an ajax request", () => {
-        expect(XMLHttpRequest).toBeCalled();
-      });
-
-      it("should invoke callback", () => {
-        expect(cb).toBeCalled();
-      });
-    });
-
-    describe("when called with a pending request", () => {
-      const cb = jest.fn();
+    describe('when called', () => {
+      const cb = jest.fn()
 
       beforeEach(() => {
-        requestFn("foo", cb);
-        requestFn("bar", cb);
-      });
+        requestFn('foo', cb)
+      })
 
-      it("should abort a request", () => {
-        expect(abortMock).toBeCalled();
-      });
-    });
-  });
-});
+      it('should perform an ajax request', () => {
+        expect(XMLHttpRequest).toBeCalled()
+      })
+
+      it('should invoke callback', () => {
+        expect(cb).toBeCalled()
+      })
+    })
+
+    describe('when called with a pending request', () => {
+      const cb = jest.fn()
+
+      beforeEach(() => {
+        requestFn('foo', cb)
+        requestFn('bar', cb)
+      })
+
+      it('should abort a request', () => {
+        expect(abortMock).toBeCalled()
+      })
+    })
+  })
+})
