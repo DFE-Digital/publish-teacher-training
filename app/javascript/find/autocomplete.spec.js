6d51403dbc4a75b0804f390e9f8702b7a4e7a99e
David Kewal <davidrk@hey.com>
2023-04-25 11:40:22 +0100

    [xxx] Turn on Gias search for prod

--- app/javascript/find/autocomplete.spec.js
+++ app/javascript/find/autocomplete.spec.js
@@ -2,7 +2,7 @@
  * @jest-environment jsdom
  */
 
-import initAutocomplete, { getPath, request } from './autocomplete'
+import initAutocomplete from './autocomplete'
 
 describe('Autocomplete', () => {
   describe('initAutocomplete', () => {
@@ -25,63 +25,6 @@ describe('Autocomplete', () => {
 
     it('should instantiate an autocomplete', () => {
       expect(document.querySelector('#outer-container')).toMatchSnapshot()
-    })
-  })
-
-  describe('getPath', () => {
-    it('should return a path', () => {
-      const path = getPath('/endpoint', 'queryString')
-      expect(path).toBe('/endpoint?query=queryString')
-    })
-  })
-
-  describe('request', () => {
-    let requestFn
-    const abortMock = jest.fn()
-
-    beforeEach(() => {
-      global.XMLHttpRequest = jest.fn(() => ({
-        abort: abortMock,
-        addEventListener: (_, cb) => cb(),
-        open: jest.fn(),
-        send: jest.fn(),
-        responseText: '[]',
-        readyState: 2
-      }))
-      requestFn = request('/endpoint')
-    })
-
-    it('should return a function', () => {
-      expect(typeof requestFn).toBe('function')
-    })
-
-    describe('when called', () => {
-      const cb = jest.fn()
-
-      beforeEach(() => {
-        requestFn('foo', cb)
-      })
-
-      it('should perform an ajax request', () => {
-        expect(XMLHttpRequest).toBeCalled()
-      })
-
-      it('should invoke callback', () => {
-        expect(cb).toBeCalled()
-      })
-    })
-
-    describe('when called with a pending request', () => {
-      const cb = jest.fn()
-
-      beforeEach(() => {
-        requestFn('foo', cb)
-        requestFn('bar', cb)
-      })
-
-      it('should abort a request', () => {
-        expect(abortMock).toBeCalled()
-      })
     })
   })
 })
