28fa3608d95600099969d156806c24fe2ec41fde
Max Mills <8balldigitalsolutions@gmail.com>
2023-04-26 11:14:10 +0100

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_20_162940) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_24_160722) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "btree_gin"
   enable_extension "btree_gist"
