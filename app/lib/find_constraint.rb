5db0b46094339f8c398c402e59458cc6eafe8610
smneal <smneal@users.noreply.github.com>
2023-05-03 16:30:51 +0100

    Add ftt

--- app/lib/find_constraint.rb
+++ app/lib/find_constraint.rb
@@ -2,6 +2,6 @@
 
 class FindConstraint
   def matches?(request)
-    Settings.find_url&.include?(request.host) || request.host.include?('find-pr') || request.host.include?('ptt-review')
+    Settings.find_url&.include?(request.host) || request.host.include?('find-pr') || request.host.include?('ftt-review')
   end
 end
