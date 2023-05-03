5ce559f431e6a1adf5051951c363caeb2250e3e4
smneal <smneal@users.noreply.github.com>
2023-05-03 16:21:38 +0100

    Add find constraint

--- app/lib/find_constraint.rb
+++ app/lib/find_constraint.rb
@@ -2,6 +2,6 @@
 
 class FindConstraint
   def matches?(request)
-    Settings.find_url&.include?(request.host) || request.host.include?('find-pr')
+    Settings.find_url&.include?(request.host) || request.host.include?('find-pr') || request.host.include?('ptt-review')
   end
 end
