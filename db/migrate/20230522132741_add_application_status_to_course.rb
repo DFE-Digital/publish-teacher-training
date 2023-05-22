56b47f8455d9c8b046b8df4cce0dda9590c0e48b
avinhurry <avin.hurry@digital.education.gov.uk>
2023-05-22 14:45:05 +0100

    [1283] Rubocop correction

--- db/migrate/20230522132741_add_application_status_to_course.rb
+++ db/migrate/20230522132741_add_application_status_to_course.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddApplicationStatusToCourse < ActiveRecord::Migration[7.0]
   def change
     add_column :course, :application_status, :integer, default: 0, null: false
