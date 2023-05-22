e8c31af95d65c2831d8ee8db7ad357115b9dd782
avinhurry <avin.hurry@digital.education.gov.uk>
2023-05-22 14:29:52 +0100

    [1283] Add `application_status` to course
    - Add column to analytics.yml

---
+++ db/migrate/20230522132741_add_application_status_to_course.rb
@@ -0,0 +1,6 @@
+class AddApplicationStatusToCourse < ActiveRecord::Migration[7.0]
+  def change
+    add_column :course, :application_status, :integer, default: 0, null: false
+    add_index :course, :application_status
+  end
+end
