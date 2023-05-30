eb70f7a534a6ce763d4e7f26118fb09042770363
smneal <smneal@users.noreply.github.com>
2023-05-30 19:40:01 +0100

    Add review_aks, dv_review_aks tf and make config
    Add the terraform tfvars files for the review_aks and dv_review_aks envs
    Add the global config files used by the Makefile commands

---
+++ global_config/dv_review_aks.sh
@@ -0,0 +1,8 @@
+CONFIG=dv_review_aks
+DEPLOY_ENV=${CONFIG}
+CONFIG_SHORT=rv
+AZ_SUBSCRIPTION=s189-teacher-services-cloud-development
+RESOURCE_NAME_PREFIX=s189d01
+ENV_TAG=Dev
+PLATFORM=aks
+CLUSTER_SHORT=dv
---
+++ terraform/aks/workspace_variables/dv_review_aks_backend.tfvars
@@ -0,0 +1,3 @@
+resource_group_name  = "s189d01-ptt-rv-rg"
+storage_account_name = "s189d01ptttfstatervsa"
+container_name       = "ptt-tfstate"
