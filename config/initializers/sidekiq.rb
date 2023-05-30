3fde987bc6a295e2eb037110f4988b92f4c07df6
smneal <smneal@users.noreply.github.com>
2023-05-30 19:45:35 +0100

    Ruby updates

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-if ENV.key?('VCAP_SERVICES')
+if ENV.key?('VCAP_SERVICES') || ENV.key?('REDIS_WORKER_URL')
   Sidekiq.configure_server do |config|
     config.redis = {
       url: ENV.fetch('REDIS_WORKER_URL')
