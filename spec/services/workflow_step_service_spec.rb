cb7d9d72e30ef31fafd3fea272853ae78b5b56ec
Shujat Khalid <khalidshujat@gmail.com>
2023-05-30 12:13:33 +0100

    refactored workflow_step_service and added additional tests

--- spec/services/workflow_step_service_spec.rb
+++ spec/services/workflow_step_service_spec.rb
@@ -3,93 +3,121 @@
 require 'rails_helper'
 
 describe WorkflowStepService do
-  let(:accredited_provider) { build(:provider, :accredited_provider) }
-
-  let(:provider) do
-    build(
-      :provider,
-      :lead_school,
-      provider_name: 'Provider 1',
-      accrediting_provider_enrichments: [{
-        'Description' => 'Something great about the accredited provider',
-        'UcasProviderCode' => accredited_provider.provider_code
-      }]
-    )
-  end
-  
   subject do
     described_class.call(course)
-    end
+  end
 
   describe '#call' do
-    context 'when course.provider.lead_school? && course.provider.accredited_bodies.length == 1' do
-    let(:course) { create(:course, accrediting_provider: accredited_provider, provider: provider) }
+    context 'when course.is_school_direct? && when course.provider.accredited_bodies.length == 0' do
+      let(:provider) do
+        build(
+          :provider,
+          accrediting_provider_enrichments:
+        )
+      end
+      let(:course) { create(:course, accrediting_provider: accredited_provider, provider:) }
+      let(:accrediting_provider_enrichments) { nil }
+      let(:accredited_provider) { nil }
+
       it 'returns the expected workflow steps' do
-        expected_steps = [
-          :courses_list,
-          :level,
-          :subjects,
-          :engineers_teach_physics,
-          :modern_languages,
-          :age_range,
-          :outcome,
-          :funding_type,
-          :full_or_part_time,
-          :school,
-          :can_sponsor_student_visa,
-          :can_sponsor_skilled_worker_visa,
-          :applications_open,
-          :start_date,
-          :confirmation
+        expected_steps = %i[
+          courses_list
+          level
+          subjects
+          engineers_teach_physics
+          modern_languages
+          age_range
+          outcome
+          funding_type
+          full_or_part_time
+          school
+          accredited_provider
+          can_sponsor_skilled_worker_visa
+          applications_open
+          start_date
+          confirmation
         ]
 
         expect(subject).to eq(expected_steps)
       end
     end
 
-    # context 'when course.is_further_education?' do
-    # let(:course) { create(:course, :further_education) }
-    # it 'returns the expected workflow steps' do
-    #     expected_steps = [
-    #       :courses_list,
-    #       :level,
-    #       :outcome,
-    #       :full_or_part_time,
-    #       :school,
-    #       :applications_open,
-    #       :start_date,
-    #       :confirmation
-    #     ]
+    context 'when course.is_school_direct? && course.provider.accredited_bodies.length == 1' do
+      let(:provider) do
+        build(
+          :provider,
+          accrediting_provider_enrichments:
+        )
+      end
+      let(:course) { create(:course, accrediting_provider: accredited_provider, provider:) }
+      let(:accrediting_provider_enrichments) { [{ 'Description' => 'Something great about the accredited provider', 'UcasProviderCode' => accredited_provider.provider_code }] }
+      let(:accredited_provider) { build(:provider, :accredited_provider) }
 
-    #     expect(subject).to eq(expected_steps)
-    #   end        
-    # end
+      it 'returns the expected workflow steps' do
+        expected_steps = %i[
+          courses_list
+          level
+          subjects
+          engineers_teach_physics
+          modern_languages
+          age_range
+          outcome
+          funding_type
+          full_or_part_time
+          school
+          can_sponsor_skilled_worker_visa
+          applications_open
+          start_date
+          confirmation
+        ]
 
-    # context 'when course.is_uni_or_scitt?' do
-    #     binding.pry
-    #     let(:course) { create(:course, :uni_or_scitt) }
-  
-    #     it 'returns the expected workflow steps' do
-    #       expected_steps = [
-    #         :courses_list,
-    #         :level,
-    #         :subjects,
-    #         :engineers_teach_physics,
-    #         :modern_languages,
-    #         :age_range,
-    #         :outcome,
-    #         :apprenticeship,
-    #         :full_or_part_time,
-    #         :school,
-    #         :can_sponsor_student_visa,
-    #         :can_sponsor_skilled_worker_visa,
-    #         :applications_open,
-    #         :start_date,
-    #         :confirmation
-    #       ]
-  
-    #       expect(subject).to eq(expected_steps)
-    #     end
-    #   end
+        expect(subject).to eq(expected_steps)
+      end
+    end
+  end
+
+  context 'when course.is_further_education?' do
+    let(:course) { create(:course, level: 'further_education', subjects: [find_or_create(:further_education_subject)]) }
+
+    it 'returns the expected workflow steps' do
+      expected_steps = %i[
+        courses_list
+        level
+        outcome
+        full_or_part_time
+        school
+        applications_open
+        start_date
+        confirmation
+      ]
+
+      expect(subject).to eq(expected_steps)
+    end
+  end
+
+  context 'when course.is_uni_or_scitt?' do
+    let(:provider) { create(:provider, :accredited_provider) }
+    let(:course) { create(:course, provider:) }
+
+    it 'returns the expected workflow steps' do
+      expected_steps = %i[
+        courses_list
+        level
+        subjects
+        engineers_teach_physics
+        modern_languages
+        age_range
+        outcome
+        apprenticeship
+        full_or_part_time
+        school
+        can_sponsor_skilled_worker_visa
+        applications_open
+        start_date
+        confirmation
+      ]
+
+      expect(subject).to eq(expected_steps)
+    end
   end
 end
