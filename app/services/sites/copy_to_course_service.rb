75c020ad2de4af27af0a9342749386d331d99496
defong <de_fong2k@yahoo.com>
2023-06-07 19:24:15 +0100

    [1332] Remove `vac_status` from CopyToCourseService

--- app/services/sites/copy_to_course_service.rb
+++ app/services/sites/copy_to_course_service.rb
@@ -3,13 +3,8 @@
 module Sites
   class CopyToCourseService
     def execute(new_site:, new_course:)
-      new_vac_status = SiteStatus.default_vac_status_given(
-        study_mode: new_course.study_mode
-      )
-
       new_course.site_statuses.create(
         site: new_site,
-        vac_status: new_vac_status,
         status: :new_status
       )
     end
