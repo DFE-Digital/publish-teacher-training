5576a4d25c531c93bf972a0c6f0d89c3d88e1f35
defong <de_fong2k@yahoo.com>
2023-06-08 08:44:32 +0100

    [1332] Remove `vac_status` from migrations

--- db/migrate/20191007125535_mark_withdrawn_courses_as_withdrawn.rb
+++ db/migrate/20191007125535_mark_withdrawn_courses_as_withdrawn.rb
@@ -3,15 +3,15 @@
 class MarkWithdrawnCoursesAsWithdrawn < ActiveRecord::Migration[6.0]
   def up
     say_with_time 'Marking all courses with site statuses that have been suspended with no vacancies' do
-      courses = Course.where(site_statuses: SiteStatus.where(vac_status: 'no_vacancies', status: 'suspended'))
+      courses = Course.where(site_statuses: SiteStatus.where(status: 'suspended'))
       published_courses = courses.select(&:is_published?)
-      courses_with_all_suspended_no_vacancy_sites = published_courses.select do |course|
+      courses_with_all_suspended_sites = published_courses.select do |course|
         course.site_statuses.all? do |site_status|
-          site_status.vac_status == 'no_vacancies' && site_status.status == 'suspended'
+          site_status.status == 'suspended'
         end
       end
 
-      courses_with_all_suspended_no_vacancy_sites.each do |course|
+      courses_with_all_suspended_sites.each do |course|
         course.enrichments.most_recent.first.withdraw
       end
     end
--- db/migrate/20200131104849_add_site_status_indexes.rb
+++ db/migrate/20200131104849_add_site_status_indexes.rb
@@ -6,6 +6,5 @@ class AddSiteStatusIndexes < ActiveRecord::Migration[6.0]
   def change
     add_index :course_site, :publish, algorithm: :concurrently
     add_index :course_site, :status, algorithm: :concurrently
-    add_index :course_site, :vac_status, algorithm: :concurrently
   end
 end
