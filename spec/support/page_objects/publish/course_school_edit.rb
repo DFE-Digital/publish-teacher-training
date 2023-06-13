d41e97008775f4e572229ff0d8fe872063d3b0da
defong <de_fong2k@yahoo.com>
2023-06-13 13:22:56 +0100

    [1332] Amend school vs vacancy

--- spec/support/page_objects/publish/course_school_edit.rb
+++ spec/support/page_objects/publish/course_school_edit.rb
@@ -7,16 +7,12 @@ module PageObjects
     class CourseSchoolEdit < PageObjects::Base
       set_url '/publish/organisations/{provider_code}/{recruitment_cycle_year}/courses/{course_code}/schools'
 
-      sections :vacancies, Sections::Vacancy, '.govuk-checkboxes__item'
+      sections :schools, Sections::EditSchool, '.govuk-checkboxes__item'
 
       element :submit, 'button.govuk-button[type="submit"]'
 
-      def vacancy_names
-        vacancies.map { |el| el.find('.govuk-label').text }
-      end
-
-      def vacancy_checked_values
-        vacancies.map(&:checked?)
+      def school_names
+        schools.map { |el| el.find('.govuk-label').text }
       end
     end
   end
