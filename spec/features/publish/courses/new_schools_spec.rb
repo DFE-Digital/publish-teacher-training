033c0c6dd5667a081f9dfc05ee531428e6e93cd5
Max Mills <8balldigitalsolutions@gmail.com>
2023-06-15 18:21:55 +0100

    new school spec passing

--- spec/features/publish/courses/new_schools_spec.rb
+++ spec/features/publish/courses/new_schools_spec.rb
@@ -49,8 +49,8 @@ feature 'selection schools', { can_edit_current_and_next_cycles: false } do
   end
 
   def then_i_am_met_with_the_accredited_provider_page
-    expect(page).to have_current_path("/publish/organisations/#{provider.provider_code}/#{Settings.current_recruitment_cycle_year}/courses/accredited-provider/new", ignore_query: true)
-    expect(page).to have_content('Accredited provider')
+    expect(page).to have_current_path("/publish/organisations/#{provider.provider_code}/#{Settings.current_recruitment_cycle_year}/courses/study-sites/new", ignore_query: true)
+    expect(page).to have_content('Study sites')
   end
 
   def then_i_am_met_with_errors
