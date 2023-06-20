8a5b4ae0f1d630ad3195a212f3ec1d6651578e75
avinhurry <avin.hurry@digital.education.gov.uk>
2023-06-20 20:09:03 +0100

    [19] Add updating logic

--- app/controllers/publish/courses/study_sites_controller.rb
+++ app/controllers/publish/courses/study_sites_controller.rb
@@ -18,22 +18,16 @@ module Publish
         redirect_to next_step
       end
 
-      # TODO: will need updating
       def edit
         authorize(provider)
-
-        @course_school_form = CourseSchoolForm.new(@course)
-        @course_school_form.valid? if show_errors_on_publish?
       end
 
-      # TODO: will need updating
       def update
         authorize(provider)
 
-        @course_school_form = CourseSchoolForm.new(@course, params: school_params)
-        if @course_school_form.save!
-          course_updated_message(section_key)
+        update_course_study_sites
 
+        if course.save
           redirect_to details_publish_provider_recruitment_cycle_course_path(
             provider.provider_code,
             recruitment_cycle.year,
@@ -44,42 +38,28 @@ module Publish
         end
       end
 
-      def back
-        authorize(@provider, :edit?)
-        if @provider.study_sites.count > 1
-          redirect_to new_publish_provider_recruitment_cycle_courses_study_sites_path(path_params)
-        else
-          redirect_to @back_link_path
+      private
+
+      def update_course_study_sites
+        study_site_ids = params[:course][:study_sites_ids]
+
+        @study_sites ||= provider.study_sites.find(study_site_ids.compact_blank!)
+
+        course.study_sites.delete(course.study_sites.reject { |study_site| study_site_ids.include?(study_site.id.to_s) })
+
+        study_site_ids.each do |study_site_id|
+          study_site = @study_sites.find { |site| site.id.to_s == study_site_id }
+          course.study_sites << study_site unless course.study_sites.include?(study_site)
         end
       end
-
-      private
 
       def current_step
         :study_site
       end
 
-      def error_keys
-        [:study_sites]
-      end
-
       def set_default_study_site
         params['course'] ||= {}
         params['course']['study_sites_ids'] = [@provider.study_sites.first.id]
-      end
-
-      def school_params
-        return { study_site_ids: nil } if params[:publish_course_school_form][:study_site_ids].all?(&:empty?)
-
-        params.require(:publish_course_school_form).permit(study_site_ids: [])
-      end
-
-      def build_course
-        @course = provider.courses.find_by!(course_code: params[:code])
-      end
-
-      def section_key
-        'Study site'.pluralize(school_params[:study_site_ids].compact_blank.count)
       end
     end
   end
