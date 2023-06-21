6ebfd6e75e6b1f4a72b84ab52f74f944cf97029e
avinhurry <avin.hurry@digital.education.gov.uk>
2023-06-21 16:04:04 +0100

    [19] Refactor

--- app/controllers/publish/courses/study_sites_controller.rb
+++ app/controllers/publish/courses/study_sites_controller.rb
@@ -12,7 +12,7 @@ module Publish
 
       def new
         authorize(@provider, :edit?)
-        return unless @provider.study_sites.count == 1
+        return unless @provider.study_sites.one?
 
         set_default_study_site
         redirect_to next_step
@@ -42,15 +42,9 @@ module Publish
 
       def update_course_study_sites
         study_site_ids = params[:course][:study_sites_ids]
-
         @study_sites ||= provider.study_sites.find(study_site_ids.compact_blank!)
 
-        course.study_sites.delete(course.study_sites.reject { |study_site| study_site_ids.include?(study_site.id.to_s) })
-
-        study_site_ids.each do |study_site_id|
-          study_site = @study_sites.find { |site| site.id.to_s == study_site_id }
-          course.study_sites << study_site unless course.study_sites.include?(study_site)
-        end
+        course.study_sites = @study_sites.select { |site| study_site_ids.include?(site.id.to_s) }
       end
 
       def current_step
