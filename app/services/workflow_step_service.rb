563fc220615c5f79825c53d677286cf5092addb6
avinhurry <avin.hurry@digital.education.gov.uk>
2023-07-03 20:38:18 +0100

    [283] Refactor workflow step service

--- app/services/workflow_step_service.rb
+++ app/services/workflow_step_service.rb
@@ -8,29 +8,11 @@ class WorkflowStepService
   end
 
   def call
-    if course.recruitment_cycle_after_2023? # This can be removed after the 2023 cycle has ended
-      if course.is_further_education?
-        next_cycle_further_education_workflow_steps - remove_study_site_if_feature_flag_disabled_or_current_cycle(course)
-      elsif course.is_uni_or_scitt?
-        next_cycle_uni_or_scitt_workflow_steps - visas_to_remove(course) - remove_study_site_if_feature_flag_disabled_or_current_cycle(course)
-      elsif course.is_school_direct?
-        if course.provider.accredited_bodies.length == 1
-          school_direct_workflow_steps - (visas_to_remove(course) + [:accredited_provider]) - remove_study_site_if_feature_flag_disabled_or_current_cycle(course)
-        else
-          school_direct_workflow_steps - remove_study_site_if_feature_flag_disabled_or_current_cycle(course) - visas_to_remove(course)
-        end
-      end
-    elsif course.is_further_education?
-      further_education_workflow_steps - remove_study_site_if_feature_flag_disabled_or_current_cycle(course)
-    elsif course.is_uni_or_scitt?
-      uni_or_scitt_workflow_steps - visas_to_remove_2023cycle(course) - remove_study_site_if_feature_flag_disabled_or_current_cycle(course)
-    elsif course.is_school_direct?
-      if course.provider.accredited_bodies.length == 1
-        school_direct_workflow_steps - (visas_to_remove_2023cycle(course) + [:accredited_provider]) - remove_study_site_if_feature_flag_disabled_or_current_cycle(course)
-      else
-        school_direct_workflow_steps - remove_study_site_if_feature_flag_disabled_or_current_cycle(course) - visas_to_remove_2023cycle(course)
-      end
-    end
+    if course.recruitment_cycle_after_2023?
+      workflow_for_recruitment_cycle_after2023
+    else
+      workflow_for_recruitment_cycle_before2023
+    end - remove_study_site_if_feature_flag_disabled_or_current_cycle(course)
   end
 
   private
@@ -135,6 +117,34 @@ class WorkflowStepService
       [:can_sponsor_skilled_worker_visa]
     else
       [:can_sponsor_student_visa]
+    end
+  end
+
+  def workflow_for_recruitment_cycle_after2023
+    if course.is_further_education?
+      next_cycle_further_education_workflow_steps
+    elsif course.is_uni_or_scitt?
+      next_cycle_uni_or_scitt_workflow_steps - visas_to_remove(course)
+    elsif course.is_school_direct?
+      school_direct_workflow_steps_with_accredited_provider_check - visas_to_remove(course)
+    end
+  end
+
+  def workflow_for_recruitment_cycle_before2023
+    if course.is_further_education?
+      further_education_workflow_steps
+    elsif course.is_uni_or_scitt?
+      uni_or_scitt_workflow_steps - visas_to_remove_2023cycle(course)
+    elsif course.is_school_direct?
+      school_direct_workflow_steps_with_accredited_provider_check - visas_to_remove_2023cycle(course)
+    end
+  end
+
+  def school_direct_workflow_steps_with_accredited_provider_check
+    if course.provider.accredited_bodies.length == 1
+      school_direct_workflow_steps - [:accredited_provider]
+    else
+      school_direct_workflow_steps
     end
   end
 
