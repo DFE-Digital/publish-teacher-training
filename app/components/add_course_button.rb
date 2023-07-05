3700179be8fe802c00d7e342fc5f74d2d0a3129b
avinhurry <avin.hurry@digital.education.gov.uk>
2023-07-05 13:18:42 +0100

    [318] Hide the add course button under the following conditions:
    - If the provider doesn't have any accredited providers attached and the provider is not an accredited provider
    - If the provider doesn't have any schools
    - If the provider doesn't have any study sites
    - Link to these sections if the add course button is hidden

---
+++ app/components/add_course_button.rb
@@ -0,0 +1,80 @@
+# frozen_string_literal: true
+
+class AddCourseButton < ViewComponent::Base
+  include RecruitmentCycleHelper
+  attr_reader :provider
+
+  def initialize(provider:)
+    @provider = provider
+    super
+  end
+
+  private
+
+  def incomplete_sections_hash
+    {
+      study_site_not_present_and_feature_active?: publish_provider_recruitment_cycle_study_sites_path(provider.provider_code, provider.recruitment_cycle_year),
+      accredited_provider_not_present?: publish_provider_recruitment_cycle_accredited_providers_path(provider.provider_code, provider.recruitment_cycle_year),
+      site_not_present?: publish_provider_recruitment_cycle_schools_path(provider.provider_code, provider.recruitment_cycle_year)
+    }
+  end
+
+  def incomplete_section_label_suffix(section)
+    labels = {
+      study_site_not_present_and_feature_active?: 'study site',
+      accredited_provider_not_present?: 'accredited provider',
+      site_not_present?: 'school'
+    }
+
+    labels[section]
+  end
+
+  def required_organisation_details_present?
+    if study_sites_active?
+      study_site_present? &&
+        accredited_provider_present? &&
+        site_present?
+    else
+      accredited_provider_present? &&
+        site_present?
+    end
+  end
+
+  def study_site_present?
+    provider.study_sites.any?
+  end
+
+  def accredited_provider_present?
+    provider.accredited_providers.any?
+  end
+
+  def site_present?
+    provider.sites.any?
+  end
+
+  def study_site_not_present_and_feature_active?
+    !study_site_present? && study_sites_active?
+  end
+
+  def accredited_provider_not_present?
+    return if provider.accredited_provider?
+
+    !accredited_provider_present?
+  end
+
+  def site_not_present?
+    !site_present?
+  end
+
+  def not_accredited_provider?
+    !provider.accredited_provider?
+  end
+
+  def study_sites_active?
+    FeatureService.enabled?(:study_sites)
+  end
+
+  def incomplete_section_article(section)
+    incomplete_section_label_suffix(section) == 'accredited provider' ? 'an' : 'a'
+  end
+end
