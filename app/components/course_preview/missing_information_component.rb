5e68903d18b7865680824cb8abfdaf429c8eaa37
avinhurry <avin.hurry@digital.education.gov.uk>
2023-07-20 13:02:11 +0100

    Use anchor tags to help with readability

--- app/components/course_preview/missing_information_component.rb
+++ app/components/course_preview/missing_information_component.rb
@@ -35,18 +35,18 @@ module CoursePreview
     def degree_link = degrees_start_publish_provider_recruitment_cycle_course_path(provider_code, recruitment_cycle_year, course_code, goto_preview: true)
     def fee_uk_eu_link = fees_publish_provider_recruitment_cycle_course_path(provider_code, recruitment_cycle_year, course_code, goto_preview: true)
     def gcse_link = gcses_pending_or_equivalency_tests_publish_provider_recruitment_cycle_course_path(provider_code, recruitment_cycle_year, course_code, goto_preview: true)
-    def how_school_placements_work_link = "#{about_publish_provider_recruitment_cycle_course_path(provider_code, recruitment_cycle_year, course_code, goto_preview: true)}#how-school-placements-work"
-    def train_with_disability_link = "#{about_publish_provider_recruitment_cycle_path(provider_code, recruitment_cycle_year, course_code:, goto_preview: true)}#train-with-disability"
-    def train_with_us_link = "#{about_publish_provider_recruitment_cycle_path(provider_code, recruitment_cycle_year, course_code:, goto_preview: true)}#train-with-us"
+    def how_school_placements_work_link = about_publish_provider_recruitment_cycle_course_path(provider_code, recruitment_cycle_year, course_code, goto_preview: true, anchor: 'how-school-placements-work')
+    def train_with_disability_link = about_publish_provider_recruitment_cycle_path(provider_code, recruitment_cycle_year, course_code:, goto_preview: true, anchor: 'train-with-disability')
+    def train_with_us_link = about_publish_provider_recruitment_cycle_path(provider_code, recruitment_cycle_year, course_code:, goto_preview: true, anchor: 'train-with-us')
 
     def about_accrediting_provider_link
       if FeatureService.enabled?(:accredited_provider_search) && accrediting_provider_present?(course)
-        "#{edit_publish_provider_recruitment_cycle_accredited_provider_path(provider_code, recruitment_cycle_year, accredited_provider_code: @course.accredited_provider_code, goto_preview: true)}#accredited-provider-form-description-field"
+        edit_publish_provider_recruitment_cycle_accredited_provider_path(provider_code, recruitment_cycle_year, accredited_provider_code: @course.accredited_provider_code, goto_preview: true, anchor: 'accredited-provider-form-description-field')
       elsif FeatureService.enabled?(:accredited_provider_search) && !accrediting_provider_present?(course)
         publish_provider_recruitment_cycle_accredited_providers_path(provider_code,
                                                                      recruitment_cycle_year)
       else
-        "#{about_publish_provider_recruitment_cycle_path(provider_code, recruitment_cycle_year, course_code:, goto_preview: true)}#accrediting-provider-#{accrediting_provider.provider_code}"
+        about_publish_provider_recruitment_cycle_path(provider_code, recruitment_cycle_year, course_code:, goto_preview: true, anchor: "accrediting-provider-#{accrediting_provider.provider_code}")
       end
     end
   end
