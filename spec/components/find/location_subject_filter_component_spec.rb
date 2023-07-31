eed4f748786ba715710cb30d47aa0645c4e8c86d
avinhurry <avin.hurry@digital.education.gov.uk>
2023-07-31 11:25:22 +0100

    Autocorrect new RSpec/ReceiveMessages cop

--- spec/components/find/location_subject_filter_component_spec.rb
+++ spec/components/find/location_subject_filter_component_spec.rb
@@ -7,9 +7,7 @@ module Find
     it 'renders the correct summary when location is used' do
       results = instance_double(Find::ResultsView, location_filter?: true, provider_filter?: false, filter_params_for: '/')
 
-      allow(results).to receive(:filter_params_with_unescaped_commas).and_return('/')
-      allow(results).to receive(:location_search).and_return('Brighton')
-      allow(results).to receive(:subjects).and_return([{ subject_name: 'Art and design' }])
+      allow(results).to receive_messages(filter_params_with_unescaped_commas: '/', location_search: 'Brighton', subjects: [{ subject_name: 'Art and design' }])
 
       page = render_inline(described_class.new(results:))
 
@@ -20,8 +18,7 @@ module Find
     it 'renders the correct summary when england is used' do
       results = instance_double(Find::ResultsView, provider_filter?: false, england_filter?: true, location_filter?: false, filter_params_for: '/')
 
-      allow(results).to receive(:filter_params_with_unescaped_commas).and_return('/')
-      allow(results).to receive(:subjects).and_return([{ subject_name: 'Art and design' }])
+      allow(results).to receive_messages(filter_params_with_unescaped_commas: '/', subjects: [{ subject_name: 'Art and design' }])
 
       page = render_inline(described_class.new(results:))
 
@@ -32,9 +29,7 @@ module Find
     it 'renders the correct summary when provider is used' do
       results = instance_double(Find::ResultsView, provider_filter?: true, england_filter?: false, location_filter?: false, filter_params_for: '/')
 
-      allow(results).to receive(:filter_params_with_unescaped_commas).and_return('/')
-      allow(results).to receive(:subjects).and_return([{ subject_name: 'Art and design' }])
-      allow(results).to receive(:provider).and_return('University of Brighton')
+      allow(results).to receive_messages(filter_params_with_unescaped_commas: '/', subjects: [{ subject_name: 'Art and design' }], provider: 'University of Brighton')
 
       page = render_inline(described_class.new(results:))
 
@@ -45,9 +40,7 @@ module Find
     it 'renders two courses correctly' do
       results = instance_double(Find::ResultsView, location_filter?: true, provider_filter?: false, filter_params_for: '/')
 
-      allow(results).to receive(:filter_params_with_unescaped_commas).and_return('/')
-      allow(results).to receive(:location_search).and_return('Brighton')
-      allow(results).to receive(:subjects).and_return([{ subject_name: 'Art and design' }, { subject_name: 'Maths' }])
+      allow(results).to receive_messages(filter_params_with_unescaped_commas: '/', location_search: 'Brighton', subjects: [{ subject_name: 'Art and design' }, { subject_name: 'Maths' }])
 
       page = render_inline(described_class.new(results:))
 
@@ -58,9 +51,7 @@ module Find
     it 'renders three courses correctly' do
       results = instance_double(Find::ResultsView, provider_filter?: false, location_filter?: true, filter_params_for: '/')
 
-      allow(results).to receive(:filter_params_with_unescaped_commas).and_return('/')
-      allow(results).to receive(:location_search).and_return('Brighton')
-      allow(results).to receive(:subjects).and_return([{ subject_name: 'Art and design' }, { subject_name: 'Maths' }, { subject_name: 'English' }])
+      allow(results).to receive_messages(filter_params_with_unescaped_commas: '/', location_search: 'Brighton', subjects: [{ subject_name: 'Art and design' }, { subject_name: 'Maths' }, { subject_name: 'English' }])
 
       page = render_inline(described_class.new(results:))
 
--- spec/services/find/result_summary_service_spec.rb
+++ spec/services/find/result_summary_service_spec.rb
@@ -8,9 +8,7 @@ module Find
       it 'renders the correct summary when location is used' do
         results = instance_double(ResultsView, location_filter?: true, england_filter?: false, provider_filter?: false)
 
-        allow(results).to receive(:location_search).and_return('Brighton')
-        allow(results).to receive(:subjects).and_return([{ subject_name: 'Art and design' }])
-        allow(results).to receive(:course_count).and_return(0)
+        allow(results).to receive_messages(location_search: 'Brighton', subjects: [{ subject_name: 'Art and design' }], course_count: 0)
 
         expect(described_class.new(results:).call).to include('No art and design courses found in Brighton')
       end
@@ -18,8 +16,7 @@ module Find
       it 'renders the correct summary when england is used' do
         results = instance_double(ResultsView, location_filter?: false, england_filter?: true, provider_filter?: false)
 
-        allow(results).to receive(:subjects).and_return([{ subject_name: 'Art and design' }])
-        allow(results).to receive(:course_count).and_return(0)
+        allow(results).to receive_messages(subjects: [{ subject_name: 'Art and design' }], course_count: 0)
 
         expect(described_class.new(results:).call).to include('No art and design courses found in England')
       end
@@ -27,9 +24,7 @@ module Find
       it 'renders the correct summary when provider is used' do
         results = instance_double(ResultsView, location_filter?: false, england_filter?: false, provider_filter?: true)
 
-        allow(results).to receive(:subjects).and_return([{ subject_name: 'Art and design' }])
-        allow(results).to receive(:provider).and_return('University of Brighton')
-        allow(results).to receive(:course_count).and_return(0)
+        allow(results).to receive_messages(subjects: [{ subject_name: 'Art and design' }], provider: 'University of Brighton', course_count: 0)
 
         expect(described_class.new(results:).call).to include('No art and design courses found from University of Brighton')
       end
@@ -39,9 +34,7 @@ module Find
       it 'renders the correct summary when location is used' do
         results = instance_double(ResultsView, location_filter?: true, england_filter?: false, provider_filter?: false)
 
-        allow(results).to receive(:location_search).and_return('Brighton')
-        allow(results).to receive(:subjects).and_return([{ subject_name: 'Art and design' }])
-        allow(results).to receive(:course_count).and_return(1)
+        allow(results).to receive_messages(location_search: 'Brighton', subjects: [{ subject_name: 'Art and design' }], course_count: 1)
 
         expect(described_class.new(results:).call).to include('1 art and design course in Brighton')
       end
@@ -49,8 +42,7 @@ module Find
       it 'renders the correct summary when england is used' do
         results = instance_double(ResultsView, location_filter?: false, england_filter?: true, provider_filter?: false)
 
-        allow(results).to receive(:subjects).and_return([{ subject_name: 'Art and design' }])
-        allow(results).to receive(:course_count).and_return(1)
+        allow(results).to receive_messages(subjects: [{ subject_name: 'Art and design' }], course_count: 1)
 
         expect(described_class.new(results:).call).to include('1 art and design course in England')
       end
@@ -58,9 +50,7 @@ module Find
       it 'renders the correct summary when provider is used' do
         results = instance_double(ResultsView, location_filter?: false, england_filter?: false, provider_filter?: true)
 
-        allow(results).to receive(:subjects).and_return([{ subject_name: 'Art and design' }])
-        allow(results).to receive(:provider).and_return('University of Brighton')
-        allow(results).to receive(:course_count).and_return(1)
+        allow(results).to receive_messages(subjects: [{ subject_name: 'Art and design' }], provider: 'University of Brighton', course_count: 1)
 
         expect(described_class.new(results:).call).to include('1 art and design course from University of Brighton')
       end
@@ -70,9 +60,7 @@ module Find
       it 'renders the correct summary when location is used' do
         results = instance_double(ResultsView, location_filter?: true, england_filter?: false, provider_filter?: false)
 
-        allow(results).to receive(:location_search).and_return('Brighton')
-        allow(results).to receive(:subjects).and_return([{ subject_name: 'Art and design' }])
-        allow(results).to receive(:course_count).and_return(2)
+        allow(results).to receive_messages(location_search: 'Brighton', subjects: [{ subject_name: 'Art and design' }], course_count: 2)
 
         expect(described_class.new(results:).call).to include('2 art and design courses in Brighton')
       end
@@ -80,8 +68,7 @@ module Find
       it 'renders the correct summary when england is used' do
         results = instance_double(ResultsView, location_filter?: false, england_filter?: true, provider_filter?: false)
 
-        allow(results).to receive(:subjects).and_return([{ subject_name: 'Art and design' }])
-        allow(results).to receive(:course_count).and_return(2)
+        allow(results).to receive_messages(subjects: [{ subject_name: 'Art and design' }], course_count: 2)
 
         expect(described_class.new(results:).call).to include('2 art and design courses in England')
       end
@@ -89,9 +76,7 @@ module Find
       it 'renders the correct summary when provider is used' do
         results = instance_double(ResultsView, location_filter?: false, england_filter?: false, provider_filter?: true)
 
-        allow(results).to receive(:subjects).and_return([{ subject_name: 'Art and design' }])
-        allow(results).to receive(:provider).and_return('University of Brighton')
-        allow(results).to receive(:course_count).and_return(2)
+        allow(results).to receive_messages(subjects: [{ subject_name: 'Art and design' }], provider: 'University of Brighton', course_count: 2)
 
         expect(described_class.new(results:).call).to include('2 art and design courses from University of Brighton')
       end
