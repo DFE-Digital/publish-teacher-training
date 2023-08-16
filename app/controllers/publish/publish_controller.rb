39e36d2d981ae74cca7e197e7131d573fd7b3314
avinhurry <avin.hurry@digital.education.gov.uk>
2023-08-16 10:00:26 +0100

    [321] Remove the interruption page
    This was not used this year as recruitment has been unrestricted for a year now. We won't need to surface this again.

--- app/controllers/publish/publish_controller.rb
+++ app/controllers/publish/publish_controller.rb
@@ -37,16 +37,10 @@ module Publish
     def check_interrupt_redirects(use_redirect_back_to: true)
       if !current_user.accepted_terms?
         redirect_to publish_accept_terms_path
-      elsif show_rollover_recruitment_page?
-        redirect_to publish_rollover_recruitment_path
       elsif use_redirect_back_to
         redirect_to session[:redirect_back_to] if session[:redirect_back_to].present?
         session.delete(:redirect_back_to)
       end
-    end
-
-    def show_rollover_recruitment_page?
-      FeatureService.enabled?('rollover.show_next_cycle_recruitment_page') && current_user.current_rollover_recruitment_acceptance.blank?
     end
 
     def clear_previous_cycle_year_in_session
