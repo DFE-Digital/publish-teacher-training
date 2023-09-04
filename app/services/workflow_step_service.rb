46895e13a88ae789836a881b218c2ee362334c7a
avinhurry <avin.hurry@digital.education.gov.uk>
2023-09-04 15:47:36 +0100

    Refactor workflow step service
    - Remove steps used before the 2024 cycle

--- app/services/workflow_step_service.rb
+++ app/services/workflow_step_service.rb
@@ -8,10 +8,13 @@ class WorkflowStepService
   end
 
   def call
-    if course.recruitment_cycle_after_2023?
-      workflow_for_recruitment_cycle_after2023
+    if course.is_school_direct?
+      school_direct_workflow_steps_with_accredited_provider_check - visas_to_remove(course)
+    elsif course.is_uni_or_scitt?
+      uni_or_scitt_workflow_steps - visas_to_remove(course)
     else
-      workflow_for_recruitment_cycle_before2023
+      course.is_further_education?
+      further_education_workflow_steps
     end
   end
 
@@ -24,6 +27,7 @@ class WorkflowStepService
       courses_list
       level
       outcome
+      funding_type
       full_or_part_time
       school
       study_site
@@ -33,15 +37,21 @@ class WorkflowStepService
     ]
   end
 
-  def next_cycle_further_education_workflow_steps
+  def uni_or_scitt_workflow_steps
     %i[
       courses_list
       level
+      subjects
+      engineers_teach_physics
+      modern_languages
+      age_range
       outcome
       funding_type
       full_or_part_time
       school
       study_site
+      can_sponsor_student_visa
+      can_sponsor_skilled_worker_visa
       applications_open
       start_date
       confirmation
@@ -70,46 +80,12 @@ class WorkflowStepService
     ]
   end
 
-  def uni_or_scitt_workflow_steps
-    %i[
-      courses_list
-      level
-      subjects
-      engineers_teach_physics
-      modern_languages
-      age_range
-      outcome
-      apprenticeship
-      full_or_part_time
-      school
-      study_site
-      can_sponsor_student_visa
-      can_sponsor_skilled_worker_visa
-      applications_open
-      start_date
-      confirmation
-    ]
-  end
-
-  def next_cycle_uni_or_scitt_workflow_steps
-    %i[
-      courses_list
-      level
-      subjects
-      engineers_teach_physics
-      modern_languages
-      age_range
-      outcome
-      funding_type
-      full_or_part_time
-      school
-      study_site
-      can_sponsor_student_visa
-      can_sponsor_skilled_worker_visa
-      applications_open
-      start_date
-      confirmation
-    ]
+  def school_direct_workflow_steps_with_accredited_provider_check
+    if course.provider.accredited_bodies.one?
+      school_direct_workflow_steps - [:accredited_provider]
+    else
+      school_direct_workflow_steps
+    end
   end
 
   def visas_to_remove(course)
@@ -117,46 +93,6 @@ class WorkflowStepService
       [:can_sponsor_skilled_worker_visa]
     else
       [:can_sponsor_student_visa]
-    end
-  end
-
-  def workflow_for_recruitment_cycle_after2023
-    if course.is_further_education?
-      next_cycle_further_education_workflow_steps
-    elsif course.is_uni_or_scitt?
-      next_cycle_uni_or_scitt_workflow_steps - visas_to_remove(course)
-    elsif course.is_school_direct?
-      school_direct_workflow_steps_with_accredited_provider_check - visas_to_remove(course)
-    end
-  end
-
-  def workflow_for_recruitment_cycle_before2023
-    if course.is_further_education?
-      further_education_workflow_steps
-    elsif course.is_uni_or_scitt?
-      uni_or_scitt_workflow_steps - visas_to_remove_2023cycle(course)
-    elsif course.is_school_direct?
-      school_direct_workflow_steps_with_accredited_provider_check - visas_to_remove_2023cycle(course)
-    end
-  end
-
-  def school_direct_workflow_steps_with_accredited_provider_check
-    if course.provider.accredited_bodies.length == 1
-      school_direct_workflow_steps - [:accredited_provider]
-    else
-      school_direct_workflow_steps
-    end
-  end
-
-  def visas_to_remove_2023cycle(course)
-    if course.funding_type.present?
-      if course.is_fee_based?
-        [:can_sponsor_skilled_worker_visa]
-      elsif course.school_direct_salaried_training_programme? || course.pg_teaching_apprenticeship?
-        [:can_sponsor_student_visa]
-      end
-    else
-      %i[can_sponsor_student_visa can_sponsor_skilled_worker_visa]
     end
   end
 end
