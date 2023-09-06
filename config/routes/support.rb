90db830f8c01c7ed66f8486038d29c3d59aa80e9
avinhurry <avin.hurry@digital.education.gov.uk>
2023-09-06 17:51:42 +0100

    [632] Remove old routes

--- config/routes/support.rb
+++ config/routes/support.rb
@@ -11,7 +11,7 @@ namespace :support do
       end
       resource :onboarding, only: %i[new create]
     end
-    resources :providers, except: %i[destroy] do
+    resources :providers, except: %i[new create destroy] do
       resource :contact_details, only: %i[edit update], path: 'contact-details'
       resource :check_user, only: %i[show update], controller: 'providers/users_check', path: 'users/check'
       resources :users, controller: 'providers/users' do
