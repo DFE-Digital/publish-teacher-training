94eb93ffe0d8e683bcc932c105b93367e5c02d15
avinhurry <avin.hurry@digital.education.gov.uk>
2023-09-07 15:04:05 +0100

    [636] Remove usages

--- app/helpers/navigation_bar_helper.rb
+++ app/helpers/navigation_bar_helper.rb
@@ -14,7 +14,7 @@ module NavigationBarHelper
       *([name: t('navigation_bar.study_sites'), url: publish_provider_recruitment_cycle_study_sites_path(provider.provider_code, provider.recruitment_cycle_year)] if recruitment_cycle_after_2023?(provider)),
       { name: t('navigation_bar.users'), url: publish_provider_users_path(provider_code: provider.provider_code), additional_url: request_access_publish_provider_path(provider.provider_code) },
       *([name: t('navigation_bar.training_partners'), url: publish_provider_recruitment_cycle_training_providers_path(provider.provider_code, provider.recruitment_cycle_year)] if provider.accredited_provider?),
-      *([name: t('navigation_bar.accredited_provider'), url: publish_provider_recruitment_cycle_accredited_providers_path(provider.provider_code, provider.recruitment_cycle_year)] unless provider.accredited_provider? && FeatureService.enabled?(:accredited_provider_search)),
+      *([name: t('navigation_bar.accredited_provider'), url: publish_provider_recruitment_cycle_accredited_providers_path(provider.provider_code, provider.recruitment_cycle_year)] unless provider.accredited_provider?),
       { name: t('navigation_bar.organisation_details'), url: details_publish_provider_recruitment_cycle_path(provider.provider_code, provider.recruitment_cycle_year) }
     ]
   end
