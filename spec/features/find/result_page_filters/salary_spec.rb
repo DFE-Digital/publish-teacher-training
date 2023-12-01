05b0382ad64bb21888d9a4004e9c75200c375354
Tomas D'Stefano <tomas_stefano@successoft.com>
2023-12-01 17:13:12 +0000

    Show all salaried program types in Find

--- spec/features/find/result_page_filters/salary_spec.rb
+++ spec/features/find/result_page_filters/salary_spec.rb
@@ -9,6 +9,7 @@ RSpec.feature 'Funding filter' do
   end
 
   scenario 'Candidate applies salary filter' do
+    given_i_have_salaried_and_fee_courses
     when_i_visit_the_find_results_page
     then_i_see_that_the_salary_checkbox_is_not_selected
 
@@ -16,6 +17,71 @@ RSpec.feature 'Funding filter' do
     and_apply_the_filters
     then_i_see_that_the_salary_checkbox_is_selected
     and_the_salary_query_parameter_is_retained
+    and_i_should_see_the_salaried_courses
+  end
+
+  def given_i_have_salaried_and_fee_courses
+    @course_higher_education_programme = create(
+      :course,
+      :secondary,
+      :open,
+      site_statuses:,
+      program_type: :higher_education_programme
+    )
+    @course_scitt_salaried_programme = create(
+      :course,
+      :secondary,
+      :open,
+      site_statuses:,
+      program_type: :scitt_salaried_programme
+    )
+    @course_higher_education_salaried_programme = create(
+      :course,
+      :secondary,
+      :open,
+      site_statuses:,
+      program_type: :higher_education_salaried_programme
+    )
+    @course_school_direct_training_programme = create(
+      :course,
+      :secondary,
+      :open,
+      site_statuses:,
+      program_type: :school_direct_training_programme
+    )
+    @course_school_direct_salaried_training_programme = create(
+      :course,
+      :secondary,
+      :open,
+      site_statuses:,
+      program_type: :school_direct_salaried_training_programme
+    )
+    @course_scitt_programme = create(
+      :course,
+      :secondary,
+      :open,
+      site_statuses:,
+      program_type: :scitt_programme
+    )
+    @course_pg_teaching_apprenticeship = create(
+      :course,
+      :secondary,
+      :open,
+      site_statuses:,
+      program_type: :pg_teaching_apprenticeship
+    )
+  end
+
+  def and_i_should_see_the_salaried_courses
+    [
+      @course_scitt_salaried_programme,
+      @course_higher_education_salaried_programme,
+      @course_school_direct_salaried_training_programme,
+      @course_pg_teaching_apprenticeship
+    ].each do |course|
+      expect(find_results_page).to have_content(course.decorate.name_and_code)
+      expect(find_results_page).to have_content(course.provider.provider_name)
+    end
   end
 
   def then_i_see_that_the_salary_checkbox_is_not_selected
@@ -35,5 +101,9 @@ RSpec.feature 'Funding filter' do
       expect(uri.path).to eq('/results')
       expect(uri.query).to eq('study_type[]=full_time&study_type[]=part_time&qualification[]=qts&qualification[]=pgce_with_qts&qualification[]=pgce+pgde&degree_required=show_all_courses&funding=salary&applications_open=true')
     end
+  end
+
+  def site_statuses
+    [create(:site_status, :findable)]
   end
 end
