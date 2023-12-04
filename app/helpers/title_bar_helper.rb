4d503bcfbd049617187277449c17fdd1c1408e2b
avinhurry <avin.hurry@digital.education.gov.uk>
2023-12-04 08:57:18 +0000

    Autocorrect Style/RedundantParentheses
    Don't use parentheses around a logical expression

--- app/helpers/title_bar_helper.rb
+++ app/helpers/title_bar_helper.rb
@@ -2,7 +2,7 @@
 
 module TitleBarHelper
   def render_title_bar?(current_user:, provider:)
-    current_user.has_multiple_providers_in_current_recruitment_cycle? || FeatureService.enabled?('rollover.can_edit_current_and_next_cycles') || ((current_user.admin? &&
-    provider))
+    current_user.has_multiple_providers_in_current_recruitment_cycle? || FeatureService.enabled?('rollover.can_edit_current_and_next_cycles') || (current_user.admin? &&
+    provider)
   end
 end
