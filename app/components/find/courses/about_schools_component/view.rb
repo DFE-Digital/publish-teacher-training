68ff9bc8f176a85e61fae95c898ca547d3c004e1
Iain McNulty <iain.mcnulty@education.gov.uk>
2024-01-15 16:31:06 +0000

    Find interface Course information guidance uses config
      Conditionally show course guidance based on provider code in config

--- app/components/find/courses/about_schools_component/view.rb
+++ app/components/find/courses/about_schools_component/view.rb
@@ -25,6 +25,26 @@ module Find
             study_sites.any? ||
             site_statuses.map(&:site).uniq.many? || preview?(params)
         end
+
+        def show_higher_education_guidance?
+          return false unless course.higher_education_programme?
+
+          course_information_config(:higher_education, :except_provider_codes).exclude?(course.provider.provider_code)
+        end
+
+        def show_scitt_guidance?
+          return false unless course.scitt_programme?
+
+          course_information_config(:scitt_programmes, :except_provider_codes).exclude?(course.provider_code)
+        end
+
+        private
+
+        def course_information_config(*path)
+          @course_information_config ||= Rails.application.config_for(:course_information)
+
+          @course_information_config.dig(:where_you_will_train, *path)
+        end
       end
     end
   end
