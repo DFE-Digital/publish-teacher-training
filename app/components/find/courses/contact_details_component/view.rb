9703ce359137267289db9d27fdf23fcfe5ad3298
Iain McNulty <iain.mcnulty@education.gov.uk>
2024-01-16 10:40:55 +0000

    Refactor Contact form display logic on course information
      Some providers may prefer to be contacted via web form rather than an
      email address. The present refactor shows the Contact form on the
      course information page if the provider is configured as such.
    
      We can add more providers to this configuration now as it's stored in
      a yaml database.

--- app/components/find/courses/contact_details_component/view.rb
+++ app/components/find/courses/contact_details_component/view.rb
@@ -13,12 +13,28 @@ module Find
           @course = course
         end
 
+        def show_contact_form_instead_of_email?
+          course.provider.provider_code.in?(course_information_config(:contact_forms).keys.map(&:to_s))
+        end
+
         def show_address?
-          course.provider_code.in?(course_information_config(:only_provider_codes)) && course.course_code.in?(course_information_config(:only_course_codes))
+          course.provider_code.in?(course_information_config(:show_address, :only_provider_codes)) && course.course_code.in?(course_information_config(:show_address, :only_course_codes))
+        end
+
+        def contact_form
+          contact_forms[course.provider.provider_code]
+        end
+
+        private
+
+        def contact_forms
+          course_information_config(:contact_forms).stringify_keys
         end
 
         def course_information_config(*path)
-          Rails.application.config_for(:course_information).dig(:show_address, *path)
+          @course_information_config ||= Rails.application.config_for(:course_information)
+
+          @course_information_config.dig(*path)
         end
       end
     end
