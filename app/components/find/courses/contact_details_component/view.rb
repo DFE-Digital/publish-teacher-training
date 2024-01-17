d583dc3ddb0fe331b67569c6bf8bed45fb4d0f2b
Iain McNulty <iain.mcnulty@education.gov.uk>
2024-01-17 09:39:15 +0000

    Add Configs::CourseInformation
      Use a utility class to access the unique configurations that apply to
      certains courses and providers which are stored in a YAML config file.

--- app/components/find/courses/contact_details_component/view.rb
+++ app/components/find/courses/contact_details_component/view.rb
@@ -7,6 +7,7 @@ module Find
         attr_reader :course
 
         delegate :provider, to: :course
+        delegate :contact_form?, :contact_form, :show_address?, to: :course_information_config
 
         def initialize(course)
           super
@@ -14,27 +15,13 @@ module Find
         end
 
         def show_contact_form_instead_of_email?
-          course.provider.provider_code.in?(course_information_config(:contact_forms).keys.map(&:to_s))
-        end
-
-        def show_address?
-          course.provider_code.in?(course_information_config(:show_address, :only_provider_codes)) && course.course_code.in?(course_information_config(:show_address, :only_course_codes))
-        end
-
-        def contact_form
-          contact_forms[course.provider.provider_code]
+          contact_form?
         end
 
         private
 
-        def contact_forms
-          course_information_config(:contact_forms).stringify_keys
-        end
-
-        def course_information_config(*path)
-          @course_information_config ||= Rails.application.config_for(:course_information)
-
-          @course_information_config.dig(*path)
+        def course_information_config
+          @course_information_config ||= Configs::CourseInformation.new(course)
         end
       end
     end
---
+++ app/services/configs/course_information.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+module Configs
+  class CourseInformation
+    def initialize(course)
+      @db = Rails.application.config_for(:course_information)
+      @course = course
+    end
+
+    def placement(*path)
+      @db.dig(*path)
+    end
+
+    def contact_form
+      @db.fetch(:contact_forms).stringify_keys[@course.provider.provider_code]
+    end
+
+    def contact_form?
+      @db.fetch(:contact_forms).stringify_keys.include?(@course.provider.provider_code)
+    end
+
+    def show_address?
+      @db.dig(:show_address, :only_provider_codes).include?(@course.provider.provider_code) &&
+        @db.dig(:show_address, :only_course_codes).include?(@course.course_code)
+    end
+  end
+end
