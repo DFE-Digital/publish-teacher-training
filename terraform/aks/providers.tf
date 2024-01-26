bf42825e9a6e368b75f4cc08ff727ec20bc8060a
Colin Saliceti <colin.saliceti@ukdevops.co.uk>
2024-01-26 12:35:41 +0000

    Enable Azure RBAC in terraform
    Include refactor to use ARM environment variables instead of the explicit
    azure_credentials variable

--- terraform/aks/providers.tf
+++ terraform/aks/providers.tf
@@ -15,15 +15,7 @@ terraform {
   }
 }
 
-locals {
-  azure_credentials = try(jsondecode(var.azure_sp_credentials_json), null)
-}
-
 provider "azurerm" {
-  subscription_id            = try(local.azure_credentials.subscriptionId, null)
-  client_id                  = try(local.azure_credentials.clientId, null)
-  client_secret              = try(local.azure_credentials.clientSecret, null)
-  tenant_id                  = try(local.azure_credentials.tenantId, null)
   skip_provider_registration = true
 
   features {}
@@ -34,6 +26,15 @@ provider "kubernetes" {
   client_certificate     = module.cluster_data.kubernetes_client_certificate
   client_key             = module.cluster_data.kubernetes_client_key
   cluster_ca_certificate = module.cluster_data.kubernetes_cluster_ca_certificate
+
+  dynamic "exec" {
+    for_each = module.cluster_data.azure_RBAC_enabled ? [1] : []
+    content {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "kubelogin"
+      args        = module.cluster_data.kubelogin_args
+    }
+  }
 }
 
 provider "statuscake" {
