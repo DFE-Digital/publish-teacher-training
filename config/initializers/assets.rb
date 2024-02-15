778d99a40840fc00d5aaffd4e39f8333984bf3f1
avinhurry <avinhurry@icloud.com>
2024-02-15 18:12:43 +0000

    [1238] Update path to govuk frontend assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,15 +1,13 @@
 # frozen_string_literal: true
 
+# govuk-frontend related assets
+Rails.application.config.assets.paths << Rails.root.join('node_modules/govuk-frontend/dist/govuk/assets/images')
+Rails.application.config.assets.paths << Rails.root.join('node_modules/govuk-frontend/dist/govuk/assets/fonts')
+
+# Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
 # GOVUK Deps
 Rails.application.config.assets.precompile += %w[
   accessible-autocomplete/dist/accessible-autocomplete.min.css
-  govuk-frontend/govuk/assets/images/govuk-opengraph-image.png
-  govuk-frontend/govuk/assets/images/favicon.ico
-  govuk-frontend/govuk/assets/images/govuk-mask-icon.svg
-  govuk-frontend/govuk/assets/images/govuk-apple-touch-icon.png
-  govuk-frontend/govuk/assets/images/govuk-apple-touch-icon-152x152.png
-  govuk-frontend/govuk/assets/images/govuk-apple-touch-icon-167x167.png
-  govuk-frontend/govuk/assets/images/govuk-apple-touch-icon-180x180.png
 ]
