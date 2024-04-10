0d4057a86bae49f6453dcdec871cd10b6d5e469f
Iain McNulty <iain.mcnulty@education.gov.uk>
2024-04-10 14:24:10 +0100

    Update some rails files

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,13 +1,23 @@
 # frozen_string_literal: true
 
-# govuk-frontend related assets
-Rails.application.config.assets.paths << Rails.root.join('node_modules/govuk-frontend/dist/govuk/assets/images')
-Rails.application.config.assets.paths << Rails.root.join('node_modules/govuk-frontend/dist/govuk/assets/fonts')
+# Be sure to restart your server when you modify this file.
 
-# Add Yarn node_modules folder to the asset load path.
+# Add additional assets to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
 # GOVUK Deps
 Rails.application.config.assets.precompile += %w[
   accessible-autocomplete/dist/accessible-autocomplete.min.css
 ]
+
+# Version of your assets, change this if you want to expire all your assets.
+Rails.application.config.assets.version = "1.0"
+
+# Precompile additional assets.
+# application.js, application.css, and all non-JS/CSS in the app/assets
+# folder are already added.
+# Rails.application.config.assets.precompile += %w( admin.js admin.css )
+
+# govuk-frontend related assets
+Rails.application.config.assets.paths << Rails.root.join('node_modules/govuk-frontend/dist/govuk/assets/images')
+Rails.application.config.assets.paths << Rails.root.join('node_modules/govuk-frontend/dist/govuk/assets/fonts')
--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -4,7 +4,7 @@
 
 # Define an application-wide content security policy
 # For further information see the following documentation
-# https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy
+# https://guides.rubyonrails.org/security.html#content-security-policy-header
 
 Rails.application.configure do
   config.content_security_policy do |policy|
