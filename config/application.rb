22665a7488851070adb3f09768d1bb47849dd65e
Iain McNulty <iain.mcnulty@education.gov.uk>
2024-04-10 15:44:03 +0100

    Upgrade RAils

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
+require_relative "boot"
 
-require_relative 'boot'
 
 require 'rails/all'
 require 'active_support/core_ext/integer/time'
@@ -14,6 +14,11 @@ module ManageCoursesBackend
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+
+    # Please, add to the `ignore` list any other `lib` subdirectories that do
+    # not contain `.rb` files, or that should not be reloaded or eager loaded.
+    # Common ones are `templates`, `generators`, or `middleware`, for example.
+    config.autoload_lib(ignore: %w(assets tasks))
 
     # Configuration for the application, engines, and railties goes here.
     #
