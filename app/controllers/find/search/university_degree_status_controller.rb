c8a9e9985c1ac2b62c3ace04e7f07fb9e5758209
Avin Hurry <avinhurry@icloud.com>
2024-05-07 15:57:36 +0100

    Redirect to visa sponsorship & use smart quote

--- app/controllers/find/search/university_degree_status_controller.rb
+++ app/controllers/find/search/university_degree_status_controller.rb
@@ -18,12 +18,7 @@ module Find
         @university_degree_status_form = UniversityDegreeStatusForm.new(university_degree_status: form_params[:university_degree_status])
 
         if @university_degree_status_form.valid?
-          redirect_to find_results_path(form_params.merge(
-                                          subjects: sanitised_subject_codes,
-                                          has_vacancies: default_vacancies,
-                                          applications_open: default_applications_open,
-                                          university_degree_status: form_params[:university_degree_status]
-                                        ))
+          redirect_to find_visa_status_path(filter_params[:find_university_degree_status_form])
         else
           render :new
         end
