2bc083040101b9620c240a62f43ecd75d488d626
Avin Hurry <avinhurry@icloud.com>
2024-05-15 16:24:00 +0100

    [1671] Flesh out tests

--- spec/features/publish/courses/new_tda_course_spec.rb
+++ spec/features/publish/courses/new_tda_course_spec.rb
@@ -26,7 +26,7 @@ feature 'Adding a teacher degree apprenticeship course', :can_edit_current_and_n
     then_i_am_on_the_add_applications_open_date_page
 
     when_i_choose_the_applications_open_date
-    and_i_choose_the_course_start_date
+    and_i_choose_the_first_start_date
     then_i_am_on_the_check_your_answers_page
 
     when_i_click_on_add_a_course
@@ -34,17 +34,31 @@ feature 'Adding a teacher degree apprenticeship course', :can_edit_current_and_n
     and_the_tda_defaults_are_saved
   end
 
-  # scenario 'changing from tda to non tda on check your answers page' do
-  # end
 
-  # scenario 'changing from non tda to tda on check your answers page' do
-  # end
+  scenario 'when choosing primary course' do
+    when_i_visit_the_courses_page
+    and_i_click_on_add_course
+    and_i_choose_a_primary_course
+    and_i_choose_a_primary_age_range
+    then_i_see_the_degree_awarding_option
 
-  # scenario 'when choosing a further education course' do
-  # end
+    when_i_choose_a_degree_awarding_qualification
+    # We skip the pages for the TDA: funding type, part-time/full time
+    then_i_am_on_the_choose_schools_page
 
-  # scenario 'when choosing primary course' do
-  # end
+    when_i_choose_the_school
+    and_i_choose_the_study_site
+    # We skip the visa sponsorship question
+    then_i_am_on_the_add_applications_open_date_page
+
+    when_i_choose_the_applications_open_date
+    and_i_choose_the_first_start_date
+    then_i_am_on_the_check_your_answers_page
+
+    when_i_click_on_add_a_course
+    then_the_tda_course_is_created
+    and_the_tda_defaults_are_saved
+  end
 
   def given_i_am_authenticated_as_a_provider_user
     @user = create(
@@ -79,6 +93,13 @@ feature 'Adding a teacher degree apprenticeship course', :can_edit_current_and_n
     and_i_click_continue
   end
 
+  def and_i_choose_a_primary_course
+    choose 'Primary'
+    and_i_click_continue
+    choose 'Primary with English'
+    and_i_click_continue
+  end
+
   def and_i_select_a_subject
     select 'Dance', from: 'First subject'
     and_i_click_continue
@@ -86,6 +107,11 @@ feature 'Adding a teacher degree apprenticeship course', :can_edit_current_and_n
 
   def and_i_choose_an_age_range
     choose '14 to 19'
+    and_i_click_continue
+  end
+
+  def and_i_choose_a_primary_age_range
+    choose '3 to 7'
     and_i_click_continue
   end
 
@@ -126,17 +152,26 @@ feature 'Adding a teacher degree apprenticeship course', :can_edit_current_and_n
   end
 
   def when_i_choose_the_applications_open_date
-    first('input[name="course[applications_open_from]"]').set(true)
+    first('input.govuk-radios__input').set(true)
     and_i_click_continue
   end
 
-  def and_i_choose_the_course_start_date; end
+  def and_i_choose_the_first_start_date
+    first('input.govuk-radios__input').set(true)
+    and_i_click_continue
+  end
 
-  def then_i_am_on_the_check_your_answers_page; end
+  def then_i_am_on_the_check_your_answers_page
+    expect(page).to have_current_path(confirmation_publish_provider_recruitment_cycle_courses_path(provider_code: provider.provider_code, recruitment_cycle_year: 2025), ignore_query: true)
+  end
 
-  def when_i_click_on_add_a_course; end
+  def when_i_click_on_add_a_course
+    click_on 'Add course'
+  end
 
-  def then_the_tda_course_is_created; end
+  def then_the_tda_course_is_created
+    expect(provider.courses.first.undergraduate_degree_with_qts?).to be(true)
+  end
 
   def and_the_tda_defaults_are_saved; end
 
