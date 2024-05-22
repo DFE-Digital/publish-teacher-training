4102930646f084bcf240a91c4395beceed4ac995
Avin Hurry <avinhurry@icloud.com>
2024-05-22 16:13:12 +0100

    [1697] Add test for non tda to tda

--- spec/features/publish/courses/editing_course_outcome_spec.rb
+++ spec/features/publish/courses/editing_course_outcome_spec.rb
@@ -41,6 +41,34 @@ feature 'Editing course outcome', { can_edit_current_and_next_cycles: false } do
       and_i_submit
       then_the_default_options_for_a_tda_course_should_be_applied
     end
+
+    context 'fee course' do
+      scenario 'changing the outcome from TDA to non TDA' do
+        given_i_am_authenticated_as_a_provider_user_in_the_next_cycle
+        and_the_tda_feature_flag_is_active
+        and_there_is_a_tda_course_i_want_to_edit
+        when_i_visit_the_course_outcome_page_in_the_next_cycle
+        and_i_choose_qts
+        and_i_choose_the_fee_paying
+        and_i_choose_part_time
+        and_i_choose_to_sponsor_a_student_visa
+        then_i_see_the_correct_attributes_in_the_database_for_fee_paying
+      end
+    end
+
+    context 'salaried course' do
+      scenario 'changing the outcome from TDA to non TDA' do
+        given_i_am_authenticated_as_a_provider_user_in_the_next_cycle
+        and_the_tda_feature_flag_is_active
+        and_there_is_a_tda_course_i_want_to_edit
+        when_i_visit_the_course_outcome_page_in_the_next_cycle
+        and_i_choose_qts
+        and_i_choose_salaried
+        and_i_choose_part_time
+        and_i_choose_to_sponsor_a_skilled_worker_visa
+        then_i_see_the_correct_attributes_in_the_database_for_salaried
+      end
+    end
   end
 
   def given_i_am_authenticated_as_a_provider_user_in_the_next_cycle
@@ -58,6 +86,10 @@ feature 'Editing course outcome', { can_edit_current_and_next_cycles: false } do
 
   def and_there_is_a_qts_course_i_want_to_edit
     given_a_course_exists(:resulting_in_qts)
+  end
+
+  def and_there_is_a_tda_course_i_want_to_edit
+    given_a_course_exists(:undergraduate_degree_with_qts)
   end
 
   def and_there_is_a_non_qts_course_i_want_to_edit
@@ -82,6 +114,10 @@ feature 'Editing course outcome', { can_edit_current_and_next_cycles: false } do
     publish_courses_outcome_edit_page.submit.click
   end
 
+  def and_i_update
+    click_on 'Update'
+  end
+
   def then_i_should_see_a_success_message
     expect(page).to have_content(I18n.t('success.saved', value: 'Qualification'))
   end
@@ -100,6 +136,27 @@ feature 'Editing course outcome', { can_edit_current_and_next_cycles: false } do
 
   def and_i_choose_undergraduate_degree_with_qts
     publish_courses_outcome_edit_page.undergraduate_degree_with_qts.choose
+  end
+
+  def and_i_choose_qts
+    publish_courses_outcome_edit_page.qts.choose
+    and_i_submit
+  end
+
+  def and_i_choose_the_fee_paying
+    choose 'Fee - no salary'
+    and_i_update
+  end
+
+  def and_i_choose_to_sponsor_a_student_visa
+    choose 'Yes'
+    and_i_update
+  end
+
+  def and_i_choose_part_time
+    uncheck 'Full time'
+    check 'Part time'
+    and_i_update
   end
 
   def and_the_tda_feature_flag_is_active
@@ -142,4 +199,27 @@ feature 'Editing course outcome', { can_edit_current_and_next_cycles: false } do
     expect(course.can_sponsor_skilled_worker_visa == false).to be(true)
     expect(course.can_sponsor_student_visa == false).to be(true)
   end
+
+  def then_i_see_the_correct_attributes_in_the_database_for_fee_paying
+    course.reload
+    expect(course.study_mode == 'part_time').to be(true)
+    expect(course.funding_type == 'fee').to be(true)
+    expect(course.can_sponsor_skilled_worker_visa == false).to be(true)
+    expect(course.can_sponsor_student_visa == true).to be(true)
+  end
+
+  def and_i_choose_salaried
+    choose 'Salary'
+    and_i_update
+  end
+
+  def then_i_see_the_correct_attributes_in_the_database_for_salaried
+    course.reload
+    expect(course.study_mode == 'part_time').to be(true)
+    expect(course.funding_type == 'salary').to be(true)
+    expect(course.can_sponsor_skilled_worker_visa == true).to be(true)
+    expect(course.can_sponsor_student_visa == false).to be(true)
+  end
+
+  alias_method :and_i_choose_to_sponsor_a_skilled_worker_visa, :and_i_choose_to_sponsor_a_student_visa
 end
