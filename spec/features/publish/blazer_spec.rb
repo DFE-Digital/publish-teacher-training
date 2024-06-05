b51e3d0c5f1837fd56d85c4fef299f594d93217b
Avin Hurry <avinhurry@icloud.com>
2024-06-05 19:04:13 +0100

    [1719] Move route to publish and add constraint

---
+++ spec/features/publish/blazer_spec.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+feature 'Blazer SQL tool', { can_edit_current_and_next_cycles: false } do
+  scenario 'i can access blazer as an admin' do
+    given_i_am_authenticated_as_an_admin
+    when_i_visit_the_blazer_page
+    then_i_am_taken_to_the_blazer_page
+  end
+
+  scenario 'i cannot access blazer as a non admin' do
+    given_i_am_authenticated_as_a_non_admin
+    when_i_visit_the_blazer_page
+    then_i_see_a_404
+  end
+
+  def given_i_am_authenticated_as_a_non_admin
+    given_i_am_authenticated(user: create(:user, :with_provider))
+  end
+
+  def given_i_am_authenticated_as_an_admin
+    given_i_am_authenticated(user: create(:user, :admin))
+  end
+
+  def when_i_visit_the_blazer_page
+    visit(blazer_path)
+  end
+
+  def then_i_am_taken_to_the_blazer_page
+    expect(page).to have_current_path blazer_path
+  end
+
+  def then_i_should_be_redirected_to_the_courses_index_page
+    expect(publish_provider_courses_index_page).to be_displayed
+  end
+
+  def then_i_see_a_404
+    expect(page.status_code).to eq(404)
+  end
+end
