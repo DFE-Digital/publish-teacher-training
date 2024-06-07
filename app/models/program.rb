605c8bffb1f282a9c555dcd906cf4ee36f0f9f97
David Young <david@dcyoung.dev>
2024-06-07 15:21:15 +0100

    Updated self methods
    Uses `class << self`

--- app/models/program.rb
+++ app/models/program.rb
@@ -1,118 +1,139 @@
 # frozen_string_literal: true
 
 class Program
-  def self.all
-    {
-      higher_education_programme: HigherEducationProgramme,
-      higher_education_salaried_programme: HigherEducationSalariedProgramme,
-      school_direct_training_programme: SchoolDirectTrainingProgramme,
-      school_direct_salaried_training_programme: SchoolDirectSalariedTrainingProgramme,
-      scitt_programme: SCITTProgramme,
-      scitt_salaried_programme: SCITTSalariedProgramme,
-      pg_teaching_apprenticeship: PostgraduateTeachingApprenticeship,
-      teacher_degree_apprenticeship: TeacherDegreeApprenticeship
-    }
-  end
+  class << self
+    def all
+      {
+        higher_education_programme: HigherEducationProgramme,
+        higher_education_salaried_programme: HigherEducationSalariedProgramme,
+        school_direct_training_programme: SchoolDirectTrainingProgramme,
+        school_direct_salaried_training_programme: SchoolDirectSalariedTrainingProgramme,
+        scitt_programme: SCITTProgramme,
+        scitt_salaried_programme: SCITTSalariedProgramme,
+        pg_teaching_apprenticeship: PostgraduateTeachingApprenticeship,
+        teacher_degree_apprenticeship: TeacherDegreeApprenticeship
+      }
+    end
 
-  def self.from_type(program_type)
-    return UnknownProgramme unless program_type
+    def from_type(program_type)
+      return UnknownProgramme unless program_type
 
-    all.fetch(program_type.to_sym, UnknownProgramme)
-  end
+      all.fetch(program_type.to_sym, UnknownProgramme)
+    end
 
-  def self.funding_type
-    NotImplementedError
-  end
+    def funding_type
+      NotImplementedError
+    end
 
-  def self.fee_based?
-    funding_type.fee?
-  end
+    def fee_based?
+      funding_type.fee?
+    end
 
-  def self.sponsors_student_visa? = false
-  def self.sponsors_skilled_worker_visa? = false
+    def sponsors_student_visa? = false
 
-  def self.where_salaried
-    where_funding_types(%w[salary apprenticeship])
-  end
+    def sponsors_skilled_worker_visa? = false
 
-  def self.where_funding_types(funding_types = [])
-    funding_types = Array(funding_types)
-    all.select { |_key, value| funding_types.include?(value.funding_type) }.keys
-  end
+    def where_salaried
+      where_funding_types(%w[salary apprenticeship])
+    end
 
-  def self.where_sponsor_student_visa
-    all.select { |_key, value| value.sponsors_student_visa? }.keys
-  end
+    def where_funding_types(funding_types = [])
+      funding_types = Array(funding_types)
+      all.select { |_key, value| funding_types.include?(value.funding_type) }.keys
+    end
 
-  def self.where_sponsor_skilled_worker_visa
-    all.select { |_key, value| value.sponsors_skilled_worker_visa? }.keys
+    def where_sponsor_student_visa
+      all.select { |_key, value| value.sponsors_student_visa? }.keys
+    end
+
+    def where_sponsor_skilled_worker_visa
+      all.select { |_key, value| value.sponsors_skilled_worker_visa? }.keys
+    end
   end
 end
 
 class UnknownProgramme < Program
-  def self.funding_type
-    nil
-  end
+  class << self
+    def funding_type
+      nil
+    end
 
-  def self.fee_based?
-    false
+    def fee_based?
+      false
+    end
   end
 end
 
 class HigherEducationProgramme < Program
-  def self.funding_type
-    ActiveSupport::StringInquirer.new('fee')
-  end
+  class << self
+    def funding_type
+      ActiveSupport::StringInquirer.new('fee')
+    end
 
-  def self.sponsors_student_visa? = true
+    def sponsors_student_visa? = true
+  end
 end
 
 class HigherEducationSalariedProgramme < Program
-  def self.funding_type
-    ActiveSupport::StringInquirer.new('salary')
+  class << self
+    def funding_type
+      ActiveSupport::StringInquirer.new('salary')
+    end
   end
 end
 
 class SchoolDirectTrainingProgramme < Program
-  def self.funding_type
-    ActiveSupport::StringInquirer.new('fee')
-  end
+  class << self
+    def funding_type
+      ActiveSupport::StringInquirer.new('fee')
+    end
 
-  def self.sponsors_student_visa? = true
+    def sponsors_student_visa? = true
+  end
 end
 
 class SchoolDirectSalariedTrainingProgramme < Program
-  def self.funding_type
-    ActiveSupport::StringInquirer.new('salary')
-  end
+  class << self
+    def funding_type
+      ActiveSupport::StringInquirer.new('salary')
+    end
 
-  def self.sponsors_skilled_worker_visa? = true
+    def sponsors_skilled_worker_visa? = true
+  end
 end
 
 class SCITTProgramme < Program
-  def self.funding_type
-    ActiveSupport::StringInquirer.new('fee')
-  end
+  class << self
+    def funding_type
+      ActiveSupport::StringInquirer.new('fee')
+    end
 
-  def self.sponsors_student_visa? = true
+    def sponsors_student_visa? = true
+  end
 end
 
 class SCITTSalariedProgramme < Program
-  def self.funding_type
-    ActiveSupport::StringInquirer.new('salary')
+  class << self
+    def funding_type
+      ActiveSupport::StringInquirer.new('salary')
+    end
   end
 end
 
 class PostgraduateTeachingApprenticeship < Program
-  def self.funding_type
-    ActiveSupport::StringInquirer.new('apprenticeship')
-  end
+  class << self
+    def funding_type
+      ActiveSupport::StringInquirer.new('apprenticeship')
+    end
 
-  def self.sponsors_skilled_worker_visa? = true
+    def sponsors_skilled_worker_visa? = true
+  end
 end
 
 class TeacherDegreeApprenticeship < Program
-  def self.funding_type
-    ActiveSupport::StringInquirer.new('apprenticeship')
+  class << self
+    def funding_type
+      ActiveSupport::StringInquirer.new('apprenticeship')
+    end
   end
 end
