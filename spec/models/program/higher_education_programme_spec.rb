5d11e122040580ecbec21820538c6b11dec2682b
David Young <david@dcyoung.dev>
2024-06-10 12:24:12 +0100

    Separated spec files

---
+++ spec/models/program/higher_education_programme_spec.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+RSpec.describe HigherEducationProgramme do
+  describe '.funding_type' do
+    it 'returns an ActiveSupport::StringInquirer' do
+      expect(described_class.funding_type).to be_a(ActiveSupport::StringInquirer)
+    end
+
+    it 'returns "fee"' do
+      expect(described_class.funding_type).to eq('fee')
+    end
+
+    it 'responds to fee?' do
+      expect(described_class.funding_type).to be_fee
+    end
+  end
+
+  describe '.sponsors_student_visa?' do
+    it { expect(described_class.sponsors_student_visa?).to be_truthy }
+  end
+
+  describe '.sponsors_skilled_worker_visa?' do
+    it { expect(described_class.sponsors_skilled_worker_visa?).to be_falsey }
+  end
+end
---
+++ spec/models/program/pg_teaching_apprenticeship_spec.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+RSpec.describe PostgraduateTeachingApprenticeship do
+  describe '.funding_type' do
+    it 'returns an ActiveSupport::StringInquirer' do
+      expect(described_class.funding_type).to be_a(ActiveSupport::StringInquirer)
+    end
+
+    it 'returns "apprenticeship"' do
+      expect(described_class.funding_type).to eq('apprenticeship')
+    end
+
+    it 'responds to apprenticeship?' do
+      expect(described_class.funding_type).to be_apprenticeship
+    end
+  end
+
+  describe '.sponsors_student_visa?' do
+    it { expect(described_class.sponsors_student_visa?).to be_falsey }
+  end
+
+  describe '.sponsors_skilled_worker_visa?' do
+    it { expect(described_class.sponsors_skilled_worker_visa?).to be_truthy }
+  end
+end
