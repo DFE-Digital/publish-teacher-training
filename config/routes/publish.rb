4513648fd1ac7e641980e737d559cfab881462b5
Avin Hurry <avinhurry@icloud.com>
2024-06-11 14:24:06 +0100

    [1719] Restrict access to authorised admins only

--- config/routes/publish.rb
+++ config/routes/publish.rb
@@ -4,7 +4,7 @@ require 'sidekiq/web'
 require 'sidekiq/cron/web'
 
 mount Sidekiq::Web, at: '/sidekiq', constraints: SystemAdminConstraint.new
-mount Blazer::Engine, at: '/blazer', constraints: SystemAdminConstraint.new
+mount Blazer::Engine, at: '/blazer', constraints: BlazerAdminConstraint.new
 
 root to: 'publish/providers#index'
 
