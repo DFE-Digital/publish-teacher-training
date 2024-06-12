0c7ba62d29633dbf8f787a868463acf1c931c2a1
John Ake <johnake@hotmail.com>
2024-06-12 16:52:16 +0100

    Create AKS maintenance page

---
+++ maintenance_page/scripts/failback.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+set -eu
+
+NAMESPACE=$(jq -r '.namespace' terraform/aks/workspace_variables/${CONFIG}.tfvars.json)
+
+# Remove if the service does not declare the external ingress i.e. null_host_header = false
+# https://github.com/DFE-Digital/terraform-modules/blob/101f2fa721661cbf0ad05e8105c3628900351008/domains/environment_domains/variables.tf#L15
+echo Reset external ingress
+kubectl -n ${NAMESPACE} apply -f maintenance_page/manifests/${CONFIG}/ingress_external_to_main.yml
+
+echo Reset external ingress2
+kubectl -n ${NAMESPACE} apply -f maintenance_page/manifests/${CONFIG}/ingress_external_to_main2.yml
+
+echo Reset external ingress3
+kubectl -n ${NAMESPACE} apply -f maintenance_page/manifests/${CONFIG}/ingress_external_to_main3.yml
+
+echo Reset internal ingress
+kubectl -n ${NAMESPACE} apply -f maintenance_page/manifests/${CONFIG}/ingress_internal_to_main.yml
+
+echo Delete temp ingress
+kubectl -n ${NAMESPACE} delete  --ignore-not-found=true -f maintenance_page/manifests/${CONFIG}/ingress_temp_to_main.yml
+
+echo Delete maintenance ingress
+kubectl -n ${NAMESPACE} delete  --ignore-not-found=true -f maintenance_page/manifests/${CONFIG}/ingress_maintenance.yml
+
+echo Delete maintenance service
+kubectl -n ${NAMESPACE} delete  --ignore-not-found=true -f maintenance_page/manifests/maintenance/service_maintenance.yml
+
+echo Delete maintenance app
+kubectl -n ${NAMESPACE} delete  --ignore-not-found=true -f maintenance_page/manifests/maintenance/deployment_maintenance.yml
