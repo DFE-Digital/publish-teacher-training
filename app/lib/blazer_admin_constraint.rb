c123a3516080f02deb075b3f3fbe690761662a60
Avin Hurry <avinhurry@icloud.com>
2024-06-13 20:49:19 +0100

    [1719] Refactor based on PR comments:
    https://github.com/DFE-Digital/publish-teacher-training/pull/4255#discussion_r1638001505
    
    https://github.com/DFE-Digital/publish-teacher-training/pull/4274/files#r1637807144
    
    https://github.com/DFE-Digital/publish-teacher-training/pull/4274/files#r1637804825

--- app/lib/blazer_admin_constraint.rb
+++ app/lib/blazer_admin_constraint.rb
@@ -5,13 +5,6 @@ class BlazerAdminConstraint
     signin_user = UserSession.load_from_session(request.session)
     return false if signin_user.blank?
 
-    admin_user = User.admins.kept.find_by(email: signin_user.email)
-    admin_user.present? && authorised_for_blazer?(signin_user)
-  end
-
-  private
-
-  def authorised_for_blazer?(signin_user)
-    User.with_blazer_access.find_by(email: signin_user.email).present?
+    User.with_blazer_access.kept.exists?(email: signin_user.email)
   end
 end
