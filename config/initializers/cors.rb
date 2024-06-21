8e891095543b3f15ce45154e5102ef1847fcacf9
Tomas D'Stefano <tomas_stefano@successoft.com>
2024-06-21 09:58:22 +0100

    Add extra find url to rack cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,7 +2,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins Settings.base_url, Settings.find_url
+    origins Settings.base_url, Settings.find_url, Settings.extra_find_url
     resource '*', headers: :any, methods: %i[get post patch put]
   end
 end
