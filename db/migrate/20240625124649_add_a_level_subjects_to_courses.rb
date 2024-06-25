197c7965077553c9ac2eb57a98ddf8bd90bb80c9
Tomas D'Stefano <tomas_stefano@successoft.com>
2024-06-25 18:20:34 +0100

    Add frozen literal

--- db/migrate/20240625124649_add_a_level_subjects_to_courses.rb
+++ db/migrate/20240625124649_add_a_level_subjects_to_courses.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddALevelSubjectsToCourses < ActiveRecord::Migration[7.1]
   def change
     add_column :course, :a_level_subject_requirements, :jsonb, default: []
