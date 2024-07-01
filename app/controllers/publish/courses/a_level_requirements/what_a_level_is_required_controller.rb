52d9246fac906cebf92c6a8b5fe8225b6b95868a
Tomas D'Stefano <tomas_stefano@successoft.com>
2024-07-01 07:32:00 +0100

    Fix bugs
    Adding missing locales
    And remove the code that was moved one hierarchy up

--- app/controllers/publish/courses/a_level_requirements/what_a_level_is_required_controller.rb
+++ app/controllers/publish/courses/a_level_requirements/what_a_level_is_required_controller.rb
@@ -5,7 +5,6 @@ module Publish
     module ALevelRequirements
       class WhatALevelIsRequiredController < ALevelRequirementsController
         before_action :load_a_level_subject_requirement
-        after_action :set_flash_message, only: :step_params
 
         def step_params
           params[current_step] = ActionController::Parameters.new(@a_level_subject_requirement) if @a_level_subject_requirement.present?
@@ -13,12 +12,6 @@ module Publish
         end
 
         private
-
-        def load_a_level_subject_requirement
-          return if params[:uuid].blank?
-
-          @a_level_subject_requirement = @course.find_a_level_subject_requirement!(params[:uuid])
-        end
 
         def add_flash_message
           flash[:success] = t("course.#{@wizard.current_step.model_name.i18n_key}.success_message")
