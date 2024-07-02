899124c1eb473e920343392884c936657418c71b
asatwal <asatwal01@googlemail.com>
2024-07-02 10:04:10 +0100

    Enable workload identity federation in App for ALL envs

--- config/initializers/dfe_analytics.rb
+++ config/initializers/dfe_analytics.rb
@@ -48,8 +48,8 @@ DfE::Analytics.configure do |config|
   #
   config.enable_analytics = proc { FeatureService.enabled?(:send_request_data_to_bigquery) }
 
-  # The environment we're running in. This value will be attached
-  # to all events we send to BigQuery.
-  #
-  # config.environment = ENV.fetch('RAILS_ENV', 'development')
+  # Whether to use azure workload identity federation for authentication
+  # instead of the BigQuery API JSON Key. Note that this also will also
+  # use a new version of the BigQuery streaming APIs.
+  config.azure_federated_auth = true
 end
