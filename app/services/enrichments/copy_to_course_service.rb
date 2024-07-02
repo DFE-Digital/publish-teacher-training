647eaf6fafffc9e3ff51749a50bd2502b14ee63e
Avin Hurry <avinhurry@icloud.com>
2024-07-02 13:40:16 +0100

    [1889] Do not rollover other_requirements and personal_qualities
    We recently removed other_requirements and personal_qualities. We do not want to copy these over to the next cycle.
    
    This code can be removed once rollover for the 2023-2024 recruitment cycle is complete

--- app/services/enrichments/copy_to_course_service.rb
+++ app/services/enrichments/copy_to_course_service.rb
@@ -4,6 +4,10 @@ module Enrichments
   class CopyToCourseService
     def execute(enrichment:, new_course:)
       new_enrichment = enrichment.dup
+      new_json_data = new_enrichment.json_data.dup
+      new_json_data.delete('PersonalQualities')
+      new_json_data.delete('OtherRequirements')
+      new_enrichment.json_data = new_json_data
       new_enrichment.last_published_timestamp_utc = nil
       new_enrichment.rolled_over!
       new_course.enrichments << new_enrichment
