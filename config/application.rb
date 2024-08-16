eb6c04ca3d182e85ba5b10e2644db1ed20e05a6e
David Young <david@dcyoung.dev>
2024-08-16 17:51:19 +0100

    Added hosts

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,8 @@ module ManageCoursesBackend
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
 
+    config.hosts << "*.app.github.dev"
+
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
