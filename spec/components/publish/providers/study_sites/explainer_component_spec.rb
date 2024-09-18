ce52388bf119954b884fd36f0f0cc63febff83ba
David Young <david@dcyoung.dev>
2024-09-18 16:14:18 +0100

    Updated Study Site explainer content
    This content was updated to match requirements
    
    Co-authored-by: Lori Bailey <44073106+elceebee@users.noreply.github.com>

--- spec/components/publish/providers/study_sites/explainer_component_spec.rb
+++ spec/components/publish/providers/study_sites/explainer_component_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Publish::Providers::StudySites::ExplainerComponent, type: :compon
   it 'renders Study Site information for the Provider' do
     render_inline(described_class.new)
 
-    expect(page).to have_text('A study site, such as a university campus, is where trainees do theoretical training. Add study sites for your organisation and then attach one or more to a course from the ‘Basic details’ tab on the course page.')
-    expect(page).to have_text('Candidates will see a list of the attached study sites in the ‘Training locations’ section on the course page on Find.')
+    expect(page).to have_text('A study site, such as a university campus, is where trainees do theoretical training. Add study sites for your organisation and then attach one or more to a course from the ‘Basic details’ tab on the course page.', normalize_ws: true)
+    expect(page).to have_text('Candidates will see a list of the attached study sites on course pages when they search for courses.', normalize_ws: true)
   end
 end
