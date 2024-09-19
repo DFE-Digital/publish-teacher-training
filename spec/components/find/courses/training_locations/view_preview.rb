232506f2d49768a7905cfdf5a0dc893462db029a
Avin Hurry <avinhurry@icloud.com>
2024-09-19 21:55:38 +0100

    [2288] Add both salaried and fee previews

--- spec/components/find/courses/training_locations/view_preview.rb
+++ spec/components/find/courses/training_locations/view_preview.rb
@@ -4,32 +4,71 @@ module Find
   module Courses
     module TrainingLocations
       class ViewPreview < ViewComponent::Preview
-        def no_study_sites_and_one_placement
+        def no_study_sites_and_one_placement_fee_paying
           course = Course.new(course_code: 'FIND',
                               sites: [Site.new],
+                              funding: 'fee',
                               provider: Provider.new(provider_code: 'DFE', recruitment_cycle: RecruitmentCycle.current)).decorate
           render Find::Courses::TrainingLocations::View.new(course:)
         end
 
-        def one_study_site_and_one_placement
+        def one_study_site_and_one_placement_fee_paying
           course = Course.new(course_code: 'FIND',
                               sites: [Site.new],
+                              funding: 'fee',
                               study_sites: [Site.new],
                               provider: Provider.new(provider_code: 'DFE', recruitment_cycle: RecruitmentCycle.current)).decorate
           render Find::Courses::TrainingLocations::View.new(course:)
         end
 
-        def two_study_sites_and_one_placement
+        def two_study_sites_and_one_placement_fee_paying
           course = Course.new(course_code: 'FIND',
                               sites: [Site.new],
+                              funding: 'fee',
                               study_sites: [Site.new, Site.new],
                               provider: Provider.new(provider_code: 'DFE', recruitment_cycle: RecruitmentCycle.current)).decorate
           render Find::Courses::TrainingLocations::View.new(course:)
         end
 
-        def two_study_sites_and_two_placements
+        def two_study_sites_and_two_placements_fee_paying
           course = Course.new(course_code: 'FIND',
                               sites: [Site.new, Site.new],
+                              funding: 'fee',
+                              study_sites: [Site.new, Site.new],
+                              provider: Provider.new(provider_code: 'DFE', recruitment_cycle: RecruitmentCycle.current)).decorate
+          render Find::Courses::TrainingLocations::View.new(course:)
+        end
+
+        def no_study_sites_and_one_placement_salaried
+          course = Course.new(course_code: 'FIND',
+                              funding: 'salary',
+                              sites: [Site.new],
+                              provider: Provider.new(provider_code: 'DFE', recruitment_cycle: RecruitmentCycle.current)).decorate
+          render Find::Courses::TrainingLocations::View.new(course:)
+        end
+
+        def one_study_site_and_one_placement_salaried
+          course = Course.new(course_code: 'FIND',
+                              sites: [Site.new],
+                              funding: 'salary',
+                              study_sites: [Site.new],
+                              provider: Provider.new(provider_code: 'DFE', recruitment_cycle: RecruitmentCycle.current)).decorate
+          render Find::Courses::TrainingLocations::View.new(course:)
+        end
+
+        def two_study_sites_and_one_placement_salaried
+          course = Course.new(course_code: 'FIND',
+                              sites: [Site.new],
+                              funding: 'salary',
+                              study_sites: [Site.new, Site.new],
+                              provider: Provider.new(provider_code: 'DFE', recruitment_cycle: RecruitmentCycle.current)).decorate
+          render Find::Courses::TrainingLocations::View.new(course:)
+        end
+
+        def two_study_sites_and_two_placements_salaried
+          course = Course.new(course_code: 'FIND',
+                              sites: [Site.new, Site.new],
+                              funding: 'salary',
                               study_sites: [Site.new, Site.new],
                               provider: Provider.new(provider_code: 'DFE', recruitment_cycle: RecruitmentCycle.current)).decorate
           render Find::Courses::TrainingLocations::View.new(course:)
