81dec006436093017de739c42d0c890d5b07f9b9
Iain McNulty <iain.mcnulty@education.gov.uk>
2024-09-24 17:32:02 +0100

    Add course factory with 2 sites

--- spec/factories/courses.rb
+++ spec/factories/courses.rb
@@ -228,6 +228,15 @@ FactoryBot.define do
       site_statuses { [build(:site_status, :findable, vac_status: :full_time_vacancies, site: build(:site, latitude: 51.5079, longitude: 0.0877, address1: '1 Foo Street', postcode: 'BN1 1AA'))] }
     end
 
+    trait :with_2_full_time_sites do
+      site_statuses do
+        [
+          build(:site_status, :findable, vac_status: :full_time_vacancies, site: build(:site, latitude: 51.5079, longitude: 0.0877, address1: '1 Foo Street', postcode: 'BN1 1AA')),
+          build(:site_status, :findable, vac_status: :full_time_vacancies, site: build(:site, latitude: 51.5079, longitude: 0.0897, address1: '1 Bar Street', postcode: 'BN1 1DD'))
+        ]
+      end
+    end
+
     trait :draft_enrichment do
       enrichments { [build(:course_enrichment, :initial_draft, course: nil)] }
     end
