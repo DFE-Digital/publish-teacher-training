48ac39fe214272fc52854e4375a3cd226319effb
Tomas D'Stefano <tomas_stefano@successoft.com>
2024-09-30 11:22:39 +0100

    Make sure methods are implemented on fake course class

--- spec/components/find/courses/sumary_component/view_preview.rb
+++ spec/components/find/courses/sumary_component/view_preview.rb
@@ -67,6 +67,10 @@ module Find
             has_bursary
           end
 
+          def teacher_degree_apprenticeship?
+            true
+          end
+
           def enrichment_attribute(params)
             send(params)
           end
