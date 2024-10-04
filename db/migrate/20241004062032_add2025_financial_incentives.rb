b2aae814c17add72f38e82d1699f11a05dedd020
georgeschena <georgeschena@hotmail.com>
2024-10-04 07:23:33 +0100

    Update financial incentives

---
+++ db/migrate/20241004062032_add2025_financial_incentives.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+class Add2025FinancialIncentives < ActiveRecord::Migration[7.2]
+  def up
+    say_with_time 'populating 2025 financial incentives' do
+      FinancialIncentive.destroy_all
+
+      year = 2025
+
+      Subjects::FinancialIncentiveCreatorService.new(year:).execute
+    end
+  end
+
+  def down
+    say_with_time 'populating 2024 financial incentives' do
+      FinancialIncentive.destroy_all
+
+      year = 2024
+
+      Subjects::FinancialIncentiveCreatorService.new(year:).execute
+    end
+  end
+end
