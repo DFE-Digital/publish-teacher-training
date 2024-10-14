be55e88513a8ed8977a961578031e759958b6517
David Young <david@dcyoung.dev>
2024-10-14 12:18:47 +0100

    Added a AccreditedProviderUpdater service
    This service can update the Accredited Provider for a given Providers.
    The Provider will be updated and the Courses can be updated also.

---
+++ app/services/publish/accredited_provider_updater.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+module Publish
+  class AccreditedProviderUpdater
+    attr_reader :provider, :new_accredited_provider, :recruitment_cycle
+
+    def initialize(provider_code:, recruitment_cycle_year:, new_accredited_provider_code:)
+      @recruitment_cycle = RecruitmentCycle.find_by!(year: recruitment_cycle_year)
+      @provider = Provider.find_by!(provider_code:, recruitment_cycle: @recruitment_cycle)
+      @new_accredited_provider = Provider.find_by!(provider_code: new_accredited_provider_code, recruitment_cycle: @recruitment_cycle)
+    end
+
+    def update_provider_and_courses
+      update_provider && update_courses
+    end
+
+    def update_provider
+      accredited_provider_enrichment = AccreditingProviderEnrichment.new(
+        {
+          UcasProviderCode: new_accredited_provider.provider_code,
+          Description: ''
+        }
+      )
+      accrediting_provider_enrichments = provider.accrediting_provider_enrichments || []
+      accrediting_provider_enrichments << accredited_provider_enrichment
+
+      provider.update!(accrediting_provider: 'not_an_accredited_provider',
+                       accrediting_provider_enrichments:)
+    end
+
+    def update_courses
+      courses = Course.where(provider:)
+      courses.find_each { |course| course.update!(accredited_provider_code: new_accredited_provider.provider_code) }
+    end
+  end
+end
