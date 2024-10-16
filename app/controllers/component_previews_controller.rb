399bbed297b1d0d6033f72cd4e766282d271f11b
Iain McNulty <iain.mcnulty@education.gov.uk>
2024-10-16 13:34:16 +0100

    Add link to View Component previews in Support

--- app/controllers/component_previews_controller.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-class ComponentPreviewsController < ViewComponentsController
-  include Authentication
-  helper_method :current_user
-end
--- config/routes.rb
+++ config/routes.rb
@@ -23,4 +23,8 @@ Rails.application.routes.draw do
     draw(:support)
     draw(:api)
   end
+
+  direct :publish_support_view_component_previews, path_only: true do
+    Rails.application.config.view_component.preview_route
+  end
 end
