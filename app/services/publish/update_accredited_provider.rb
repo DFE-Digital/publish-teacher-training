fc1bf32c33b107509224e6438a8a1ec2d074da4f
Iain McNulty <iain.mcnulty@education.gov.uk>
2024-10-17 16:59:46 +0100

    Update user permissions

--- app/services/publish/update_accredited_provider.rb
+++ app/services/publish/update_accredited_provider.rb
@@ -1,33 +1,59 @@
 # frozen_string_literal: true
 
 module Publish
+  # Change the accredited provider of a training_provider
+  #
+  # 1. Remove [1,2,3] Accredited Providers
+  # 2. Add the target AccreditedProvider to the TrainingProvider
+  # 3. Update all the courses that were accredited by old Accredited Providers to be accredited by the new Provider. Include courses with no Accredited Provider
+  # 4. Remove the User Permissions of all users that had permissions on the removed accredited providers and have no permissions on any remaining Accredited Providers
+  # 5. Add User Permissions for the Training Provider to any User associated with the new Accrediting Provider
+  #
   class UpdateAccreditedProvider
-    attr_reader :from_provider, :to_provider, :recruitment_cycle
+    attr_reader :training_provider, :accredited_provider, :recruitment_cycle
 
-    def initialize(from_provider_code:, recruitment_cycle_year:, to_provider_code:)
+    def initialize(training_provider_code:, recruitment_cycle_year:, to_provider_code:)
       @recruitment_cycle = RecruitmentCycle.find_by!(year: recruitment_cycle_year)
-      @from_provider = Provider.find_by!(provider_code: from_provider_code, recruitment_cycle: @recruitment_cycle)
-      @to_provider = Provider.find_by!(provider_code: to_provider_code, recruitment_cycle: @recruitment_cycle)
+      @training_provider = Provider.find_by!(provider_code: training_provider_code, recruitment_cycle: @recruitment_cycle)
+      @accredited_provider = Provider.find_by!(provider_code: to_provider_code, recruitment_cycle: @recruitment_cycle)
     end
 
-    def update_provider_and_courses
-      update_provider && update_courses
+    def call
+      update_users && update_provider && update_courses
     end
 
     def update_provider
-      from_provider.update_columns(accrediting_provider: 'not_an_accredited_provider',
-                                   accrediting_provider_enrichments: new_accrediting_provider_enrichments)
+      training_provider.update_columns(accrediting_provider: 'not_an_accredited_provider',
+                                       accrediting_provider_enrichments: new_accrediting_provider_enrichments)
     end
 
     def update_courses
-      courses = from_provider.courses
-      courses.update_all(accredited_provider_code: to_provider.provider_code)
+      training_provider
+        .courses.where(accredited_provider_code: nil)
+        .or(training_provider.courses.where.not(accredited_provider_code: accredited_provider.provider_code))
+        .update_all(accredited_provider_code: accredited_provider.provider_code)
+    end
+
+    # How do we identify the users that need their permissions removed?
+    def update_users
+      # providers_codes_to_be_removed = training_provider.accredited_providers.pluck(:id)
+      new_provider_users = accredited_provider.users
+
+      # Create permissions on the training provider for all the accredited provider users
+      #
+      records = new_provider_users.map { |u| { user_id: u.id, provider_id: training_provider.id } }
+      UserPermission.create!(*records)
+
+      # Remove permissions on the training provider for all users who do not
+      # have permissions on the accredited provider
+      users_to_remove_ids = training_provider.users.where.not(id: UserPermission.select(:user_id).where(provider_id: accredited_provider.id))
+      training_provider.user_permissions.where(user_id: users_to_remove_ids).delete_all
     end
 
     private
 
     def new_accrediting_provider_enrichments
-      existing_accrediting_provider_enrichments = from_provider.accrediting_provider_enrichments || []
+      existing_accrediting_provider_enrichments = training_provider.accrediting_provider_enrichments || []
 
       return existing_accrediting_provider_enrichments if new_accredited_provider_code_in_enrichments?(
         existing_accrediting_provider_enrichments
@@ -35,7 +61,7 @@ module Publish
 
       accredited_provider_enrichment = AccreditingProviderEnrichment.new(
         {
-          UcasProviderCode: to_provider.provider_code,
+          UcasProviderCode: accredited_provider.provider_code,
           Description: ''
         }
       )
@@ -44,7 +70,7 @@ module Publish
     end
 
     def new_accredited_provider_code_in_enrichments?(accrediting_provider_enrichments)
-      ucas_provider_code = to_provider.provider_code
+      ucas_provider_code = accredited_provider.provider_code
       accrediting_provider_enrichments.map(&:UcasProviderCode).include?(ucas_provider_code)
     end
   end
--- spec/services/publish/update_accredited_provider_spec.rb
+++ spec/services/publish/update_accredited_provider_spec.rb
@@ -4,12 +4,12 @@ require 'rails_helper'
 
 RSpec.describe Publish::UpdateAccreditedProvider do
   subject do
-    described_class.new(from_provider_code:,
+    described_class.new(training_provider_code:,
                         to_provider_code:,
                         recruitment_cycle_year:)
   end
 
-  let(:from_provider_code) { 'A0' }
+  let(:training_provider_code) { 'T0' }
   let(:to_provider_code) { 'A1' }
   let(:recruitment_cycle_year) { 2024 }
 
@@ -25,7 +25,7 @@ RSpec.describe Publish::UpdateAccreditedProvider do
     context 'when Provider is an Accredited Provider' do
       let!(:provider) do
         create(:provider, :accredited_provider,
-               provider_code: from_provider_code,
+               provider_code: training_provider_code,
                recruitment_cycle:,
                accrediting_provider_enrichments: nil)
       end
@@ -41,7 +41,7 @@ RSpec.describe Publish::UpdateAccreditedProvider do
     context 'when Provider has an Accredited Provider' do
       let!(:provider) do
         create(:provider,
-               provider_code: from_provider_code,
+               provider_code: training_provider_code,
                recruitment_cycle:,
                accrediting_provider: 'not_an_accredited_provider',
                accrediting_provider_enrichments: [{ UcasProviderCode: 'B1', Description: '' }])
@@ -59,7 +59,7 @@ RSpec.describe Publish::UpdateAccreditedProvider do
     context 'when Provider is already Accredited by the new Accredited Provider' do
       let!(:provider) do
         create(:provider,
-               provider_code: from_provider_code,
+               provider_code: training_provider_code,
                recruitment_cycle:,
                accrediting_provider: 'not_an_accredited_provider',
                accrediting_provider_enrichments: [{ UcasProviderCode: to_provider_code, Description: '' }])
@@ -76,26 +76,57 @@ RSpec.describe Publish::UpdateAccreditedProvider do
   end
 
   describe '#update_courses' do
-    let!(:provider) { create(:provider, provider_code: from_provider_code, recruitment_cycle:) }
+    let!(:training_provider) { create(:provider, provider_code: training_provider_code, recruitment_cycle:) }
     let!(:to_provider) { create(:provider, provider_code: to_provider_code, recruitment_cycle:) }
 
-    context 'when the course does not have an Accredited Provider' do
-      let!(:course) { create(:course, provider:, accredited_provider_code: nil) }
+    context 'when the accredited_provider is blank' do
+      let!(:course) { create(:course, provider: training_provider, accredited_provider_code: nil) }
 
-      it "updates the course's Accrediting Provider" do
-        subject.update_courses
-
-        expect(course.reload.accredited_provider_code).to eq(to_provider_code)
+      it "changes the course's Accrediting Provider" do
+        expect { subject.update_courses }.to(change { course.reload.accredited_provider_code }.from(nil).to('A1'))
       end
     end
 
-    context 'when the course has an Accredited Provider' do
-      let!(:course) { create(:course, provider:, accredited_provider_code: 'B1') }
+    context 'when the accredited_provider is not target or source' do
+      let!(:course) { create(:course, provider: training_provider, accredited_provider_code: 'A2') }
 
-      it "updates the course's Accrediting Provider" do
-        subject.update_courses
+      it "changes the course's Accrediting Provider" do
+        expect { subject.update_courses }.to(change { course.reload.accredited_provider_code }.from('A2').to('A1'))
+      end
+    end
 
-        expect(course.reload.accredited_provider_code).to eq(to_provider_code)
+    context 'when the accredited_provider is target' do
+      let!(:course) { create(:course, provider: training_provider, accredited_provider_code: 'A1') }
+
+      it "does not change course's Accrediting Provider" do
+        expect { subject.update_courses }.not_to(change { course.reload.accredited_provider_code })
+      end
+    end
+  end
+
+  describe '#update_users' do
+    let!(:to_provider) { create(:provider, provider_code: to_provider_code, recruitment_cycle:) }
+
+    context 'when the original accredited_provider has users with permissions on the training provider' do
+      let!(:training_provider) do
+        create(:provider,
+               provider_code: training_provider_code,
+               recruitment_cycle:,
+               accrediting_provider: 'not_an_accredited_provider',
+               accrediting_provider_enrichments: [{ UcasProviderCode: 'B1', Description: '' }])
+      end
+      let(:old_accredited_provider) { create(:provider, provider_code: 'B1', recruitment_cycle:) }
+      let!(:user_to_remove) { create(:user, providers: [training_provider, old_accredited_provider]) }
+      let!(:user_to_add) { create(:user, providers: [to_provider]) }
+
+      it "changes the course's Accrediting Provider" do
+        expect(user_to_remove.reload.user_permissions.pluck(:provider_id)).to include(training_provider.id)
+        expect(user_to_add.reload.user_permissions.pluck(:provider_id)).not_to include(training_provider.id)
+
+        subject.call
+
+        expect(user_to_add.reload.user_permissions.pluck(:provider_id)).to include(training_provider.id)
+        expect(user_to_remove.reload.user_permissions.pluck(:provider_id)).not_to include(training_provider.id)
       end
     end
   end
