d70cd1f2d64e2278eba073f8ab19f3411f6727e6
David Young <david@dcyoung.dev>
2024-10-24 10:01:59 +0100

    Add migration to publish a 2024 course

---
+++ db/data/20241024085622_publish_2024_course.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+class Publish2024Course < ActiveRecord::Migration[7.2]
+  def up
+    current_user = User.find_by!(email: 'david4.young@education.gov.uk')
+    course = Course.find_by!(uuid: 'b8da2ddd-d7c8-440e-ba21-67eab7eeba53')
+
+
+    # Code extracted from
+    # https://github.com/DFE-Digital/publish-teacher-training/blob/adbc010be7e3457a986fae93bcfead66a7aa0ef9/app/controllers/publish/courses_controller.rb#L133
+    Course.transaction do
+      course.publish_sites
+      course.publish_enrichment(current_user)
+      course.application_status_open!
+      # NotificationService::CoursePublished.call(course: @course)
+    end
+  end
+
+  def down
+    raise ActiveRecord::IrreversibleMigration
+  end
+end
