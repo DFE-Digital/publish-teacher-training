5e65ccb05f9e617391de7665f18ad548953a4cfc
Iain McNulty <iain.mcnulty@education.gov.uk>
2024-10-25 17:27:59 +0100

    Cache the course count in the api

--- app/controllers/api/public/v1/courses_controller.rb
+++ app/controllers/api/public/v1/courses_controller.rb
@@ -7,13 +7,23 @@ module API
         def index
           render jsonapi: paginate(courses),
                  include: include_param,
-                 meta: { count: courses.count('course.id') },
+                 meta: { count: cached_course_count },
                  class: API::Public::V1::SerializerService.call
         rescue ActiveRecord::StatementInvalid
           render json: { status: 400, message: 'Invalid changed_since value, the format should be an ISO8601 UTC timestamp, for example: `2019-01-01T12:01:00Z`' }.to_json, status: :bad_request
         end
 
         private
+
+        def cached_course_count
+          if params[:no_cache]
+            courses.count('course.id')
+          else
+            Rails.cache.fetch('api_course_count', expires_in: 5.minutes) do
+              courses.count('course.id')
+            end
+          end
+        end
 
         def courses
           @courses ||= APICourseSearchService.call(
