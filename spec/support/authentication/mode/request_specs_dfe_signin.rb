81b8b61621295aaea0033a5161629644f907d69c
Iain McNulty <iain.mcnulty@education.gov.uk>
2024-10-31 11:59:26 +0000

    Automatically set the request host to Settings.base_url in tests
      The PublishConstraint in the routes will only match publish paths if
      the request host matches the Setting.base_url

---
+++ spec/support/authentication/mode/request_specs_dfe_signin.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+# PublishConstraint requires the server domain matches the request host to match routes
+# Settings.base_url&.include?(request.host)
+RSpec.configure do |config|
+  config.before type: :request do
+    host! URI(Settings.base_url).host
+  end
+end
