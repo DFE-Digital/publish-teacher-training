ea5af622b665b2e2ab2663759695e6ce15de233d
Daniel Dye <daniel.dye@education.gov.uk>
2024-10-31 13:18:10 +0000

    Address Rails/ActionControllerFlashBeforeRender violations

--- app/controllers/publish/users_check_controller.rb
+++ app/controllers/publish/users_check_controller.rb
@@ -11,8 +11,7 @@ module Publish
       return unless @user_form.save!
 
       UserAssociationsService::Create.call(user: @user_form.model, provider:) if @user_form.model.providers.exclude?(provider)
-      redirect_to publish_provider_users_path(params[:provider_code])
-      flash[:success] = 'User added'
+      redirect_to publish_provider_users_path(params[:provider_code]), flash: { success: 'User added' }
     end
 
     private
