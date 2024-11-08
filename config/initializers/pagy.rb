5219c66b028ca30cad9858ee838048f44e8c0da4
gms-gs <george.schena@education.gov.uk>
2024-11-08 11:44:43 +0000

    Set Pagy limit to 10 and set overflow exception

--- config/initializers/pagy.rb
+++ config/initializers/pagy.rb
@@ -1,3 +1,9 @@
 # frozen_string_literal: true
 
 require 'pagy/extras/metadata'
+require 'pagy/extras/overflow'
+
+# `empty_page` is default for the UI
+# exception should be used in all API contexts
+Pagy::DEFAULT[:overflow] = :empty_page
+Pagy::DEFAULT[:limit] = 10
