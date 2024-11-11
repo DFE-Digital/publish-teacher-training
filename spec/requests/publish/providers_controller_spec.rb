09bd615fae19ae371c4b1e3b39227a62aab8ca81
Iain McNulty <iain.mcnulty@education.gov.uk>
2024-11-11 17:01:55 +0000

    Excercise all scenarios for 200, 404 and 403 to ProvidersController

--- spec/requests/publish/providers_controller_spec.rb
+++ spec/requests/publish/providers_controller_spec.rb
@@ -23,4 +23,59 @@ describe 'Publish::ProvidersController' do
       end
     end
   end
+
+  describe '/publish/providers/{authorized_provider_code}' do
+    let(:provider) { user.providers.first }
+
+    context 'when the user is authenticated' do
+      it 'is redirect to courses page of the users first provider' do
+        login_user(user)
+        get "/publish/organisations/#{provider.provider_code}"
+        expect(response).to redirect_to("/publish/organisations/#{provider.provider_code}/#{provider.recruitment_cycle_year}/courses")
+      end
+    end
+
+    context 'when the user is not authenticated' do
+      it 'is redirect to sign-in path' do
+        get "/publish/organisations/#{provider.provider_code}"
+        expect(response).to redirect_to('/sign-in')
+      end
+    end
+  end
+
+  describe '/publish/providers/{unauthorized_provider_code}' do
+    let(:other_provider) { create(:provider) }
+
+    context 'when the user is authenticated' do
+      it 'is forbidden' do
+        login_user(user)
+        get "/publish/organisations/#{other_provider.provider_code}"
+        expect(response).to have_http_status(:forbidden)
+      end
+    end
+
+    context 'when the user is not authenticated' do
+      it 'is redirect to sign-in path' do
+        get "/publish/organisations/#{other_provider.provider_code}"
+        expect(response).to redirect_to('/sign-in')
+      end
+    end
+  end
+
+  describe '/publish/providers/{invalid_provider_code}' do
+    context 'when the user is authenticated' do
+      it 'is not found' do
+        login_user(user)
+        get '/publish/organisations/ZZZ'
+        expect(response).to have_http_status(:not_found)
+      end
+    end
+
+    context 'when the user is not authenticated' do
+      it 'is redirect to sign-in path' do
+        get '/publish/organisations/ZZZ'
+        expect(response).to redirect_to('/sign-in')
+      end
+    end
+  end
 end
