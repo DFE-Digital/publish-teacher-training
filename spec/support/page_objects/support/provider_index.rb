1c68edd98304535a0d581c504c5da5bc1e6d6808
Iain McNulty <iain.mcnulty@education.gov.uk>
2024-11-14 14:08:33 +0000

    Add specs for filtering providers in support by provider type

--- spec/support/page_objects/support/provider_index.rb
+++ spec/support/page_objects/support/provider_index.rb
@@ -9,6 +9,10 @@ module PageObjects
       element :apply_filters, 'input.govuk-button'
       element :provider_name_or_code_search, '#provider_search.govuk-input'
       element :course_code_search, '#course_search.govuk-input'
+      element :accredited_provider_filter, '#accrediting_provider.govuk-checkboxes__input'
+      element :scitt_provider_filter, '#provider_type_scitt.govuk-checkboxes__input'
+      element :university_provider_filter, '#provider_type_university.govuk-checkboxes__input'
+      element :lead_school_provider_filter, '#provider_type_lead_school.govuk-checkboxes__input'
       element :remove_filters, 'a.govuk-link', text: 'Clear'
 
       def providers
