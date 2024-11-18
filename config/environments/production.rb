ae5315c97ac6f82b22fcca3010e7ece8ead7e48f
Tomas D'Stefano <tomas_stefano@successoft.com>
2024-11-18 15:19:32 +0000

    Remove application name config from logs and move log configuration
    Application name is blank so is an useless configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -52,7 +52,7 @@ Rails.application.configure do
   config.semantic_logger.add_appender(
     io: $stdout,
     level: config.log_level,
-    formatter: config.rails_semantic_logger.format,
+    formatter: config.rails_semantic_logger.format
   )
 
   config.active_record.logger = nil # Don't log SQL in production
