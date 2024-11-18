19e02f66e3a24d0d7d548fe8ad11e0c164177f90
Iain McNulty <iain.mcnulty@education.gov.uk>
2024-11-18 11:52:23 +0000

    Rename provider.accredited_provider_id to accredited_provider_number
      Rails convention stipulates that columns with _id suffix represent a
      foreign key in a database association.
      The accredited_provider_number is not a foreign key within our
      database so we should avoid confusion by distinguishing it from normal
      foreign key format.

--- db/migrate/20241115143033_rename_provider_accredited_provider_id_column.rb
+++ db/migrate/20241115143033_rename_provider_accredited_provider_id_column.rb
@@ -1,4 +1,5 @@
 class RenameProviderAccreditedProviderIdColumn < ActiveRecord::Migration[7.2]
   def change
+    rename_column :provider, :accredited_provider_id, :accredited_provider_number
   end
 end
