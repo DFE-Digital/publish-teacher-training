4fb1958f8047e68f9805daf006f7fd1741517460
Iain McNulty <iain.mcnulty@education.gov.uk>
2024-11-19 10:50:06 +0000

    Add Course::PublishService
      Extract the process of publishing a course to a service

---
+++ app/services/courses/publish_service.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+module Courses
+  class PublishService
+    def initialize(course:, user:)
+      @course = course
+      @user = user
+    end
+
+    def call
+      Rails.logger.tagged('Course::PublishService') do |l|
+        l.info "publishing course uuid: #{course.uuid}"
+      end
+
+      return false unless course.publishable?
+
+      publish_course
+      send_notification
+
+      course
+    end
+
+    private
+
+    attr_reader :user, :course
+
+    def publish_course
+      Course.transaction do
+        course.publish_sites
+        course.publish_enrichment(user)
+        course.application_status_open!
+      end
+    end
+
+    def send_notification
+      NotificationService::CoursePublished.call(course:)
+    end
+  end
+end
---
+++ spec/services/courses/publish_service_spec.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Courses::PublishService do
+  subject { described_class.new(course:, user:) }
+
+  let(:uuid) { 'b39fe8fe-7cc5-42b8-a06f-d4461b7eb84e' }
+  let(:course) { create(:course, :publishable, uuid:) }
+  let(:user) { create(:user, :admin) }
+
+  describe 'publishable' do
+    it 'gets published' do
+      subject.call
+      expect(course.reload).to be_published
+    end
+
+    it 'returns the course' do
+      return_value = subject.call
+      expect(course.reload).to eq(return_value)
+    end
+  end
+
+  describe 'course is unpublishable' do
+    let(:course) { create(:course, uuid:) }
+
+    it 'returns false' do
+      expect(subject.call).to be(false)
+    end
+  end
+
+  describe 'publishing the course fails' do
+    it 'the course is unpublished' do
+      allow(Course).to receive(:find_by).with({ uuid: uuid }).and_return(course)
+      allow(course).to receive(:publish_sites).and_raise(AASM::UnknownStateMachineError)
+      subject.call
+    rescue AASM::UnknownStateMachineError
+      expect(course.reload).not_to be_published
+    end
+  end
+end
