137a46e4adb8a4724f23d41e8620c6795009197e
Daniel Dye <daniel.dye@education.gov.uk>
2024-11-22 12:14:19 +0000

    Stop pre-loading accredited course list associations
    Pre-loading the accredited course list associations slows down the entire response time by 10x.
    
    This is likely due to ineffective indexes - To be investigated further.

--- spec/services/exports/accredited_course_list_spec.rb
+++ spec/services/exports/accredited_course_list_spec.rb
@@ -5,39 +5,40 @@ require 'rails_helper'
 module Exports
   describe AccreditedCourseList do
     let(:course) do
-      CourseDecorator.new(
-        create(
-          :course,
-          site_statuses: [create(:site_status, :full_time_vacancies, :findable)],
-          enrichments: [build(:course_enrichment, :published)]
-        )
+      create(
+        :course,
+        site_statuses: [create(:site_status, :full_time_vacancies, :findable)],
+        enrichments: [build(:course_enrichment, :published)]
       )
     end
 
-    subject { described_class.new([course]) }
+    let(:decorated_course) { CourseDecorator.new(course) }
+
+    subject { described_class.new(courses: Course.where(id: course)) }
 
     describe '#data' do
       let(:expected_output) do
         {
-          'Provider code' => course.provider.provider_code,
-          'Provider' => course.provider.provider_name,
-          'Course code' => course.course_code,
-          'Course' => course.name,
-          'Study mode' => course.study_mode&.humanize,
-          'Programme type' => course.program_type&.humanize,
-          'Qualification' => course.outcome,
-          'Status' => course.content_status&.to_s&.humanize,
-          'View on Find' => course.find_url,
-          'Applications open from' => I18n.l(course.applications_open_from&.to_date)
+          'Provider code' => decorated_course.provider.provider_code,
+          'Provider' => decorated_course.provider.provider_name,
+          'Course code' => decorated_course.course_code,
+          'Course' => decorated_course.name,
+          'Study mode' => decorated_course.study_mode&.humanize,
+          'Programme type' => decorated_course.program_type&.humanize,
+          'Qualification' => decorated_course.outcome,
+          'Status' => decorated_course.content_status&.to_s&.humanize,
+          'View on Find' => decorated_course.find_url,
+          'Applications open from' => I18n.l(decorated_course.applications_open_from&.to_date),
+          'Campus Codes' => decorated_course.sites&.map(&:code)&.join(' ')
         }
       end
 
       it 'sets the correct headers' do
-        expect(subject.data).to include(expected_output.keys.join(','))
+        expect(subject.data.lines[0].strip).to eq(expected_output.keys.join(','))
       end
 
       it 'sets the correct row values' do
-        expect(subject.data).to include(expected_output.values.join(','))
+        expect(subject.data.lines[1].strip).to eq(expected_output.values.join(','))
       end
     end
   end
