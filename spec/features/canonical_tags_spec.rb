39183714e55dd1a107058dbd9b60706a807d5589
gms-gs <george.schena@education.gov.uk>
2024-11-27 09:37:07 +0000

    Add tests

---
+++ spec/features/canonical_tags_spec.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+feature 'Canonical tags', :with_find_constraint do
+  scenario 'Find pages contain canonical tags' do
+    visit '/'
+
+    expect(page).to have_css("link[rel='canonical'][href='http://www.example.com/']", visible: :all)
+    expect(page).to have_css("meta[property='og:url'][content='http://www.example.com/']", visible: :all)
+  end
+end
+
+feature 'Canonical tags', :with_publish_constraint do
+  scenario 'Publish pages contain canonical tags' do
+    visit '/'
+
+    expect(page).to have_css("link[rel='canonical'][href='http://www.example.com/sign-in/']", visible: :all)
+    expect(page).to have_css("meta[property='og:url'][content='http://www.example.com/sign-in/']", visible: :all)
+  end
+end
