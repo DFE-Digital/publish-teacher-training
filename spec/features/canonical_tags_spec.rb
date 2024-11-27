0e145fd6026b9574a611f2a8f865f41d4a9285a5
gms-gs <george.schena@education.gov.uk>
2024-11-27 09:49:54 +0000

    fix linting

--- spec/features/canonical_tags_spec.rb
+++ spec/features/canonical_tags_spec.rb
@@ -2,20 +2,22 @@
 
 require 'rails_helper'
 
-feature 'Canonical tags', :with_find_constraint do
-  scenario 'Find pages contain canonical tags' do
-    visit '/'
+feature 'Canonical tags' do
+  context 'when visiting Find pages', :with_find_constraint do
+    scenario 'Find pages contain canonical tags' do
+      visit '/'
 
-    expect(page).to have_css("link[rel='canonical'][href='http://www.example.com/']", visible: :all)
-    expect(page).to have_css("meta[property='og:url'][content='http://www.example.com/']", visible: :all)
+      expect(page).to have_css("link[rel='canonical'][href='http://www.example.com/']", visible: :all)
+      expect(page).to have_css("meta[property='og:url'][content='http://www.example.com/']", visible: :all)
+    end
   end
-end
 
-feature 'Canonical tags', :with_publish_constraint do
-  scenario 'Publish pages contain canonical tags' do
-    visit '/'
+  context 'when visiting Publish pages', :with_publish_constraint do
+    scenario 'Publish pages contain canonical tags' do
+      visit '/'
 
-    expect(page).to have_css("link[rel='canonical'][href='http://www.example.com/sign-in/']", visible: :all)
-    expect(page).to have_css("meta[property='og:url'][content='http://www.example.com/sign-in/']", visible: :all)
+      expect(page).to have_css("link[rel='canonical'][href='http://www.example.com/sign-in/']", visible: :all)
+      expect(page).to have_css("meta[property='og:url'][content='http://www.example.com/sign-in/']", visible: :all)
+    end
   end
 end
