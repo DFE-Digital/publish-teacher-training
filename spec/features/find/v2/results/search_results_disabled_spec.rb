0249fef5ff3659faa5d70fe0f166fbd4e6579f9f
Tomas D'Stefano <tomas_stefano@successoft.com>
2024-11-28 15:43:57 +0000

    Rename files and removing the word feature flag

--- spec/features/find/v2/results/search_results_disabled_spec.rb
+++ spec/features/find/v2/results/search_results_disabled_spec.rb
@@ -2,7 +2,7 @@
 
 require 'rails_helper'
 
-feature 'V2 results - feature flag disabled' do
+feature 'V2 results - disabled' do
   before do
     allow(Settings.features).to receive_messages(v2_results: false)
   end
