a767dff8f773b7b3c34d8f15bef68bf2e55e0549
Iain McNulty <iain.mcnulty@education.gov.uk>
2024-11-29 16:52:38 +0000

    Add ProviderPartnership model
      - validate presence of training and accredited partners
      - validate that each are the correct "type"

---
+++ app/models/provider_partnership.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+class ProviderPartnership < ApplicationRecord
+  belongs_to :training_provider, class_name: 'Provider'
+  belongs_to :accredited_provider, class_name: 'Provider'
+
+  validate :accredited_provider_must_be_accredited
+  validate :training_provider_must_not_be_accredited
+
+  ##
+  ##   Validations
+  ##
+
+  def accredited_provider_must_be_accredited
+    return if accredited_provider.blank?
+
+    accredited_provider.accredited? ||
+      errors.add(:accredited_provider, 'must be accredited')
+  end
+
+  def training_provider_must_not_be_accredited
+    return if training_provider.blank?
+
+    training_provider.accredited? &&
+      errors.add(:training_provider, 'must not be accredited')
+  end
+end
