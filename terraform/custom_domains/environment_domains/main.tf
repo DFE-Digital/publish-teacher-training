7a72c8c7c9aff639b5b3975c448823f28d40add5
Neill Turner <neill.turner@education.gov.uk>
2024-12-02 12:59:39 +0000

    Replace terrafile

--- terraform/custom_domains/environment_domains/main.tf
+++ terraform/custom_domains/environment_domains/main.tf
@@ -1,7 +1,7 @@
 # Used to create domains to be managed by front door.
 module "domains" {
   for_each              = var.hosted_zone
-  source                = "git::https://github.com/DFE-Digital/terraform-modules.git//domains/environment_domains?ref=main"
+  source                = "./vendor/modules/domains//domains/environment_domains"
   zone                  = each.key
   front_door_name       = each.value.front_door_name
   resource_group_name   = each.value.resource_group_name
@@ -17,6 +17,6 @@ module "domains" {
 
 # Takes values from hosted_zone.domain_name.cnames (or txt_records, a-records). Use for domains which are not associated with front door.
 module "dns_records" {
-  source      = "git::https://github.com/DFE-Digital/terraform-modules.git//dns/records?ref=main"
+  source      = "./vendor/modules/domains//dns/records"
   hosted_zone = var.hosted_zone
 }
