252756d172d160793e176033e73825cda22edf80
Iain McNulty <iain.mcnulty@education.gov.uk>
2024-12-06 15:43:15 +0000

    More debugging

--- spec/system/support/providers/copy_courses_to_provider_spec.rb
+++ spec/system/support/providers/copy_courses_to_provider_spec.rb
@@ -17,14 +17,27 @@ RSpec.describe 'Support', service: :publish do
   let(:user) { create(:user, :admin) }
 
   before do
+    filepath = '/root/.cache/selenium/chromedriver/linux64/131.0.6778.87/chromedriver'
     Rails.logger.debug('/root/.cache/selenium/chromedriver/linux64/131.0.6778.87/chromedriver')
-    file_exists = File.exist?('/root/.cache/selenium/chromedriver/linux64/131.0.6778.87/chromedriver')
+    file_exists = File.exist?(filepath)
     puts "File.exist?('/root/.cache/selenium/chromedriver/linux64/131.0.6778.87/chromedriver')"
     puts file_exists
     if file_exists
-      file_stat = File.stat('/root/.cache/selenium/chromedriver/linux64/131.0.6778.87/chromedriver')
+      file_stat = File.stat(filepath)
       puts 'file_stat.executable?'
       puts file_stat.executable?
+      puts  "File.stat('/root/.cache/selenium/chromedriver/linux64/131.0.6778.87/chromedriver').mode.to_s(8)[3..5]"
+      puts  file_stat.mode.to_s(8)[3..5]
+      puts '`whoami`'
+      puts `whoami`
+      puts  file_stat.mode.to_s(8)[3..5]
+      puts  file_stat.mode.to_s(8)[3..5]
+      puts  'file_stat.gid'
+      puts  file_stat.gid
+      puts  'file_stat.uid'
+      puts  file_stat.uid
+      puts '`id root`'
+      puts `id root`
     end
 
     sign_in_system_test(user:)
