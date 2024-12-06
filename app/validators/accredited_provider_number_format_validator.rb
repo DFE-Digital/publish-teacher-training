247a292d3faa86b9da87b372c5a489fbb023a31b
Iain McNulty <iain.mcnulty@education.gov.uk>
2024-12-06 13:44:10 +0000

    Use specific errors for University and SCITT

--- app/validators/accredited_provider_number_format_validator.rb
+++ app/validators/accredited_provider_number_format_validator.rb
@@ -3,9 +3,9 @@
 class AccreditedProviderNumberFormatValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     if record.scitt?
-      record.errors.add(attribute, :format) unless value.to_s.match?(/\A5\d{3}\Z/)
+      record.errors.add(attribute, :scitt_format) unless value.to_s.match?(/\A5\d{3}\Z/)
     elsif record.university?
-      record.errors.add(attribute, :format) unless value.to_s.match?(/\A1\d{3}\Z/)
+      record.errors.add(attribute, :university_format) unless value.to_s.match?(/\A1\d{3}\Z/)
     end
   end
 end
