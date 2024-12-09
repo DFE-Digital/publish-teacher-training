e29525f3f7a7b4cc09cb20b90b0c110bfd43d52c
Iain McNulty <iain.mcnulty@education.gov.uk>
2024-12-09 17:08:47 +0000

    Add ProviderPartnership validations

--- app/models/provider_partnership.rb
+++ app/models/provider_partnership.rb
@@ -3,4 +3,22 @@
 class ProviderPartnership < ApplicationRecord
   belongs_to :training_provider, class_name: 'Provider'
   belongs_to :accredited_provider, class_name: 'Provider'
+
+  validates :accredited_provider, uniqueness: { scope: %i[training_provider_id] }
+  validate :accredited_provider_must_be_accredited
+  validate :training_provider_must_not_be_accredited
+
+  def accredited_provider_must_be_accredited
+    return if accredited_provider.blank?
+
+    accredited_provider.accredited? ||
+      errors.add(:accredited_provider, :must_be_accredited)
+  end
+
+  def training_provider_must_not_be_accredited
+    return if training_provider.blank?
+
+    training_provider.accredited? &&
+      errors.add(:training_provider, :must_not_be_accredited)
+  end
 end
--- spec/models/provider_partnership_spec.rb
+++ spec/models/provider_partnership_spec.rb
@@ -13,4 +13,51 @@ describe ProviderPartnership do
   it 'has a description' do
     expect(partnership.description).to eq('Great partnership')
   end
+
+  describe 'validations' do
+    before { subject.validate }
+
+    context 'blank accredited provider' do
+      subject(:partnership) { build(:provider_partnership, accredited_provider: nil) }
+
+      it 'has correct error message' do
+        expect(subject.errors.messages[:accredited_provider]).to include('Accredited provider must exist')
+      end
+    end
+
+    context 'duplicate record' do
+      subject(:duplicate) { build(:provider_partnership, accredited_provider: partnership.accredited_provider, training_provider: partnership.training_provider) }
+
+      let(:partnership) { create(:provider_partnership) }
+
+      it 'has correct error message' do
+        duplicate.validate
+        expect(duplicate.errors.messages[:accredited_provider]).to include('This partnership already exists')
+      end
+    end
+
+    context 'accredited provider must be accredited' do
+      subject(:partnership) { build(:provider_partnership, accredited_provider: build(:provider)) }
+
+      it 'has correct error message' do
+        expect(subject.errors.messages[:accredited_provider]).to include('Accredited provider must be accredited')
+      end
+    end
+
+    context 'accredited provider must not be accredited' do
+      subject(:partnership) { build(:provider_partnership, training_provider: build(:accredited_provider)) }
+
+      it 'has correct error message' do
+        expect(subject.errors.full_messages).to include('Training provider must not be accredited')
+      end
+    end
+
+    context 'blank training provider' do
+      subject(:partnership) { build(:provider_partnership, training_provider: nil) }
+
+      it 'has correct error message' do
+        expect(subject.errors.full_messages).to include('Training provider must exist')
+      end
+    end
+  end
 end
