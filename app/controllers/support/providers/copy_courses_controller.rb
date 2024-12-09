afcf251cf10a8c8316adbc6378f5263a6989adaa
Iain McNulty <iain.mcnulty@education.gov.uk>
2024-12-09 14:51:39 +0000

    Add CopyCoursesForm for managing form errors

--- app/controllers/support/providers/copy_courses_controller.rb
+++ app/controllers/support/providers/copy_courses_controller.rb
@@ -4,25 +4,31 @@ module Support
   module Providers
     class CopyCoursesController < SupportController
       before_action :recruitment_cycle
+
       def new
-        @provider = Provider.find(params[:provider_id])
+        @target_provider = Provider.find(params[:provider_id])
+        @copy_courses_form = CopyCoursesForm.new(@target_provider)
       end
 
       def create
-        @provider = Provider.find(params[:provider_id])
-        @from_provider = recruitment_cycle.providers.find_by(provider_code: params[:course][:autocompleted_provider_code])
+        @target_provider = Provider.find(params[:provider_id])
+        @copy_courses_form = CopyCoursesForm.new(@target_provider, recruitment_cycle.providers.find_by(provider_code: params[:course][:autocompleted_provider_code]))
 
-        sites_copy_to_course = params[:schools] ? Sites::CopyToCourseService : ->(*) {}
+        if @copy_courses_form.valid?
+          sites_copy_to_course = params[:schools] ? Sites::CopyToCourseService : ->(*) {}
 
-        copier = ::Courses::CopyToProviderService.new(sites_copy_to_course:, enrichments_copy_to_course: Enrichments::CopyToCourseService.new, force: true)
+          copier = ::Courses::CopyToProviderService.new(sites_copy_to_course:, enrichments_copy_to_course: Enrichments::CopyToCourseService.new, force: true)
 
-        Provider.transaction do
-          @from_provider.courses.map do |course|
-            copier.execute(course:, new_provider: @provider)
+          Provider.transaction do
+            @copy_courses_form.provider.courses.map do |course|
+              copier.execute(course:, new_provider: @copy_courses_form.target_provider)
+            end
           end
-        end
 
-        redirect_to support_recruitment_cycle_provider_path(recruitment_cycle.year, @provider.id)
+          redirect_to support_recruitment_cycle_provider_path(recruitment_cycle.year, @copy_courses_form.target_provider.id)
+        else
+          render :new
+        end
       end
     end
   end
