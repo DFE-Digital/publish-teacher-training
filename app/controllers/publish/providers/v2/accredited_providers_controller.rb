bedd9c4a8efea88a09d2158c42cf7dcdfc0c5dec
Iain McNulty <iain.mcnulty@education.gov.uk>
2024-12-13 12:58:47 +0000

    Refer to provider_partnership_form everywhere

--- app/controllers/publish/providers/v2/accredited_providers_controller.rb
+++ app/controllers/publish/providers/v2/accredited_providers_controller.rb
@@ -7,21 +7,21 @@ module Publish
         def index; end
 
         def new
-          @provider_partnership = provider.accredited_partnerships.build(accredited_provider: Provider.find(params[:accredited_provider_id]))
+          provider_partnership = provider.accredited_partnerships.build
+          @provider_partnership_form = ProviderPartnershipForm.new(current_user, provider_partnership, params: edit_partnership_params)
         end
 
         def edit
-          @provider_partnership = provider.accredited_partnerships.find_by(accredited_provider: partner)
+          provider_partnership = provider.accredited_partnerships.find_by(accredited_provider: partner)
 
-          @accredited_provider_form = AccreditedProviderForm.new(current_user, provider, params: edit_partnership_params)
+          params = { accredited_provider_id: partner.id, description: provider_partnership.description }
+          @provider_partnership_form = ProviderPartnershipForm.new(current_user, provider_partnership, params:)
         end
 
         def create
-          @provider_partnership = provider.accredited_partnerships.build(description: partnership_params[:description], accredited_provider: Provider.find(partnership_params[:accredited_provider_id]))
+          @provider_partnership_form = ProviderPartnershipForm.new(current_user, @provider_partnership, params: partnership_params)
 
-          @accredited_provider_form = AccreditedProviderForm.new(current_user, provider, params: partnership_params)
-
-          if @provider_partnership.valid? && @accredited_provider_form.stash
+          if @provider_partnership_form.stash
             redirect_to check_publish_provider_recruitment_cycle_accredited_providers_path(@provider.provider_code, recruitment_cycle.year)
           else
             render :new
@@ -29,9 +29,10 @@ module Publish
         end
 
         def update
-          @partnership = provider.accredited_partnerships.find_by(accredited_provider_id: partner.id)
+          @provider_partnership = provider.accredited_partnerships.find_by(accredited_provider_id: partner.id)
+          @provider_partnership_form = ProviderPartnershipForm.new(current_user, @provider_partnership, params: partnership_params)
 
-          if @partnership.update(partnership_params)
+          if @provider_partnership_form.save!
             flash[:success] = t('publish.providers.v2.accredited_providers.edit.updated')
             redirect_to publish_provider_recruitment_cycle_accredited_providers_path(@provider.provider_code, recruitment_cycle.year)
           else
@@ -55,7 +56,7 @@ module Publish
         end
 
         def partnership_params
-          params.require(:provider_partnership).permit(:accredited_provider_id, :description)
+          params.require(:provider_partnership_form).permit(:accredited_provider_id, :description)
         end
 
         def edit_partnership_params
