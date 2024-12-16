4f821cd33c8a3aeaf5705abddec54ec667e035ab
Iain McNulty <iain.mcnulty@education.gov.uk>
2024-12-16 20:21:37 +0000

    rubocop

--- spec/features/publish/courses/add_ratifying_provider_when_publishing_a_course_spec.rb
+++ spec/features/publish/courses/add_ratifying_provider_when_publishing_a_course_spec.rb
@@ -62,7 +62,7 @@ feature 'Publishing a course when course accrediting provider is invalid', { can
 
   def and_the_provider_has_a_valid_accrediting_provider
     provider = @user.providers.first
-    provider.accredited_partnerships.create(accredited_provider:accredited_provider, description: 'Description')
+    provider.accredited_partnerships.create(accredited_provider: accredited_provider, description: 'Description')
   end
 
   def and_the_provider_has_no_accredited_provider
