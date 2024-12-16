3bfeab580d408a57f013cdedabee38b7680f23db
Tomas D'Stefano <tomas_stefano@successoft.com>
2024-12-16 10:25:59 +0000

    Add one more scenario when two filters are select for funding
    Always good to have more scenarios tested!

--- spec/features/find/v2/results/search_results_enabled_spec.rb
+++ spec/features/find/v2/results/search_results_enabled_spec.rb
@@ -79,31 +79,44 @@ feature 'V2 results - enabled' do
     and_the_special_education_needs_filter_is_checked
   end
 
-  scenario 'when I filter by salaried' do
-    given_there_are_courses_with_all_funding_types
-    when_i_visit_the_find_results_page
-    and_i_filter_by_salaried_courses
-    then_i_see_only_salaried_courses
-    and_the_salary_filter_is_checked
-    and_i_see_that_there_is_one_course_found
-  end
+  context 'when filter by funding type' do
+    before do
+      given_there_are_courses_with_all_funding_types
+    end
 
-  scenario 'when I filter by fee' do
-    given_there_are_courses_with_all_funding_types
-    when_i_visit_the_find_results_page
-    and_i_filter_by_fee_courses
-    then_i_see_only_fee_courses
-    and_the_fee_filter_is_checked
-    and_i_see_that_there_is_one_course_found
-  end
+    scenario 'when I filter by salaried' do
+      when_i_visit_the_find_results_page
+      and_i_filter_by_salaried_courses
+      then_i_see_only_salaried_courses
+      and_the_salary_filter_is_checked
+      and_i_see_that_there_is_one_course_found
+    end
 
-  scenario 'when I filter by apprenticeship' do
-    given_there_are_courses_with_all_funding_types
-    when_i_visit_the_find_results_page
-    and_i_filter_by_apprenticeship_courses
-    then_i_see_only_apprenticeship_courses
-    and_the_apprenticeship_filter_is_checked
-    and_i_see_that_there_is_one_course_found
+    scenario 'when I filter by fee' do
+      when_i_visit_the_find_results_page
+      and_i_filter_by_fee_courses
+      then_i_see_only_fee_courses
+      and_the_fee_filter_is_checked
+      and_i_see_that_there_is_one_course_found
+    end
+
+    scenario 'when I filter by fee and salaried' do
+      when_i_visit_the_find_results_page
+      and_i_filter_by_fee_courses
+      and_i_filter_by_salaried_courses
+      then_i_see_fee_and_salaried_courses
+      and_the_fee_filter_is_checked
+      and_the_salary_filter_is_checked
+      and_i_see_that_there_are_two_courses_found
+    end
+
+    scenario 'when I filter by apprenticeship' do
+      when_i_visit_the_find_results_page
+      and_i_filter_by_apprenticeship_courses
+      then_i_see_only_apprenticeship_courses
+      and_the_apprenticeship_filter_is_checked
+      and_i_see_that_there_is_one_course_found
+    end
   end
 
   scenario 'when no results' do
@@ -327,6 +340,12 @@ feature 'V2 results - enabled' do
   def then_i_see_only_fee_courses
     expect(page).to have_content('Biology (S872)')
     expect(page).to have_no_content('Chemistry (K592)')
+    expect(page).to have_no_content('Computing (L364)')
+  end
+
+  def then_i_see_fee_and_salaried_courses
+    expect(page).to have_content('Biology (S872)')
+    expect(page).to have_content('Chemistry (K592)')
     expect(page).to have_no_content('Computing (L364)')
   end
 
