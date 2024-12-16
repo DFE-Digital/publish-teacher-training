de6f2188e80705682019d367375402e5d7b462f0
Iain McNulty <iain.mcnulty@education.gov.uk>
2024-12-16 16:05:20 +0000

    Move TrainingProviders to Providers module

---
+++ app/controllers/publish/providers/training_providers/courses_controller.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+module Publish
+  module Providers
+    module TrainingProviders
+      class CoursesController < PublishController
+        def index
+          authorize(provider, :index?)
+
+          @courses = fetch_courses
+        end
+
+        private
+
+        def training_provider
+          @training_provider ||= provider.training_providers.find_by(provider_code: params[:training_provider_code])
+        end
+
+        def fetch_courses
+          training_provider
+            .courses
+            .includes(:enrichments, :site_statuses, provider: [:recruitment_cycle])
+            .where(accredited_provider_code: provider.provider_code)
+            .order(:name)
+            .map(&:decorate)
+        end
+      end
+    end
+  end
+end
---
+++ app/controllers/publish/providers/training_providers_controller.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+module Publish
+  module Providers
+    class TrainingProvidersController < PublishController
+      def index
+        authorize(provider, :can_list_training_providers?)
+
+        @training_providers = provider.training_providers.include_accredited_courses_counts(provider.provider_code).order(:provider_name)
+        @course_counts = @training_providers.to_h { |p| [p.provider_code, p.accredited_courses_count] }
+      end
+    end
+  end
+end
