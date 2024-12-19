89e64585779282cb941d6eedf01ade4c7236f589
Iain McNulty <iain.mcnulty@education.gov.uk>
2024-12-19 17:45:30 +0000

    Add ProviderPartnership routes and controllers in Support
      Will want to rename the route from parnters to partnerships maybe

--- app/helpers/goto_confirmation_helper.rb
+++ app/helpers/goto_confirmation_helper.rb
@@ -19,7 +19,17 @@ module GotoConfirmationHelper
     end
   end
 
-  def back_link_for_adding_accrediting_provider_path(param_form_key:, params:, recruitment_cycle_year:, provider:)
+  def back_link_for_adding_accredited_partner_path(param_form_key:, params:, recruitment_cycle_year:, provider:)
+    with_partnerships = if goto_confirmation?(param_form_key:, params:)
+                          check_support_recruitment_cycle_provider_accredited_partners_path(recruitment_cycle_year, provider)
+                        elsif param_form_key == :support_accredited_provider_form
+                          search_support_recruitment_cycle_provider_accredited_partners_path
+                        else
+                          support_recruitment_cycle_provider_accredited_partners_path(recruitment_cycle_year, provider)
+                        end
+
+    return with_partnerships if Settings.features.provider_partnerships
+
     if goto_confirmation?(param_form_key:, params:)
       check_support_recruitment_cycle_provider_accredited_providers_path(recruitment_cycle_year, provider)
     elsif param_form_key == :support_accredited_provider_form
@@ -29,7 +39,17 @@ module GotoConfirmationHelper
     end
   end
 
-  def publish_back_link_for_adding_accrediting_provider_path(param_form_key:, params:, recruitment_cycle_year:, provider:)
+  def publish_back_link_for_adding_provider_partnership_path(param_form_key:, params:, recruitment_cycle_year:, provider:)
+    with_partnerships = if goto_confirmation?(param_form_key:, params:)
+                          check_publish_provider_recruitment_cycle_accredited_partnerships_path(provider.provider_code, recruitment_cycle_year)
+                        elsif param_form_key == :publish_accredited_provider_form
+                          search_publish_provider_recruitment_cycle_accredited_partnerships_path(provider.provider_code, recruitment_cycle_year)
+                        else
+                          publish_provider_recruitment_cycle_accredited_partnerships_path(provider.provider_code, recruitment_cycle_year)
+                        end
+
+    return with_partnerships if Settings.features.provider_partnerships
+
     if goto_confirmation?(param_form_key:, params:)
       check_publish_provider_recruitment_cycle_accredited_providers_path(provider.provider_code, recruitment_cycle_year)
     elsif param_form_key == :publish_accredited_provider_form
