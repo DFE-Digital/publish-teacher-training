c9f40a175dfbef4c9567216bfaa081c24e8dd24a
Iain McNulty <iain.mcnulty@education.gov.uk>
2025-01-09 12:32:57 +0000

    Rename files and test concepts

--- spec/features/publish/courses/new_accredited_provider_spec.rb
+++ spec/features/publish/courses/new_accredited_provider_spec.rb
@@ -5,11 +5,11 @@ require 'rails_helper'
 feature 'selection accredited_bodies', { can_edit_current_and_next_cycles: false } do
   before do
     given_i_am_authenticated_as_a_provider_user
-    when_i_visit_the_new_accredited_providers_page
+    when_i_visit_the_new_ratifying_partner_page
   end
 
   scenario 'selecting multiple accredited_bodies' do
-    when_i_select_an_accredited_provider
+    when_i_select_a_ratifying_partner
     and_i_click_continue
     then_i_am_met_with_the_applications_open_page
   end
@@ -33,16 +33,16 @@ feature 'selection accredited_bodies', { can_edit_current_and_next_cycles: false
     given_i_am_authenticated(user: @user)
   end
 
-  def when_i_visit_the_new_accredited_providers_page
-    publish_courses_new_accredited_provider_page.load(provider_code: provider.provider_code, recruitment_cycle_year: Settings.current_recruitment_cycle_year, query: accredited_provider_params)
+  def when_i_visit_the_new_ratifying_partner_page
+    publish_courses_new_ratifying_partner_page.load(provider_code: provider.provider_code, recruitment_cycle_year: Settings.current_recruitment_cycle_year, query: accredited_provider_params)
   end
 
-  def when_i_select_an_accredited_provider
-    publish_courses_new_accredited_provider_page.find("#course_accredited_provider_code_#{@accredited_provider_code.downcase}").click
+  def when_i_select_a_ratifying_partner
+    publish_courses_new_ratifying_partner_page.find("#course_accredited_provider_code_#{@accredited_provider_code.downcase}").click
   end
 
   def and_i_click_continue
-    publish_courses_new_accredited_provider_page.continue.click
+    publish_courses_new_ratifying_partner_page.continue.click
   end
 
   def provider
@@ -56,6 +56,6 @@ feature 'selection accredited_bodies', { can_edit_current_and_next_cycles: false
 
   def then_i_am_met_with_errors
     expect(page).to have_content('There is a problem')
-    expect(page).to have_content('Select an accredited provider')
+    expect(page).to have_content('Select a ratifying partner')
   end
 end
--- spec/support/page_objects/publish/courses/new_ratifying_partner.rb
+++ spec/support/page_objects/publish/courses/new_ratifying_partner.rb
@@ -3,7 +3,7 @@
 module PageObjects
   module Publish
     module Courses
-      class NewAccreditedProvider < PageObjects::Base
+      class NewRatifyingPartner < PageObjects::Base
         set_url '/publish/organisations/{provider_code}/{recruitment_cycle_year}/courses/accredited-provider/new{?query*}'
 
         elements :suggested_accredited_bodies, '[data-qa="course__ratifying_partner_option"]'
