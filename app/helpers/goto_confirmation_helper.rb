43166d7ea3ad360bb3223e3c8de04adb1cb42a06
Iain McNulty <iain.mcnulty@education.gov.uk>
2025-01-15 16:40:07 +0000

    Update accredited provider search to work with accredited partnerships

--- app/helpers/goto_confirmation_helper.rb
+++ app/helpers/goto_confirmation_helper.rb
@@ -29,7 +29,17 @@ module GotoConfirmationHelper
     end
   end
 
-  def publish_back_link_for_adding_accrediting_provider_path(param_form_key:, params:, recruitment_cycle_year:, provider:)
+  def publish_back_link_for_adding_provider_partnership_path(param_form_key:, params:, recruitment_cycle_year:, provider:)
+    with_partnerships = if goto_confirmation?(param_form_key:, params:)
+                          check_publish_provider_recruitment_cycle_accredited_partnerships_path(provider.provider_code, recruitment_cycle_year)
+                        elsif param_form_key == :publish_accredited_provider_form
+                          search_publish_provider_recruitment_cycle_accredited_providers_path(provider.provider_code, recruitment_cycle_year)
+                        else
+                          publish_provider_recruitment_cycle_accredited_partnerships_path(provider.provider_code, recruitment_cycle_year)
+                        end
+
+    return with_partnerships if Settings.features.provider_partnerships
+
     if goto_confirmation?(param_form_key:, params:)
       check_publish_provider_recruitment_cycle_accredited_providers_path(provider.provider_code, recruitment_cycle_year)
     elsif param_form_key == :publish_accredited_provider_form
