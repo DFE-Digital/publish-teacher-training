c26ad1acf2df2274e2bb196d1b630e1631bc30fc
Iain McNulty <iain.mcnulty@education.gov.uk>
2025-01-17 11:20:23 +0000

    Accredited provider spec locked to enrichments

--- spec/features/support/providers/accredited_providers_spec.rb
+++ spec/features/support/providers/accredited_providers_spec.rb
@@ -4,6 +4,7 @@ require 'rails_helper'
 
 feature 'Accredited provider flow', { can_edit_current_and_next_cycles: false } do
   before do
+    allow(Settings.features).to receive_messages(provider_partnerships: false)
     given_i_am_authenticated_as_an_admin_user
     and_there_are_accredited_providers_in_the_database
     and_i_visit_the_index_page
