ac5b7583abe1cc92a2c47fb74e93b3bd6eb7c9fc
Daniel Dye <daniel.dye@education.gov.uk>
2025-01-17 16:17:49 +0000

    Add new HeaderComponent to eventually replace Header
    We retain the existing Header component for Publish while we design how the new header should work alongside the organisation switcher.

--- app/components/support_title_bar.rb
+++ app/components/support_title_bar.rb
@@ -12,7 +12,7 @@ class SupportTitleBar < ViewComponent::Base
   end
 
   def change_cycle_link
-    govuk_link_to t('page_titles.rollover.change_cycle'), support_path, class: 'title-bar__link govuk-link--no-visited-state'
+    govuk_link_to t('page_titles.rollover.change_cycle'), support_root_path, class: 'title-bar__link govuk-link--no-visited-state'
   end
 
   def change_items
---
+++ spec/components/previews/header_component_preview.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+class HeaderComponentPreview < ViewComponent::Preview
+  def default
+    render HeaderComponent.new(service_name: 'Service Name')
+  end
+
+  def with_service_navigation
+    render HeaderComponent.new(service_name: 'Find and Publish support console') do |header|
+      header.with_navigation_item('Home', '/')
+      header.with_navigation_item('Home', '/')
+    end
+  end
+end
