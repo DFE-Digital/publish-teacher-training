84e3d4486c5dbdcdc76c2a8756f9c47a902d8840
Iain McNulty <iain.mcnulty@education.gov.uk>
2025-01-20 12:53:27 +0000

    Add Support provider partnership views

--- app/helpers/goto_confirmation_helper.rb
+++ app/helpers/goto_confirmation_helper.rb
@@ -19,7 +19,17 @@ module GotoConfirmationHelper
     end
   end
 
-  def back_link_for_adding_accrediting_provider_path(param_form_key:, params:, recruitment_cycle_year:, provider:)
+  def back_link_for_adding_accredited_partner_path(param_form_key:, params:, recruitment_cycle_year:, provider:)
+    with_partnerships = if goto_confirmation?(param_form_key:, params:)
+                          check_support_recruitment_cycle_provider_accredited_partnerships_path(recruitment_cycle_year, provider)
+                        elsif param_form_key == :support_accredited_provider_form
+                          search_support_recruitment_cycle_provider_accredited_partnerships_path
+                        else
+                          support_recruitment_cycle_provider_accredited_partnerships_path(recruitment_cycle_year, provider)
+                        end
+
+    return with_partnerships if Settings.features.provider_partnerships
+
     if goto_confirmation?(param_form_key:, params:)
       check_support_recruitment_cycle_provider_accredited_providers_path(recruitment_cycle_year, provider)
     elsif param_form_key == :support_accredited_provider_form
