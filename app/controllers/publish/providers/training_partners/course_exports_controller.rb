a6f8e3e2f6c0d8a43d66c3a86c420d0f400b21b0
Iain McNulty <iain.mcnulty@education.gov.uk>
2025-01-22 09:51:42 +0000

    Training partnerships controllers

---
+++ app/controllers/publish/providers/training_partners/course_exports_controller.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+module Publish
+  module Providers
+    module TrainingPartners
+      class CourseExportsController < ApplicationController
+        def index
+          authorize(provider, :can_list_training_providers?)
+
+          respond_to do |format|
+            format.csv do
+              send_data(data_export.data, filename: data_export.filename, disposition: :attachment)
+            end
+          end
+        end
+
+        private
+
+        def courses
+          @courses ||= provider.current_accredited_courses
+        end
+
+        def data_export
+          @data_export ||= Exports::AccreditedCourseList.new(courses:)
+        end
+      end
+    end
+  end
+end
