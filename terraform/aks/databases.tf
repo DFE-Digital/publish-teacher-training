75f9490a82d70b3e64ea9f1f001192f38e9955b1
Tomas D'Stefano <tomas_stefano@successoft.com>
2025-01-22 13:53:21 +0000

    Use postgis image in review apps

--- terraform/aks/databases.tf
+++ terraform/aks/databases.tf
@@ -56,4 +56,5 @@ module "postgres" {
   azure_enable_high_availability = var.postgres_enable_high_availability
   azure_maintenance_window       = var.postgres_azure_maintenance_window
   server_version                 = "14"
+  server_docker_image            = "postgis/postgis:14-3.5"
 }
