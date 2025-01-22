925e03c9712a8a9fb92fcc0669f32607e2b3d6a3
Iain McNulty <iain.mcnulty@education.gov.uk>
2025-01-22 14:33:56 +0000

    Add training partner tests

--- spec/features/publish/viewing_training_partners_and_their_courses_spec.rb
+++ spec/features/publish/viewing_training_partners_and_their_courses_spec.rb
@@ -4,14 +4,14 @@ require 'rails_helper'
 
 feature 'Viewing courses as an accredited provider', { can_edit_current_and_next_cycles: false } do
   before do
-    allow(Settings.features).to receive(:provider_partnerships).and_return(false)
+    allow(Settings.features).to receive(:provider_partnerships).and_return(true)
     given_i_am_authenticated_as_an_accredited_provider_user
     and_some_courses_exist_with_one_i_accredit
-    when_i_visit_the_publish_training_provider_index_page
+    when_i_visit_the_publish_training_partner_index_page
   end
 
   scenario 'i can see who lists me as their accredited provider' do
-    then_i_should_see_a_list_of_training_providers
+    then_i_should_see_a_list_of_training_partners
     and_i_should_see_a_count_of_the_courses_i_accredit
   end
 
@@ -25,39 +25,40 @@ feature 'Viewing courses as an accredited provider', { can_edit_current_and_next
   end
 
   def and_some_courses_exist_with_one_i_accredit
+    create(:provider_partnership, training_provider:, accredited_provider: accrediting_provider)
     given_a_course_exists(
       enrichments: [build(:course_enrichment, :published)],
-      provider: create(:provider),
+      provider: training_provider,
       accrediting_provider:
     )
 
-    create(:course, enrichments: [build(:course_enrichment, :published)], provider: create(:provider))
+    create(:course, enrichments: [build(:course_enrichment, :published)], provider: training_provider)
   end
 
-  def when_i_visit_the_publish_training_provider_index_page
-    publish_training_provider_index_page.load(
+  def when_i_visit_the_publish_training_partner_index_page
+    publish_training_partner_index_page.load(
       provider_code: accrediting_provider.provider_code, recruitment_cycle_year: accrediting_provider.recruitment_cycle_year
     )
   end
 
-  def then_i_should_see_a_list_of_training_providers
-    expect(publish_training_provider_index_page.training_provider_rows.size).to eq(1)
+  def then_i_should_see_a_list_of_training_partners
+    expect(publish_training_partner_index_page.training_provider_rows.size).to eq(1)
 
-    expect(publish_training_provider_index_page.training_provider_rows.first.name).to have_text(training_provider.provider_name)
+    expect(publish_training_partner_index_page.training_provider_rows.first.name).to have_text(training_provider.provider_name)
   end
 
   def and_i_should_see_a_count_of_the_courses_i_accredit
-    expect(publish_training_provider_index_page.training_provider_rows.first.course_count).to have_text('1')
+    expect(publish_training_partner_index_page.training_provider_rows.first.course_count).to have_text('1')
   end
 
   def and_i_click_on_a_training_provider
-    publish_training_provider_index_page.training_provider_rows.first.name.click
+    publish_training_partner_index_page.training_provider_rows.first.name.click
   end
 
   def then_i_see_the_courses_i_accredit_for
-    expect(publish_training_providers_course_index_page).to be_displayed
-    expect(publish_training_providers_course_index_page.courses.size).to eq(1)
-    expect(publish_training_providers_course_index_page.courses.first.name).to have_text(course.name)
+    expect(publish_training_partners_course_index_page).to be_displayed
+    expect(publish_training_partners_course_index_page.courses.size).to eq(1)
+    expect(publish_training_partners_course_index_page.courses.first.name).to have_text(course.name)
   end
 
   def accrediting_provider
@@ -65,6 +66,6 @@ feature 'Viewing courses as an accredited provider', { can_edit_current_and_next
   end
 
   def training_provider
-    @training_provider ||= course.provider
+    @training_partner ||= create(:provider)
   end
 end
---
+++ spec/support/page_objects/publish/training_partner_index.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require_relative '../sections/training_partner'
+
+module PageObjects
+  module Publish
+    class TrainingPartnerIndex < PageObjects::Base
+      set_url '/publish/organisations/{provider_code}/{recruitment_cycle_year}/training-partners'
+
+      sections :training_provider_rows, Sections::TrainingPartner, '[data-qa="provider__training_partners_list"]'
+    end
+  end
+end
