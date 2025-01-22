484dc5a0095cc7811de43e31b55483234a017076
Iain McNulty <iain.mcnulty@education.gov.uk>
2025-01-22 14:33:56 +0000

    Add training partner tests

---
+++ spec/support/page_objects/publish/training_partners/course_index.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+module PageObjects
+  module Publish
+    module TrainingPartners
+      class CourseIndex < PageObjects::Base
+        set_url '/publish/organisations/{provider_code}/{recruitment_cycle_year}/training-partners/{training_provider_code}/courses'
+
+        sections :courses, '[data-qa="courses__table-section"]' do
+          element :name, '[data-qa="courses-table__course-name"]'
+        end
+      end
+    end
+  end
+end
---
+++ spec/support/page_objects/sections/training_partner.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative 'base'
+
+module PageObjects
+  module Sections
+    class TrainingPartner < PageObjects::Sections::Base
+      element :name, '[data-qa="training_partner_name"]'
+      element :course_count, '[data-qa="course_count"]'
+    end
+  end
+end
