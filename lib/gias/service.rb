30ac1d20d2b9bca89abf59b3e4ddaf113c9581f3
Iain McNulty <iain.mcnulty@education.gov.uk>
2025-01-31 16:59:17 +0000

    Add Gias Downloader
      - add file_fixture for gias response
      - add specs

---
+++ lib/gias/service.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+module Gias
+  class Service
+    private_class_method :new
+
+    def call
+      raise NoMethodError, '#call must be implemented'
+    end
+
+    def self.call(...)
+      new(...).call
+    end
+  end
+end
---
+++ spec/lib/gias/downloader_spec.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe Gias::Downloader do
+  # The downloaded.csv has a windows-1252 character in the school name
+  before do
+    stub_request(:get, 'https://ea-edubase-api-prod.azurewebsites.net/edubase/downloads/public/edubasealldata20250129.csv')
+      .to_return(status: 200, headers: {}, body: file_fixture('lib/gias/downloaded.csv'))
+  end
+
+  around do |example|
+    Timecop.freeze(Time.zone.local(2025, 1, 30)) do
+      example.run
+    end
+  end
+
+  it 'downloads the file' do
+    FileUtils.rm_f('tmp/gias_school.csv')
+    expect { described_class.call }.to change { File.exist?('tmp/gias_school.csv') }.from(false).to(true)
+  ensure
+    FileUtils.rm_f('tmp/gias_school.csv')
+  end
+
+  it 'raises DownloadError when request is not success' do
+    stub_request(:get, 'https://ea-edubase-api-prod.azurewebsites.net/edubase/downloads/public/edubasealldata20250129.csv')
+      .to_return(status: 301, headers: {})
+    expect { described_class.call }.to raise_error(Gias::DownloadError)
+  end
+end
