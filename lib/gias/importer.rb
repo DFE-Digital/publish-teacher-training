e91bb1b6d3c7ee0fd4b9d8d285a322050f2e7809
Iain McNulty <iain.mcnulty@education.gov.uk>
2025-02-04 16:06:09 +0000

    Add Gias Importer

---
+++ lib/gias/importer.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require 'csv'
+
+module Gias
+  class Importer < Service
+    def initialize(csv_path)
+      @csv_path = csv_path
+    end
+
+    attr_reader :csv_path
+
+    def call
+      Log.log('Gias::Importer', 'Importing GIAS schools...')
+
+      school_records = CSV.foreach(csv_path, headers: true).map(&:to_h)
+
+      GiasSchool.upsert_all(school_records, unique_by: :urn)
+
+      Log.log('Gias::Importer', 'GIAS Data Imported!')
+    end
+  end
+end
