7adfa306f16acf17ac81fe989913d5b3da522820
Tomas D'Stefano <tomas_stefano@successoft.com>
2025-02-04 21:31:47 +0000

    Move fetch coordinates

--- app/services/geolocation/location_name_strategy.rb
+++ app/services/geolocation/location_name_strategy.rb
@@ -2,11 +2,8 @@
 
 module Geolocation
   class LocationNameStrategy < LocationLookupStrategy
-    def fetch_coordinates
+    def call
       @client.geocode(@location)
-    rescue StandardError => e
-      capture_error(e)
-      nil
     end
 
     def cache_key
