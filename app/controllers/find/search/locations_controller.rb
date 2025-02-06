4b9d844d4e4be17f0c75d1a5a37e9505e722e572
Iain McNulty <iain.mcnulty@education.gov.uk>
2025-02-06 14:22:16 +0000

    Reverse merge default radius
      Location search may require that we override the default radius of a
      search.

--- app/controllers/find/search/locations_controller.rb
+++ app/controllers/find/search/locations_controller.rb
@@ -59,7 +59,7 @@ module Find
 
       def get_params_for_selected_option(all_params)
         if location_option_selected?
-          all_params.except('provider.provider_name').merge(radius: ResultsView::MILES)
+          all_params.except('provider.provider_name').reverse_merge(radius: ResultsView::MILES)
         elsif across_england_option_selected?
           all_params.except(:latitude, :longitude, :radius, :loc, :lq, 'provider.provider_name', :sortby)
         elsif provider_option_selected?
