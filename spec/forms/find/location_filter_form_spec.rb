afec43601933ce114e487c6bae17664e42b0b4c4
Iain McNulty <iain.mcnulty@education.gov.uk>
2025-02-06 14:08:12 +0000

    Set search radius of 50 on large geocoded areas
      When a user searches for Cornwall, we want to widen the default radius
      from 10 to 50 miles, otherwise many training locations in Cornwall are
      not returned in the search results
    
      We identify a large area by the presence of
      "administrative_area_level_2" in the types property returned by Google
      Geocoder API:
    
      ```
          "types": [
            "administrative_area_level_2",
            "political"
          ]
      ```

--- spec/forms/find/location_filter_form_spec.rb
+++ spec/forms/find/location_filter_form_spec.rb
@@ -6,6 +6,49 @@ module Find
   describe LocationFilterForm, type: :model do
     subject { described_class.new(params) }
 
+    describe 'large_area' do
+      let(:params) { { lq: 'Cornwall', l: '1' } }
+
+      it 'merges a default radius of 50 when user searches a large area' do
+        stub_request(:get, 'https://maps.googleapis.com/maps/api/geocode/json?address=Cornwall&components=country:UK&key=replace_me&language=en&sensor=false')
+          .to_return(status: 200, headers: {}, body: file_fixture('google/geocode/cornwall.json').read)
+
+        subject.valid?
+
+        expect(subject.params).to eq(
+          { lq: 'Cornwall',
+            l: '1',
+            latitude: 50.5036299,
+            longitude: -4.6524982,
+            loc: 'Cornwall, UK',
+            c: 'England',
+            radius: 50 }
+        )
+      end
+    end
+
+    describe 'non large_area' do
+      let(:params) { { lq: 'London', l: '1' } }
+
+      it 'does not merge a radius for searches on small areas' do
+        stub_request(:get, 'https://maps.googleapis.com/maps/api/geocode/json?address=London&components=country:UK&key=replace_me&language=en&sensor=false')
+          .to_return(status: 200, headers: {}, body: file_fixture('google/geocode/london.json').read)
+
+        subject.valid?
+
+        expect(subject.params).to eq(
+          {
+            lq: 'London',
+            l: '1',
+            latitude: 51.5072178,
+            longitude: -0.1275862,
+            loc: 'London, UK',
+            c: 'England'
+          }
+        )
+      end
+    end
+
     describe 'validations' do
       before { subject.valid? }
 
