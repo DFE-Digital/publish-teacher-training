fef3cdc24a7f9eee66037234998b30f92137f2cb
Iain McNulty <iain.mcnulty@education.gov.uk>
2025-02-13 12:48:21 +0000

    Update data-qa for ratifying_provider

---
+++ spec/support/page_objects/publish/courses/details.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+module PageObjects
+  module Publish
+    module Courses
+      class Details < PageObjects::Base
+        set_url '/publish/organisations/{provider_code}/{recruitment_cycle_year}/courses/{course_code}/details'
+
+        section :details, '[id="basic_details"]' do
+          element :ratifying_provider_row, '[data-qa="course__ratifying_provider"]'
+        end
+      end
+    end
+  end
+end
