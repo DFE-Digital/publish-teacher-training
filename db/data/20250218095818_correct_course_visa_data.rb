e8ef7f280e5763b591d0c121e19d81f0f7251fad
Iain McNulty <iain.mcnulty@education.gov.uk>
2025-02-18 10:19:53 +0000

    DataMigration correct visa sponsorship data
      Salary courses shouldn't be sponsoring student visa.
      Fee courses shouldn't sponsoring worker visa.
      Apprenticeship courses shouldn't be sponsoring student visa.

---
+++ db/data/20250218095818_correct_course_visa_data.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class CorrectCourseVisaData < ActiveRecord::Migration[8.0]
+  def up
+    Course.where(funding: 'apprenticeship', can_sponsor_student_visa: true).update_all(can_sponsor_student_visa: false)
+    Course.where(funding: 'salary', can_sponsor_student_visa: true).update_all(can_sponsor_student_visa: false)
+    Course.where(funding: 'fee', can_sponsor_skilled_worker_visa: true).update_all(can_sponsor_skilled_worker_visa: false)
+  end
+
+  def down
+    raise ActiveRecord::IrreversibleMigration
+  end
+end
