4e959578bdbcf8f9b763a7b18c987c50a4263c61
gms-gs <george.schena@education.gov.uk>
2025-02-27 17:57:32 +0000

    Update content on new secondary subjects page

--- app/helpers/find/financial_incentive_helper.rb
+++ app/helpers/find/financial_incentive_helper.rb
@@ -3,6 +3,7 @@
 module Find
   module FinancialIncentiveHelper
     include ActiveSupport::NumberHelper
+    include ActionView::Helpers::TagHelper
 
     def financial_information(financial_incentive)
       return unless FeatureFlag.active?(:bursaries_and_scholarships_announced) && financial_incentive.present?
@@ -10,17 +11,19 @@ module Find
       scholarship = financial_incentive.scholarship
       bursary = financial_incentive.bursary_amount
 
-      if scholarship && bursary
-        I18n.t(
-          '.find.v2.subjects.fee_value.fee.hint.bursaries_and_scholarship_html',
-          bursary_amount: number_to_currency(bursary),
-          scholarship_amount: number_to_currency(scholarship)
-        )
-      elsif scholarship
-        I18n.t('.find.v2.subjects.fee_value.fee.hint.scholarship_only_html', scholarship_amount: number_to_currency(scholarship))
-      elsif bursary
-        I18n.t('.find.v2.subjects.fee_value.fee.hint.bursaries_only_html', bursary_amount: number_to_currency(bursary))
-      end
+      content = if scholarship && bursary
+                  I18n.t(
+                    '.find.v2.subjects.fee_value.fee.hint.bursaries_and_scholarship_html',
+                    bursary_amount: number_to_currency(bursary),
+                    scholarship_amount: number_to_currency(scholarship)
+                  )
+                elsif scholarship
+                  I18n.t('.find.v2.subjects.fee_value.fee.hint.scholarship_only_html', scholarship_amount: number_to_currency(scholarship))
+                elsif bursary
+                  I18n.t('.find.v2.subjects.fee_value.fee.hint.bursaries_only_html', bursary_amount: number_to_currency(bursary))
+                end
+
+      content_tag(:p, content, class: 'govuk-hint govuk-!-font-size-16 govuk-!-margin-top-0 govuk-!-margin-bottom-0') if content
     end
   end
 end
