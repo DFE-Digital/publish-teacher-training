fffaeaa7a9c4a9432161ba814a372edaf609a315
Iain McNulty <iain.mcnulty@education.gov.uk>
2025-03-10 11:50:44 +0000

    Update add_school_to_provider_spec

--- spec/features/support/providers/schools/add_school_to_provider_spec.rb
+++ spec/features/support/providers/schools/add_school_to_provider_spec.rb
@@ -6,150 +6,118 @@ feature 'Adding school to provider as an admin', :with_publish_constraint, { can
   before do
     given_i_am_authenticated_as_an_admin_user
     and_there_is_a_provider
+    and_there_is_a_gias_school
   end
 
   describe 'Adding school to organisation' do
     scenario 'With valid details' do
       given_i_visit_the_support_provider_schools_index_page
       and_i_click_add_school
-      and_i_fill_in_school_name
-      and_i_fill_in_address1
-      and_i_fill_in_address3
-      and_i_fill_in_postcode
-      and_i_continue
-      then_i_should_be_on_the_check_page
-      and_the_school_should_not_be_added_to_the_database
+      then_i_am_on_the_school_search_page
 
-      when_i_click_change_school_name
-      and_i_enter_a_new_school_name
-      and_i_continue
+      when_i_search_with_an_empty_query
+      then_i_should_see_an_error_message
 
-      then_i_should_see_the_school_name_listed
-      and_i_click_add_school_button
-      then_i_see_the_success_message
-      and_the_school_is_added_to_the_database
+      when_i_search_with_an_unmatched_query
+      then_i_should_see_a_no_results_message
+      then_i_click_change_your_search
 
-      and_i_click_add_school
-      and_i_fill_in_school_name
-      and_i_fill_in_address1
-      and_i_fill_in_address3
-      and_i_fill_in_postcode
-      and_i_continue
-      then_i_should_be_on_the_check_page
+      when_i_search_with_an_partial_query
+      then_i_should_see_school_options
 
-      and_i_click_save_and_add_another_school_button
-      then_i_see_the_success_message_on_new_page
-      and_another_school_is_added_to_the_database
-    end
+      when_i_choose_the_school_i_want_to_add
+      then_i_see_the_confirmation_page
 
-    scenario 'With invalid details' do
-      given_i_visit_the_support_provider_schools_new_page
-      and_i_continue
-
-      then_it_should_display_the_correct_error_messages
+      when_i_click_add_school
+      then_i_see_a_confirmation_message
     end
   end
 
-  def then_it_should_display_the_correct_error_messages
-    expect(support_provider_school_create_page.error_summary).to have_text('Enter a name')
-    expect(support_provider_school_create_page.error_summary).to have_text('Enter address line 1')
-    expect(support_provider_school_create_page.error_summary).to have_text('Enter a town or city')
-    expect(support_provider_school_create_page.error_summary).to have_text('Enter a postcode')
+  def given_i_am_authenticated_as_an_admin_user
+    given_i_am_authenticated(user: create(:user, :admin))
   end
 
-  def given_i_visit_the_support_provider_schools_new_page
-    support_provider_school_create_page.load(recruitment_cycle_year: Settings.current_recruitment_cycle_year, provider_id: @provider.id)
+  def and_there_is_a_provider
+    @provider = create(:provider, provider_name: 'School of Cats', provider_code: 'V01')
   end
 
-  def and_another_school_is_added_to_the_database
-    expect(@provider.sites.count).to eq 2
-  end
-
-  def and_the_school_is_added_to_the_database
-    expect(@provider.sites.count).to eq 1
-  end
-
-  def then_i_see_the_success_message_on_new_page
-    expect(support_provider_schools_index_page).to have_text('School added')
-  end
-
-  def then_i_see_the_success_message
-    expect(support_provider_schools_index_page).to have_text('School added')
-  end
-
-  def and_i_click_save_and_add_another_school_button
-    click_link_or_button 'Save school and add another'
-  end
-
-  def and_i_click_add_school_button
-    click_link_or_button 'Add school'
-  end
-
-  def then_i_should_see_the_school_name_listed
-    expect(page).to have_text('New school')
-  end
-
-  def and_i_enter_a_new_school_name
-    support_provider_school_create_page.school_form.location_name.set('New school')
-  end
-
-  def when_i_click_change_school_name
-    support_provider_schools_check_page.change_school_name.click
-  end
-
-  def and_the_school_should_not_be_added_to_the_database
-    expect(@provider.sites.count).to eq 0
-  end
-
-  def then_i_should_be_on_the_check_page
-    expect(support_provider_schools_check_page).to be_displayed(provider_id: @provider.id)
-  end
-
-  def and_i_continue
-    support_provider_school_create_page.submit.click
-  end
-
-  def and_i_click_add_school
-    click_link_or_button 'Add school'
-  end
-
-  def and_i_fill_in_school_name
-    support_provider_school_create_page
-      .school_form
-      .location_name
-      .set('The school')
-  end
-
-  def and_i_fill_in_address1
-    support_provider_school_create_page
-      .school_form
-      .building_and_street
-      .set('The address')
-  end
-
-  def and_i_fill_in_address3
-    support_provider_school_create_page
-      .school_form
-      .town_or_city
-      .set('The town')
-  end
-
-  def and_i_fill_in_postcode
-    support_provider_school_create_page
-      .school_form
-      .postcode
-      .set('TR1 1UN')
+  def and_there_is_a_gias_school
+    @gias_school = create(:gias_school, {
+                            urn: '123456',
+                            name: 'Distinct School',
+                            address1: '123 Fake Street',
+                            town: 'Newtown',
+                            postcode: 'RD9 0AN'
+                          })
   end
 
   def given_i_visit_the_support_provider_schools_index_page
     support_provider_schools_index_page.load(recruitment_cycle_year: Settings.current_recruitment_cycle_year, provider_id: @provider.id)
   end
 
-  def and_there_is_a_provider
-    @provider = create(:provider, provider_name: 'School of Cats')
+  def then_i_am_on_the_school_search_page
+    expect(page).to have_current_path(search_support_recruitment_cycle_provider_schools_path(recruitment_cycle_year: Settings.current_recruitment_cycle_year, provider_id: @provider.id))
   end
 
-  def given_i_am_authenticated_as_an_admin_user
-    given_i_am_authenticated(user: create(:user, :admin))
+  def when_i_search_with_an_empty_query
+    fill_in 'support-providers-schools-search-form-query-field', with: ''
+    click_continue
+  end
+
+  def then_i_should_see_an_error_message
+    within('.govuk-error-summary') do
+      expect(page).to have_content('There is a problem')
+      expect(page).to have_content('Enter a school, university, college, URN or postcode')
+    end
+  end
+
+  def when_i_search_with_an_unmatched_query
+    fill_in 'support-providers-schools-search-form-query-field', with: 'zzz'
+    click_continue
+  end
+
+  def then_i_should_see_a_no_results_message
+    expect(page).to have_content('No results found for ‘zzz’')
+  end
+
+  def when_i_search_with_an_partial_query
+    fill_in 'support-providers-schools-search-form-query-field', with: 'Dis'
+    click_continue
+  end
+
+  def then_i_should_see_school_options
+    expect(page).to have_content('1 result found for ‘Dis’')
+  end
+
+  def when_i_choose_the_school_i_want_to_add
+    choose @gias_school.name
+    click_continue
+  end
+
+  def then_i_click_change_your_search
+    click_link_or_button 'Change your search'
+  end
+
+  def then_i_see_the_confirmation_page
+    expect(page).to have_content('Add school - School of Cats (V01)')
+    expect(page).to have_content('Check your answers')
+    expect(page).to have_content('School NameDistinct School')
+    expect(page).to have_content('URN123456')
+    expect(page).to have_content('Address123 Fake StreetNewtownRD9 0AN')
+  end
+
+  def and_i_click_add_school
+    click_link_or_button 'Add school'
+  end
+  alias_method :when_i_click_add_school, :and_i_click_add_school
+
+  def then_i_see_a_confirmation_message
+    within('.govuk-notification-banner--success') do
+      expect(page).to have_content('School added')
+    end
+  end
+
+  def click_continue
+    click_link_or_button 'Continue'
   end
 end
