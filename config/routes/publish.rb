4cb7363f92a6ab4726f98478f44c6f72b30bba76
Iain McNulty <iain.mcnulty@education.gov.uk>
2025-03-10 15:41:37 +0000

    Wrap school search routes in collection block

--- config/routes/publish.rb
+++ config/routes/publish.rb
@@ -330,9 +330,11 @@ namespace :publish, as: :publish do
             delete :delete, to: 'schools#destroy'
           end
 
-          get '/search', on: :collection, to: 'school_search#new'
-          post '/search', on: :collection, to: 'school_search#create'
-          put '/search', on: :collection, to: 'school_search#update'
+          collection do
+            get '/search', to: 'school_search#new'
+            post '/search', to: 'school_search#create'
+            put '/search', to: 'school_search#update'
+          end
         end
 
         resource :check_study_site, only: %i[show update], controller: 'study_sites_check', path: 'study-sites/check'
