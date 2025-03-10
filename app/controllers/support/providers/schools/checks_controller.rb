1deacdf162566e80327f2d784f269a76f84d461c
Iain McNulty <iain.mcnulty@education.gov.uk>
2025-03-10 11:50:25 +0000

    Update Check your answers for adding a school

--- app/controllers/support/providers/schools/checks_controller.rb
+++ app/controllers/support/providers/schools/checks_controller.rb
@@ -10,20 +10,24 @@ module Support
 
         def update
           if @school_form.save!
-            flash[:success] = t('support.providers.schools.added')
+            redirect_to support_recruitment_cycle_provider_schools_path
+            flash[:success] = t('.added')
           else
-            render template: 'support/schools/new'
+            render :show
           end
         end
 
         private
 
         def new_form
-          @school_form = SchoolForm.new(provider, site)
+          @school_form = SchoolForm.new(provider, site, params: { gias_school_id: params[:school_id] })
         end
 
         def site
-          @site ||= provider.sites.build
+          @site ||= begin
+            gias_school = GiasSchool.find(params[:school_id])
+            @provider.sites.school.build(gias_school.school_attributes)
+          end
         end
 
         def provider
