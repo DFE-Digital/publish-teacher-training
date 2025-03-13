b5ea7cfb63cb3d8d651a219f57a00c9ec8e18dca
Iain McNulty <iain.mcnulty@education.gov.uk>
2025-03-13 17:03:01 +0000

    Add rake task to create provider partnerships from Accredited enrichment
      We have a data migration that does the same thing but we would like to
      be able to run this idempotent task as we approach the launch of
      provider partnerships

---
+++ spec/lib/tasks/create_provider_partnerships_spec.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+require 'rake'
+
+describe 'creates providers partnerships from enrichments' do
+  subject do
+    Rake::Task['provider_partnerships:create_from_enrichments'].invoke
+  end
+
+  let!(:accredited_provider) { create(:accredited_provider) }
+  let!(:description) { 'The Description' }
+  let!(:training_provider) { create(:provider, accrediting_provider_enrichments: [AccreditingProviderEnrichment.new(UcasProviderCode: accredited_provider.provider_code, Description: description)]) }
+
+  Rails.application.load_tasks if Rake::Task.tasks.empty?
+
+  it 'calls Courses::PublishService service' do
+    expect { subject }.to change(training_provider.accredited_partnerships, :count).by(1)
+    expect(training_provider.accredited_partnerships.last).to have_attributes(
+      description: 'The Description',
+      accredited_provider_id: accredited_provider.id
+    )
+  end
+end
