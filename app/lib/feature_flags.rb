ae9d7f0994f88a12848faa6e291531dc343eb4ff
Tomas D'Stefano <tomas_stefano@successoft.com>
2025-03-14 14:41:00 +0000

    Remove prefiltering feature flag

--- app/lib/feature_flags.rb
+++ app/lib/feature_flags.rb
@@ -6,7 +6,6 @@ class FeatureFlags
       [:maintenance_mode, 'Puts Find into maintenance mode', 'Find and Publish team'],
       [:maintenance_banner, 'Displays the maintenance mode banner', 'Find and Publish team'],
       [:bursaries_and_scholarships_announced, 'Display scholarship and bursary information', 'Find and Publish team'],
-      [:prefiltering_find_redesign, 'Prefiltering Find redesign', 'Find and Publish team']
     ]
   end
 end
