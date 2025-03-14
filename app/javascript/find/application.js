d8b12fd76fb7a0ea86b8df8cf5f2a85a6e2e10a7
Tomas D'Stefano <tomas_stefano@successoft.com>
2025-03-14 15:46:11 +0000

    Remove more old code from the old live site
    * Location suggestions - old one
    * Old provider autocomplete
    * remove results view objects
    * remove more unused view objects

--- app/javascript/find/application.js
+++ app/javascript/find/application.js
@@ -7,7 +7,6 @@ import initAutocomplete from './autocomplete'
 import { Application } from '@hotwired/stimulus'
 import FilterSearchController from './controllers/filter_search_controller'
 import SubjectsAutocompleteController from './controllers/subjects_autocomplete_controller'
-import ProvidersAutocompleteController from './controllers/providers_autocomplete_controller'
 import ProviderAutocompleteController from './controllers/provider_autocomplete_controller'
 import RemoteAutocompleteController from './controllers/remote_autocomplete_controller'
 import LocationsAutocompleteController from './controllers/locations_autocomplete_controller'
@@ -21,9 +20,6 @@ Stimulus.register('remote-autocomplete', RemoteAutocompleteController)
 Stimulus.register('locations-autocomplete', LocationsAutocompleteController)
 Stimulus.register('visibility', VisibilityController)
 
-// V1
-Stimulus.register('providers-autocomplete', ProvidersAutocompleteController)
-
 // V2
 Stimulus.register('v2-provider-autocomplete', ProviderAutocompleteController)
 
@@ -31,9 +27,3 @@ window.jQuery = jQuery
 window.$ = jQuery
 
 initAll()
-
-initAutocomplete({
-  element: 'location-autocomplete',
-  input: 'location',
-  path: '/location-suggestions'
-})
