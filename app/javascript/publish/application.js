c81b196521c647e347d23261b35cd170445853ef
Daniel Dye <daniel.dye@education.gov.uk>
2025-03-17 11:52:34 +0000

    Remove autocomplete tests and use vitest instead

--- app/javascript/publish/application.js
+++ app/javascript/publish/application.js
@@ -1,6 +1,4 @@
 // Entry point for the build script in your package.json
-
-import 'babel-polyfill'
 import jQuery from 'jquery'
 import { initAll } from 'govuk-frontend'
 
--- app/javascript/utils/request_helper.spec.js
+++ app/javascript/utils/request_helper.spec.js
@@ -1,12 +1,13 @@
 /**
- * @jest-environment jsdom
+ * @vitest-environment jsdom
  */
 
+import { vi, describe, expect, test, beforeEach } from 'vitest'
 import { request, getPath } from './request_helper'
 
 describe('Request helpers', () => {
   describe('getPath', () => {
-    it('should return a path', () => {
+    test('should return a path', () => {
       const path = getPath('/endpoint', 'queryString')
       expect(path).toBe('/endpoint?query=queryString')
     })
@@ -14,49 +15,49 @@ describe('Request helpers', () => {
 
   describe('request', () => {
     let requestFn
-    const abortMock = jest.fn()
+    const abortMock = vi.fn()
 
     beforeEach(() => {
-      global.XMLHttpRequest = jest.fn(() => ({
+      global.XMLHttpRequest = vi.fn(() => ({
         abort: abortMock,
         addEventListener: (_, cb) => cb(),
-        open: jest.fn(),
-        send: jest.fn(),
+        open: vi.fn(),
+        send: vi.fn(),
         responseText: '[]',
         readyState: 2
       }))
       requestFn = request('/endpoint')
     })
 
-    it('should return a function', () => {
+    test('should return a function', () => {
       expect(typeof requestFn).toBe('function')
     })
 
     describe('when called', () => {
-      const cb = jest.fn()
+      const cb = vi.fn()
 
       beforeEach(() => {
         requestFn('foo', cb)
       })
 
-      it('should perform an ajax request', () => {
+      test('should perform an ajax request', () => {
         expect(XMLHttpRequest).toBeCalled()
       })
 
-      it('should invoke callback', () => {
+      test('should invoke callback', () => {
         expect(cb).toBeCalled()
       })
     })
 
     describe('when called with a pending request', () => {
-      const cb = jest.fn()
+      const cb = vi.fn()
 
       beforeEach(() => {
         requestFn('foo', cb)
         requestFn('bar', cb)
       })
 
-      it('should abort a request', () => {
+      test('should abort a request', () => {
         expect(abortMock).toBeCalled()
       })
     })
