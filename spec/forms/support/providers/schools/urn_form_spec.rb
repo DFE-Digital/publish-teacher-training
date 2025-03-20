03920fdcfca972ff9f74d8bdd7ce403243da7d09
Iain McNulty <iain.mcnulty@education.gov.uk>
2025-03-20 14:12:41 +0000

    Add URNForm to manage URN values

---
+++ spec/forms/support/providers/schools/urn_form_spec.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+module Support
+  module Providers
+    module Schools
+      describe URNForm, type: :model do
+        subject { described_class.new(provider, params:) }
+
+        let(:provider) { create(:provider) }
+        let(:params) { { values: 'y,r,u,a,pain' } }
+
+        describe 'validations' do
+          before { subject.validate }
+
+          context 'blank values' do
+            let(:params) { { values: nil } }
+
+            it 'is invalid' do
+              expect(subject.errors[:values]).to include('Enter URNs')
+              expect(subject.valid?).to be(false)
+            end
+          end
+
+          context 'valid params' do
+            it 'is valid' do
+              expect(subject.valid?).to be(true)
+            end
+          end
+        end
+
+        describe '#stash' do
+          context 'valid details' do
+            it 'returns true' do
+              expect(subject.stash).to be true
+
+              expect(subject.errors.messages).to be_blank
+            end
+          end
+        end
+      end
+    end
+  end
+end
