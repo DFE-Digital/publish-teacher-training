539b3dfbecc2ab8c416ad40d755406fb5cb8829f
Iain McNulty <iain.mcnulty@education.gov.uk>
2025-03-20 14:12:05 +0000

    Add URNParser to parse textarea input of URNs

---
+++ lib/urn_parser.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class URNParser
+  def initialize(urns)
+    @urns = urns
+  end
+
+  def call
+    processed = @urns.tr(',', "\n")
+    processed = processed.tr("\t\r\s", '')
+    processed.split("\n").compact_blank.uniq
+  end
+end
---
+++ spec/lib/urn_parser_spec.rb
@@ -0,0 +1,59 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe URNParser do
+  subject { described_class.new(urns).call }
+
+  describe 'with duplicates' do
+    let(:urns) { '123456,123456' }
+
+    it 'removes duplicates' do
+      expect(subject).to eq(['123456'])
+    end
+  end
+
+  describe 'with CRNL' do
+    let(:urns) { "123456\r\n654321" }
+
+    it 'parses all the URNs correctly' do
+      expect(subject).to eq(%w[
+                              123456
+                              654321
+                            ])
+    end
+  end
+
+  describe 'with trailing / leading whitespace' do
+    let(:urns) { '  123456,  654321   ' }
+
+    it 'parses all the URNs correctly' do
+      expect(subject).to eq(%w[
+                              123456
+                              654321
+                            ])
+    end
+  end
+
+  describe 'with blank lines' do
+    let(:urns) { "\n\n123456\n\n654321\n\n" }
+
+    it 'parses all the URNs correctly' do
+      expect(subject).to eq(%w[
+                              123456
+                              654321
+                            ])
+    end
+  end
+
+  describe 'punctuation that is not a comma' do
+    let(:urns) { ')(*+_][,*&^%$£' }
+
+    it 'parses all the URNs correctly' do
+      expect(subject).to eq(%w[
+                              )(*+_\]\[
+                              *&^%$£
+                            ])
+    end
+  end
+end
