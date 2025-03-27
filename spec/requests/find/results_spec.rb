208d2b693d84b0efba0f493ac285214aab3bed4c
Tomas D'Stefano <tomas_stefano@successoft.com>
2025-03-27 12:23:50 +0000

    Fallback to page 1 if page parameter can not be parsed
    Some requests are returning 500.because requests are adding custom value to the page parameter.O
    
    e.g /results?page=something
    
    This commit handle this parameter by fallback to first page if
    something is wrong with params[:page]

---
+++ spec/requests/find/results_spec.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+module Find
+  describe '/results' do
+    before do
+      host! 'www.find-example.com'
+    end
+
+    context 'when page parameter is invalid' do
+      before do
+        get '/results', params: { page: 'some-site-.co.uk' }
+      end
+
+      it 'responds successfully' do
+        expect(response).to have_http_status(:ok)
+      end
+    end
+  end
+end
