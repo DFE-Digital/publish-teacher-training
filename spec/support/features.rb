1ac7c9d8a736bfa5bc6c2dd95e4b9a9e18f2ab9f
Iain McNulty <iain.mcnulty@education.gov.uk>
2025-04-03 17:38:46 +0100

    Add feature supports to system specs

--- spec/support/features.rb
+++ spec/support/features.rb
@@ -2,11 +2,13 @@
 
 RSpec.configure do |config|
   config.include FeatureHelpers::Authentication, type: :feature
+  config.include FeatureHelpers::Authentication, type: :system
   config.include FeatureHelpers::NewCourseParam, type: :feature
   config.include FeatureHelpers::GovukComponents, type: :feature
   config.include FeatureHelpers::CourseSteps, type: :feature
   config.include FeatureHelpers::PageWithQuery, type: :feature
   config.include DfESignInUserHelper, type: :feature
+  config.include DfESignInUserHelper, type: :system
   config.include FeatureHelpers::PageObject::Support, :support_features
   config.include FeatureHelpers::PageObject::Publish, :publish_features
   config.include FeatureHelpers::PageObject::Find, :find_features
