79b864c9f4b7f8d524ec9b92ffe66884882f89aa
Tomas D'Stefano <tomas_stefano@successoft.com>
2025-04-10 13:22:09 +0100

    Implement batch delivery with staggered job execution
    Add a `BatchDelivery` service to process records in batches with
    staggered timings.
    
    This implementation is used to schedule jobs efficiently by dividing
    a relation into manageable chunks and spacing out their execution using
    a configurable interval.
    
    Initialize `BatchDelivery` with a relation, batch size, and stagger interval:
    
    ```
    BatchDelivery.new(relation:, batch_size: 10, stagger_over: 1.hour).each do |batch_time, records|
      MyJob.set(wait_until: batch_time).perform_later(records.pluck(:id))
    end
    ```
    
    - Ensures jobs are spread out over time to prevent max db connections..

---
+++ app/services/batch_delivery.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+class BatchDelivery
+  attr_accessor :relation, :stagger_over, :batch_size
+
+  def initialize(relation:, stagger_over: 5.hours, batch_size: 100)
+    @relation = relation
+    @stagger_over = stagger_over
+    @batch_size = batch_size
+  end
+
+  def each(&block)
+    next_batch_time = Time.zone.now
+    relation_count = relation.count
+
+    interval_between_batches ||= begin
+      number_of_batches = (relation_count.to_f / batch_size).ceil
+      number_of_batches < 2 ? stagger_over : stagger_over / (number_of_batches - 1).to_f
+    end
+
+    relation.find_in_batches(batch_size:) do |applications|
+      block.call(next_batch_time, applications)
+      next_batch_time += interval_between_batches
+    end
+  end
+end
