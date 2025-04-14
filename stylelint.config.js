8784528f7b13d3309e4b96753b7a1ced02db7066
Daniel Dye <daniel.dye@education.gov.uk>
2025-04-14 12:04:57 +0100

    Add stylelint
    Use the same configuration as Apply: https://github.com.mcas.ms/DFE-Digital/apply-for-teacher-training/blob/main/.stylelintrc.mjs
    
    Run `yarn stylelint --fix` to address fixable (mostly ordering) issues.

---
+++ stylelint.config.js
@@ -0,0 +1,20 @@
+import propertyGroups from 'stylelint-config-recess-order/groups'
+
+export default {
+  extends: ['stylelint-config-gds/scss', 'stylelint-config-recess-order'],
+  plugins: ['stylelint-order'],
+  rules: {
+    // Configure the rule manually.
+    'order/properties-order': propertyGroups.map((group) => ({
+      ...group,
+      emptyLineBefore: 'always',
+      noEmptyLineBetween: true
+    })),
+    'value-keyword-case': [
+      'lower',
+      {
+        camelCaseSvgKeywords: true
+      }
+    ]
+  }
+}
