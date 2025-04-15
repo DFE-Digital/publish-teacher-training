2752b0b7117d59e39fabae36cd9281eef9fdb2b8
Tomas D'Stefano <tomas_stefano@successoft.com>
2025-04-15 14:41:58 +0100

    Move recruitment cycle colour to a method instead of locales

--- app/helpers/support/recruitment_cycles_helper.rb
+++ app/helpers/support/recruitment_cycles_helper.rb
@@ -3,10 +3,18 @@ module Support
     def recruitment_cycle_status_tag(recruitment_cycle)
       options = {
         text: t("support.recruitment_cycles.index.status.#{recruitment_cycle.status}.text"),
-        colour: t("support.recruitment_cycles.index.status.#{recruitment_cycle.status}.colour"),
+        colour: recruitment_cycle_status_colour(recruitment_cycle),
       }
 
       govuk_tag(**options)
+    end
+
+    def recruitment_cycle_status_colour(recruitment_cycle)
+      {
+        current: "green",
+        upcoming: "yellow",
+        inactive: "grey",
+      }[recruitment_cycle.status]
     end
   end
 end
