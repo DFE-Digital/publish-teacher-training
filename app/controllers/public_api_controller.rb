4833276b1268ad5c6fa42bc9313abfa1335afd03
Daniel Dye <daniel.dye@education.gov.uk>
2025-04-16 17:39:25 +0100

    Simplify the RadiusQuickLinkSuggestionsController
    If no query params are included, an empty array is returned.

--- app/controllers/public_api_controller.rb
+++ app/controllers/public_api_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class PublicAPIController < ActionController::API
+  include AbstractController::Translation
   include DfE::Analytics::Requests
   include Pagy::Backend
   include ErrorHandlers::Pagy
