96f93f690ee761850641368b1fd65d03ffae35fa
Daniel Dye <daniel.dye@education.gov.uk>
2025-04-16 17:39:25 +0100

    Simplify the RadiusQuickLinkSuggestionsController
    If no query params are included, an empty array is returned.

--- app/controllers/api/radius_quick_link_suggestions_controller.rb
+++ app/controllers/api/radius_quick_link_suggestions_controller.rb
@@ -1,60 +1,38 @@
 module API
   class RadiusQuickLinkSuggestionsController < PublicAPIController
+    MAX_RADIUS = Courses::SearchForm::RADIUS_VALUES.max
     MAX_COURSES_COUNT = 100
 
     def index
-      @search_params = params.to_unsafe_h.except(:controller, :action)
-
-      return render_json_error(status: 400, message: I18n.t(".api.radius_query_link_suggestions.errors.bad_request")) if @search_params.blank?
-
       render json: no_results_quick_links
     end
 
   private
 
     def no_results_quick_links
-      result = []
-
-      valid_buckets = bucket_counts_from_largest_search_radius.slice_after { |it| it[:count].to_i >= MAX_COURSES_COUNT }.first
-      return result if valid_buckets.blank?
-
-      valid_buckets.each do |it|
-        radius = it[:radius]
-        count = it[:count]
-
-        text = I18n.t(
-          ".api.radius_query_link_suggestions.radius_in_miles",
-          count: radius,
-          miles: I18n.t(".api.radius_query_link_suggestions.miles", count: radius),
-          course_count: course_count_translation(count),
-        )
-
-        result << {
-          text: text,
-          url: find_results_path(**@search_params, radius: radius),
+      Array.wrap(bucket_counts_from_largest_search_radius.slice_after { it[:count].to_i >= MAX_COURSES_COUNT }.first).map do
+        {
+          text: t(".radius_in_miles", count: it[:radius], course_count: course_count_translation(count: it[:count])),
+          url: find_results_path(**request.query_parameters, radius: it[:radius]),
         }
       end
-
-      result
     end
 
     def bucket_counts_from_largest_search_radius
-      radius_links_query = ::Courses::Query.call(params: @search_params.merge(radius: 200)).limit(MAX_COURSES_COUNT + 1)
-      radius_buckets = Courses::SearchForm::RADIUS_VALUES
+      radius_links_query = Courses::Query.call(params: params.merge(radius: MAX_RADIUS)).limit(MAX_COURSES_COUNT + 1)
 
-      radius_buckets.filter_map do |radius|
+      Courses::SearchForm::RADIUS_VALUES.filter_map do |radius|
         count = radius_links_query.count { |course| course.minimum_distance_to_search_location.to_f <= radius }
-        { radius: radius, count: count } if count.positive?
+
+        { radius:, count: } if count.positive?
       end
     end
 
-    def course_count_translation(count)
+    def course_count_translation(count:)
       if count >= MAX_COURSES_COUNT
-        I18n.t(".api.radius_query_link_suggestions.over_100_courses")
+        t(".over_100_courses")
       else
-        I18n.t(".api.radius_query_link_suggestions.course_count",
-               course_count: count,
-               course_word: I18n.t(".api.radius_query_link_suggestions.course_word", count: count))
+        t(".course_count", count:)
       end
     end
   end
