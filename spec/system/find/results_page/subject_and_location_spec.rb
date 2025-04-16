d043d47201be87c7b272a944c6e951d15469d369
Iain McNulty <iain.mcnulty@education.gov.uk>
2025-04-16 13:21:32 +0100

    Separate specs between homepage and results page
      Our system tests are unweildy. I've tried to separate them into
      smaller more specific scenarios.
    
      One apparent discrimintaion I can make is to separate specs that
      happen on the home page and those that happen on the results page.

---
+++ spec/system/find/results_page/subject_and_location_spec.rb
@@ -0,0 +1,444 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe "Search results by subject and location", :js, service: :find do
+  include FiltersFeatureSpecsHelper
+
+  before do
+    Timecop.travel(Find::CycleTimetable.mid_cycle)
+    allow(Rails).to receive(:cache).and_return(ActiveSupport::Cache.lookup_store(:memory_store))
+
+    given_courses_exist_in_various_locations
+    when_i_visit_the_results_page
+  end
+
+  scenario "when I filter by location" do
+    when_i_start_typing_an_invalid_location
+    then_i_see_no_autocomplete_suggestions
+
+    when_i_start_typing_london_location
+    then_i_see_location_suggestions
+    and_the_location_suggestions_for_london_is_cached
+
+    when_i_select_the_first_suggestion
+    and_i_click_to_search_courses_in_london
+    then_i_see_only_courses_within_selected_location_within_default_radius
+    and_the_default_radius_is_selected
+    and_the_location_search_for_coordinates_is_cached
+
+    when_i_increase_the_radius_to_15_miles
+    and_i_click_search
+    then_i_see_courses_up_to_15_miles_distance
+    and_the_15_miles_radius_is_selected
+
+    when_i_increase_the_radius_to_20_miles
+    and_i_click_search
+    then_i_see_courses_up_to_20_miles_distance
+    and_the_20_miles_radius_is_selected
+  end
+
+  scenario "when I filter by location and subject" do
+    when_i_start_typing_an_invalid_location
+    then_i_see_no_autocomplete_suggestions
+
+    when_i_start_typing_london_location
+    then_i_see_location_suggestions
+
+    when_i_select_the_first_suggestion
+    and_i_click_to_search_courses_in_london
+    then_i_see_only_courses_within_selected_location_within_default_radius
+
+    and_select_primary_subject
+    and_i_click_search
+    then_i_see_only_courses_within_selected_location_and_primary_subject_within_default_radius
+  end
+
+  scenario "when I filter by subject" do
+    when_i_search_for_math
+    and_i_choose_the_first_subject_suggestion
+    and_i_click_search
+    then_i_see_only_mathematics_courses
+  end
+
+  scenario "when I search by provider" do
+    when_i_search_for_a_provider
+    and_i_choose_the_first_provider_suggestion
+    and_i_click_search
+    then_i_see_only_courses_from_that_provider
+    and_the_provider_field_is_visible
+  end
+
+  scenario "when search results update after filter changes" do
+    when_i_search_for_math
+    and_i_choose_the_first_subject_suggestion
+
+    when_i_start_typing_london_location
+    then_i_see_location_suggestions
+
+    when_i_select_the_first_suggestion
+    and_i_increase_the_radius_to_15_miles
+    and_i_click_to_search_courses_in_london
+
+    when_i_filter_by_courses_that_sponsor_visa
+    and_i_click_apply_filters
+
+    then_i_see_mathematics_courses_in_15_miles_from_london_that_sponsors_visa
+  end
+
+  scenario "when searching using old location parameters" do
+    when_i_search_courses_in_london_using_old_parameters
+    then_i_see_only_courses_within_selected_location_within_default_radius
+    and_the_default_radius_is_selected
+    and_the_location_search_for_coordinates_is_cached
+    and_london_is_displayed_in_text_field
+  end
+
+  def given_courses_exist_in_various_locations
+    london = build(:location, :london)
+    romford = build(:location, :romford)
+    watford = build(:location, :watford)
+    edinburgh = build(:location, :edinburgh)
+    primary_subject = find_or_create(:primary_subject, :primary)
+    mathematics_subject = find_or_create(:secondary_subject, :mathematics)
+
+    @london_primary_course = create(
+      :course,
+      :primary,
+      :open,
+      name: "Primary - London",
+      provider: create(:provider, provider_name: "First university"),
+      site_statuses: [create(:site_status, :findable, site: create(:site, latitude: london.latitude, longitude: london.longitude))],
+      subjects: [primary_subject],
+    )
+
+    @romford_primary_course = create(
+      :course,
+      :primary,
+      :open,
+      name: "Primary - Romford",
+      provider: create(:provider, provider_name: "Second university"),
+      site_statuses: [create(:site_status, :findable, site: create(:site, latitude: romford.latitude, longitude: romford.longitude))],
+      subjects: [primary_subject],
+    )
+
+    @watford_primary_course = create(
+      :course,
+      :primary,
+      :open,
+      name: "Primary - Watford",
+      site_statuses: [create(:site_status, :findable, site: create(:site, latitude: watford.latitude, longitude: watford.longitude))],
+      subjects: [primary_subject],
+    )
+
+    @edinburgh_mathematics_course = create(
+      :course,
+      :primary,
+      :can_not_sponsor_visa,
+      :open,
+      name: "Primary - Edinburgh",
+      site_statuses: [create(:site_status, :findable, site: create(:site, latitude: edinburgh.latitude, longitude: edinburgh.longitude))],
+      subjects: [primary_subject],
+    )
+
+    @london_mathematics_course = create(
+      :course,
+      :secondary,
+      :open,
+      name: "Mathematics - London",
+      can_sponsor_student_visa: true,
+      site_statuses: [create(:site_status, :findable, site: create(:site, latitude: london.latitude, longitude: london.longitude))],
+      subjects: [mathematics_subject],
+    )
+
+    @romford_mathematics_course = create(
+      :course,
+      :secondary,
+      :can_not_sponsor_visa,
+      :open,
+      name: "Mathematics - Romford",
+      site_statuses: [create(:site_status, :findable, site: create(:site, latitude: romford.latitude, longitude: romford.longitude))],
+      subjects: [mathematics_subject],
+    )
+
+    @watford_mathematics_course = create(
+      :course,
+      :secondary,
+      :can_not_sponsor_visa,
+      :open,
+      name: "Mathematics - Watford",
+      site_statuses: [create(:site_status, :findable, site: create(:site, latitude: watford.latitude, longitude: watford.longitude))],
+      subjects: [mathematics_subject],
+    )
+
+    @edinburgh_mathematics_course = create(
+      :course,
+      :secondary,
+      :can_not_sponsor_visa,
+      :open,
+      name: "Mathematics - Edinburgh",
+      site_statuses: [create(:site_status, :findable, site: create(:site, latitude: edinburgh.latitude, longitude: edinburgh.longitude))],
+      subjects: [mathematics_subject],
+    )
+  end
+
+  def when_i_filter_by_courses_that_sponsor_visa
+    check "Only show courses with visa sponsorship", visible: :all
+  end
+
+  def and_i_click_apply_filters
+    click_link_or_button "Apply filters", match: :first
+  end
+
+  def when_i_visit_the_results_page
+    visit find_results_path
+  end
+
+  def when_i_start_typing_an_invalid_location
+    when_i_start_typing_non_existent_city_location
+  end
+
+  def when_i_start_typing_non_existent_city_location
+    stub_request(
+      :get,
+      "https://maps.googleapis.com/maps/api/place/autocomplete/json?components=country:uk&input=NonExistentCity&key=replace_me&language=en&types=geocode",
+    ).with(
+      headers: {
+        "Accept" => "*/*",
+        "Accept-Encoding" => "gzip;q=1.0,deflate;q=0.6,identity;q=0.3",
+        "Connection" => "keep-alive",
+        "Keep-Alive" => "30",
+        "User-Agent" => "Faraday v#{Faraday::VERSION}",
+      },
+    ).to_return(status: 200, body: file_fixture("google_old_places_api_client/autocomplete/non_existent_city.json"), headers: { "Content-Type" => "application/json" })
+
+    fill_in "City, town or postcode", with: "NonExistentCity"
+  end
+
+  def then_i_see_no_autocomplete_suggestions
+    expect(page).to have_css("#location-field__listbox", visible: :hidden)
+  end
+
+  def and_the_location_suggestions_for_london_is_cached
+    expect(Rails.cache.read("geolocation:suggestions:lon")).to eq(
+      [
+        {
+          name: "London, UK",
+          place_id: "ChIJdd4hrwug2EcRmSrV3Vo6llI",
+          types: %w[locality political],
+        },
+      ],
+    )
+  end
+
+  def and_the_location_search_for_coordinates_is_cached
+    expect(Rails.cache.read("geolocation:query:london-uk")).to eq(
+      {
+        formatted_address: "London, UK",
+        latitude: 51.5072178,
+        longitude: -0.1275862,
+        country: "England",
+        types: %w[locality political],
+      },
+    )
+  end
+
+  def when_i_start_typing_london_location
+    stub_request(
+      :get,
+      "https://maps.googleapis.com/maps/api/place/autocomplete/json?components=country:uk&input=Lon&key=replace_me&language=en&types=geocode",
+    ).with(
+      headers: {
+        "Accept" => "*/*",
+        "Accept-Encoding" => "gzip;q=1.0,deflate;q=0.6,identity;q=0.3",
+        "Connection" => "keep-alive",
+        "Keep-Alive" => "30",
+        "User-Agent" => "Faraday v#{Faraday::VERSION}",
+      },
+    ).to_return(status: 200, body: file_fixture("google_old_places_api_client/autocomplete/london.json"), headers: { "Content-Type" => "application/json" })
+
+    fill_in "City, town or postcode", with: "Lon"
+  end
+
+  def then_i_see_location_suggestions
+    expect(page).to have_css("#location-field__listbox", visible: :visible)
+    expect(page.find_by_id("location-field__listbox")).to have_content("London, UK")
+  end
+
+  def when_i_select_the_first_suggestion
+    page.find_by_id("location-field__option--0").click
+  end
+
+  def and_i_click_to_search_courses_in_london
+    stub_london_location_search
+
+    and_i_click_search
+  end
+
+  def then_i_see_only_courses_within_selected_location_within_default_radius
+    expect(results).to have_content(@london_primary_course.name_and_code)
+    expect(results).to have_content(@london_mathematics_course.name_and_code)
+
+    expect(results).to have_no_content(@romford_primary_course.name_and_code)
+    expect(results).to have_no_content(@romford_mathematics_course.name_and_code)
+    expect(results).to have_no_content(@watford_primary_course.name_and_code)
+    expect(results).to have_no_content(@watford_mathematics_course.name_and_code)
+  end
+
+  def and_the_default_radius_is_selected
+    expect(page).to have_select("Search radius", selected: "10 miles")
+  end
+
+  def and_the_15_miles_radius_is_selected
+    expect(page).to have_select("Search radius", selected: "15 miles")
+  end
+
+  def and_the_20_miles_radius_is_selected
+    expect(page).to have_select("Search radius", selected: "20 miles")
+  end
+
+  def and_i_click_search
+    click_link_or_button "Search"
+  end
+
+  def when_i_increase_the_radius_to_15_miles
+    select "15 miles", from: "radius"
+  end
+  alias_method :and_i_increase_the_radius_to_15_miles, :when_i_increase_the_radius_to_15_miles
+
+  def then_i_see_courses_up_to_15_miles_distance
+    expect(results).to have_content(@london_primary_course.name_and_code)
+    expect(results).to have_content(@london_mathematics_course.name_and_code)
+    expect(results).to have_content(@romford_primary_course.name_and_code)
+    expect(results).to have_content(@romford_mathematics_course.name_and_code)
+
+    expect(results).to have_no_content(@watford_primary_course.name_and_code)
+    expect(results).to have_no_content(@watford_mathematics_course.name_and_code)
+  end
+
+  def when_i_increase_the_radius_to_20_miles
+    select "20 miles", from: "radius"
+  end
+
+  def then_i_see_courses_up_to_20_miles_distance
+    expect(results).to have_content(@london_primary_course.name_and_code)
+    expect(results).to have_content(@london_mathematics_course.name_and_code)
+    expect(results).to have_content(@romford_primary_course.name_and_code)
+    expect(results).to have_content(@romford_mathematics_course.name_and_code)
+    expect(results).to have_content(@watford_primary_course.name_and_code)
+    expect(results).to have_content(@watford_mathematics_course.name_and_code)
+  end
+
+  def and_select_primary_subject
+    fill_in "Subject", with: "Pri"
+
+    and_i_choose_the_first_subject_suggestion
+  end
+
+  def and_i_choose_the_first_subject_suggestion
+    page.find('input[name="subject_name"]').native.send_keys(:return)
+  end
+
+  def then_i_see_only_courses_within_selected_location_and_primary_subject_within_default_radius
+    expect(results).to have_content(@london_primary_course.name_and_code)
+    expect(results).to have_no_content(@london_mathematics_course.name_and_code)
+    expect(results).to have_no_content(@romford_primary_course.name_and_code)
+    expect(results).to have_no_content(@romford_mathematics_course.name_and_code)
+    expect(results).to have_no_content(@watford_primary_course.name_and_code)
+    expect(results).to have_no_content(@watford_mathematics_course.name_and_code)
+  end
+
+  def when_i_search_for_math
+    fill_in "Subject", with: "Mat"
+  end
+
+  def then_i_see_only_mathematics_courses
+    expect(results).to have_content(@london_mathematics_course.name_and_code)
+    expect(results).to have_content(@romford_mathematics_course.name_and_code)
+    expect(results).to have_content(@watford_mathematics_course.name_and_code)
+
+    expect(results).to have_no_content(@london_primary_course.name_and_code)
+    expect(results).to have_no_content(@romford_primary_course.name_and_code)
+    expect(results).to have_no_content(@watford_primary_course.name_and_code)
+  end
+
+  def then_i_see_mathematics_courses_in_15_miles_from_london_that_sponsors_visa
+    expect(results).to have_content(@london_mathematics_course.name_and_code)
+
+    expect(results).to have_no_content(@romford_mathematics_course.name_and_code)
+    expect(results).to have_no_content(@watford_mathematics_course.name_and_code)
+    expect(results).to have_no_content(@london_primary_course.name_and_code)
+    expect(results).to have_no_content(@romford_primary_course.name_and_code)
+    expect(results).to have_no_content(@watford_primary_course.name_and_code)
+  end
+
+  def when_i_search_for_a_provider
+    page.find(
+      "summary.govuk-details__summary",
+      text: "Search by training provider",
+    ).click
+
+    fill_in "Enter a provider name", with: "uni"
+  end
+
+  def and_i_choose_the_first_provider_suggestion
+    page.find_by_id("provider-code-field__option--0").click
+  end
+
+  def then_i_see_only_courses_from_that_provider
+    expect(results).to have_content("First university")
+
+    providers = Provider.where.not(provider_name: "First university")
+
+    providers.each do |provider|
+      expect(results).to have_no_content(provider.provider_name)
+    end
+  end
+
+  def and_the_provider_field_is_visible
+    expect(page).to have_css("details.govuk-details[open]")
+  end
+
+  def when_i_search_courses_in_london_using_old_parameters
+    stub_london_location_search
+
+    visit find_results_path(lq: "London, UK")
+  end
+
+  def and_london_is_displayed_in_text_field
+    expect(
+      page.find_field("City, town or postcode").value,
+    ).to eq("London, UK")
+  end
+
+private
+
+  def results
+    page.first(".app-search-results")
+  end
+
+  def search_params
+    query_params(URI(page.current_url)).symbolize_keys.except(:utm_source, :utm_medium)
+  end
+
+  def stub_london_location_search
+    stub_request(
+      :get,
+      "https://maps.googleapis.com/maps/api/geocode/json?address=London,%20UK&components=country:UK&key=replace_me&language=en",
+    )
+      .with(
+        headers: {
+          "Accept" => "*/*",
+          "Accept-Encoding" => "gzip;q=1.0,deflate;q=0.6,identity;q=0.3",
+          "Connection" => "keep-alive",
+          "Keep-Alive" => "30",
+          "User-Agent" => "Faraday v#{Faraday::VERSION}",
+        },
+      )
+      .to_return(
+        status: 200,
+        body: file_fixture("google_old_places_api_client/geocode/london.json").read,
+        headers: { "Content-Type" => "application/json" },
+      )
+  end
+end
---
+++ spec/system/find/results_page/tracking_spec.rb
@@ -0,0 +1,256 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe "Search results tracking", :js, service: :find do
+  before do
+    Timecop.travel(Find::CycleTimetable.mid_cycle)
+    allow(Settings.features).to receive(:send_request_data_to_bigquery).and_return(true)
+
+    double = instance_double(Find::Analytics::SearchResultsEvent)
+    allow(Find::Analytics::SearchResultsEvent).to receive(:new).and_return(double)
+    allow(double).to receive(:send_event)
+
+    given_some_courses_exist
+    when_i_visit_the_results_page
+  end
+
+  scenario "when applying filters" do
+    and_i_filter_for_primary_courses
+    and_i_apply_filters_using_the_top_button
+    then_search_result_is_tracked_with_applied_filters_with_top_applied_filter
+
+    when_i_apply_filters_using_the_bottom_button
+    then_search_result_is_tracked_with_applied_filters_with_bottom_applied_filter
+  end
+
+  scenario "when searching within the results page" do
+    when_i_filter_for_send_courses
+    and_i_search_for_art_and_design_subject
+    and_i_click_search
+    then_search_result_is_tracked_with_new_search
+  end
+
+  scenario "when sorting courses" do
+    when_i_sort_by_provider_ascending
+    and_i_click_sort
+    then_search_result_order_is_tracked
+  end
+
+  scenario "when bookmarking search results page" do
+    when_i_visit_the_results_page_bookmarked_an_old_tracked_url
+    then_search_result_is_tracked_with_new_search_using_results_as_utm_medium
+  end
+
+  def given_some_courses_exist
+    create(
+      :course,
+      :with_full_time_sites,
+      :secondary,
+      :open,
+      name: "Biology",
+      course_code: "2DTK",
+      provider: build(:provider, provider_name: "London university", provider_code: "19S"),
+    )
+    create(
+      :course,
+      :with_full_time_sites,
+      :published_teacher_degree_apprenticeship,
+      :secondary,
+      :open,
+      name: "Mathematics",
+      course_code: "TDA1",
+      provider: build(:provider, provider_name: "Bristol university", provider_code: "23T"),
+      degree_grade: "not_required",
+    )
+    create(
+      :course,
+      :with_full_time_sites,
+      :primary,
+      :open,
+      :with_special_education_needs,
+      name: "Primary (SEND)",
+      course_code: "P123",
+      provider: build(:provider, provider_name: "Bath university", provider_code: "PO1"),
+    )
+    create(
+      :course,
+      :with_full_time_sites,
+      :secondary,
+      :open,
+      :with_special_education_needs,
+      name: "Art and design (SEND)",
+      course_code: "F314",
+      provider: build(:provider, provider_name: "York university", provider_code: "RO1"),
+      subjects: [find_or_create(:secondary_subject, :art_and_design)],
+    )
+    create(
+      :course,
+      :with_full_time_sites,
+      :open,
+      name: "Further Education",
+      course_code: "F3D",
+      provider: build(:provider, provider_name: "Birmingham university", provider_code: "JL1"),
+      level: "further_education",
+    )
+    create(
+      :course,
+      :with_full_time_sites,
+      :primary,
+      :open,
+      name: "Primary",
+      course_code: "Y565",
+      provider: build(:provider, provider_name: "Brighton university", provider_code: "1UR"),
+    )
+
+    subject_group = create(:subject_group, name: "Arts, humanities and social sciences")
+    find_or_create(:secondary_subject, :art_and_design).update(subject_group:)
+  end
+
+  def and_all_accordions_are_open
+    page.all(".govuk-accordion__section-button").map(&:click)
+  end
+
+  def when_i_browse_teacher_degree_apprenticeship_courses
+    and_all_accordions_are_open
+    click_link_or_button "Browse teacher degree apprenticeship courses."
+  end
+
+  def when_i_visit_the_results_page
+    visit find_results_path
+  end
+
+  def and_i_filter_for_primary_courses
+    check "Primary", visible: :all
+  end
+
+  def and_i_apply_filters_using_the_top_button
+    click_link_or_button "Apply filters", match: :first
+  end
+
+  def then_search_result_is_tracked_with_applied_filters_with_top_applied_filter
+    wait_for do
+      expect(Find::Analytics::SearchResultsEvent).to have_received(:new).with(
+        hash_including(
+          total: 2,
+          page: 1,
+          search_params: hash_including(order: "course_name_ascending", subjects: %w[00]),
+          track_params: hash_including(utm_source: "results", utm_medium: "apply_filters_top"),
+          results: array_including(
+            have_attributes(course_code: "Y565", provider_code: "1UR"),
+            have_attributes(course_code: "P123", provider_code: "PO1"),
+          ),
+        ),
+      )
+    end
+  end
+
+  def when_i_apply_filters_using_the_bottom_button
+    page.all("button", text: "Apply filters").last.click
+  end
+
+  def then_search_result_is_tracked_with_applied_filters_with_bottom_applied_filter
+    wait_for do
+      expect(Find::Analytics::SearchResultsEvent).to have_received(:new).with(
+        hash_including(
+          total: 2,
+          page: 1,
+          search_params: hash_including(order: "course_name_ascending", subjects: %w[00]),
+          track_params: hash_including(utm_source: "results", utm_medium: "apply_filters_bottom"),
+          results: array_including(
+            have_attributes(course_code: "Y565", provider_code: "1UR"),
+            have_attributes(course_code: "P123", provider_code: "PO1"),
+          ),
+        ),
+      )
+    end
+  end
+
+  def when_i_filter_for_send_courses
+    check "Only show courses with a SEND specialism", visible: :all
+  end
+
+  def and_i_search_for_art_and_design_subject
+    fill_in "Subject", with: "Art"
+
+    and_i_choose_the_first_subject_suggestion
+  end
+
+  def and_i_choose_the_first_subject_suggestion
+    page.find('input[name="subject_name"]').native.send_keys(:return)
+  end
+
+  def and_i_click_search
+    click_link_or_button "Search"
+  end
+
+  def then_search_result_is_tracked_with_new_search
+    wait_for do
+      expect(Find::Analytics::SearchResultsEvent).to have_received(:new).with(
+        hash_including(
+          total: 1,
+          page: 1,
+          search_params: hash_including(order: "course_name_ascending", subject_code: "W1", subject_name: "Art and design", send_courses: true),
+          track_params: hash_including(utm_source: "results", utm_medium: "search"),
+          results: array_including(
+            have_attributes(course_code: "F314", provider_code: "RO1"),
+          ),
+        ),
+      )
+    end
+  end
+
+  def when_i_sort_by_provider_ascending
+    select "Training provider (A-Z)"
+  end
+
+  def and_i_click_sort
+    click_link_or_button "Sort"
+  end
+
+  def then_search_result_order_is_tracked
+    wait_for do
+      expect(Find::Analytics::SearchResultsEvent).to have_received(:new).with(
+        hash_including(
+          total: 6,
+          page: 1,
+          search_params: hash_including(order: "provider_name_ascending"),
+          track_params: hash_including(utm_source: "results", utm_medium: "sort"),
+          results: array_including(
+            have_attributes(course_code: "P123", provider_code: "PO1"),
+            have_attributes(course_code: "F3D", provider_code: "JL1"),
+            have_attributes(course_code: "Y565", provider_code: "1UR"),
+            have_attributes(course_code: "TDA1", provider_code: "23T"),
+            have_attributes(course_code: "2DTK", provider_code: "19S"),
+            have_attributes(course_code: "F314", provider_code: "RO1"),
+          ),
+        ),
+      )
+    end
+  end
+
+  def when_i_visit_the_results_page_bookmarked_an_old_tracked_url
+    visit find_results_path(utm_source: "home", utm_medium: "main_search")
+  end
+
+  def then_search_result_is_tracked_with_new_search_using_results_as_utm_medium
+    wait_for do
+      expect(Find::Analytics::SearchResultsEvent).to have_received(:new).twice.with(
+        hash_including(
+          total: 6,
+          page: 1,
+          search_params: {},
+          track_params: an_instance_of(ActionController::Parameters),
+          results: array_including(
+            have_attributes(course_code: "F314", provider_code: "RO1"),
+            have_attributes(course_code: "2DTK", provider_code: "19S"),
+            have_attributes(course_code: "F3D", provider_code: "JL1"),
+            have_attributes(course_code: "TDA1", provider_code: "23T"),
+            have_attributes(course_code: "Y565", provider_code: "1UR"),
+            have_attributes(course_code: "P123", provider_code: "PO1"),
+          ),
+        ),
+      ).twice
+    end
+  end
+end
