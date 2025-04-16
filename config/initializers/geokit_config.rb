9e76fb2bc5d98283116c3dcaff8f5244163a6332
Daniel Dye <daniel.dye@education.gov.uk>
2025-04-16 17:36:47 +0100

    GoogleGeocoder and places API should use the same API key
    Remove the `Settings.gcp_api_key` config and use only `Settings.google.gcp_api_key`.

--- config/initializers/geokit_config.rb
+++ config/initializers/geokit_config.rb
@@ -30,7 +30,7 @@ Geokit::Geocoders.request_timeout = 3
 
 # You can also use the free API key instead of signed requests
 # See https://developers.google.com/maps/documentation/geocoding/#api_key
-Geokit::Geocoders::GoogleGeocoder.api_key = Settings.gcp_api_key
+Geokit::Geocoders::GoogleGeocoder.api_key = Settings.google.gcp_api_key
 
 # You can also set multiple API KEYS for different domains that may be directed
 # to this same application.
