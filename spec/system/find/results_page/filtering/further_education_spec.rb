a881648a1d12fdd6eb4bdb8c30bcbd809ef4f2af
Iain McNulty <iain.mcnulty@education.gov.uk>
2025-04-23 16:43:55 +0100

    Move methods from helper to files that only use it

--- spec/system/find/results_page/filtering/further_education_spec.rb
+++ spec/system/find/results_page/filtering/further_education_spec.rb
@@ -31,4 +31,33 @@ RSpec.describe "when I filter by further education only courses", :js, service: 
     and_the_further_education_filter_is_checked
     and_i_see_that_there_is_one_course_found
   end
+
+  def given_there_are_courses_containing_all_levels
+    create(:course, :with_full_time_sites, :primary, name: "Biology", course_code: "S872")
+    create(:course, :with_full_time_sites, :secondary, name: "Chemistry", course_code: "K592")
+    create(:course, :with_full_time_sites, :further_education, name: "Further education", course_code: "K594")
+  end
+
+  def when_i_visit_the_find_results_page_using_the_old_age_group_parameter
+    visit(find_results_path(age_group: "further_education"))
+  end
+
+  def when_i_visit_the_find_results_page_using_the_old_pgce_pgde_parameter
+    visit(find_results_path(qualification: ["pgce pgde"]))
+  end
+
+  def and_i_filter_by_further_education_courses
+    check "Further education courses", visible: :all
+    and_i_apply_the_filters
+  end
+
+  def then_i_see_only_further_education__courses
+    expect(results).to have_content("Further education (K594)")
+    expect(results).to have_no_content("Biology (S872)")
+    expect(results).to have_no_content("Chemistry (K592)")
+  end
+
+  def and_the_further_education_filter_is_checked
+    expect(page).to have_checked_field("Further education courses", visible: :all)
+  end
 end
--- spec/system/find/results_page/filtering/start_date_spec.rb
+++ spec/system/find/results_page/filtering/start_date_spec.rb
@@ -31,4 +31,90 @@ RSpec.describe "when filtering by start date", :js, service: :find do
     then_i_see_all_courses_regardless_of_start_date
     and_i_see_that_six_courses_are_found
   end
+
+  def given_courses_exist_with_varied_start_dates
+    @january_course = create(
+      :course,
+      :with_full_time_sites,
+      name: "Art and design",
+      start_date: DateTime.new(current_recruitment_cycle_year, 1, 1),
+    )
+    @august_course = create(
+      :course,
+      :with_full_time_sites,
+      name: "Biology",
+      start_date: DateTime.new(current_recruitment_cycle_year, 8, 1),
+    )
+    @beginning_of_september_course = create(
+      :course,
+      :with_full_time_sites,
+      name: "Computing",
+      start_date: DateTime.new(current_recruitment_cycle_year, 9, 1),
+    )
+    @middle_of_september_course = create(
+      :course,
+      :with_full_time_sites,
+      name: "English",
+      start_date: DateTime.new(current_recruitment_cycle_year, 9, 15),
+    )
+    @end_of_september_course = create(
+      :course,
+      :with_full_time_sites,
+      name: "Primary with english",
+      start_date: DateTime.new(current_recruitment_cycle_year, 9, 30),
+    )
+    @october_course = create(
+      :course,
+      :with_full_time_sites,
+      name: "Spanish",
+      start_date: DateTime.new(current_recruitment_cycle_year, 10, 1),
+    )
+  end
+
+  def then_i_see_only_courses_starting_in_september
+    expect(results).to have_content(@beginning_of_september_course.name)
+    expect(results).to have_content(@middle_of_september_course.name)
+    expect(results).to have_content(@end_of_september_course.name)
+
+    expect(results).to have_no_content(@january_course.name)
+    expect(results).to have_no_content(@august_course.name)
+    expect(results).to have_no_content(@october_course.name)
+  end
+
+  def then_i_see_only_courses_not_starting_in_september
+    expect(results).to have_content(@january_course.name)
+    expect(results).to have_content(@august_course.name)
+    expect(results).to have_content(@october_course.name)
+
+    expect(results).to have_no_content(@beginning_of_september_course.name)
+    expect(results).to have_no_content(@middle_of_september_course.name)
+    expect(results).to have_no_content(@end_of_september_course.name)
+  end
+
+  def when_i_filter_by_all_start_date_options
+    when_i_filter_by_september_start_date
+    when_i_filter_by_non_september_start_dates
+  end
+
+  def then_i_see_all_courses_regardless_of_start_date
+    expect(results).to have_content(@january_course.name)
+    expect(results).to have_content(@august_course.name)
+    expect(results).to have_content(@october_course.name)
+    expect(results).to have_content(@beginning_of_september_course.name)
+    expect(results).to have_content(@middle_of_september_course.name)
+    expect(results).to have_content(@end_of_september_course.name)
+  end
+
+  def and_i_see_that_six_courses_are_found
+    expect(page).to have_content("6 courses found")
+    expect(page).to have_title("6 courses found")
+  end
+
+  def when_i_filter_by_non_september_start_dates
+    check "All other dates", visible: :all
+  end
+
+  def when_i_filter_by_september_start_date
+    check "September #{current_recruitment_cycle_year}", visible: :all
+  end
 end
