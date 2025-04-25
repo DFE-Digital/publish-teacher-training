86210d02c5c8b811de7e398105cc22112241f3f2
Daniel Dye <daniel.dye@education.gov.uk>
2025-04-25 14:14:42 +0100

    Clean up system test config
    When :js is provided, Capybara automatically sets the #current_driver to the #javascript_driver.
    
    We only need to ensure system specs are driven by the #current_driver, which is configured in rails_helper.

--- spec/support/system_test_config.rb
+++ spec/support/system_test_config.rb
@@ -3,8 +3,7 @@
 # Use different Capybara ports when running tests in parallel
 Capybara.server_port = 9887 + ENV["TEST_ENV_NUMBER"].to_i if ENV["TEST_ENV_NUMBER"]
 
-# Cannot use puma, we may need to upgrade rackup > 3
-Capybara.server = :webrick
+Capybara.server = :puma
 
 Capybara.register_driver :selenium_chrome_headless do |app|
   options = Selenium::WebDriver::Chrome::Options.new.tap do |opts|
@@ -26,25 +25,7 @@ Capybara.javascript_driver = :selenium_chrome_headless
 Capybara.automatic_label_click = true
 
 RSpec.configure do |config|
-  screen_size = [3400, 3400]
-
-  config.before(:each, type: :system) do
-    service = self.class.metadata[:service]
-
-    Capybara.app_host = "http://www.#{service}-test.lvh.me"
-    driven_by :rack_test
-  end
-
-  config.before(:each, :js, type: :system) do
-    driven_by :selenium_chrome_headless, using: :headless_chrome, screen_size:
-
-    if Capybara.current_driver == :selenium_chrome_headless
-      logs = page.driver.browser.manage.logs.get(:browser)
-      logs.each { |log| puts log.message }
-    end
-  end
-
   config.before(:each, :js_browser, type: :system) do
-    driven_by :selenium, using: :chrome, screen_size:
+    Capybara.current_driver = :selenium_chrome
   end
 end
