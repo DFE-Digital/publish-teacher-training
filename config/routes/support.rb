f5c607b701214ff832b5b489ba8c79d4f37d98a6
Iain McNulty <iain.mcnulty@education.gov.uk>
2025-04-28 11:56:30 +0100

    Remove unused accredited partnership routes in Support

--- config/routes/support.rb
+++ config/routes/support.rb
@@ -50,7 +50,7 @@ namespace :support do
       end
 
       scope module: :providers do
-        resources :accredited_partnerships, param: :accredited_provider_code, only: %i[index new edit create update], path: "accredited-partnerships" do
+        resources :accredited_partnerships, param: :accredited_provider_code, only: %i[index destroy], path: "accredited-partnerships" do
           member do
             get :delete
             delete :delete, to: "accredited_partnerships#destroy"
@@ -58,8 +58,6 @@ namespace :support do
           get "/check", on: :collection, to: "accredited_partnerships/checks#show"
           put "/check", on: :collection, to: "accredited_partnerships/checks#update"
         end
-
-        resources :copy_courses, only: %i[new create]
 
         resources :accredited_providers, param: :accredited_provider_code, only: %i[], path: "accredited-providers" do
           get "/search", on: :collection, to: "accredited_provider_search#new"
