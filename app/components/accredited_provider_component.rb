503496996a1d19744c0a4ee0085639f568b47b62
Iain McNulty <iain.mcnulty@education.gov.uk>
2025-04-28 13:59:19 +0100

    Update AccreditedProviderComponent

--- app/components/accredited_provider_component.rb
+++ app/components/accredited_provider_component.rb
@@ -3,13 +3,11 @@
 class AccreditedProviderComponent < ViewComponent::Base
   include PublishHelper
 
-  attr_reader :provider_name, :remove_path, :about_accredited_provider, :change_about_accredited_provider_path
+  attr_reader :provider, :remove_path
 
-  def initialize(provider_name:, remove_path:, about_accredited_provider:, change_about_accredited_provider_path:)
+  def initialize(provider:, remove_path:)
     super
-    @provider_name = provider_name
+    @provider = provider
     @remove_path = remove_path
-    @about_accredited_provider = about_accredited_provider
-    @change_about_accredited_provider_path = change_about_accredited_provider_path
   end
 end
