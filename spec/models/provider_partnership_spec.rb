c74dd91dfa3639dc438491b8ba85e04724dcb6dd
Iain McNulty <iain.mcnulty@education.gov.uk>
2025-04-28 16:43:26 +0100

    Update specs

--- spec/models/provider_partnership_spec.rb
+++ spec/models/provider_partnership_spec.rb
@@ -3,15 +3,11 @@
 require "rails_helper"
 
 describe ProviderPartnership do
-  let(:partnership) { create(:provider_partnership, description: "Great partnership") }
+  let(:partnership) { create(:provider_partnership) }
 
   it "creates a partnership" do
     expect(partnership.accredited_provider).to be_accredited
     expect(partnership.training_provider).not_to be_accredited
-  end
-
-  it "has a description" do
-    expect(partnership.description).to eq("Great partnership")
   end
 
   describe "validations" do
