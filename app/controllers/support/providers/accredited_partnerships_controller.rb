d24d945032631fc1e5c06a62868b145f36fb6a34
Iain McNulty <iain.mcnulty@education.gov.uk>
2025-04-28 12:00:40 +0100

    Delete unused actions from Support::AccreditedPartnershipsController

--- app/controllers/support/providers/accredited_partnerships_controller.rb
+++ app/controllers/support/providers/accredited_partnerships_controller.rb
@@ -13,47 +13,6 @@ module Support
         @pagy, @partnerships = pagy(provider.accredited_partnerships)
       end
 
-      def new
-        partnership = provider.accredited_partnerships.build
-        partnership.assign_attributes(accredited_provider_id: params[:accredited_provider_id] || ProviderPartnershipForm.new(current_user, provider).accredited_provider_id)
-
-        @accredited_provider_form = ProviderPartnershipForm.new(current_user, partnership)
-        return unless partnership.invalid?
-
-        redirect_to search_support_recruitment_cycle_provider_accredited_providers_path(RecruitmentCycle.current.year), flash: { error: { message: "#{Provider.find(params[:accredited_provider_id]).name_and_code} partnership already exists" } }
-      end
-
-      def edit
-        partnership = provider.accredited_partnerships.find_by(accredited_provider: partner)
-        params = { accredited_provider_id: partner.id, description: partnership.description }
-        @accredited_provider_form = ::ProviderPartnershipForm.new(current_user, partnership, params:)
-      end
-
-      def create
-        @accredited_provider_form = ::ProviderPartnershipForm.new(current_user, provider, params: accredited_provider_params)
-
-        if @accredited_provider_form.stash
-          redirect_to check_support_recruitment_cycle_provider_accredited_partnerships_path(accredited_provider_id: partnership_params[:accredited_provider_id])
-        else
-          render :new
-        end
-      end
-
-      def update
-        partnership = provider.accredited_partnerships.find_by(accredited_provider: partner)
-        @accredited_provider_form = ::ProviderPartnershipForm.new(current_user, partnership, params: accredited_provider_params)
-
-        if @accredited_provider_form.save!
-          redirect_to support_recruitment_cycle_provider_accredited_partnerships_path(
-            recruitment_cycle_year: @recruitment_cycle.year,
-            provider_id: @provider.id,
-          ), flash: { success: t(".updated") }
-
-        else
-          render(:edit)
-        end
-      end
-
       def delete
         cannot_delete
         @accredited_provider = partner
@@ -81,25 +40,11 @@ module Support
       end
 
       def accredited_provider_id
-        params[:accredited_provider_id] || @accredited_provider_form.accredited_provider_id
-      end
-
-      def accredited_provider_form
-        @accredited_provider_form ||= ::ProviderPartnershipForm.new(current_user, provider, params: { accredited_provider_id: params[:accredited_provider_id] })
-      end
-
-      def accredited_provider_params
-        params.require(:provider_partnership_form)
-              .except(:goto_confirmation)
-              .permit(::ProviderPartnershipForm::FIELDS)
+        params[:accredited_provider_id]
       end
 
       def partner
         recruitment_cycle.providers.find_by(provider_code: params[:accredited_provider_code])
-      end
-
-      def partnership_params
-        params.expect(provider_partnership_form: %i[accredited_provider_id description])
       end
     end
   end
