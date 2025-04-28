833f95d24dd42a152b5037d4fcba3f24d0dbc0c5
Iain McNulty <iain.mcnulty@education.gov.uk>
2025-04-28 16:39:00 +0100

    Remove description from factory

--- spec/factories/provider_partnerships.rb
+++ spec/factories/provider_partnerships.rb
@@ -4,6 +4,5 @@ FactoryBot.define do
   factory :provider_partnership do
     accredited_provider { association(:accredited_provider) }
     training_provider { association(:provider) }
-    description { Faker::Lorem.sentence.to_s }
   end
 end
