c16e774fb0f80dbfb67f25fb4985c8c9f8d8d7a0
Daniel Dye <daniel.dye@education.gov.uk>
2025-04-30 14:07:13 +0100

    Remove references to Settings.find_valid_referers

--- app/helpers/find/view_helper.rb
+++ app/helpers/find/view_helper.rb
@@ -13,18 +13,21 @@ module Find
     def permitted_referrer?
       return false if request.referer.blank?
 
-      request.referer.include?(request.host_with_port) ||
-        Settings.find_valid_referers.any? { |url| request.referer.start_with?(url) }
+      [*Settings.find_hosts, *Settings.publish_hosts].include?(referer.host)
     end
 
     def course_back_link
-      referer = URI(request.referer)
       if referer.request_uri =~ %r{^/results}
-
         request.referer
       elsif referer && session[:results_path] =~ %r{^/results}
         session[:results_path]
       end
+    end
+
+  private
+
+    def referer
+      URI.parse(request.referer)
     end
   end
 end
