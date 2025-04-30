4f288e107553c62434119c362e924d073eb16134
Daniel Dye <daniel.dye@education.gov.uk>
2025-04-30 10:37:14 +0100

    Update CSP configuration and use JS nonce

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -10,26 +10,14 @@ Rails.application.configure do
   config.content_security_policy do |policy|
     all_domains = [*Settings.find_hosts, *Settings.publish_hosts, *Settings.api_hosts]
 
-    policy.default_src :self
-    policy.font_src    :self, :data, "fonts.googleapis.com", *all_domains
-    policy.img_src     :self,
-                       :https,
-                       :data
-
-    policy.object_src  :none
-    policy.script_src  :self,
-                       "'sha256-aBGeCwtg0DKytYpKh9kIMvmYL0sooy9+McqPAZ5feTk='", # add_js_enabled_class_to_body.html.erb
-                       *all_domains
-
-    policy.connect_src :self,
-                       "https://*.sentry.io",
-                       "https://www.google.com",
-                       *all_domains
-
-    policy.style_src   :self, *all_domains
-
-    policy.frame_src   :self,
-                       *all_domains
+    policy.default_src     :self
+    policy.font_src        :self, :data, "fonts.googleapis.com", *all_domains
+    policy.img_src         :self, :https, :data
+    policy.object_src      :none
+    policy.script_src      :self, *all_domains
+    policy.style_src       :self, *all_domains
+    policy.frame_ancestors :none
+    policy.connect_src     :self, "https://*.sentry.io", *all_domains
 
     # Specify URI for violation reports
     # policy.report_uri "/csp-violation-report-endpoint"
