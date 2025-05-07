0cf50fe60ccd0024fff12810d0345b6ebe981d6f
Daniel Dye <daniel.dye@education.gov.uk>
2025-05-07 12:10:48 +0100

    Set application time zone to Europe/London

--- config/application.rb
+++ config/application.rb
@@ -33,7 +33,7 @@ module ManageCoursesBackend
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = "Europe/London"
     # config.eager_load_paths << Rails.root.join("extras")
 
     config.active_record.pluralize_table_names = false
