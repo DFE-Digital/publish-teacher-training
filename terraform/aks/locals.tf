3ab7adf211fa2e872d6b40ce841859d23d70af9a
vipin-dfe <vipin.chadha@education.gov.uk>
2025-05-16 11:13:21 +0100

    Add review app ingress for api

--- terraform/aks/locals.tf
+++ terraform/aks/locals.tf
@@ -1,6 +1,6 @@
 locals {
   app_name_suffix             = var.app_name == null ? var.app_environment : "${var.app_environment}-${var.app_name}"
-  review_additional_hostnames = var.app_environment == "review" ? ["find-${local.app_name_suffix}.${var.cluster}.teacherservices.cloud"] : ["find-${local.app_name_suffix}.${var.cluster}.development.teacherservices.cloud"]
+  review_additional_hostnames = var.app_environment == "review" ? ["find-${local.app_name_suffix}.${var.cluster}.teacherservices.cloud", "publish-${local.app_name_suffix}-api.${var.cluster}.teacherservices.cloud" ] : ["find-${local.app_name_suffix}.${var.cluster}.development.teacherservices.cloud"]
   db_setup_command            = ["/bin/sh", "-c", "bundle exec rails db:setup && bundle exec rails server -b 0.0.0.0"]
   worker_startup_command      = ["/bin/sh", "-c", "bundle exec sidekiq -c 5 -C config/sidekiq.yml"]
   postgres_extensions         = ["PG_BUFFERCACHE", "PG_STAT_STATEMENTS", "BTREE_GIN", "BTREE_GIST", "CITEXT", "UUID-OSSP", "POSTGIS"]
