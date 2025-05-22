a83c52b433bb13a53757103b97e1d9e742719e82
Iain McNulty <iain.mcnulty@education.gov.uk>
2025-05-22 11:31:59 +0100

    Remove presence validation on GiasSchool properties
      These properties can be blank on the official GIAS source so we
      shouldn't be validating their presence

--- spec/models/gias_school_spec.rb
+++ spec/models/gias_school_spec.rb
@@ -8,9 +8,6 @@ describe GiasSchool do
   it { is_expected.to be_valid }
   it { is_expected.to validate_presence_of(:urn) }
   it { is_expected.to validate_presence_of(:name) }
-  it { is_expected.to validate_presence_of(:address1) }
-  it { is_expected.to validate_presence_of(:town) }
-  it { is_expected.to validate_presence_of(:postcode) }
   it { is_expected.to validate_uniqueness_of(:urn).case_insensitive }
 
   context "callbacks" do
