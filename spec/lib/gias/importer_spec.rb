47bce0e08e21fee1de5a556044e8f5d9e17572f4
Iain McNulty <iain.mcnulty@education.gov.uk>
2025-05-22 11:34:09 +0100

    Save GiasSchools with callbacks
      Only save records when they have changed. New records will be saved.

--- spec/lib/gias/importer_spec.rb
+++ spec/lib/gias/importer_spec.rb
@@ -38,9 +38,56 @@ RSpec.describe Gias::Importer do
         postcode: "EC3A 5DE",
         website: "www.thealdgateschool.org",
         telephone: "02072831147",
-        searchable: "'100000':1 '123':7 'name':3,5 'ol':6 'ol123':8 'old':2,4,9 'town':10",
+        searchable: "'100000':1 '5de':9 'aldgate':3,6 'ec3a':8 'ec3a5de':10 'london':11 'school':4,7 'the':2,5",
         latitude: 51.513968813644965,
         longitude: -0.077530631715809 },
     )
+  end
+
+  context "when school hasn't changed" do
+    let!(:test_csv) do
+      StringIO.new(<<~CSV_DATA)
+        urn,name,type_code,group_code,status_code,phase_code,minimum_age,maximum_age,ukprn,address1,address2,address3,town,county,postcode,website,telephone,latitude,longitude
+        100000,The Aldgate School,02,4,1,2,3,11,10079319,St James's Passage,Duke's Place,"","","",EC3A 5DE,www.thealdgateschool.org,02072831147,51.513968813644965,-0.077530631715809
+      CSV_DATA
+    end
+
+    it "does not save the school again" do
+      freeze_time
+      now = Time.zone.now
+
+      described_class.call(test_csv)
+
+      school = GiasSchool.last
+
+      expect(school.reload).to have_attributes(
+        { urn: "100000",
+          name: "The Aldgate School",
+          type_code: "02",
+          group_code: "4",
+          status_code: "1",
+          phase_code: "2",
+          minimum_age: "3",
+          maximum_age: "11",
+          ukprn: "10079319",
+          address1: "St James's Passage",
+          address2: "Duke's Place",
+          address3: "",
+          town: "",
+          county: "",
+          postcode: "EC3A 5DE",
+          website: "www.thealdgateschool.org",
+          telephone: "02072831147",
+          searchable: "'100000':1 '5de':9 'aldgate':3,6 'ec3a':8 'ec3a5de':10 'school':4,7 'the':2,5",
+          latitude: 51.513968813644965,
+          longitude: -0.077530631715809 },
+      )
+
+      expect(GiasSchool.last.updated_at).to be_within(1.second).of(now)
+
+      travel_to 1.minute.from_now do
+        expect { described_class.call(test_csv) }.not_to(change { GiasSchool.last.updated_at })
+      end
+    end
   end
 end
