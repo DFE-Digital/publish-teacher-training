8151eb6cfb6317412efe30293d8fee3e8b2da8e2
Tomas D'Stefano <tomas_stefano@successoft.com>
2025-05-23 11:44:34 +0100

    Improve rollover progress information
    It was difficult to understand what is happening.
    
    Now we can see the providers that weren't rolled over
    to check if is correct.
    
    Also fixed:
    
    * Calculate total providers only the ones eligible for rollover - provider with published/withdrawn courses.
    
    Added:
    
    * Show the amount of providers with no provider courses
    * Show the amount of remaining providers to be rolled over
    * Show a better of rollover status

---
+++ app/services/rollover_progress.rb
@@ -0,0 +1,99 @@
+class RolloverProgress
+  include ActiveModel::Model
+
+  attr_reader :previous_target_cycle, :target_cycle
+
+  def initialize(target_cycle:)
+    @target_cycle = target_cycle
+    @previous_target_cycle = RecruitmentCycle.find_by(year: @target_cycle.year.to_i - 1)
+  end
+
+  def status
+    {
+      text: I18n.t("#{i18n_path}.#{rollover_status}"),
+      colour: colours[rollover_status],
+    }
+  end
+
+  def summary
+    return I18n.t("#{i18n_path}.no_previous_cycle") if previous_target_cycle.blank?
+
+    I18n.t(
+      "#{i18n_path}.summary",
+      total_eligible_providers_count:,
+      rolled_over_providers_count:,
+      rollover_percentage:,
+    )
+  end
+
+  def remaining_to_rollover_count
+    total_eligible_providers_count - rolled_over_providers_count
+  end
+
+  delegate :count, to: :providers_without_published_courses, prefix: true
+
+  delegate :count, to: :total_eligible_providers, prefix: true
+
+  delegate :count, to: :rolled_over_providers, prefix: true
+
+  def rollover_percentage
+    return 0 if total_eligible_providers_count.zero? || rolled_over_providers_count.zero?
+
+    (rolled_over_providers_count.to_f / total_eligible_providers_count * 100).round(2)
+  end
+
+  def providers_without_published_courses
+    @previous_target_cycle.providers.where.not(
+      id: total_eligible_providers.select(:id),
+    ).distinct
+  end
+
+  def total_eligible_providers
+    @total_eligible_providers ||= @previous_target_cycle.providers.where(
+      id: providers_with_own_rollable_courses.select(:id),
+    ).or(
+      @previous_target_cycle.providers.where(
+        id: providers_with_rollable_accredited_courses.select(:id),
+      ),
+    ).distinct
+  end
+
+  def rolled_over_providers
+    @rolled_over_providers ||= @target_cycle.providers.where("created_at < ?", @target_cycle.application_start_date)
+  end
+
+private
+
+  def providers_with_own_rollable_courses
+    @previous_target_cycle.providers
+      .joins(courses: :most_recent_enrichment)
+      .where(course_enrichment: { status: %i[published withdrawn] })
+      .distinct
+  end
+
+  def providers_with_rollable_accredited_courses
+    @previous_target_cycle.providers
+      .joins(accredited_courses: :most_recent_enrichment)
+      .where(course_enrichment: { status: %i[published withdrawn] })
+      .distinct
+  end
+
+  def i18n_path
+    "#{self.class.i18n_scope}.attributes.#{self.class.model_name.i18n_key}"
+  end
+
+  def rollover_status
+    if @target_cycle.upcoming?
+      :in_progress
+    else
+      :finished
+    end
+  end
+
+  def colours
+    {
+      in_progress: "yellow",
+      finished: "green",
+    }
+  end
+end
