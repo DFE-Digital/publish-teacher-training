6f4a4c2929b53a08a4f358e23220c3f87266261c
Tomas D'Stefano <tomas_stefano@successoft.com>
2025-05-28 16:44:56 +0100

    Split rollover progress from db and view

--- app/helpers/support/recruitment_cycles_helper.rb
+++ app/helpers/support/recruitment_cycles_helper.rb
@@ -16,5 +16,34 @@ module Support
         inactive: "grey",
       }[recruitment_cycle.status]
     end
+
+    def rollover_status(target_cycle:)
+      colours = {
+        in_progress: "yellow",
+        finished: "green",
+      }
+
+      status = if target_cycle.upcoming?
+                 :in_progress
+               else
+                 :finished
+               end
+
+      {
+        text: t(".rollover_status.#{status}"),
+        colour: colours[status],
+      }
+    end
+
+    def rollover_summary(previous_target_cycle:, total_eligible_providers_count:, rolled_over_providers_count:, rollover_percentage:)
+      return t(".rollover_status.no_previous_cycle") if previous_target_cycle.blank?
+
+      t(
+        ".rollover_status.summary",
+        total_eligible_providers_count:,
+        rolled_over_providers_count:,
+        rollover_percentage:,
+      )
+    end
   end
 end
