d5c71cef20c8717d5449f4a585dcd93c0c220457
gms-gs <george.schena@education.gov.uk>
2025-05-28 16:23:16 +0100

    Fix spacing between sections in preview

--- app/javascript/publish/controllers/input_preview_controller.js
+++ app/javascript/publish/controllers/input_preview_controller.js
@@ -14,23 +14,20 @@ export default class extends Controller {
       // Convert bullet points: * text -> <li>text</li> (removes the '*')
       content = content.replace(/^\* ([^\n]+)(?:\s*\(([^\)]+)\))?$/gm, (match, text, url) => {
         if (url) {
-          // If a URL is provided in the parentheses, create a link
           listContent += `<li><a class="govuk-link" href="${url}">${text}</a></li>`
         } else {
-          // Otherwise, just create a list item with plain text
           listContent += `<li>${text}</li>`
         }
-        return "" // Remove the bullet point line after adding to listContent
+        return "" // Remove bullet point line
       })
 
-      // Only wrap the list items in <ul class="govuk-list govuk-list--bullet"> if listContent is not empty
       if (listContent) {
         listContent = `<ul class="govuk-list govuk-list--bullet">${listContent}</ul>`
       }
 
+      // Convert newlines to <br> and wrap in <p>
       content = content.replace(/\n/g, '<br>')
-      previewContent += `${content}${listContent}`
-      previewContent += `<p>&nbsp;</p>` // Empty paragraph to create a space between sections
+      previewContent += `<p>${content}</p>${listContent}`
     })
 
     this.previewTarget.innerHTML = previewContent
