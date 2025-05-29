4da7b7541801dda896c04550434bd6b561e512cb
gms-gs <george.schena@education.gov.uk>
2025-05-29 12:37:22 +0100

    Add preview to Where and how you will train

--- app/javascript/publish/controllers/input_preview_controller.js
+++ app/javascript/publish/controllers/input_preview_controller.js
@@ -4,52 +4,64 @@ export default class extends Controller {
   static targets = ['input', 'preview']
 
   updatePreview(event) {
-    let previewContent = ""
+    const previewMap = new Map()
 
+    // Group inputs by their preview target (individual or shared)
     this.inputTargets.forEach(input => {
-      const rawContent = input.value
-      const lines = rawContent.split('\n')
+      const previewKey = input.dataset.previewOutputTarget || 'shared'
 
-      let blockHtml = ""
-      let currentListItems = []
-
-      const flushList = () => {
-        if (currentListItems.length > 0) {
-          blockHtml += `<ul class="govuk-list govuk-list--bullet">${currentListItems.join('')}</ul>`
-          currentListItems = []
-        }
+      if (!previewMap.has(previewKey)) {
+        previewMap.set(previewKey, [])
       }
-
-      lines.forEach(line => {
-        const bulletMatch = line.match(/^\* (.+)$/)
-        if (bulletMatch) {
-          let text = bulletMatch[1]
-
-          text = text.replace(
-            /\[([^\]]+)\]\(([^)]+)\)/g,
-            '<a class="govuk-link" href="$2">$1</a>'
-          )
-
-          currentListItems.push(`<li>${text}</li>`)
-        } else {
-          flushList()
-
-          if (line.trim() === '') {
-            blockHtml += `<p></p>`
-          } else {
-            const processed = line.replace(
-              /\[([^\]]+)\]\(([^)]+)\)/g,
-              '<a class="govuk-link" href="$2">$1</a>'
-            )
-            blockHtml += `<p>${processed}</p>`
-          }
-        }
-      })
-
-      flushList()
-      previewContent += blockHtml
+      previewMap.get(previewKey).push(input)
     })
 
-    this.previewTarget.innerHTML = previewContent
+    // Update each preview region
+    previewMap.forEach((inputs, key) => {
+      const previewEl = key === 'shared'
+        ? this.previewTargets[0]
+        : this.previewTargets.find(p => p.dataset.previewTarget === key)
+
+      if (!previewEl) return
+
+      let previewContent = ''
+
+      inputs.forEach(input => {
+        const rawContent = input.value
+        const lines = rawContent.split('\n')
+
+        let blockHtml = ''
+        let currentListItems = []
+
+        const flushList = () => {
+          if (currentListItems.length > 0) {
+            blockHtml += `<ul class="govuk-list govuk-list--bullet">${currentListItems.join('')}</ul>`
+            currentListItems = []
+          }
+        }
+
+        lines.forEach(line => {
+          const bulletMatch = line.match(/^\* (.+)$/)
+          if (bulletMatch) {
+            let text = bulletMatch[1]
+            text = text.replace(/\[([^\]]+)\]\(([^)]+)\)/g, '<a class="govuk-link" href="$2">$1</a>')
+            currentListItems.push(`<li>${text}</li>`)
+          } else {
+            flushList()
+            if (line.trim() === '') {
+              blockHtml += `<p></p>`
+            } else {
+              const processed = line.replace(/\[([^\]]+)\]\(([^)]+)\)/g, '<a class="govuk-link" href="$2">$1</a>')
+              blockHtml += `<p>${processed}</p>`
+            }
+          }
+        })
+
+        flushList()
+        previewContent += blockHtml
+      })
+
+      previewEl.innerHTML = previewContent
+    })
   }
 }
