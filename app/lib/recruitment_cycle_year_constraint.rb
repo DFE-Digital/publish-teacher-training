5ee8439d85d56bf0c8a91d35350036d64fe64895
Tomas D'Stefano <tomas_stefano@successoft.com>
2025-06-03 15:46:56 +0100

    WIP

--- app/lib/recruitment_cycle_year_constraint.rb
+++ app/lib/recruitment_cycle_year_constraint.rb
@@ -1,6 +1,6 @@
 class RecruitmentCycleYearConstraint
   def matches?(request)
-    recruitment_cycle_year_param = request.params[:recruitment_cycle_year]
+    recruitment_cycle_year_param = request.params[:recruitment_cycle_year] || request.params[:year]
 
     return false if recruitment_cycle_year_param.blank?
 
