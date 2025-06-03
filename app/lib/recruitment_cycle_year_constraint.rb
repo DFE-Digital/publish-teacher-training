9b188c5ef39eb123dc5b94b9bfa9d20725bb62c2
Tomas D'Stefano <tomas_stefano@successoft.com>
2025-06-03 12:20:35 +0100

    WIP

---
+++ app/lib/recruitment_cycle_year_constraint.rb
@@ -0,0 +1,9 @@
+class RecruitmentCycleYearConstraint
+  def matches?(request)
+    recruitment_cycle_year_param = request.params[:recruitment_cycle_year]
+
+    return false if recruitment_cycle_year_param.blank?
+
+    recruitment_cycle_year_param >= RecruitmentCycle.current.year
+  end
+end
