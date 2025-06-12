78938d51031f7bff8bd5d26c0a5bfe8fc9bcd06b
Iain McNulty <iain.mcnulty@education.gov.uk>
2025-06-12 15:56:26 +0100

    Use traits_for_enum in authentication factory

--- spec/factories/authentications.rb
+++ spec/factories/authentications.rb
@@ -2,6 +2,7 @@ FactoryBot.define do
   factory :authentication do
     authenticable { create(:candidate) }
     subject_key { "urn:fdc:gov.uk:2022:FxfvtauQJr_igWuhWM1VammyaszsOfndSyGsgmibajM" }
-    provider { "govuk_one_login" }
+    traits_for_enum :provider
+    provider { :govuk_one_login }
   end
 end
