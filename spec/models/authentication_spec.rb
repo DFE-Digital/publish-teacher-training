93d0ebcbb00bad0bea8d312b94a63ad617e1079d
Iain McNulty <iain.mcnulty@education.gov.uk>
2025-06-12 14:03:06 +0100

    Validate Authentication authenticable unique to provider

--- spec/models/authentication_spec.rb
+++ spec/models/authentication_spec.rb
@@ -1,9 +1,17 @@
 require "rails_helper"
 
 RSpec.describe Authentication, type: :model do
-  let!(:auth) { create(:authentication) }
+  let(:auth) { create(:authentication, :developer) }
 
   it "is associated with an authenticable" do
     expect(auth.authenticable).to be_a(Candidate)
+  end
+
+  it "authenticable is unique given provider" do
+    candidate = create(:candidate, :logged_in)
+
+    authentication = candidate.authentications.last.dup
+    expect(authentication).not_to be_valid
+    expect(authentication.errors.full_messages).to include("Authenticable should be unique to the given provider")
   end
 end
