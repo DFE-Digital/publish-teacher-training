8667710bf0c7992c1450943768a40fa6ee31500d
Tomas D'Stefano <tomas_stefano@successoft.com>
2025-06-18 16:49:26 +0100

    Add environment as confirmation of rollover too
    More safer.
    
    Also add autocomplete off for all confirm fields

--- spec/forms/support/review_rollover_form_spec.rb
+++ spec/forms/support/review_rollover_form_spec.rb
@@ -1,7 +1,10 @@
 require "rails_helper"
 
 RSpec.describe Support::ReviewRolloverForm, type: :model do
-  subject { described_class.new(confirmation:) }
+  subject { described_class.new(confirmation:, environment:) }
+
+  let(:confirmation) { "" }
+  let(:environment) { "" }
 
   context "when confirmation is blank" do
     let(:confirmation) { "" }
@@ -21,8 +24,27 @@ RSpec.describe Support::ReviewRolloverForm, type: :model do
     end
   end
 
-  context "when confirmation is correct" do
+  context "when environment is blank" do
+    let(:environment) { "" }
+
+    it "is invalid" do
+      expect(subject).not_to be_valid
+      expect(subject.errors[:environment]).to include("You must type the environment name to proceed.")
+    end
+  end
+
+  context "when environment is incorrect" do
+    let(:confirmation) { "something else" }
+
+    it "is invalid with custom message" do
+      expect(subject).not_to be_valid
+      expect(subject.errors[:environment]).to include("You must type the environment name to proceed.")
+    end
+  end
+
+  context "when confirmation and environment is correct" do
     let(:confirmation) { "confirm rollover" }
+    let(:environment) { "test" }
 
     it "is valid" do
       expect(subject).to be_valid
