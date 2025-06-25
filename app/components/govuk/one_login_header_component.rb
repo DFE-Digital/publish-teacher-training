c44b421d8a266a37771acd8f5e5cd62d10bab162
Iain McNulty <iain.mcnulty@education.gov.uk>
2025-06-25 13:02:45 +0100

    Add GovukOneLoginHeaderComponent
      - Copy govuk-one-login/service-header markup, css and js
      - Wrap the markup in a ViewComponent
      - Initialize the javascript (mobile dropdown in header)

---
+++ app/components/govuk/one_login_header_component.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+module Govuk
+  class OneLoginHeaderComponent < GovukComponent::HeaderComponent
+    attr_reader :current_user
+
+    # data-module="one-login-header" is used to initialize the js on mobile
+    def initialize(current_user:, html_attributes: { data: { module: "one-login-header" } })
+      super(html_attributes:)
+
+      @current_user = current_user
+    end
+
+    def sign_out_link
+      button_to("Sign out", find_sign_out_path, class: %w[
+        one-login-header__nav__link--one-login
+        one-login-header__button-link
+        one-login-header__nav__link
+      ], method: :delete)
+    end
+
+    def sign_in_link
+      button_to("Sign in", path, class: %w[
+        one-login-header__nav__link--one-login
+        one-login-header__button-link
+        one-login-header__nav__link
+      ], method: :post)
+    end
+
+    def path
+      "/auth/find-developer"
+    end
+  end
+end
