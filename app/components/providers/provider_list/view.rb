8864d87081126f90eb3699476024bf919fe8b29d
sarahcrack <73823091+sarahcrack@users.noreply.github.com>
2025-07-01 13:20:28 +0100

    refactor: extract AccreditedProviderDetails component and simplify accredited partnerships view
    - Create a new component to render accredited provider details
    - Render the AccreditedProviderDetails component from the accredited partnerships show page
    - Add spec for AccreditedProviderDetails component
    - Remove conditional rendering code from ProviderListComponent
    - Refactor to pass explicit locals to accredited provider table row partial instead of the full partnership object
    - Add provider partnerships to index action for use in index view, moving data fetching from the view into the controller

--- app/components/providers/provider_list/view.rb
+++ app/components/providers/provider_list/view.rb
@@ -6,14 +6,9 @@ module Providers
       include Support::TimeHelper
       include Publish::ValueHelper
 
-      def initialize(provider:, display_accredited_provider_name: false, display_is_the_organisation_an_accredited_provider: false, display_accredited_provider_number: false, display_contact_details: false, display_change_links: false, classes: [], html_attributes: {})
+      def initialize(provider:, classes: [], html_attributes: {})
         super(classes:, html_attributes:)
         @provider = provider
-        @display_accredited_provider_name = display_accredited_provider_name
-        @display_is_the_organisation_an_accredited_provider = display_is_the_organisation_an_accredited_provider
-        @display_accredited_provider_number = display_accredited_provider_number
-        @display_contact_details = display_contact_details
-        @display_change_links = display_change_links
       end
 
       def formatted_provider_type
@@ -22,26 +17,6 @@ module Providers
 
       def formatted_accrediting_provider
         @provider.accredited? ? "Yes" : "No"
-      end
-
-      def display_accredited_provider_name?
-        @display_accredited_provider_name
-      end
-
-      def display_is_the_organisation_an_accredited_provider?
-        @display_is_the_organisation_an_accredited_provider
-      end
-
-      def display_accredited_provider_number?
-        @display_accredited_provider_number
-      end
-
-      def display_contact_details?
-        @display_contact_details
-      end
-
-      def display_change_links?
-        @display_change_links
       end
     end
   end
