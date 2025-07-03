78afd7def5c005379a8cc0dff41774c80f7f447e
Iain McNulty <iain.mcnulty@education.gov.uk>
2025-07-03 12:45:04 +0100

    Candidate authentication factory should be developer

--- spec/factories/authentications.rb
+++ spec/factories/authentications.rb
@@ -3,6 +3,6 @@ FactoryBot.define do
     authenticable { create(:candidate) }
     subject_key { "urn:fdc:gov.uk:2022:FxfvtauQJr_igWuhWM1VammyaszsOfndSyGsgmibajM" }
     traits_for_enum :provider
-    provider { :govuk_one_login }
+    provider { :developer }
   end
 end
