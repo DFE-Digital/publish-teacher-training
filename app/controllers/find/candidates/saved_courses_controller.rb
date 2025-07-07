6737fda8e9d1a8f5d702f96ed20a93ea86591df2
gms-gs <george.schena@education.gov.uk>
2025-07-07 17:32:26 +0100

    Add ability to delete a saved course

--- app/controllers/find/candidates/saved_courses_controller.rb
+++ app/controllers/find/candidates/saved_courses_controller.rb
@@ -17,12 +17,37 @@ module Find
         end
       end
 
+      def undo
+        course = Course.find(params[:course_id])
+
+        if SaveCourseService.call(candidate: @candidate, course:)
+          redirect_to find_candidate_saved_courses_path
+        else
+          redirect_to find_candidate_saved_courses_path(course, error: t(".save_failed"))
+        end
+      end
+
       def destroy
         saved_course = @candidate.saved_courses.find(params[:id])
-        course = Course.find(saved_course.course_id)
+        course = saved_course.course
 
         if saved_course.destroy
-          redirect_to_course(course)
+          undo_link = view_context.render(
+            partial: "find/candidates/saved_courses/undo_link",
+            locals: { label: t(".undo"), undo_path: undo_find_candidate_saved_courses_path, course: },
+          )
+
+          flash[:success_with_body] = {
+            title: t(".success_message_title"),
+            body: t(
+              ".success_message_html",
+              provider_name: course.provider_name,
+              course_name_and_code: course.name_and_code,
+              undo_link: undo_link,
+            ),
+          }
+
+          redirect_to find_candidate_saved_courses_path
         else
           redirect_to_course(course, error: t(".unsave_failed"))
         end
