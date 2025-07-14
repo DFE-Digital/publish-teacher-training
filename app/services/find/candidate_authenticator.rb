5ee12b5792bef635ddcb7125482bd9f5ff332129
Iain McNulty <iain.mcnulty@education.gov.uk>
2025-07-14 18:20:02 +0100

    Update Candiate Authenticator

--- app/services/find/candidate_authenticator.rb
+++ app/services/find/candidate_authenticator.rb
@@ -9,25 +9,46 @@ module Find
     # 1. Find existing Candidate via provider authentication
     # 2. Update Candidate email if it has changed
     # 3. Create a new candidate if one does not exist yet
+    # @return Candidate
     def call
-      email_address = oauth.info.email
-      authentication = ::Authentication.find_by(subject_key: oauth.uid)
-
       if authentication.present?
-        candidate = authentication.authenticable
+        sign_in!
+      else
+        sign_up!
+      end
+    end
+
+  private
+
+    # @return Candidate
+    def sign_up!
+      candidate = nil
+      Candidate.transaction do
+        candidate = Candidate.create(email_address:)
+
+        provider = ::Authentication.provider_map(oauth.provider)
+        candidate.authentications.build(provider:, subject_key: oauth.uid)
+        candidate.save!
+      end
+      candidate
+    end
+
+    # @return Candidate
+    def sign_in!
+      authentication.authenticable.tap do |candidate|
         unless candidate.email_address.casecmp?(email_address)
           candidate.update(email_address:)
         end
-      else
-        Candidate.transaction do
-          candidate = Candidate.create(email_address:)
-
-          provider = ::Authentication.provider_map(oauth.provider)
-          candidate.authentications.build(provider:, subject_key: oauth.uid)
-          candidate.save!
-        end
       end
-      candidate
+    end
+
+    # @return Authentication
+    def authentication
+      @authentication ||= ::Authentication.find_by(subject_key: oauth.uid)
+    end
+
+    def email_address
+      oauth.info.email
     end
   end
 end
