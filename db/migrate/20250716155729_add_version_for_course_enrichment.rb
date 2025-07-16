07f6a26b5c2625f80d65946f93d7fde7d6b4a680
Simon L <simon.lange@education.gov.uk>
2025-07-16 18:55:14 +0100

    adding new JSON fields to course enrichment for long form course content

---
+++ db/migrate/20250716155729_add_version_for_course_enrichment.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+# This migration adds a version column to the course_enrichments table.
+class AddVersionForCourseEnrichment < ActiveRecord::Migration[8.0]
+  # This migration adds a version column to the course_enrichments table.
+  # It sets the default version to 1 for existing records and updates
+  # records created after October 1, 2025, to have a version of 2.
+  def up
+    add_column :course_enrichment, :version, :integer, default: 1, null: false
+
+    # Ensure existing records have the initial version set
+    CourseEnrichment.update_all(version: 1)
+    CourseEnrichment.where("created_at > ?", Time.new(2025, 10, 1)).update_all(version: 2)
+  end
+
+  # This method is used to remove the version column if needed.
+  # It is not strictly necessary but can be useful for rollback purposes.
+  # If you want to keep the versioning, you can remove this method.
+  def down
+    remove_column :course_enrichment, :version
+  end
+end
