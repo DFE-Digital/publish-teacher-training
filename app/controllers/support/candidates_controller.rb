5358827a4f6b57e07ed4085b34b7109a3efb43f9
Iain McNulty <iain.mcnulty@education.gov.uk>
2025-07-18 16:58:59 +0100

    Add pagination

--- app/controllers/support/candidates_controller.rb
+++ app/controllers/support/candidates_controller.rb
@@ -1,7 +1,7 @@
 module Support
   class CandidatesController < ApplicationController
     def index
-      @candidates = Candidate.order(created_at: :desc)
+      @pagy, @candidates = pagy(Candidate.order(created_at: :desc))
     end
   end
 end
