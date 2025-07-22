01fc45dd043e979ec04a272d9ccab26fa5002ac2
Iain McNulty <iain.mcnulty@education.gov.uk>
2025-07-22 16:41:38 +0100

    An existing authentication should not trigger unique validation error
      When an authentication is created, when we check if it is valid,
      we should exclude itself from duplication checks

--- spec/models/authentication_spec.rb
+++ spec/models/authentication_spec.rb
@@ -2,16 +2,20 @@ require "rails_helper"
 
 RSpec.describe Authentication, type: :model do
   let(:auth) { create(:authentication, :developer) }
+  let(:candidate) { create(:candidate, :logged_in) }
 
   it "is associated with an authenticable" do
     expect(auth.authenticable).to be_a(Candidate)
   end
 
   it "authenticable is unique given provider" do
-    candidate = create(:candidate, :logged_in)
-
     authentication = candidate.authentications.last.dup
     expect(authentication).not_to be_valid
     expect(authentication.errors.full_messages).to include("Authenticable should be unique to the given provider")
+  end
+
+  it "is valid" do
+    authentication = candidate.authentications.first
+    expect(authentication).to be_valid
   end
 end
