33bddd16603a2dc309059029b8dce97097cd05b4
Tomas D'Stefano <tomas_stefano@successoft.com>
2025-07-23 11:57:04 +0100

    Add ProcessSummary lifecycle methods
    - Add .start!, #finish!, and #fail! instance methods
    - Ensure summaries are initialized with correct status and timestamps

--- app/models/data_hub/process_summary.rb
+++ app/models/data_hub/process_summary.rb
@@ -10,6 +10,36 @@ module DataHub
 
     validates :status, :type, :started_at, presence: true
 
+    def self.start!
+      create!(
+        started_at: Time.current,
+        status: :started,
+        short_summary: {},
+        full_summary: {},
+      )
+    end
+
+    def finish!(short_summary:, full_summary:)
+      update!(
+        finished_at: Time.current,
+        status: :finished,
+        short_summary: short_summary,
+        full_summary: full_summary,
+      )
+    end
+
+    def fail!(error)
+      update!(
+        finished_at: Time.current,
+        status: :failed,
+        short_summary: {
+          error_class: error.class.to_s,
+          error_message: error.message,
+          backtrace: error.backtrace&.take(10),
+        },
+      )
+    end
+
     def duration_in_seconds
       return unless started_at && finished_at
 
--- app/services/register_school_importer/import_orchestrator.rb
+++ app/services/register_school_importer/import_orchestrator.rb
@@ -7,12 +7,7 @@ module RegisterSchoolImporter
     end
 
     def run!
-      import_record = DataHub::RegisterSchoolImportSummary.create!(
-        started_at: Time.current,
-        status: "started",
-        short_summary: {},
-        full_summary: {},
-      )
+      import_record = DataHub::RegisterSchoolImportSummary.start!
 
       summary = Importer.new(
         recruitment_cycle: @recruitment_cycle,
@@ -20,25 +15,14 @@ module RegisterSchoolImporter
         school_creator_class: @school_creator_class,
       ).call
 
-      import_record.update!(
-        finished_at: Time.current,
-        status: "finished",
+      import_record.finish!(
         short_summary: summary.meta,
         full_summary: summary.full_summary,
       )
 
       import_record
     rescue StandardError => e
-      import_record.update!(
-        finished_at: Time.current,
-        status: "failed",
-        short_summary: {
-          error_class: e.class.to_s,
-          error_message: e.message,
-          backtrace: e.backtrace,
-        },
-      )
-
+      import_record.fail!(e)
       raise e
     end
   end
