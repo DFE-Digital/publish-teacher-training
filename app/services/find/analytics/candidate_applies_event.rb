c2d6c750e6698112e643b0f6bb5f34b94472a457
gms-gs <george.schena@education.gov.uk>
2025-07-28 11:16:34 +0100

    [Candidate] Track event Candidate applies

---
+++ app/services/find/analytics/candidate_applies_event.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+module Find
+  module Analytics
+    class CandidateAppliesEvent < ApplicationEvent
+      attr_accessor :candidate_id, :course_id
+
+      def event_name
+        :candidate_applies
+      end
+
+      def event_data
+        {
+          namespace: "find",
+          candidate_id:,
+          course_id:,
+          was_course_saved: was_course_saved?,
+          timestamp: Time.now.utc,
+        }
+      end
+
+      def was_course_saved?
+        candidate_id.present? && SavedCourse.exists?(candidate_id:, course_id:)
+      end
+    end
+  end
+end
