bb3980500c489fe457f2043f0801701810616b8e
gms-gs <george.schena@education.gov.uk>
2025-07-30 18:16:26 +0100

    Longform: Update ‘Fees and financial support’ page in Publish

---
+++ app/forms/publish/course_content_course_fees_and_financial_support_form.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+module Publish
+  class CourseContentCourseFeesAndFinancialSupportForm < BaseModelForm
+    include RecruitmentCycleHelper
+    include FundingTypeFormMethods
+
+    alias_method :course_enrichment, :model
+    delegate :is_fee_based?, :version, to: :course_enrichment
+
+    FIELDS = %i[fee_uk_eu fee_international fee_schedule additional_fees financial_support].freeze
+
+    attr_accessor(*FIELDS)
+
+    validates :fee_schedule, words_count: { maximum: 50 }, if: :is_fee_based?
+    validates :additional_fees, words_count: { maximum: 50 }, if: :is_fee_based?
+
+    validates :financial_support,
+              words_count: { maximum: 250 },
+              if: -> { is_fee_based? && version.to_i == 1 }
+
+    validates :financial_support,
+              words_count: { maximum: 50 },
+              if: -> { is_fee_based? && version.to_i == 2 }
+
+    validates :fee_uk_eu,
+              numericality: { allow_blank: true,
+                              only_integer: true,
+                              greater_than_or_equal_to: 0,
+                              less_than_or_equal_to: 100_000 },
+              if: :is_fee_based?
+
+    validates :fee_international,
+              numericality: { allow_blank: true,
+                              only_integer: true,
+                              greater_than_or_equal_to: 0,
+                              less_than_or_equal_to: 100_000 },
+              if: :is_fee_based?
+
+  private
+
+    def declared_fields
+      FIELDS
+    end
+  end
+end
