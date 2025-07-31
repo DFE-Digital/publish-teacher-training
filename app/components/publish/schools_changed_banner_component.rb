fd29f00214a361750cc5b05d9cca7c1165a2d0fe
Tomas D'Stefano <tomas_stefano@successoft.com>
2025-07-31 12:34:20 +0100

    Add notification banner to schools (2026 cycle only)
    Add a notification banner to the ‘Schools’ landing page in Publish
    that communicates the changes that have been made to the providers account.

---
+++ app/components/publish/schools_changed_banner_component.rb
@@ -0,0 +1,39 @@
+module Publish
+  class SchoolsChangedBannerComponent < ViewComponent::Base
+    def initialize(provider:)
+      super
+
+      @provider = provider
+      @recruitment_cycle = provider.recruitment_cycle
+    end
+
+    def render?
+      @recruitment_cycle.schools_outcome?
+    end
+
+    def added_count
+      @added_count ||= @provider.sites.school.register_import.count
+    end
+
+    def removed_count
+      @removed_count ||= @provider
+        .sites
+        .school
+        .with_discarded
+        .where(discarded_via_script: true)
+        .count
+    end
+
+    def show_added?
+      added_count.positive?
+    end
+
+    def show_removed?
+      removed_count.positive?
+    end
+
+    def show_minimal?
+      !show_added? && !show_removed?
+    end
+  end
+end
--- app/controllers/publish/providers/schools/added_schools_controller.rb
+++ app/controllers/publish/providers/schools/added_schools_controller.rb
@@ -4,7 +4,11 @@ module Publish
   module Providers
     module Schools
       class AddedSchoolsController < ApplicationController
+        before_action :render_not_found, unless: :schools_outcome?
+
         def index
+          authorize(provider, :index?)
+
           @added_schools = @provider.sites.school.register_import.order(:location_name)
         end
       end
