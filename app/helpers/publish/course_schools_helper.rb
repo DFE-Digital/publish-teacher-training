ffbe4d5a7ae43fc5464808116dce437368c336d6
sarahcrack <73823091+sarahcrack@users.noreply.github.com>
2025-08-01 09:08:44 +0100

    Adapt content on Add a course flow, Schools page, following review
    - update CourseSchoolsHelper spec
    - refactor: CourseSchoolsHelper

--- app/helpers/publish/course_schools_helper.rb
+++ app/helpers/publish/course_schools_helper.rb
@@ -1,17 +1,21 @@
 module Publish
   module CourseSchoolsHelper
     def school_label_for(course)
-      t("publish.courses.schools.heading.#{course.salaried? ? 'salaried' : 'unsalaried'}")
+      t("publish.courses.schools.heading.#{course_type_key(course)}")
     end
 
     def school_warning_text(course)
-      school_type = t("publish.courses.schools.#{course.salaried? ? 'salaried' : 'unsalaried'}").downcase
+      school_type = t("publish.courses.schools.#{course_type_key(course)}").downcase
       t("publish.courses.schools.new.warning_text", school_type: school_type)
     end
 
     def school_label_with_plural(course, count:)
-      prefix = t("publish.courses.schools.#{course.salaried? ? 'salaried' : 'unsalaried'}")
+      prefix = t("publish.courses.schools.#{course_type_key(course)}")
       t("publish.courses.schools.label", count: count, prefix: prefix)
+    end
+
+    def course_type_key(course)
+      course.salaried? ? "salaried" : "unsalaried"
     end
   end
 end
