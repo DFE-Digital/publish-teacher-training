34e8dd666d3ab0879e3de53a46bf68597e0289c8
Tomas D'Stefano <tomas_stefano@successoft.com>
2025-08-04 18:01:23 +0100

    Add banner to attach schools to a course page

---
+++ app/components/publish/school_simple_notification_banner_component.rb
@@ -0,0 +1,12 @@
+class Publish::SchoolSimpleNotificationBannerComponent < ViewComponent::Base
+  def initialize(recruitment_cycle:, provider:)
+    @recruitment_cycle = recruitment_cycle
+    @provider = provider
+
+    super
+  end
+
+  def render?
+    @recruitment_cycle.rollover_period_2026?
+  end
+end
