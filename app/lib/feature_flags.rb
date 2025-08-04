e75b32e6eb6b42c34b494da57f6ed15cdc90ce50
gms-gs <george.schena@education.gov.uk>
2025-08-04 17:22:48 +0100

    Add long_form_content feature flag

--- app/lib/feature_flags.rb
+++ app/lib/feature_flags.rb
@@ -8,6 +8,7 @@ class FeatureFlags
       [:bursaries_and_scholarships_announced, "Display scholarship and bursary information", "Find and Publish team"],
       [:hide_applications_open_date, "Hides the the option to create, edit and view a courses open date"],
       [:candidate_accounts, "Enable candidate accounts feature", "Find and Publish team"],
+      [:long_form_content, "Enable long form content", "Find and Publish team"],
     ]
   end
 end
