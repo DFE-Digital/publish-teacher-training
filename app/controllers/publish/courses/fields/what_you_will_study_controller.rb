ffb186a88ed9372762996455848c09e336c82dd4
sarahcrack <73823091+sarahcrack@users.noreply.github.com>
2025-08-05 12:41:03 +0100

    Refactor: Rename course_content namespace to fields and update routes and fix controller/form references
    - Adjust routing structure and params handling for provider and course
    - Fix constant references for copy fields and form classes
    - Move forms to Publish::Fields namespace and update strong params
    - Resolve NameErrors and namespace issues
    - update ViewHelper and description content to render error message for theoretical training activities
    - error message on description page can only currently be viewed if you change the version to 1 for :theoretical_training_activities in app/models/course_enrichment.rb
    - changed what_you_will_study locale file to sit under 'fields'

--- app/controllers/publish/courses/fields/what_you_will_study_controller.rb
+++ app/controllers/publish/courses/fields/what_you_will_study_controller.rb
@@ -1,26 +1,26 @@
-# app/controllers/publish/courses/course_content/what_you_will_study_controller.rb
+# app/controllers/publish/courses/fields/what_you_will_study_controller.rb
 module Publish
   module Courses
-    module CourseContent
+    module Fields
       class WhatYouWillStudyController < ApplicationController
         include CopyCourseContent
         before_action :authorise_with_pundit
         def edit
-          @course_content_what_you_will_study_form = Publish::CourseContentWhatYouWillStudyForm.new(
+          @what_you_will_study_form = Publish::Fields::WhatYouWillStudyForm.new(
             course_enrichment,
           )
-          @copied_fields = copy_content_check(Publish::CourseContentWhatYouWillStudyForm::WHAT_YOU_WILL_STUDY_FIELDS)
+          @copied_fields = copy_content_check(::Courses::Copy::WHAT_YOU_WILL_STUDY_FIELDS)
           @copied_fields_values = copied_fields_values if @copied_fields.present?
 
-          @course_content_what_you_will_study_form.valid? if show_errors_on_publish?
+          @what_you_will_study_form.valid? if show_errors_on_publish?
         end
 
         def update
-          @course_content_what_you_will_study_form = Publish::CourseContentWhatYouWillStudyForm.new(
+          @what_you_will_study_form = Publish::Fields::WhatYouWillStudyForm.new(
             course_enrichment,
             params: what_you_will_study_params,
           )
-          if @course_content_what_you_will_study_form.save!
+          if @what_you_will_study_form.save!
             course_updated_message CourseEnrichment.human_attribute_name("what_you_will_study")
             redirect_to publish_provider_recruitment_cycle_course_path(provider.provider_code,
                                                                        recruitment_cycle.year,
@@ -35,7 +35,7 @@ module Publish
       private
 
         def what_you_will_study_params
-          params.require(:publish_course_content_what_you_will_study_form).permit(*CourseContentWhatYouWillStudyForm::FIELDS)
+          params.require(:publish_fields_what_you_will_study_form).permit(*Publish::Fields::WhatYouWillStudyForm::FIELDS)
         end
 
         def authorise_with_pundit
@@ -43,7 +43,7 @@ module Publish
         end
 
         def course_to_authorise
-          @course_to_authorise ||= provider.courses.find_by!(course_code: params[:course_code])
+          @course_to_authorise ||= provider.courses.find_by!(course_code: params[:code])
         end
 
         def course
