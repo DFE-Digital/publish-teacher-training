4d449537e4bd7a9c74c4bb43d4cde1a1bf873fc8
RMcVelia <rick.mcvelia@gmail.com>
2025-08-06 15:18:56 +0100

    Add front door rate limiting

--- terraform/custom_domains/environment_domains/variables.tf
+++ terraform/custom_domains/environment_domains/variables.tf
@@ -7,3 +7,18 @@ variable "multiple_hosted_zones" {
   type = bool
   default = false
 }
+
+variable "allow_aks" {
+  type = bool
+  default = false
+}
+
+variable "block_ip" {
+  type = bool
+  default = false
+}
+
+variable "rate_limit_max" {
+  type = number
+  default = null
+}
