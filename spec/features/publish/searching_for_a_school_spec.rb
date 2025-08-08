45ffd037721299a5ac594d0744201ea8825bae75
Iain McNulty <iain.mcnulty@education.gov.uk>
2025-08-08 13:39:55 +0100

    Replace "Bernard" from test subjects
      Tests can become flaky if we use text that might be autogenerated by
      Faker. Try use text that won't match something generated by Faker

--- spec/features/publish/searching_for_a_school_spec.rb
+++ spec/features/publish/searching_for_a_school_spec.rb
@@ -41,7 +41,7 @@ private
   end
 
   def and_there_are_schools_in_the_database
-    @school = create(:gias_school, name: "Bernard", urn: "466415", postcode: "NW1 5WS", town: "Damonmouth")
+    @school = create(:gias_school, name: "Enda's", urn: "466415", postcode: "NW1 5WS", town: "Damonmouth")
     @school_two = create(:gias_school, name: "School Two")
     @school_three = create(:gias_school, name: "School Three")
   end
