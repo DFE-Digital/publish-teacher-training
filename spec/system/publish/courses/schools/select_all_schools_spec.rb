4e8d835819335351523287ea758d5b01f5eb51ea
Tomas D'Stefano <tomas_stefano@successoft.com>
2025-08-15 14:26:39 +0100

    Add flash notice when enqueue schools update

--- spec/system/publish/courses/schools/select_all_schools_spec.rb
+++ spec/system/publish/courses/schools/select_all_schools_spec.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require "rails_helper"
+require "sidekiq/testing"
 
 RSpec.describe "Publish - Select all schools", :js, type: :system do
   before do
@@ -23,22 +24,42 @@ RSpec.describe "Publish - Select all schools", :js, type: :system do
     then_i_should_see_the_no_schools_validation_error
   end
 
+  context "when many schools" do
+    around do |example|
+      Sidekiq::Testing.inline! do
+        example.run
+      end
+    end
+
+    scenario "enqueue when many schools to update" do
+      given_there_are_many_schools
+      when_i_visit_the_publish_course_school_edit_page
+      when_i_select_all_schools
+      and_i_submit
+      then_i_should_see_the_enqueued_success_message
+      and_many_schools_should_be_attached_to_courses
+    end
+  end
+
   def given_i_am_authenticated_as_a_provider_user
-    given_i_am_authenticated(
-      user: create(
-        :user,
-        providers: [
-          build(
-            :provider,
-            sites: [
-              build(:site, location_name: "Site 1"),
-              build(:site, location_name: "Site 2"),
-              build(:site, location_name: "Site 3"),
-            ],
-          ),
-        ],
-      ),
+    @provider = build(
+      :provider,
+      sites: [
+        build(:site, location_name: "Site 1"),
+        build(:site, location_name: "Site 2"),
+        build(:site, location_name: "Site 3"),
+      ],
     )
+    @user = create(
+      :user,
+      providers: [provider],
+    )
+    given_i_am_authenticated(user: @user)
+    @provider.reload
+  end
+
+  def given_there_are_many_schools
+    @schools = create_list(:site, 30, provider: @provider)
   end
 
   def and_there_is_a_course_i_want_to_edit
@@ -74,8 +95,19 @@ RSpec.describe "Publish - Select all schools", :js, type: :system do
       .to contain_exactly("Site 1", "Site 2", "Site 3")
   end
 
+  def and_many_schools_should_be_attached_to_courses
+    expect(course.reload.sites.map(&:location_name))
+      .to match_array(["Site 1", "Site 2", "Site 3", @schools.map(&:location_name)].flatten)
+  end
+
   def then_i_should_see_the_no_schools_validation_error
     expect(page).to have_content("Select at least one school")
+  end
+
+  def then_i_should_see_the_enqueued_success_message
+    expect(page).to have_content(
+      "We are now processing your added schools, please wait a minute until all your schools are added",
+    )
   end
 
   def given_a_course_exists(sites: [])
@@ -84,7 +116,5 @@ RSpec.describe "Publish - Select all schools", :js, type: :system do
 
   attr_reader :course
 
-  def provider
-    @current_user.providers.first
-  end
+  attr_reader :provider
 end
