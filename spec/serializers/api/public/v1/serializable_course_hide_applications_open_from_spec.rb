47468ed366a4e61b1a09271c55d4b93c567b6e64
gms-gs <george.schena@education.gov.uk>
2025-08-15 10:29:19 +0100

    Update spec

--- spec/serializers/api/public/v1/serializable_course_hide_applications_open_from_spec.rb
+++ spec/serializers/api/public/v1/serializable_course_hide_applications_open_from_spec.rb
@@ -9,23 +9,11 @@ RSpec.describe API::Public::V1::SerializableCourse do
   let(:course) { create(:course, :with_accrediting_provider, enrichments: [enrichment], funding: "apprenticeship") }
   let(:resource) { described_class.new(object: course) }
 
-  it "returns applications_open_from from the recruitment cycle when the flag is on" do
-    FeatureFlag.activate(:hide_applications_open_date)
-
+  it "returns applications_open_from from the recruitment cycle" do
     expect(json["attributes"]).to have_key("applications_open_from")
 
     expect(json["attributes"]["applications_open_from"]).to eq(
       course.recruitment_cycle.application_start_date.iso8601,
-    )
-  end
-
-  it "returns the course applications_open_from when the flag is off" do
-    FeatureFlag.deactivate(:hide_applications_open_date)
-
-    expect(json["attributes"]).to have_key("applications_open_from")
-
-    expect(json["attributes"]["applications_open_from"]).to eq(
-      course.applications_open_from.iso8601,
     )
   end
 end
