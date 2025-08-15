46c2b11c50691b752b6db030cdb8f78399d09aca
Simon L <simon.lange@education.gov.uk>
2025-08-15 16:45:47 +0100

    Fixing the preview page so that it also works for long form course content

--- app/components/course_preview/missing_information_component.rb
+++ app/components/course_preview/missing_information_component.rb
@@ -36,6 +36,11 @@ module CoursePreview
     def fee_uk_eu_link = fees_publish_provider_recruitment_cycle_course_path(provider_code, recruitment_cycle_year, course_code, goto_preview: true)
     def gcse_link = gcses_pending_or_equivalency_tests_publish_provider_recruitment_cycle_course_path(provider_code, recruitment_cycle_year, course_code, goto_preview: true)
     def how_school_placements_work_link = school_placements_publish_provider_recruitment_cycle_course_path(provider_code, recruitment_cycle_year, course_code, goto_preview: true)
+    def school_placement_link = fields_school_placement_publish_provider_recruitment_cycle_course_path(provider_code, recruitment_cycle_year, course_code, goto_preview: true)
+    def what_you_will_study_link = fields_what_you_will_study_publish_provider_recruitment_cycle_course_path(provider_code, recruitment_cycle_year, course_code, goto_preview: true)
+    def interview_process_link = fields_interview_process_publish_provider_recruitment_cycle_course_path(provider_code, recruitment_cycle_year, course_code, goto_preview: true)
+    def fees_and_financials_link = fields_fees_and_financial_support_publish_provider_recruitment_cycle_course_path(provider_code, recruitment_cycle_year, course_code, goto_preview: true)
+    def where_you_will_train_link = fields_where_you_will_train_publish_provider_recruitment_cycle_course_path(provider_code, recruitment_cycle_year, course_code, goto_preview: true)
 
     def train_with_us_link
       if FeatureFlag.active?(:long_form_content) || Current.recruitment_cycle.after_2025?
