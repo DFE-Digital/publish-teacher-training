ed503efe44de33d18d201ac43e1c32adf7dcfd5e
Tomas D'Stefano <tomas_stefano@successoft.com>
2025-08-19 16:50:17 +0100

    Tweaking more rollover job

--- app/jobs/rollover_job.rb
+++ app/jobs/rollover_job.rb
@@ -3,12 +3,17 @@
 class RolloverJob < ApplicationJob
   queue_as :default
 
-  BATCH_SIZE = 2
+  BATCH_SIZE = 1
+  STAGGER_OVER = 3
 
   def perform(recruitment_cycle_id)
     relation = RecruitmentCycle.current_recruitment_cycle.providers
 
-    BatchDelivery.new(relation:, stagger_over: 2.hours, batch_size: BATCH_SIZE).each do |batch_time, providers|
+    BatchDelivery.new(
+      relation:,
+      stagger_over: STAGGER_OVER.hours,
+      batch_size: BATCH_SIZE,
+    ).each do |batch_time, providers|
       providers.pluck(:provider_code).each do |provider_code|
         RolloverProviderJob.perform_at(batch_time, provider_code, recruitment_cycle_id)
       end
