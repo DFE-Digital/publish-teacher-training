9a1ad424b119f8637035f499ccca9a371fa6c811
Simon L <simon.lange@education.gov.uk>
2025-08-20 22:06:04 +0100

    removed october to december start date of course as specified by product

--- app/models/concerns/courses/edit_options/start_date_concern.rb
+++ app/models/concerns/courses/edit_options/start_date_concern.rb
@@ -8,30 +8,28 @@ module Courses
         def start_date_options
           recruitment_year = provider.recruitment_cycle.year.to_i
 
-          available_options = ["October #{recruitment_year - 1}",
-                               "November #{recruitment_year - 1}",
-                               "December #{recruitment_year - 1}",
+          available_options = [
+            "January #{recruitment_year}",
+            "February #{recruitment_year}",
+            "March #{recruitment_year}",
+            "April #{recruitment_year}",
+            "May #{recruitment_year}",
+            "June #{recruitment_year}",
+            "July #{recruitment_year}",
+            "August #{recruitment_year}",
+            "September #{recruitment_year}",
+            "October #{recruitment_year}",
+            "November #{recruitment_year}",
+            "December #{recruitment_year}",
 
-                               "January #{recruitment_year}",
-                               "February #{recruitment_year}",
-                               "March #{recruitment_year}",
-                               "April #{recruitment_year}",
-                               "May #{recruitment_year}",
-                               "June #{recruitment_year}",
-                               "July #{recruitment_year}",
-                               "August #{recruitment_year}",
-                               "September #{recruitment_year}",
-                               "October #{recruitment_year}",
-                               "November #{recruitment_year}",
-                               "December #{recruitment_year}",
-
-                               "January #{recruitment_year + 1}",
-                               "February #{recruitment_year + 1}",
-                               "March #{recruitment_year + 1}",
-                               "April #{recruitment_year + 1}",
-                               "May #{recruitment_year + 1}",
-                               "June #{recruitment_year + 1}",
-                               "July #{recruitment_year + 1}"]
+            "January #{recruitment_year + 1}",
+            "February #{recruitment_year + 1}",
+            "March #{recruitment_year + 1}",
+            "April #{recruitment_year + 1}",
+            "May #{recruitment_year + 1}",
+            "June #{recruitment_year + 1}",
+            "July #{recruitment_year + 1}",
+          ]
 
           if instance_of?(Course) && persisted?
             available_options
