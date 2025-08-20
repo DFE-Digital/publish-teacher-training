63bf865fe138117b8b51af04dfccb908974d290a
Tomas D'Stefano <tomas_stefano@successoft.com>
2025-08-20 19:08:14 +0100

    Do not retry batches jobs for rollover

--- app/jobs/rollover_providers_batch_job.rb
+++ app/jobs/rollover_providers_batch_job.rb
@@ -1,11 +1,12 @@
 class RolloverProvidersBatchJob < ApplicationJob
   queue_as :default
+  retry_on StandardError, attempts: 0
 
   def perform(provider_codes, recruitment_cycle_id, summary_id)
     summary = DataHub::RolloverProcessSummary.find(summary_id)
 
     provider_codes.each do |provider_code|
-      RolloverProviderJob.perform_later(provider_code, recruitment_cycle_id, summary_id)
+      RolloverProviderJob.perform_async(provider_code, recruitment_cycle_id, summary_id)
     end
 
     summary.add_batch_enqueue_result(provider_codes:)
