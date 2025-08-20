f8b9e3cc90262db1cdd75843ab38a1fbea6f3784
Simon L <simon.lange@education.gov.uk>
2025-08-20 11:54:15 +0100

    Add tests for course enrichment publishing during rollover

--- spec/controllers/publish/courses_controller_spec.rb
+++ spec/controllers/publish/courses_controller_spec.rb
@@ -34,6 +34,28 @@ module Publish
           code: course.course_code,
         }
       end
+
+      describe "When rolled over v1 enrichment exists" do
+        let(:v1_enrichment) { create(:course_enrichment, :v1, status: "rolled_over", course:) }
+        let(:v2_enrichment) { create(:course_enrichment, :v2, status: "draft", course:) }
+
+        before do
+          allow(FeatureFlag).to receive(:active?).with(:long_form_content).and_return(false)
+          v1_enrichment
+          allow(FeatureFlag).to receive(:active?).with(:long_form_content).and_return(true)
+          v2_enrichment
+        end
+
+        it "updates the v2 enrichment to published" do
+          post :publish, params: {
+            recruitment_cycle_year: provider.recruitment_cycle.year,
+            provider_code: provider.provider_code,
+            code: course.course_code,
+          }
+
+          expect(v2_enrichment.reload.status).to eq "published"
+        end
+      end
     end
 
     describe "#apply" do
