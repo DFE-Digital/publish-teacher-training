7324d8099fc7abc45010c548893bd2d335a3afb1
sarahcrack <73823091+sarahcrack@users.noreply.github.com>
2025-08-20 12:30:13 +0100

    refactor: Long form bug party: Interview process (interview location)
     - refactor helper to return nil if blank (this ensures that "Not entered' still appears in the summary when there is no content)
    - use String#underscore

--- app/helpers/publish/course_interview_location_helper.rb
+++ app/helpers/publish/course_interview_location_helper.rb
@@ -1,7 +1,9 @@
 module Publish
   module CourseInterviewLocationHelper
     def display_interview_location(raw_value)
-      key = raw_value.to_s.tr(" ", "_")
+      return nil if raw_value.blank?
+
+      key = raw_value.to_s.underscore
       I18n.t("publish.providers.courses.description_content.interview_location.#{key}", default: raw_value.to_s.humanize)
     end
   end
