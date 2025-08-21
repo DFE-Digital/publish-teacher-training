02434a7cd221dcab094cfd9c6d656fafe75b4e56
Tomas D'Stefano <tomas_stefano@successoft.com>
2025-08-21 13:39:06 +0100

    Copy same study sites codes for providers with no code duplications
    And the ones with duplication we should resolve

--- app/services/data_hub/rollover/study_site_code_orchestrator.rb
+++ app/services/data_hub/rollover/study_site_code_orchestrator.rb
@@ -1,48 +1,74 @@
 module DataHub
   module Rollover
     class StudySiteCodeOrchestrator
-      attr_reader :target_provider, :sites_to_copy, :used_codes
+      attr_reader :target_provider, :sites_to_copy, :existing_codes
 
       def initialize(target_provider:, sites_to_copy:)
         @target_provider = target_provider
-        @sites_to_copy = sites_to_copy
-        @used_codes = Set.new(target_provider.sites.pluck(:code) + target_provider.study_sites.pluck(:code))
+        @sites_to_copy   = sites_to_copy
+        @existing_codes  = Set.new(
+          target_provider.sites.pluck(:code) +
+          target_provider.study_sites.pluck(:code),
+        )
       end
 
       def call
-        sites_to_copy.map do |site|
-          assigned_code = generate_unique_code_for_site(site)
-          { site:, code: assigned_code }
-        end
+        return handle_unique_study_site_codes if no_source_duplicates?
+
+        handle_duplicated_study_site_codes
       end
 
     private
 
-      def generate_unique_code_for_site(site)
-        code = if used_codes.include?(site.code)
-                 find_next_available_code
-               else
-                 site.code
-               end
+      def no_source_duplicates?
+        codes = sites_to_copy.map(&:code)
+        codes.size == codes.uniq.size
+      end
 
-        used_codes.add(code)
+      # When all study site codes are unique: return original codes
+      def handle_unique_study_site_codes
+        sites_to_copy.map { |site| { site:, code: site.code } }
+      end
+
+      # When source contains duplicate codes: reserve originals and generate new ones
+      def handle_duplicated_study_site_codes
+        used = existing_codes.dup
+
+        sites_to_copy.group_by(&:code).flat_map do |original_code, group|
+          group.each_with_index.map do |site, index|
+            code = if index.zero?
+                     reserve_or_fallback(original_code, used)
+                   else
+                     generate_new_code(used)
+                   end
+
+            { site:, code: }
+          end
+        end
+      end
+
+      def reserve_or_fallback(code, used)
+        if used.include?(code)
+          generate_new_code(used)
+        else
+          used.add(code)
+          code
+        end
+      end
+
+      def generate_new_code(used)
+        available_desirable = Site::DESIRABLE_CODES - used.to_a
+        code = available_desirable.first ||
+          (Site::EASILY_CONFUSED_CODES - used.to_a).first
+
+        unless code
+          seq_source = used.to_a - Site::POSSIBLE_CODES
+          highest    = seq_source.compact.max || "Z"
+          code       = highest.next
+        end
+
+        used.add(code)
         code
-      end
-
-      def find_next_available_code
-        available_desirable = Site::DESIRABLE_CODES - used_codes.to_a
-        return available_desirable.first if available_desirable.any?
-
-        available_confused = Site::EASILY_CONFUSED_CODES - used_codes.to_a
-        return available_confused.first if available_confused.any?
-
-        generate_sequential_code
-      end
-
-      def generate_sequential_code
-        sequential_codes = used_codes.to_a - Site::POSSIBLE_CODES
-        highest = sequential_codes.compact.max || "Z"
-        highest.next
       end
     end
   end
