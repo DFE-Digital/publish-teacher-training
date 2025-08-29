b72a7e5f1c1e1f4dca8c47acf29a15ca557e849f
Iain McNulty <iain.mcnulty@education.gov.uk>
2025-08-29 12:32:28 +0100

    Add PromotTrainingProviderAccreditation service

---
+++ app/services/providers/promote_training_provider_accreditation.rb
@@ -0,0 +1,44 @@
+module Providers
+  class PromoteTrainingProviderAccreditation
+    def initialize(training_provider, number)
+      @training_provider = training_provider
+      @number = number
+    end
+
+    def call
+      Provider.transaction do
+        # Destroy partnerships
+        partners = training_provider.accredited_partners.pluck(:provider_name)
+        training_provider.accredited_partnerships.destroy_all
+        log "Partnerships destroyed:"
+        partners.each do |partner|
+          log partner.indent(2)
+        end
+
+        # Update provider_type
+        if training_provider.lead_school?
+          training_provider.update(provider_type: :scitt)
+        end
+
+        # Reset accredited provider on Courses
+        training_provider.courses.each { it.update!(accredited_provider_code: nil) }
+
+        # Update Provider attributes
+        training_provider.update(accredited_provider_number: number, accredited: true)
+
+        # Save with a bang
+        training_provider.save!
+
+        # Log the result
+        log "#{@training_provider.name_and_code} has been upgraded to an accredited provider!"
+      end
+    end
+
+  private
+
+    def log(text)
+      Rails.logger.tagged("PromoteTrainingProviderAccreditation") { it.info(text) }
+    end
+    attr_reader :training_provider, :number
+  end
+end
