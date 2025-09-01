ca7c3bec13da231ac921e09b4260a6e16878ed57
sarahcrack <73823091+sarahcrack@users.noreply.github.com>
2025-09-01 16:23:01 +0100

    Find: add new callout box - 'How to choose a training provider'
    - create new component
    - create component spec
    - for use in 'Why train with us' section

---
+++ spec/components/shared/courses/how_to_choose_a_training_provider/view_spec.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe Shared::Courses::HowToChooseATrainingProvider::View, type: :component do
+  include Rails.application.routes.url_helpers
+
+  context "fee paying course" do
+    it "renders the correct content" do
+      provider = build(:provider, selectable_school: false)
+      course = build(
+        :course,
+        funding: "fee",
+        provider:,
+      ).decorate
+
+      result = render_inline(described_class.new(course))
+
+      expect(result).to have_css(".app-callout__title", text: "How to choose a training provider")
+      expect(result).to have_css("p.govuk-body", text: "Your course will vary depending on whether the training provider is a university, a school or another organisation.")
+      expect(result).to have_link(
+        "Find out how to choose a training provider",
+        href: I18n.t("find.get_into_teaching.url_type_of_course_provider"),
+      )
+    end
+  end
+end
