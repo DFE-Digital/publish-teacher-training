844f6ab9303032c15050d3ca77bf288f62e5568e
Iain McNulty <iain.mcnulty@education.gov.uk>
2025-09-01 12:15:15 +0100

    Updating fees specs

--- spec/system/publish/courses/long_form_content/financial_support_spec.rb
+++ spec/system/publish/courses/long_form_content/financial_support_spec.rb
@@ -2,7 +2,7 @@
 
 require "spec_helper"
 
-RSpec.describe "Publishing a course with long form content", service: :publish do
+RSpec.describe "Updating fees and financial support", service: :publish do
   include DfESignInUserHelper
 
   let(:user) { create(:user) }
@@ -11,82 +11,116 @@ RSpec.describe "Publishing a course with long form content", service: :publish d
     sign_in_system_test(user:)
   end
 
-  scenario "A user CANT update fees and financial support page if fees are blank" do
-    FeatureFlag.activate(:long_form_content)
+  context "long form content enabled" do
+    before { FeatureFlag.activate(:long_form_content) }
 
-    given_there_is_a_draft_course
-    when_i_visit_the_course_page
-    then_i_edit_the_fees_and_financial_support_fields(uk_fee: nil, international_fee: nil)
+    scenario "A user CANT update fees and financial support page if fees are blank" do
+      given_there_is_a_draft_course
+      when_i_visit_the_course_page
+      and_i_edit_the_fees_and_financial_support_fields(uk_fee: nil, international_fee: nil)
 
-    expect(page).to have_content("Enter fee for UK citizens")
-    expect(page).to have_content("Enter fee for non-UK citizens")
+      expect(page).to have_content("Enter fee for UK citizens")
+      expect(page).to have_content("Enter fee for non-UK citizens")
+    end
+
+    scenario "A user CANT update fees and financial support page if fees above or equal to 100_000" do
+      given_there_is_a_draft_course
+      when_i_visit_the_course_page
+      and_i_edit_the_fees_and_financial_support_fields(uk_fee: 200_000, international_fee: 200_000)
+
+      then_i_see_error_messages_for_course_fees
+    end
+
+    scenario "A user CANT update fees and financial support page if optional fields are above the word count" do
+      given_there_is_a_draft_course
+      when_i_visit_the_course_page
+      and_i_edit_the_fees_and_financial_support_fields(
+        fee_schedule: generate_text(51),
+        additional_fees: generate_text(51),
+        financial_support: generate_text(251),
+      )
+
+      then_i_see_error_messages_for_fees_content_length
+    end
+
+    scenario "update fees and financial support page" do
+      given_there_is_a_draft_course
+      when_i_visit_the_course_page
+      and_i_edit_the_fees_and_financial_support_fields(
+        fee_schedule: "Scheduling fees content",
+        additional_fees: "Fee additions content",
+        financial_support: "Support of financial content",
+      )
+
+      then_i_am_redirected_to_the_course_details_page
+      and_i_see_a_success_message
+      and_all_the_fees_content_is_visible
+    end
+
+    scenario "A user CAN see the new long form course content fields if the current cycle is 2026 or beyond" do
+      Timecop.travel(Find::CycleTimetable.mid_cycle) do
+        given_there_is_a_draft_course
+        when_i_visit_the_course_page
+
+        then_the_old_summary_rows_are_visible
+
+        then_change_links_use_new_routes
+      end
+    end
   end
 
-  scenario "A user CANT update fees and financial support page if fees above or equal to 100_000" do
-    FeatureFlag.activate(:long_form_content)
+  context "before longform content" do
+    scenario "A user CANT see the new long form course content fields if the current cycle is before 2026" do
+      Timecop.travel(Find::CycleTimetable.mid_cycle(2025)) do
+        given_there_is_a_draft_course
+        when_i_visit_the_course_page
 
-    given_there_is_a_draft_course
-    when_i_visit_the_course_page
-    then_i_edit_the_fees_and_financial_support_fields(uk_fee: 200_000, international_fee: 200_000)
+        then_the_summary_rows_are_visible
 
-    expect(page).to have_content("Course fee for UK citizens must be less than or equal to £100,000")
-    expect(page).to have_content("Course fee for non-UK citizens must be less than or equal to £100,000")
+        and_change_links_use_old_routes
+      end
+    end
   end
 
-  scenario "A user CANT update fees and financial support page if optional fields are above the word count" do
-    FeatureFlag.activate(:long_form_content)
-
-    given_there_is_a_draft_course
-    when_i_visit_the_course_page
-    then_i_edit_the_fees_and_financial_support_fields(
-      fee_schedule: generate_text(51),
-      additional_fees: generate_text(51),
-      financial_support: generate_text(251),
-    )
-
+  def then_i_see_error_messages_for_fees_content_length
     expect(page).to have_content("Fee schedule must be 50 words or less")
     expect(page).to have_content("Additional fees must be 50 words or less")
     expect(page).to have_content("Financial support must be 50 words or less")
   end
 
-  scenario "A user CAN see the new long form course content fields if the current cycle is 2026 or beyond" do
-    FeatureFlag.activate(:long_form_content)
+  def then_i_am_redirected_to_the_course_details_page
+    expect(page).to have_current_path("/publish/organisations/#{@course.provider.provider_code}/#{@course.start_date.year}/courses/#{@course.course_code}")
+  end
 
-    current_recruitment_cycle = RecruitmentCycle.create!(
-      year: 2026,
-      application_start_date: Date.new(2025, 10, 9),
-      application_end_date: Date.new(2026, 9, 30),
-      available_for_support_users_from: Date.new(2025, 10, 9),
-      available_in_publish_from: Date.new(2025, 10, 9),
-    )
+  def then_i_see_error_messages_for_course_fees
+    expect(page).to have_content("Course fee for UK citizens must be less than or equal to £100,000")
+    expect(page).to have_content("Course fee for non-UK citizens must be less than or equal to £100,000")
+  end
 
-    given_there_is_a_draft_course(recruitment_cycle: current_recruitment_cycle)
-    when_i_visit_the_course_page
-
+  def then_the_old_summary_rows_are_visible
     expect(page).to have_content("Fee for UK citizens")
     expect(page).to have_content("Fee for international citizens")
     expect(page).to have_content("Fees and financial support")
-
-    then_change_links_use_new_routes
   end
 
-  scenario "A user CANT see the new long form course content fields if the current cycle is before 2026" do
-    current_recruitment_cycle = RecruitmentCycle.create!(
-      year: 2024,
-      application_start_date: Date.new(2023, 10, 9),
-      application_end_date: Date.new(2024, 9, 30),
-      available_for_support_users_from: Date.new(2023, 10, 9),
-      available_in_publish_from: Date.new(2023, 10, 9),
-    )
-
-    given_there_is_a_draft_course(recruitment_cycle: current_recruitment_cycle)
-    when_i_visit_the_course_page
-
+  def then_the_summary_rows_are_visible
     expect(page).to have_content("Fee for UK students")
     expect(page).to have_content("Fee for international students")
     expect(page).to have_content("Fees and financial support (optional)")
+  end
 
-    then_change_links_use_old_routes
+  def and_all_the_fees_content_is_visible
+    within_summary_row("Fees and financial support") do
+      expect(page).to have_content("Scheduling fees content\nFee additions content\nSupport of financial content")
+    end
+
+    within_summary_row("Fee for UK citizens") do
+      expect(page).to have_content("£2,000")
+    end
+
+    within_summary_row("Fee for non-UK citizens") do
+      expect(page).to have_content("£2,000")
+    end
   end
 
   def when_i_visit_the_course_page
@@ -94,14 +128,14 @@ RSpec.describe "Publishing a course with long form content", service: :publish d
     expect(page).to have_content(@course.name)
   end
 
-  def then_i_edit_the_fees_and_financial_support_fields(
+  def and_i_edit_the_fees_and_financial_support_fields(
     uk_fee: 2000,
     international_fee: 2000,
     fee_schedule: "Paragraph 1",
     additional_fees: "Paragraph 2",
     financial_support: "Paragraph 3"
   )
-    all("a", text: "Change")[3].click
+    find_link("Change Fees and financial support").click
     expect(page).to have_content("Fees and financial support")
 
     expect(page).to have_current_path("/publish/organisations/#{@course.provider.provider_code}/#{@course.recruitment_cycle_year}/courses/#{@course.course_code}/fields/fees-and-financial-support")
@@ -116,8 +150,8 @@ RSpec.describe "Publishing a course with long form content", service: :publish d
     click_link_or_button "Update fees and financial support"
   end
 
-  def given_there_is_a_draft_course(recruitment_cycle: RecruitmentCycle.current)
-    provider_in_cycle = create(:provider, recruitment_cycle: recruitment_cycle)
+  def given_there_is_a_draft_course
+    provider_in_cycle = create(:provider)
     user.providers << provider_in_cycle
 
     course_enrichment = build(:course_enrichment, :initial_draft, course_length: :TwoYears)
@@ -132,22 +166,26 @@ RSpec.describe "Publishing a course with long form content", service: :publish d
       enrichments: [course_enrichment],
       sites: [create(:site, location_name: "location 1")],
       study_sites: [create(:site, :study_site)],
-      applications_open_from: recruitment_cycle.application_start_date + 1.day,
-      start_date: Date.new(recruitment_cycle.year.to_i, 9, 1),
     )
   end
 
-  def then_change_links_use_old_routes
-    all("a", text: "Change")[6].click
+  def and_change_links_use_old_routes
+    page.find_link("Change fees and financial support").click
     expect(page).to have_current_path("/publish/organisations/#{@course.provider.provider_code}/#{@course.recruitment_cycle_year}/courses/#{@course.course_code}/fees-and-financial-support")
   end
 
   def then_change_links_use_new_routes
-    all("a", text: "Change")[3].click
+    page.find_link("Change Fees and financial support").click
     expect(page).to have_current_path("/publish/organisations/#{@course.provider.provider_code}/#{@course.recruitment_cycle_year}/courses/#{@course.course_code}/fields/fees-and-financial-support")
   end
 
   def generate_text(word_count)
     "#{Faker::Lorem.words(number: word_count).join(' ').capitalize}."
+  end
+
+  def and_i_see_a_success_message
+    within(".govuk-notification-banner") do
+      expect(page).to have_content("Success\nFees and financial support updated")
+    end
   end
 end
