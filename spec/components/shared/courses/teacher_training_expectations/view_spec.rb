99631ab769b208b30495064761b4069c83a551d0
sarahcrack <73823091+sarahcrack@users.noreply.github.com>
2025-09-02 10:50:24 +0100

    Find: add new callout box - 'What to expect in teacher training'
    - create new component
    - create component spec
    - for use in 'What you will study’ section

---
+++ spec/components/shared/courses/teacher_training_expectations/view_spec.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe Shared::Courses::TeacherTrainingExpectations::View, type: :component do
+  include Rails.application.routes.url_helpers
+
+  context "fee paying course" do
+    it "renders the correct content" do
+      provider = build(:provider, selectable_school: false)
+      course = build(
+        :course,
+        funding: "fee",
+        provider:,
+      ).decorate
+
+      result = render_inline(described_class.new(course))
+
+      expect(result).to have_css(".app-callout__title", text: "What to expect in teacher training")
+      expect(result).to have_css("p.govuk-body", text: "You'll spend most of your time teaching in a classroom, with some time dedicated to studying. This may include seminars, workshops and assignments.")
+      expect(result).to have_link(
+        "Find out what to expect in teacher training",
+        href: I18n.t("find.get_into_teaching.url_initial_teacher_training"),
+      )
+    end
+  end
+end
