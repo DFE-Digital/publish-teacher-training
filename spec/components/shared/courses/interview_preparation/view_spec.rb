9c4ef8390c0f970b3bb509bd72e863556f232fac
sarahcrack <73823091+sarahcrack@users.noreply.github.com>
2025-09-02 11:03:24 +0100

    Find: add new callout box - 'Preparing for your interview’
    - create new component
    - create component spec
    - for use in 'Interview process’ section

---
+++ spec/components/shared/courses/interview_preparation/view_spec.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe Shared::Courses::InterviewPreparation::View, type: :component do
+  include Rails.application.routes.url_helpers
+
+  context "fee paying course" do
+    it "renders the correct content" do
+      provider = build(:provider, selectable_school: false)
+      course = build(
+        :course,
+        funding: "fee",
+        provider:,
+      ).decorate
+
+      result = render_inline(described_class.new(course))
+
+      expect(result).to have_css(".app-callout__title", text: "Preparing for your interview")
+      expect(result).to have_css("p.govuk-body", text: "Interviews sometimes include a presentation, or maths or English tests to be completed in advance. The exact interview process will vary depending on the teacher training provider.")
+      expect(result).to have_link(
+        "Find out how to prepare for your interview",
+        href: I18n.t("find.get_into_teaching.url_interview_preparation"),
+      )
+    end
+  end
+end
