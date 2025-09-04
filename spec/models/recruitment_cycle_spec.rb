c253c241b96752740f7352593a04c6e00f7e0336
Iain McNulty <iain.mcnulty@education.gov.uk>
2025-09-04 16:55:47 +0100

    Use cycles that have a previous cycle in the CycleTimetable

--- spec/models/recruitment_cycle_spec.rb
+++ spec/models/recruitment_cycle_spec.rb
@@ -5,12 +5,12 @@ require "rails_helper"
 describe RecruitmentCycle do
   subject { current_cycle }
 
-  before { allow(Settings).to receive(:current_recruitment_cycle_year).and_return(2021) }
+  before { allow(Settings).to receive(:current_recruitment_cycle_year).and_return(2022) }
 
   let(:current_cycle) { find_or_create(:recruitment_cycle) }
   let(:next_cycle) { find_or_create(:recruitment_cycle, :next) }
 
-  its(:to_s) { is_expected.to eq("2021/22") }
+  its(:to_s) { is_expected.to eq("2022/23") }
 
   it "is valid with valid attributes" do
     expect(subject).to be_valid
