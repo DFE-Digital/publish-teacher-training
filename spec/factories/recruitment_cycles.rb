7a00fece9c1d290f54e1a47a2766cd135154c310
Iain McNulty <iain.mcnulty@education.gov.uk>
2025-09-11 16:33:56 +0100

    Update test infrastructure for new recruitment cycles
    - Add cycle timetable helpers to rails_helper
    - Update course and recruitment cycle factories for current cycles

--- spec/factories/recruitment_cycles.rb
+++ spec/factories/recruitment_cycles.rb
@@ -3,8 +3,8 @@
 FactoryBot.define do
   factory :recruitment_cycle do
     year { Settings.current_recruitment_cycle_year }
-    application_start_date { DateTime.new(year.to_i - 1, 10, 1) }
-    application_end_date { DateTime.new(year.to_i, 9, 30) }
+    application_start_date { Find::CycleTimetable.apply_opens(year) }
+    application_end_date { Find::CycleTimetable.apply_deadline(year) }
     available_in_publish_from { application_start_date.to_date - 1.month }
     available_for_support_users_from { available_in_publish_from - 1.week }
 
