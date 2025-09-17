6ab1441ee15e3c9797d2c5884e7d7495c52dab8f
Iain McNulty <iain.mcnulty@education.gov.uk>
2025-09-17 17:48:42 +0100

    Use cycle_year_for_time where we need to avoid Redis

--- spec/spec_helper_smoke.rb
+++ spec/spec_helper_smoke.rb
@@ -8,5 +8,8 @@ require "config"
 require "httparty"
 require "active_support"
 require "active_support/time"
+require "active_support/core_ext/time/zones"
+Time.zone = "Europe/London"
+load "app/services/find/cycle_timetable.rb"
 
 Config.load_and_set_settings(Config.setting_files("config", ENV.fetch("RAILS_ENV", nil)))
