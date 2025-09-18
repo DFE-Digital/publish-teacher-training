34fe22f72cd2d9f9437fefc5d7b1f69dce37c15f
Iain McNulty <iain.mcnulty@education.gov.uk>
2025-09-18 17:16:33 +0100

    Format cycle year constraint better

--- app/lib/cycle_year_constraint.rb
+++ app/lib/cycle_year_constraint.rb
@@ -1,14 +1,17 @@
 class CycleYearConstraint
   def matches?(request)
-    current_year = Find::CycleTimetable.current_year
     permitted_years = [
-      current_year - 1,
-      current_year,
-      current_year + 1,
+      Find::CycleTimetable.previous_year,
+      Find::CycleTimetable.current_year,
+      Find::CycleTimetable.next_year,
     ]
 
-    value = [request.params[:recruitment_cycle_year], request.params[:cycle_year], request.params[:year]].find(&:present?)
+    possible_year_params = [
+      request.params[:recruitment_cycle_year],
+      request.params[:cycle_year],
+      request.params[:year],
+    ].find(&:present?)
 
-    value.to_i.in?(permitted_years)
+    possible_year_params.to_i.in?(permitted_years)
   end
 end
