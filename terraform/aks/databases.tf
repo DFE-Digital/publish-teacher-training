e5aef80868974646307254fa58c1d4f50822407b
paul senior <paul.senior@education.gov.uk>
2025-10-13 11:43:45 +0100

    feat(2128): manage postgis is tf module - testing

--- terraform/aks/databases.tf
+++ terraform/aks/databases.tf
@@ -56,5 +56,4 @@ module "postgres" {
   azure_enable_high_availability = var.postgres_enable_high_availability
   azure_maintenance_window       = var.postgres_azure_maintenance_window
   server_version                 = "14"
-  server_postgis_version         = "14-3.2"
 }
