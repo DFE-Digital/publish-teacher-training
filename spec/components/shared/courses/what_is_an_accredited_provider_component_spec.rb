7fe778cb85bdc055ea8b891e50d7bee0ce8af74f
sarahcrack <73823091+sarahcrack@users.noreply.github.com>
2025-10-16 16:47:35 +0100

    Move conditional rendering logic for WhatIsAnAccreditedProviderComponent into the component and adapt spec

--- spec/components/shared/courses/what_is_an_accredited_provider_component_spec.rb
+++ spec/components/shared/courses/what_is_an_accredited_provider_component_spec.rb
@@ -5,15 +5,37 @@ require "rails_helper"
 describe Shared::Courses::WhatIsAnAccreditedProviderComponent, type: :component do
   include Rails.application.routes.url_helpers
 
-  context "accredited provider description callout box displays correctly" do
-    let(:provider) { build(:provider, selectable_school: false) }
-    let(:course) { build(:course, funding: "fee", provider:).decorate }
+  let(:provider) { build(:provider, provider_code: "ABC", selectable_school: false) }
+
+  context "when the course has a different accredited provider" do
+    let(:course) { build(:course, provider:, accredited_provider_code: "XYZ").decorate }
 
     it "renders the correct title and content" do
       result = render_inline(described_class.new(course: course))
 
       expect(result).to have_css(".app-callout__title", text: "What is an accredited provider?")
-      expect(result).to have_css("p.govuk-body", text: "Accredited providers are organisations approved by the government to manage teacher training and recommend candidates for qualified teacher status (QTS). They oversee the quality of the training, while training providers focus on delivering it.")
+      expect(result).to have_css(
+        "p.govuk-body",
+        text: "Accredited providers are organisations approved by the government to manage teacher training and recommend candidates for qualified teacher status (QTS). They oversee the quality of the training, while training providers focus on delivering it.",
+      )
+    end
+  end
+
+  context "when the course is self-accredited" do
+    let(:course) { build(:course, provider:, accredited_provider_code: nil).decorate }
+
+    it "does not render the callout box" do
+      result = render_inline(described_class.new(course: course))
+      expect(result).not_to have_css(".app-callout__title")
+    end
+  end
+
+  context "when the accredited provider is the same as the training provider" do
+    let(:course) { build(:course, provider:, accredited_provider_code: "ABC").decorate }
+
+    it "does not render the callout box" do
+      result = render_inline(described_class.new(course: course))
+      expect(result).not_to have_css(".app-callout__title")
     end
   end
 end
