afc858c3e2926f989456fb4d76d0a21b970736b4
Tomas D'Stefano <tomas_stefano@successoft.com>
2025-10-22 15:40:51 +0100

    Fix flakey spec on geocoder
    Stub randomness

--- spec/services/data_hub/geocoder/dry_run_spec.rb
+++ spec/services/data_hub/geocoder/dry_run_spec.rb
@@ -7,30 +7,38 @@ RSpec.describe DataHub::Geocoder::DryRun, type: :service do
   let(:site) { create(:site, latitude: nil, longitude: nil) }
 
   describe "#geocode" do
-    it "returns successful result with simulated UK coordinates" do
-      result = geocoder.geocode(site)
+    context "when simulating success" do
+      before do
+        allow(geocoder).to receive(:rand).with(no_args).and_return(0.1)
+        allow(geocoder).to receive(:rand).with(49.9..60.8).and_return(51.509865, 53.800755)
+        allow(geocoder).to receive(:rand).with(-8.6..1.8).and_return(-0.118092, -1.549077)
+      end
 
-      expect(result.success?).to be(true)
-      expect(result.latitude).to be_between(49.9, 60.8)
-      expect(result.longitude).to be_between(-8.6, 1.8)
-      expect(result.error_message).to be_nil
-    end
+      it "returns successful result with simulated UK coordinates" do
+        result = geocoder.geocode(site)
 
-    it "does not modify the actual record" do
-      expect { geocoder.geocode(site) }.not_to(change { site.reload.latitude })
-    end
+        expect(result.success?).to be(true)
+        expect(result.latitude).to eq(51.509865)
+        expect(result.longitude).to eq(-0.118092)
+        expect(result.error_message).to be_nil
+      end
 
-    it "simulates failure approximately 5% of the time" do
-      results = 100.times.map { geocoder.geocode(site) }
-      failures = results.count(&:failed?)
+      it "does not modify the actual record" do
+        expect { geocoder.geocode(site) }.not_to(change { site.reload.latitude })
+      end
 
-      # Should be around 5%, allow for randomness (0-15% range)
-      expect(failures).to be_between(0, 15)
+      it "generates different coordinates on each call" do
+        result1 = geocoder.geocode(site)
+        result2 = geocoder.geocode(site)
+
+        expect(result1.latitude).not_to eq(result2.latitude)
+        expect(result1.longitude).not_to eq(result2.longitude)
+      end
     end
 
     context "when simulating failure" do
       before do
-        allow(geocoder).to receive(:rand).and_return(0.96) # > 0.95 triggers failure
+        allow(geocoder).to receive(:rand).with(no_args).and_return(0.96)
       end
 
       it "returns failed result with error message" do
@@ -43,12 +51,20 @@ RSpec.describe DataHub::Geocoder::DryRun, type: :service do
       end
     end
 
-    it "generates different coordinates on each call" do
-      result1 = geocoder.geocode(site)
-      result2 = geocoder.geocode(site)
+    context "success/failure ratio" do
+      it "simulates failure approximately 5% of the time" do
+        # 95 successes, 5 failures
+        # Alternate the 'rand' value to control outcome: first 95 below 0.95 (success), last 5 above (failure)
+        result_sequence = Array.new(95, 0.1) + Array.new(5, 0.96)
+        allow(geocoder).to receive(:rand).with(no_args).and_return(*result_sequence)
+        allow(geocoder).to receive(:rand).with(49.9..60.8).and_return(52.0)
+        allow(geocoder).to receive(:rand).with(-8.6..1.8).and_return(-1.0)
 
-      expect(result1.latitude).not_to eq(result2.latitude)
-      expect(result1.longitude).not_to eq(result2.longitude)
+        results = 100.times.map { geocoder.geocode(site) }
+        failures = results.count(&:failed?)
+
+        expect(failures).to eq(5)
+      end
     end
   end
 
