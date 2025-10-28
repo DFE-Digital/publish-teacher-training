1e8945134fe0d30c4170d22dc4b65722698c21b2
Tomas D'Stefano <tomas_stefano@successoft.com>
2025-10-28 12:31:18 +0000

    Make candidate model auditable

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -1,4 +1,5 @@
 class Candidate < ApplicationRecord
+  audited
   validates :email_address, email_address: true
 
   normalizes :email_address, with: ->(value) { value&.strip&.downcase }, apply_to_nil: false
