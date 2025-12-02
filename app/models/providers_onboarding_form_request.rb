8bfdc1420b78d01cbf56eda3ab0847e982b84beb
sarahcrack <73823091+sarahcrack@users.noreply.github.com>
2025-12-02 12:46:56 +0000

    Add format validations and specs for ProvidersOnboardingFormRequest
    - Implement format validations (URN, UKPRN, email, phone, postcode, website) with tests
    - Add onboarding model format validations and RSpec coverage

--- app/models/providers_onboarding_form_request.rb
+++ app/models/providers_onboarding_form_request.rb
@@ -7,10 +7,16 @@ class ProvidersOnboardingFormRequest < ApplicationRecord
   validates :uuid, presence: true, uniqueness: true
   validates :status, presence: true, inclusion: { in: statuses.keys }
 
-  validates :form_name, :email_address, :first_name, :last_name,
-            :organisation_name, :address_line_1, :town_or_city,
-            :postcode, :phone_number, :contact_email_address, :organisation_website,
-            :accredited_provider, :ukprn, :urn, presence: true
+  validates :form_name, :first_name, :last_name,
+            :organisation_name, :address_line_1, :town_or_city, presence: true
+
+  validates :email_address, :contact_email_address, email_address: true
+  validates :postcode, postcode: true
+  validates :phone_number, phone: true
+  validates :organisation_website, presence: true, url: true
+  validates :accredited_provider, inclusion: { in: [true, false] }
+  validates :ukprn, ukprn_format: { allow_blank: false }
+  validates :urn, reference_number_format: { allow_blank: false, minimum: 5, maximum: 6, message: "Provider URN must be 5 or 6 numbers" }
 
   # Ensure that the support agent is an admin user
   validate :support_agent_is_admin, if: :support_agent_id?
