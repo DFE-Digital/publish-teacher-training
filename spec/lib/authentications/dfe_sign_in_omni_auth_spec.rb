6bd55f05872d771e0795fb41119173ef4ac79cf3
Simon L <simon.lange@education.gov.uk>
2025-12-02 18:45:17 +0000

    Create more DfESignInOmniAuth specs for clarity and consistency

--- spec/lib/authentications/dfe_sign_in_omni_auth_spec.rb
+++ spec/lib/authentications/dfe_sign_in_omni_auth_spec.rb
@@ -6,9 +6,35 @@ module Authentications
   RSpec.describe DfESignInOmniAuth do
     subject(:omni_auth) { described_class.new }
 
+    describe "#provider" do
+      it "registers the OpenID Connect strategy" do
+        expect(omni_auth.provider).to eq(:openid_connect)
+      end
+    end
+
     describe "#options" do
+      subject(:options) { omni_auth.options }
+
       it "uses the email/profile scope so values are space separated" do
-        expect(omni_auth.options[:scope]).to eq(%i[email profile])
+        expect(options[:scope]).to eq(%i[email profile])
+      end
+
+      it "configures the expected callback path and name" do
+        expect(options[:callback_path]).to eq("/auth/dfe/callback")
+        expect(options[:name]).to eq(:dfe)
+      end
+
+      it "sets the issuer and client options from Settings" do
+        expect(options[:issuer]).to eq("https://test-oidc.signin.education.gov.uk:443")
+
+        expect(options[:client_options]).to match(
+          identifier: Settings.dfe_signin.identifier,
+          port: 443,
+          scheme: "https",
+          host: "test-oidc.signin.education.gov.uk",
+          secret: Settings.dfe_signin.secret,
+          redirect_uri: "#{Settings.base_url}/auth/dfe/callback",
+        )
       end
     end
   end
