94a45148b48f34be8f379e931b0d7e701d9a716c
sarahcrack <73823091+sarahcrack@users.noreply.github.com>
2025-12-02 12:46:56 +0000

    Add format validations and specs for ProvidersOnboardingFormRequest
    - Implement format validations (URN, UKPRN, email, phone, postcode, website) with tests
    - Add onboarding model format validations and RSpec coverage

--- spec/factories/providers_onboarding_form_requests.rb
+++ spec/factories/providers_onboarding_form_requests.rb
@@ -1,26 +1,26 @@
 FactoryBot.define do
   factory :providers_onboarding_form_request do
     status { "pending" }
-    form_name { "MyText" }
+    form_name { "Onboarding Form" }
     support_agent_id { nil }
-    zendesk_link { "MyString" }
+    zendesk_link { "https://zendesk.example.com/ticket/123" }
     uuid { SecureRandom.uuid }
     provider_metadata { {} }
-    email_address { "MyText" }
-    first_name { "MyText" }
-    last_name { "MyText" }
-    organisation_name { "MyString" }
-    address_line_1 { "MyText" }
-    address_line_2 { "MyText" }
-    address_line_3 { "MyText" }
-    town_or_city { "MyText" }
-    county { "MyText" }
-    postcode { "MyText" }
-    phone_number { "MyText" }
-    contact_email_address { "MyText" }
-    organisation_website { "MyText" }
-    ukprn { "MyString" }
-    accredited_provider { false }
-    urn { "MyString" }
+    email_address { Faker::Internet.email }
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
+    organisation_name { Faker::Company.name }
+    address_line_1 { Faker::Address.street_address }
+    address_line_2 { Faker::Address.secondary_address }
+    address_line_3 { nil }
+    town_or_city { Faker::Address.city }
+    county { Faker::Address.state }
+    postcode { Faker::Address.postcode }
+    phone_number { Faker::PhoneNumber.phone_number }
+    contact_email_address { Faker::Internet.email }
+    organisation_website { Faker::Internet.url }
+    ukprn { Faker::Number.within(range: 10_000_000..19_999_999).to_s }
+    accredited_provider { [true, false].sample }
+    urn { Faker::Number.number(digits: 6).to_s }
   end
 end
--- spec/models/providers_onboarding_form_request_spec.rb
+++ spec/models/providers_onboarding_form_request_spec.rb
@@ -4,23 +4,19 @@ RSpec.describe ProvidersOnboardingFormRequest, type: :model do
   describe "validations" do
     subject { build(:providers_onboarding_form_request) }
 
+    before { create(:providers_onboarding_form_request) } # for uniqueness validation
+
     it { is_expected.to validate_presence_of(:uuid) }
     it { is_expected.to validate_uniqueness_of(:uuid).case_insensitive }
     it { is_expected.to validate_presence_of(:status) }
     it { is_expected.to validate_presence_of(:form_name) }
-    it { is_expected.to validate_presence_of(:email_address) }
     it { is_expected.to validate_presence_of(:first_name) }
     it { is_expected.to validate_presence_of(:last_name) }
     it { is_expected.to validate_presence_of(:organisation_name) }
     it { is_expected.to validate_presence_of(:address_line_1) }
     it { is_expected.to validate_presence_of(:town_or_city) }
-    it { is_expected.to validate_presence_of(:postcode) }
-    it { is_expected.to validate_presence_of(:phone_number) }
-    it { is_expected.to validate_presence_of(:contact_email_address) }
     it { is_expected.to validate_presence_of(:organisation_website) }
-    it { is_expected.to validate_presence_of(:accredited_provider) }
-    it { is_expected.to validate_presence_of(:ukprn) }
-    it { is_expected.to validate_presence_of(:urn) }
+    it { is_expected.to validate_inclusion_of(:accredited_provider).in_array([true, false]) }
     it { is_expected.to belong_to(:support_agent).class_name("User").optional }
 
     it "raises an error when status is not in the enum keys" do
@@ -39,6 +35,82 @@ RSpec.describe ProvidersOnboardingFormRequest, type: :model do
       subject.support_agent = create(:user, admin: false)
       expect(subject).not_to be_valid
       expect(subject.errors[:support_agent]).to include("must be an admin user")
+    end
+
+    context "format validations" do
+      context "UKPRN" do
+        it "rejects invalid UKPRN" do
+          subject.ukprn = "98765432"
+          expect(subject).not_to be_valid
+        end
+
+        it "accepts valid UKPRN" do
+          subject.ukprn = "12345678"
+          expect(subject).to be_valid
+        end
+      end
+
+      context "postcode" do
+        it "rejects invalid postcode" do
+          subject.postcode = "INVALID"
+          expect(subject).not_to be_valid
+        end
+
+        it "accepts valid postcode" do
+          subject.postcode = "BN1 1AA"
+          expect(subject).to be_valid
+        end
+      end
+
+      context "phone number" do
+        it "rejects invalid phone number" do
+          subject.phone_number = "INVALID"
+          expect(subject).not_to be_valid
+        end
+
+        it "accepts valid phone number" do
+          subject.phone_number = "+441234567890"
+          expect(subject).to be_valid
+        end
+      end
+
+      context "email addresses" do
+        it "rejects invalid email addresses" do
+          subject.email_address = "invalid_email"
+          subject.contact_email_address = "also_invalid"
+          expect(subject).not_to be_valid
+        end
+
+        it "accepts valid email addresses" do
+          subject.email_address = "teacher123@provider.org"
+          subject.contact_email_address = "provider@ittprovider.co.uk"
+          expect(subject).to be_valid
+        end
+      end
+
+      context "URN" do
+        it "rejects URN that is too short" do
+          subject.urn = "1234"
+          expect(subject).not_to be_valid
+        end
+
+        it "accepts URN that is 6 digits" do
+          subject.urn = "123456"
+          expect(subject).to be_valid
+        end
+      end
+
+      context "organisation website" do
+        it "accepts a valid organisation website URL" do
+          subject.organisation_website = "https://www.provider.org"
+          expect(subject).to be_valid
+        end
+
+        it "rejects an invalid organisation website URL" do
+          subject.organisation_website = "www.provider.org"
+          expect(subject).not_to be_valid
+        end
+      end
     end
   end
 end
