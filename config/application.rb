a2b2d993eb927874e88cfc16a1b2dc3481fd60a3
Iain McNulty <iain.mcnulty@education.gov.uk>
2025-12-02 13:08:15 +0000

    Do not allow Zeitgeist to autoload rubocop

--- config/application.rb
+++ config/application.rb
@@ -26,7 +26,7 @@ module ManageCoursesBackend
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w[tasks])
+    config.autoload_lib(ignore: %w[tasks rubocop])
 
     # Configuration for the application, engines, and railties goes here.
     #
