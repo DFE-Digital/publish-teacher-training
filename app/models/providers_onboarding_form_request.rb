c25573837efe132341dd3dbb7c1929dd2f0cd9f2
sarahcrack <73823091+sarahcrack@users.noreply.github.com>
2025-12-03 16:33:19 +0000

    Add submitted status validations and refactor specs for ProvidersOnboardingFormRequest
    - Add submitted status validations and refactor specs for ProvidersOnboardingFormRequest
    - Add factory trait `:submitted` for easier test setup
    - Refactored RSpec tests:
      * Split unconditional vs conditional validations
      * Grouped presence checks for submitted status
      * Added format validation tests for all required fields
      * Hardcode expected error messages for clarity
    - Implement error messages inline and via custom validators
    - add fields to DfE analytics and PII to analytics_hidden_pii.yml

--- app/models/providers_onboarding_form_request.rb
+++ app/models/providers_onboarding_form_request.rb
@@ -1,25 +1,29 @@
 class ProvidersOnboardingFormRequest < ApplicationRecord
   enum :status, %w[pending submitted expired closed rejected].index_by(&:itself)
 
+  # 3 fields need to be stored once the support team instantiate a form request
   # Every onboarding form request is optionally handled by/assigned a support agent (User)
   belongs_to :support_agent, class_name: "User", optional: true
 
   validates :uuid, presence: true, uniqueness: true
   validates :status, presence: true, inclusion: { in: statuses.keys }
-
-  validates :form_name, :first_name, :last_name,
-            :provider_name, :address_line_1, :town_or_city, presence: true
-
-  validates :email_address, :contact_email_address, email_address: true
-  validates :postcode, postcode: true
-  validates :telephone, phone: true
-  validates :website, presence: true, url: true
-  validates :accredited_provider, inclusion: { in: [true, false] }
-  validates :ukprn, ukprn_format: { allow_blank: false }
-  validates :urn, reference_number_format: { allow_blank: false, minimum: 5, maximum: 6, message: "Provider URN must be 5 or 6 numbers" }
-
   # Ensure that the support agent is an admin user
   validate :support_agent_is_admin, if: :support_agent_id?
+  validates :form_name, presence: true
+
+  # These fields need to be validated once the form details are submitted by the provider i.e. status changes to 'submitted'
+  with_options if: :submitted? do
+    validates :first_name, :last_name,
+              :provider_name, :address_line_1, :town_or_city, presence: true
+
+    validates :email_address, :contact_email_address, email_address: true
+    validates :postcode, presence: true, postcode: true
+    validates :telephone, phone: true
+    validates :website, presence: true, url: true
+    validates :accredited_provider, inclusion: { in: [true, false] }
+    validates :ukprn, ukprn_format: { allow_blank: false }
+    validates :urn, reference_number_format: { allow_blank: false, minimum: 5, maximum: 6, message: "Provider URN must be 5 or 6 numbers" }
+  end
 
 private
 
