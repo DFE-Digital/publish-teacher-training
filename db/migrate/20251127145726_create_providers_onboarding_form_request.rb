f531af36110d55ecd2b95fcdd284bef49b9e4b87
sarahcrack <73823091+sarahcrack@users.noreply.github.com>
2025-12-05 14:27:53 +0000

    ProvidersOnboardingFormRequest: change some fields to string and make support_agent optional
    - Update specs to include test for support agent being an admin user

--- db/migrate/20251127145726_create_providers_onboarding_form_request.rb
+++ db/migrate/20251127145726_create_providers_onboarding_form_request.rb
@@ -2,24 +2,23 @@ class CreateProvidersOnboardingFormRequest < ActiveRecord::Migration[8.0]
   def change
     create_table :providers_onboarding_form_request do |t|
       t.string :status, null: false, default: "pending"
-      t.text :form_name, null: false
-      t.bigint :support_agent_id
+      t.string :form_name, null: false
       t.string :zendesk_link
       t.uuid :uuid, null: false, default: -> { "uuid_generate_v4()" }
       t.jsonb :provider_metadata, default: {}
-      t.text :email_address
-      t.text :first_name
-      t.text :last_name
+      t.string :email_address
+      t.string :first_name
+      t.string :last_name
       t.string :provider_name
       t.text :address_line_1
       t.text :address_line_2
       t.text :address_line_3
-      t.text :town_or_city
-      t.text :county
-      t.text :postcode
-      t.text :telephone
-      t.text :contact_email_address
-      t.text :website
+      t.string :town_or_city
+      t.string :county
+      t.string :postcode
+      t.string :telephone
+      t.string :contact_email_address
+      t.string :website
       t.string :ukprn
       t.boolean :accredited_provider
       t.string :urn
@@ -30,7 +29,9 @@ class CreateProvidersOnboardingFormRequest < ActiveRecord::Migration[8.0]
     # unique index on uuid for quick lookup
     add_index :providers_onboarding_form_request, :uuid, unique: true
 
-    # link to User table for support_agent_id
-    add_foreign_key :providers_onboarding_form_request, :user, column: :support_agent_id, name: "FK_onboarding_request_user_support_agent_id"
+    # link to User table for support_agent_id which is optional
+    add_reference :providers_onboarding_form_request, :support_agent,
+                  foreign_key: { to_table: :user, name: "FK_onboarding_request_user_support_agent_id" },
+                  null: true
   end
 end
