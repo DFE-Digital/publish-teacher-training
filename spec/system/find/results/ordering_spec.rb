50427a9069d8e7e303e82821f60aff32a9c6a948
Iain McNulty <iain.mcnulty@education.gov.uk>
2025-12-10 16:22:45 +0000

    Add Course results ordering by course start date ascending

--- spec/system/find/results/ordering_spec.rb
+++ spec/system/find/results/ordering_spec.rb
@@ -31,6 +31,13 @@ RSpec.describe "Search results ordering", :js, service: :find do
     then_the_courses_are_ordered_by_provider_name_ascending
   end
 
+  scenario "sorting by start date" do
+    when_i_visit_the_find_results_page
+
+    and_i_sort_start_date
+    then_the_courses_are_ordered_by_start_date
+  end
+
   def given_there_are_published_courses
     warwick_provider = create(:provider, provider_name: "Warwick University")
     niot_provider = create(:provider, provider_name: "NIoT")
@@ -38,25 +45,35 @@ RSpec.describe "Search results ordering", :js, service: :find do
     cambridge_provider = create(:provider, provider_name: "Cambridge University")
     oxford_provider = create(:provider, provider_name: "Oxford University")
 
-    create(:course, :with_full_time_sites, provider: warwick_provider, name: "Computing", course_code: "23TT")
-    create(:course, :with_full_time_sites, provider: warwick_provider, name: "Art and Design", course_code: "X100")
-    create(:course, :with_full_time_sites, provider: warwick_provider, name: "Drama", course_code: "AB21")
-    create(:course, :with_full_time_sites, provider: warwick_provider, name: "Chemistry", course_code: "X121")
+    with_options start_date: 5.days.from_now, provider: warwick_provider do
+      create(:course, :with_full_time_sites, name: "Computing", course_code: "23TT")
+      create(:course, :with_full_time_sites, name: "Art and Design", course_code: "X100")
+      create(:course, :with_full_time_sites, name: "Drama", course_code: "AB21")
+      create(:course, :with_full_time_sites, name: "Chemistry", course_code: "X121")
+    end
 
-    create(:course, :with_full_time_sites, provider: niot_provider, name: "Economics", course_code: "23TX")
-    create(:course, :with_full_time_sites, provider: niot_provider, name: "Psychology", course_code: "23X7")
-    create(:course, :with_full_time_sites, provider: niot_provider, name: "History", course_code: "23X8")
+    with_options start_date: 1.day.from_now, provider: niot_provider do
+      create(:course, :with_full_time_sites, name: "Economics", course_code: "23TX")
+      create(:course, :with_full_time_sites, name: "Psychology", course_code: "23X7")
+      create(:course, :with_full_time_sites, name: "History", course_code: "23X8")
+    end
 
-    create(:course, :with_full_time_sites, provider: essex_provider, name: "Mathematics", course_code: "23X9")
-    create(:course, :with_full_time_sites, provider: essex_provider, name: "Physics", course_code: "23XB")
-    create(:course, :with_full_time_sites, provider: essex_provider, name: "Art and Design", course_code: "23XC")
+    with_options start_date: 2.days.from_now, provider: essex_provider do
+      create(:course, :with_full_time_sites, name: "Mathematics", course_code: "23X9")
+      create(:course, :with_full_time_sites, name: "Physics", course_code: "23XB")
+      create(:course, :with_full_time_sites, name: "Art and Design", course_code: "23XC")
+    end
 
-    create(:course, :with_full_time_sites, provider: cambridge_provider, name: "Music", course_code: "23XD")
-    create(:course, :with_full_time_sites, provider: cambridge_provider, name: "Dance", course_code: "23XF")
+    with_options start_date: 4.days.from_now, provider: cambridge_provider do
+      create(:course, :with_full_time_sites, name: "Music", course_code: "23XD")
+      create(:course, :with_full_time_sites, name: "Dance", course_code: "23XF")
+    end
 
-    create(:course, :with_full_time_sites, provider: oxford_provider, name: "Geography", course_code: "23XJ")
-    create(:course, :with_full_time_sites, provider: oxford_provider, name: "English", course_code: "23XK")
-    create(:course, :with_full_time_sites, provider: oxford_provider, name: "Philosophy", course_code: "T3XK")
+    with_options start_date: 3.days.from_now, provider: oxford_provider do
+      create(:course, :with_full_time_sites, name: "Geography", course_code: "23XJ")
+      create(:course, :with_full_time_sites, name: "English", course_code: "23XK")
+      create(:course, :with_full_time_sites, name: "Philosophy", course_code: "T3XK")
+    end
   end
 
   def when_i_visit_the_find_results_page
@@ -91,6 +108,12 @@ RSpec.describe "Search results ordering", :js, service: :find do
     click_link_or_button "Apply filters"
   end
 
+  def and_i_sort_start_date
+    page.find("h3", text: "Sort by", normalize_ws: true).click
+    choose "Soonest start date", visible: :hidden
+    click_link_or_button "Apply filters"
+  end
+
   def then_the_courses_are_ordered_by_course_name_ascending
     expect(result_titles).to eq(
       [
@@ -121,6 +144,23 @@ RSpec.describe "Search results ordering", :js, service: :find do
         "NIoT Psychology (23X7)",
         "Oxford University English (23XK)",
         "Oxford University Geography (23XJ)",
+      ],
+    )
+  end
+
+  def then_the_courses_are_ordered_by_start_date
+    expect(result_titles).to eq(
+      [
+        "NIoT Economics (23TX)",
+        "NIoT History (23X8)",
+        "NIoT Psychology (23X7)",
+        "Essex University Art and Design (23XC)",
+        "Essex University Mathematics (23X9)",
+        "Essex University Physics (23XB)",
+        "Oxford University English (23XK)",
+        "Oxford University Geography (23XJ)",
+        "Oxford University Philosophy (T3XK)",
+        "Cambridge University Dance (23XF)",
       ],
     )
   end
