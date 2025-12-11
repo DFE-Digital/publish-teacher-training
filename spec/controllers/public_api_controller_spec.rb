9910946f7a7442aadd517e83b274d94cbdc9cd01
Simon L <simon.lange@education.gov.uk>
2025-12-11 16:28:54 +0000

    Refactor tests in PublicAPIController spec to use described_class for ancestor checks

--- spec/controllers/public_api_controller_spec.rb
+++ spec/controllers/public_api_controller_spec.rb
@@ -5,15 +5,15 @@ require "rails_helper"
 RSpec.describe PublicAPIController, type: :controller do
   describe "default behaviors" do
     it "includes DfE::Analytics::Requests" do
-      expect(PublicAPIController.ancestors).to include(DfE::Analytics::Requests)
+      expect(described_class.ancestors).to include(DfE::Analytics::Requests)
     end
 
     it "includes Pagy::Backend" do
-      expect(PublicAPIController.ancestors).to include(Pagy::Backend)
+      expect(described_class.ancestors).to include(Pagy::Backend)
     end
 
     it "includes Pundit::Authorization" do
-      expect(PublicAPIController.ancestors).to include(Pundit::Authorization)
+      expect(described_class.ancestors).to include(Pundit::Authorization)
     end
   end
 
