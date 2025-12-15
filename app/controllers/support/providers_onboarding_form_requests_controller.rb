537124c5e41fc9112d2e8c98567ab5f1987d6b42
sarahcrack <73823091+sarahcrack@users.noreply.github.com>
2025-12-15 15:50:05 +0000

    Providers Onboarding - generate request form
    - Generate providers onboarding request
    - Success message

--- app/controllers/support/providers_onboarding_form_requests_controller.rb
+++ app/controllers/support/providers_onboarding_form_requests_controller.rb
@@ -6,15 +6,26 @@ module Support
 
     def index
       # Fetches all requests ordered by creation date, paginated and passes to view to display as a table
-      @pagy, @requests = pagy(ProvidersOnboardingFormRequest.includes(:support_agent).order(created_at: :desc))
+      @pagy, @onboarding_requests = pagy(ProvidersOnboardingFormRequest.includes(:support_agent).order(created_at: :desc))
     end
 
     def new
-      # Placeholder: form for creating a new onboarding request will be implemented in next ticket
+      @onboarding_request = ProvidersOnboardingFormRequest.new
+      @admin_users = User.where(admin: true).order(:first_name, :last_name)
     end
 
     def create
-      # Placeholder: logic for creating a new onboarding request will be implemented in next ticket
+      @onboarding_request = ProvidersOnboardingFormRequest.new(request_params)
+      @onboarding_request.uuid = SecureRandom.uuid
+      @onboarding_request.status = "pending"
+      @admin_users = User.where(admin: true).order(:first_name, :last_name)
+      @onboarding_request.form_link = publish_provider_onboarding_form_url(uuid: @onboarding_request.uuid)
+
+      if @onboarding_request.save
+        redirect_to support_providers_onboarding_form_requests_path, flash: { success: t(".success_message_html", form_link: @onboarding_request.form_link) }
+      else
+        render :new
+      end
     end
 
     def show
@@ -29,12 +40,20 @@ module Support
 
     def set_request
       # Finds the request by id param for show and update actions
-      @request = ProvidersOnboardingFormRequest.find(params[:id])
+      @onboarding_request = ProvidersOnboardingFormRequest.find(params[:id])
     end
 
     def request_params
       # Strong params for onboarding request (to be used in next ticket)
-      params.require(:providers_onboarding_form_request).permit(:form_name, :zendesk_link, :support_agent_id, :status)
+      params.require(:providers_onboarding_form_request).permit(:form_name, :zendesk_link, :support_agent_id)
+    end
+
+    def onboarding_request_params
+      params.require(:providers_onboarding_form_request).permit(
+        :provider_name, :zendesk_link, :email_address, :first_name, :last_name,
+        :address_line_1, :town_or_city, :postcode, :telephone, :contact_email_address,
+        :website, :ukprn, :accredited_provider, :urn, :support_agent_id
+      )
     end
   end
 end
