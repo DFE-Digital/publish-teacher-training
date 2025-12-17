3c1af7cac263af0294976994cef0e34d0f44f57e
Tomas D'Stefano <tomas_stefano@successoft.com>
2025-12-17 16:57:26 +0000

    Refactor active filter extractor
    Better to move to methods instead of constants

--- app/services/courses/active_filter_extractor.rb
+++ app/services/courses/active_filter_extractor.rb
@@ -1,120 +1,123 @@
 module Courses
   class ActiveFilterExtractor
-    DEFAULTS = {
-      order: proc { |p| p[:short_address].present? ? "distance" : "course_name_ascending" },
-      level: "all",
-      minimum_degree_required: "show_all_courses",
-      applications_open: true,
-    }.freeze
-
-    CONDITIONS = {
-      radius: proc { |p| p[:short_address].present? },
-    }.freeze
-
     def initialize(search_params:, search_form:)
       @search_params = search_params
       @search_form = search_form
     end
 
     def call
-      active_filters = @search_params.flat_map { |attribute, value|
-        next [] if value.blank?
-        next [] if skip_by_condition?(attribute)
-        next [] if skip_by_default?(attribute, value)
-        next [] if skip_invalid_filter?(attribute, value)
-
-        build_filter_list(attribute, value)
-      }.compact
-
-      active_filters.reject { |active_filter| active_filter.formatted_value.blank? }
+      @search_params
+        .flat_map { |attribute, value| process_attribute(attribute, value) }
+        .compact
+        .reject { |filter| filter.formatted_value.blank? }
     end
 
   private
 
-    def skip_by_condition?(attribute)
-      condition = CONDITIONS[attribute.to_sym]
+    def process_attribute(attribute, value)
+      return [] if value.blank?
+      return [] if skip_condition?(attribute) || skip_default?(attribute, value) || skip_invalid?(attribute, value)
+
+      build_filters(attribute, value)
+    end
+
+    def skip_condition?(attribute)
+      condition = conditions[attribute.to_sym]
       condition && !condition.call(@search_params)
     end
 
-    def skip_by_default?(attribute, value)
-      default = DEFAULTS[attribute.to_sym]
-      default = default.is_a?(Proc) ? default.call(@search_params) : default
+    def skip_default?(attribute, value)
+      default = resolve_default(attribute)
       value == default
     end
 
-    def skip_invalid_filter?(attribute, value)
+    def resolve_default(attribute)
+      default = defaults[attribute.to_sym]
+      default.is_a?(Proc) ? default.call(@search_params) : default
+    end
+
+    def skip_invalid?(attribute, value)
       valid_values = resolve_valid_values(attribute)
       return false unless valid_values
 
-      case value
-      when Array
-        value.any? { |v| !valid_values.include?(v) }
-      else
-        !valid_values.include?(value)
-      end
+      values_to_validate = Array(value)
+      values_to_validate.any? { |val| !valid_values.include?(val) }
     end
 
     def resolve_valid_values(attribute)
-      case attribute.to_sym
-      when :subjects, :subject_code
-        @search_form.all_subjects.map(&:value)
-      when :provider_code
-        @search_form.providers_list.map(&:code)
-      when :funding
-        @search_form.funding_options
-      when :study_types
-        @search_form.study_type_options
-      when :qualifications
-        @search_form.qualification_options
-      when :start_date
-        @search_form.start_date_options
-      when :minimum_degree_required
-        @search_form.minimum_degree_required_options
-      end
+      resolver = value_resolvers[attribute.to_sym]
+      resolver&.call(@search_form)
     end
 
-    def resolve_value(attribute, raw_value)
-      case attribute.to_sym
-      when :subjects, :subject_code
-        @search_form.all_subjects.find { |s| s.value == raw_value }&.name || raw_value
-      when :provider_code
-        @search_form.providers_list.find { |p| p.code == raw_value }&.name || raw_value
-      else
-        raw_value
-      end
+    def resolve_formatted_value(attribute, raw_value)
+      resolver = formatter_resolvers[attribute.to_sym]
+      resolver ? resolver.call(@search_form, raw_value) : raw_value
     end
 
-    def build_filter_list(attribute, value)
-      return build_location_active_filter(value) if attribute.to_sym == :short_address
+    def build_filters(attribute, value)
+      return [build_location_filter(value)] if attribute.to_sym == :short_address
 
-      case value
-      when Array
-        value.map do |v|
-          remaining = value - [v]
-          ActiveFilter.new(
-            id: attribute,
-            raw_value: v,
-            value: resolve_value(attribute, v),
-            remove_params: { attribute.to_sym => remaining.presence },
-          )
-        end
-      else
-        [ActiveFilter.new(
-          id: attribute,
-          raw_value: value,
-          value: resolve_value(attribute, value),
-          remove_params: { attribute.to_sym => nil },
-        )]
-      end
+      Array(value).map { |val| build_filter(attribute, val, value) }
     end
 
-    def build_location_active_filter(value)
+    def build_filter(attribute, current_value, all_values)
+      ActiveFilter.new(
+        id: attribute,
+        raw_value: current_value,
+        value: resolve_formatted_value(attribute, current_value),
+        remove_params: compute_removal_params(attribute, current_value, all_values),
+      )
+    end
+
+    def compute_removal_params(attribute, current_value, all_values)
+      remaining_values = Array(all_values) - [current_value]
+
+      { attribute.to_sym => remaining_values.presence }
+    end
+
+    def build_location_filter(value)
       ActiveFilter.new(
         id: :short_address,
         raw_value: value,
-        value:,
+        value: value,
         remove_params: { location: nil, radius: nil },
       )
+    end
+
+    def defaults
+      {
+        order: proc { |params| params[:short_address].present? ? "distance" : "course_name_ascending" },
+        level: "all",
+        minimum_degree_required: "show_all_courses",
+        applications_open: true,
+      }
+    end
+
+    def conditions
+      {
+        radius: proc { |params| params[:short_address].present? },
+      }
+    end
+
+    def value_resolvers
+      {
+        subjects: ->(form) { form.all_subjects.map(&:value) },
+        subject_code: ->(form) { form.all_subjects.map(&:value) },
+        provider_code: ->(form) { form.providers_list.map(&:code) },
+        funding: ->(form) { form.funding_options },
+        study_types: ->(form) { form.study_type_options },
+        qualifications: ->(form) { form.qualification_options },
+        start_date: ->(form) { form.start_date_options },
+        minimum_degree_required: ->(form) { form.minimum_degree_required_options },
+      }
+    end
+
+    def formatter_resolvers
+      {
+        subjects: ->(form, value) { form.all_subjects.find { |s| s.value == value }&.name || value },
+        subject_code: ->(form, value) { form.all_subjects.find { |s| s.value == value }&.name || value },
+        provider_code: ->(form, value) { form.providers_list.find { |p| p.code == value }&.name || value },
+      }
     end
   end
 end
