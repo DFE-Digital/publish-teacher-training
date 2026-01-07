00bda4eff6a4a453704e03dc06dbeb557d2071a0
Iain McNulty <iain.mcnulty@education.gov.uk>
2026-01-07 13:15:52 +0000

    Add filter count for provider

--- app/forms/courses/search_form.rb
+++ app/forms/courses/search_form.rb
@@ -89,6 +89,7 @@ module Courses
         interview_chosen: interview_location && 1 || nil,
         ordering_chosen:,
         radius_chosen:,
+        provider: (provider_code || provider_name).presence && 1,
         degree_chosen: minimum_degree_required == "show_all_courses" ? nil : 1,
         start_date_chosen: start_date&.count,
         sponsor_visa_chosen: can_sponsor_visa && 1 || nil,
