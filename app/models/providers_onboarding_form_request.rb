d5eb49f4b4645a5753ddc52383f89176fcdde51f
sarahcrack <73823091+sarahcrack@users.noreply.github.com>
2026-01-21 16:33:07 +0000

    Update error messages for publish provider onboarding form

--- app/models/providers_onboarding_form_request.rb
+++ app/models/providers_onboarding_form_request.rb
@@ -17,16 +17,19 @@ class ProvidersOnboardingFormRequest < ApplicationRecord
 
   # These fields need to be validated once the form details are submitted by the provider i.e. status changes to 'submitted'
   with_options if: :submitted? do
-    validates :first_name, :last_name,
-              :provider_name, :address_line_1, :town_or_city, presence: true
-
-    validates :email_address, :contact_email_address, email_address: true
-    validates :postcode, presence: true, postcode: true
-    validates :telephone, phone: true
-    validates :website, presence: true, url: true
-    validates :accredited_provider, inclusion: { in: [true, false] }
+    validates :provider_name, presence: true
     validates :ukprn, ukprn_format: { allow_blank: false }
+    validates :accredited_provider, inclusion: { in: [true, false] }
     validates :urn, reference_number_format: { allow_blank: false, minimum: 5, maximum: 6, message: "Provider URN must be 5 or 6 numbers" }
+    validates :contact_email_address, email_address: true
+    validates :telephone, phone: { allow_blank: false }
+    validates :website, presence: true, url: true
+    validates :address_line_1, presence: true
+    validates :town_or_city, presence: true
+    validates :postcode, presence: true, postcode: true
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+    validates :email_address, email_address: true
   end
 
   def form_link
