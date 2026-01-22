893e7eddf45e4f114f74abb17fc164771b5001f7
Simon L <simon.lange@education.gov.uk>
2026-01-22 16:26:09 +0000

    Update maintenance banner messages to reflect current login issues

--- spec/components/find/maintenance_banner_component_spec.rb
+++ spec/components/find/maintenance_banner_component_spec.rb
@@ -9,7 +9,7 @@ module Find
         FeatureFlag.activate(:maintenance_banner)
         result = render_inline(described_class.new)
 
-        expect(result.text).to have_content "This service will be unavailable on"
+        expect(result.text).to have_content "Login issues"
       end
     end
 
