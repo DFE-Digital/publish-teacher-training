8a9797655bb3a9e6eb68461a3793be85e491454c
Tomas D'Stefano <tomas_stefano@successoft.com>
2026-01-23 14:55:26 +0000

    Add clear all links to results event
    Now everytime an user will click on clear all links, dfe analytics
    will send the two params indicating the results page was rendered
    from a clear all link.
    
    Add spec for clear all filters component too.

--- app/components/find/courses/clear_all_filters/view.rb
+++ app/components/find/courses/clear_all_filters/view.rb
@@ -2,13 +2,20 @@ module Find
   module Courses
     module ClearAllFilters
       class View < ViewComponent::Base
-        attr_reader :active_filters, :html_class
+        attr_reader :active_filters, :html_class, :utm_medium
 
-        def initialize(active_filters:, html_class: "app-c-filter-summary__clear-filters")
+        def initialize(active_filters:, position:, html_class: "app-c-filter-summary__clear-filters")
           super
 
           @active_filters = active_filters
           @html_class = html_class
+          @utm_medium = "clear_all_filters_#{position}"
+        end
+
+        UTM_SOURCE = "results".freeze
+
+        def utm_source
+          UTM_SOURCE
         end
 
         def render?
