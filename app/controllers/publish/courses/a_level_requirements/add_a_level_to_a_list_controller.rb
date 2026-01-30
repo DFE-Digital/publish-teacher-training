f36d00e3133981be1622d5b78a8cb7633b665183
Iain McNulty <iain.mcnulty@education.gov.uk>
2026-01-30 16:09:40 +0000

    Update AddALevelToAList step
      - Mostly the same as before
      - Iterate list of subjects on the template with Change and Remove
        actions
      - Add helper methods to the state store
        * subjects is used in the template
        * any_a_levels? is used for conditional routing in the Wizard

--- app/controllers/publish/courses/a_level_requirements/add_a_level_to_a_list_controller.rb
+++ app/controllers/publish/courses/a_level_requirements/add_a_level_to_a_list_controller.rb
@@ -4,12 +4,6 @@ module Publish
   module Courses
     module ALevelRequirements
       class AddALevelToAListController < ALevelRequirementsController
-        def step_params
-          params[current_step] ||= ActionController::Parameters.new
-          params[current_step][:subjects] ||= @course.a_level_subject_requirements
-
-          params
-        end
       end
     end
   end
