8ffcefcbe4df71e132d0770a109e4a8f242ff09c
Iain McNulty <iain.mcnulty@education.gov.uk>
2026-02-03 14:29:05 +0000

    Add all next step specs for a levels wizard

--- spec/wizards/a_levels/wizard/next_step_spec.rb
+++ spec/wizards/a_levels/wizard/next_step_spec.rb
@@ -6,11 +6,40 @@ RSpec.describe "ALevelsWizard#next_step", type: :wizard do
   include_context "a_levels_wizard"
 
   context "from what_a_level_is_required" do
-    # Tests for navigation from what_a_level_is_required
+    let(:current_step) { :what_a_level_is_required }
+
+    it "proceeds to add_a_level_to_a_list" do
+      expect(wizard).to have_next_step(:add_a_level_to_a_list)
+    end
   end
 
   context "from add_a_level_to_a_list" do
-    # Tests for conditional edge based on another_a_level_needed?
+    let(:current_step) { :add_a_level_to_a_list }
+    let(:a_level_subject_requirements) do
+      [{ "uuid" => "123", "subject" => "maths", "minimum_grade_required" => "A" }]
+    end
+
+    context "when adding another A-level" do
+      before do
+        state_store.write(add_another_a_level: "yes")
+      end
+
+      it "branches to what_a_level_is_required" do
+        expect(wizard).to branch_from(:add_a_level_to_a_list)
+          .to(:what_a_level_is_required)
+      end
+    end
+
+    context "when not adding another A-level" do
+      before do
+        state_store.write(add_another_a_level: "no")
+      end
+
+      it "branches to consider_pending_a_level" do
+        expect(wizard).to branch_from(:add_a_level_to_a_list)
+          .to(:consider_pending_a_level)
+      end
+    end
   end
 
   context "from remove_a_level_subject_confirmation" do
@@ -41,10 +70,24 @@ RSpec.describe "ALevelsWizard#next_step", type: :wizard do
   end
 
   context "from consider_pending_a_level" do
-    # Tests for navigation from consider_pending_a_level
+    let(:current_step) { :consider_pending_a_level }
+    let(:a_level_subject_requirements) do
+      [{ "uuid" => "123", "subject" => "maths", "minimum_grade_required" => "A" }]
+    end
+
+    it "proceeds to a_level_equivalencies" do
+      expect(wizard).to have_next_step(:a_level_equivalencies)
+    end
   end
 
   context "from a_level_equivalencies" do
-    # Tests for navigation from a_level_equivalencies
+    let(:current_step) { :a_level_equivalencies }
+    let(:a_level_subject_requirements) do
+      [{ "uuid" => "123", "subject" => "maths", "minimum_grade_required" => "A" }]
+    end
+
+    it "proceeds to course_edit" do
+      expect(wizard).to have_next_step(:course_edit)
+    end
   end
 end
