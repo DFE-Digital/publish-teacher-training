787197824e93ca743710c5ac920bb6cceff929ee
Iain McNulty <iain.mcnulty@education.gov.uk>
2026-02-03 14:23:03 +0000

    Add next_step_spec for a_levels wizard

--- app/wizards/a_levels_wizard.rb
+++ app/wizards/a_levels_wizard.rb
@@ -5,12 +5,12 @@ class ALevelsWizard
 
   attr_accessor :recruitment_cycle_year, :provider_code, :course_code
 
-  delegate :another_a_level_needed?, to: :state_store
+  delegate :any_a_levels?, :another_a_level_needed?, :has_remaining_a_levels?, to: :state_store
 
   def steps_processor
     DfE::Wizard::StepsProcessor::Graph.draw(self) do |graph|
-      graph.conditional_root(potential_root: %i[add_a_level_to_a_list what_a_level_is_required]) do |state_store|
-        if state_store.any_a_levels?
+      graph.conditional_root(potential_root: %i[add_a_level_to_a_list what_a_level_is_required]) do |_state_store|
+        if any_a_levels?
           :add_a_level_to_a_list
         else
           :what_a_level_is_required
@@ -25,7 +25,14 @@ class ALevelsWizard
 
       graph.add_edge from: :what_a_level_is_required, to: :add_a_level_to_a_list
 
-      graph.add_edge from: :remove_a_level_subject_confirmation, to: :add_a_level_to_a_list
+      graph.add_conditional_edge(
+        from: :remove_a_level_subject_confirmation,
+        when: :any_a_levels?,
+        then: :add_a_level_to_a_list,
+        else: :course_edit,
+        label: "Has remaining A-levels?",
+      )
+
       graph.add_edge from: :consider_pending_a_level, to: :a_level_equivalencies
       graph.add_edge from: :a_level_equivalencies, to: :course_edit
 
@@ -34,6 +41,7 @@ class ALevelsWizard
         when: :another_a_level_needed?,
         then: :what_a_level_is_required,
         else: :consider_pending_a_level,
+        label: "Another A Level needed?",
       )
     end
   end
---
+++ spec/support/shared_contexts/a_levels_wizard.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+RSpec.shared_context "a_levels_wizard" do
+  subject(:wizard) do
+    ALevelsWizard.new(
+      state_store:,
+      current_step:,
+      current_step_params: { current_step => current_step_params },
+    )
+  end
+
+  let(:course) { create(:course, a_level_subject_requirements:) }
+  let(:a_level_subject_requirements) { [] }
+  let(:repository) { Repositories::ALevelRepository.new(record: course) }
+  let(:state_store) { StateStores::ALevelStore.new(repository:) }
+  let(:current_step) { :add_a_level_to_a_list }
+  let(:current_step_params) { {} }
+end
