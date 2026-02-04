fcc54362dde1ad43fc1142db207df2754f3aa850
Iain McNulty <iain.mcnulty@education.gov.uk>
2026-02-04 14:20:01 +0000

    Move publish accredited_provider_search and schools features to system

--- spec/system/publish/providers/accredited_provider_search/searching_for_an_accredited_provider_spec.rb
+++ spec/system/publish/providers/accredited_provider_search/searching_for_an_accredited_provider_spec.rb
@@ -2,7 +2,7 @@
 
 require "rails_helper"
 
-feature "Searching for an accredited provider" do
+RSpec.describe "Searching for an accredited provider" do
   before do
     given_i_am_a_lead_school_provider_user
     and_there_are_accredited_providers_in_the_database
