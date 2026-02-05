3649fb549b20f7a3bcb3880d025f464b3e3b18cb
Iain McNulty <iain.mcnulty@education.gov.uk>
2026-02-05 16:50:50 +0000

    Monkey patch GovukTechDocs TOC Helpers
      - we want our docs to be generated at /docs/ and all links to be
        prefixed with /docs/ but the GovukTechDocs gem only generates table
        of contents for pages at the top level

--- docs/config.rb
+++ docs/config.rb
@@ -3,6 +3,16 @@
 require "govuk_tech_docs"
 require "lib/govuk_tech_docs/open_api/extension"
 
+::GovukTechDocs::TableOfContents::Helpers.module_eval do
+  def select_top_level_html_files(resources)
+    prefix = config[:http_prefix]
+    home_url = prefix.end_with?("/") ? prefix : "#{prefix}/"
+
+    resources
+      .select { |r| r.path.end_with?(".html") && (r.parent.nil? || r.parent.url == home_url) }
+  end
+end
+
 GovukTechDocs.configure(self)
 
 activate :open_api
