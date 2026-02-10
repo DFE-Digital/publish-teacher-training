f975929448a703a41495db7b5bd7b9f48950963a
gms-gs <george.schena@education.gov.uk>
2026-02-10 16:59:08 +0000

    Add Note to Saved Course table

--- spec/models/saved_course_spec.rb
+++ spec/models/saved_course_spec.rb
@@ -10,5 +10,23 @@ RSpec.describe SavedCourse, type: :model do
     subject { create(:saved_course) }
 
     it { is_expected.to validate_uniqueness_of(:candidate_id).scoped_to(:course_id) }
+
+    it "allows note to be nil" do
+      saved_course = build(:saved_course, note: nil)
+      expect(saved_course).to be_valid
+    end
+
+    it "allows up to 100 words in note" do
+      saved_course = build(:saved_course, note: ("word " * 100).strip)
+
+      expect(saved_course).to be_valid
+    end
+
+    it "does not allow more than 100 words in note" do
+      saved_course = build(:saved_course, note: ("word " * 101).strip)
+
+      expect(saved_course).not_to be_valid
+      expect(saved_course.errors[:note]).to include("Your note must be 100 words or less")
+    end
   end
 end
