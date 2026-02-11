ce7138381bbb44c18e317037c6c0de6077f39351
Iain McNulty <iain.mcnulty@education.gov.uk>
2026-02-11 12:20:18 +0000

    Add success flash message to a levels equivalency controller

--- app/controllers/publish/courses/a_level_requirements/a_level_equivalencies_controller.rb
+++ app/controllers/publish/courses/a_level_requirements/a_level_equivalencies_controller.rb
@@ -4,6 +4,9 @@ module Publish
   module Courses
     module ALevelRequirements
       class ALevelEquivalenciesController < ALevelRequirementsController
+        def add_flash_message
+          flash[:success] = t("course.#{@wizard.current_step.model_name.i18n_key}.success_message")
+        end
       end
     end
   end
