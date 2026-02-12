0e3a622257c74e3d93393a85f5745a9b19680ddf
Iain McNulty <iain.mcnulty@education.gov.uk>
2026-02-12 17:58:37 +0000

    Clean up references to features in defined metadata includes

--- spec/support/features.rb
+++ spec/support/features.rb
@@ -1,17 +1,11 @@
 # frozen_string_literal: true
 
 RSpec.configure do |config|
-  config.include FeatureHelpers::Authentication, type: :feature
   config.include FeatureHelpers::Authentication, type: :system
-  config.include FeatureHelpers::NewCourseParam, type: :feature
   config.include FeatureHelpers::NewCourseParam, type: :system
-  config.include FeatureHelpers::GovukComponents, type: :feature
   config.include FeatureHelpers::GovukComponents, type: :system
-  config.include FeatureHelpers::CourseSteps, type: :feature
   config.include FeatureHelpers::CourseSteps, type: :system
-  config.include FeatureHelpers::PageWithQuery, type: :feature
   config.include FeatureHelpers::PageWithQuery, type: :system
-  config.include DfESignInUserHelper, type: :feature
   config.include DfESignInUserHelper, type: :system
   config.include FeatureHelpers::PageObject::Support, :support_features
   config.include FeatureHelpers::PageObject::Publish, :publish_features
