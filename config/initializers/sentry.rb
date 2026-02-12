1c83a2a0c001f78a6c0a8f3552cf07d42ffeb6db
Iain McNulty <iain.mcnulty@education.gov.uk>
2026-02-12 14:28:01 +0000

    Fix Sentry configuration
      - !Rails.env.production returns a string so !String == false every time
      - Sentry::Event#to_hash moved to Sentry::Event#to_h

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -7,22 +7,24 @@ PG_DETAIL_FILTERED = "[PG DETAIL FILTERED]"
 def filter_record_not_unique_exception_messages!(event, hint)
   return unless hint[:exception].is_a?(ActiveRecord::RecordNotUnique)
 
-  event.exception.values.each do |single_exception| # rubocop:disable Style/HashEachMethods
+  event.exception.each_value do |single_exception|
     single_exception.value.gsub!(PG_DETAIL_REGEX, PG_DETAIL_FILTERED)
   end
 end
 
 def skip_filter?(event)
-  event[:message] && event[:message][:message] == "One Login failure"
+  event.message.is_a?(Hash) && event.message[:message] == "One Login failure"
 end
 
 Sentry.init do |config|
   filter = ActiveSupport::ParameterFilter.new(Rails.application.config.filter_parameters)
   config.before_send = lambda do |event, hint|
-    return if skip_filter?(event) && !Rails.env.production
-
-    filter_record_not_unique_exception_messages!(event, hint)
-    filter.filter(event.to_hash)
+    if skip_filter?(event) && !Rails.env.production?
+      nil
+    else
+      filter_record_not_unique_exception_messages!(event, hint)
+      filter.filter(event.to_h)
+    end
   end
 
   config.release = ENV.fetch("COMMIT_SHA", nil)
