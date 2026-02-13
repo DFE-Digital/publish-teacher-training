01ea94b6677cfd3b8ef877c2c051eb8e255ecd0d
Iain McNulty <iain.mcnulty@education.gov.uk>
2026-02-13 16:36:54 +0000

    Sanitize Sentry event properties
      We cannot return a hash from the `before_send` block sine sentry-ruby
      v6.0 so we sanitize each property individually
    
      https://docs.sentry.io/platforms/ruby/guides/rails/data-management/sensitive-data/

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -12,19 +12,27 @@ def filter_record_not_unique_exception_messages!(event, hint)
   end
 end
 
-def skip_filter?(event)
-  event.message.is_a?(Hash) && event.message[:message] == "One Login failure"
-end
-
 Sentry.init do |config|
   filter = ActiveSupport::ParameterFilter.new(Rails.application.config.filter_parameters)
-  config.before_send = lambda do |event, hint|
-    if skip_filter?(event) && !Rails.env.production?
-      nil
-    else
-      filter_record_not_unique_exception_messages!(event, hint)
-      filter.filter(event.to_h)
+
+  config.before_send = lambda do |event, _hint|
+    filter_record_not_unique_exception_messages!(event, hint)
+
+    # Sanitize extra data
+    if event.extra
+      event.extra = filter.filter(event.extra)
     end
+    # Sanitize user data
+    if event.user
+      event.user = filter.filter(event.user)
+    end
+    # Sanitize context data (if present)
+    if event.contexts
+      event.contexts = filter.filter(event.contexts)
+    end
+
+    # Return the sanitized event object
+    event
   end
 
   config.release = ENV.fetch("COMMIT_SHA", nil)
