74ba1d99ceba14150b5df1250ea3073dbf6930a3
Iain McNulty <iain.mcnulty@education.gov.uk>
2026-03-03 09:54:02 +0000

    Set remote_ip as request.headers["X-Azure-ClientIP"]

--- app/middleware/request_logging_tags.rb
+++ app/middleware/request_logging_tags.rb
@@ -7,11 +7,14 @@ class RequestLoggingTags
     request = ActionDispatch::Request.new(env)
     session_id = request.session&.id&.public_id || "-"
 
+    # Azure FrontDoor appends the FrontDoor IP address to X-Forwarded-For header
+    # So we should use X-Azure-ClientIP header to get the client IP
+    # https://learn.microsoft.com/en-us/azure/frontdoor/front-door-http-headers-protocol
     SemanticLogger.tagged(
       request_id: request.request_id,
       session_id: session_id,
       user_agent: request.user_agent,
-      remote_ip: request.remote_ip,
+      remote_ip: request.headers["X-Azure-ClientIP"] || request.remote_ip,
       referrer: request.referrer,
     ) do
       @app.call(env)
