71c254db673b164462b3d76dafcaf55c0542623e
gms-gs <george.schena@education.gov.uk>
2026-03-04 12:05:44 +0000

    Add test to check recent searches are also deleted

--- spec/system/support/candidates/delete_candidate_spec.rb
+++ spec/system/support/candidates/delete_candidate_spec.rb
@@ -21,6 +21,22 @@ RSpec.describe "Support console deleting a candidate" do
     expect(SavedCourse.where(candidate_id: candidate.id)).to be_empty
   end
 
+  scenario "user deletes a candidate and their recent searches" do
+    candidate = create(:candidate)
+    create(:recent_search, find_candidate: candidate)
+    create(:recent_search, find_candidate: candidate, search_attributes: { provider_name: "Test provider" })
+
+    visit details_support_candidate_path(candidate)
+    click_link_or_button "Delete this candidate"
+    click_link_or_button "Remove candidate"
+
+    expect(page).to have_current_path(support_candidates_path)
+    expect(page).to have_content("Candidate successfully deleted")
+
+    expect(Candidate.exists?(candidate.id)).to be(false)
+    expect(RecentSearch.where(find_candidate_id: candidate.id)).to be_empty
+  end
+
   scenario "user sees an error when candidate deletion fails" do
     candidate = create(:candidate)
     allow(Candidate).to receive(:find).and_call_original
@@ -33,6 +49,7 @@ RSpec.describe "Support console deleting a candidate" do
 
     expect(page).to have_current_path(support_candidates_path)
     expect(page).to have_content("This candidate could not be deleted")
+
     expect(Candidate.exists?(candidate.id)).to be(true)
   end
 end
