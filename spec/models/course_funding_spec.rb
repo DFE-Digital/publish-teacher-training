a3918802f288b7550f9ab4e34d32d91eb274ec7b
Iain McNulty <iain.mcnulty@education.gov.uk>
2026-03-11 10:19:58 +0000

    Add more spec scenario coverage for bursary and scholarships

--- spec/models/course_funding_spec.rb
+++ spec/models/course_funding_spec.rb
@@ -326,7 +326,17 @@ describe CourseFunding do
   end
 
   describe "subordinate subject exclusion" do
-    context "when course has a subordinate subject" do
+    context "when course has no subordinate subject" do
+      it "considers all subjects" do
+        physics = build(:secondary_subject, bursary_amount: "29000")
+        mathematics = build(:secondary_subject, bursary_amount: "25000")
+        course = build(:course, :secondary, subjects: [physics, mathematics])
+
+        expect(described_class.new(course).max_bursary_amount).to eq("29000")
+      end
+    end
+
+    context "when course has a subordinate subject with no master bursary" do
       include_context "course with subordinate subject"
 
       it "ignores the subordinate subject's bursary" do
@@ -343,6 +353,57 @@ describe CourseFunding do
 
       it "ignores the subordinate subject's non-UK scholarship eligibility" do
         expect(funding).not_to be_scholarship_eligible_subjects
+      end
+    end
+
+    context "when course has a subordinate subject and master has bursary" do
+      let(:master) { build(:secondary_subject, subject_name: "Mathematics", bursary_amount: "25000") }
+      let(:subordinate) { build(:secondary_subject, subject_name: "Physics", bursary_amount: "29000", scholarship: "31000") }
+      let(:course) do
+        build(:course, :secondary, name: "Mathematics with Physics",
+                                   subjects: [master, subordinate],
+                                   master_subject_id: master.id,
+                                   subordinate_subject_id: subordinate.id)
+      end
+      let(:funding) { described_class.new(course) }
+
+      it "uses the master subject's bursary, not the subordinate's" do
+        expect(funding.max_bursary_amount).to eq("25000")
+      end
+
+      it "does not use the subordinate's scholarship" do
+        expect(funding).not_to be_has_scholarship
+      end
+    end
+
+    context "when Modern Languages is master with subordinate" do
+      include_context "modern languages with subordinate subject"
+
+      it "uses language subjects, ignoring both ML parent and subordinate" do
+        expect(funding.max_bursary_amount).to eq("20000")
+        expect(funding.max_scholarship_amount).to eq("22000")
+      end
+
+      it "ignores the subordinate's non-UK eligibility" do
+        mandarin.financial_incentive.non_uk_bursary_eligible = false
+        expect(funding).not_to be_bursary_eligible_subjects
+      end
+    end
+
+    context "when Modern Languages is master without subordinate" do
+      let(:modern_languages) { find_or_create(:secondary_subject, :modern_languages) }
+      let(:french) do
+        build(:modern_languages_subject, :french).tap do |s|
+          s.financial_incentive = FinancialIncentive.new(bursary_amount: "20000")
+        end
+      end
+      let(:course) do
+        build(:course, :secondary, subjects: [modern_languages, french],
+                                   master_subject_id: modern_languages.id)
+      end
+
+      it "uses language subjects" do
+        expect(described_class.new(course).max_bursary_amount).to eq("20000")
       end
     end
   end
