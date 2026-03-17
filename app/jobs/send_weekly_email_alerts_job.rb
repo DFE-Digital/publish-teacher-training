bc71826151ffb81e4637697303cc540c51d7196d
Tomas D'Stefano <tomas_stefano@successoft.com>
2026-03-17 16:41:49 +0000

    Disable retries for SendWeeklyEmailAlertsJob

--- app/jobs/send_weekly_email_alerts_job.rb
+++ app/jobs/send_weekly_email_alerts_job.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class SendWeeklyEmailAlertsJob < ApplicationJob
+  retry_on StandardError, attempts: 0
+
   def perform(since: 1.week.ago)
     Find::ProcessWeeklyEmailAlertsService.call(since:)
   end
