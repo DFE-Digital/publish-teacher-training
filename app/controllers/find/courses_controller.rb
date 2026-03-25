e5b97ac68df009bcc78d226909d65ec5997c5e39
Iain McNulty <iain.mcnulty@education.gov.uk>
2026-03-25 08:16:44 +0000

    Stop storing the search_params

--- app/controllers/find/courses_controller.rb
+++ app/controllers/find/courses_controller.rb
@@ -9,7 +9,6 @@ module Find
     before_action -> { render_not_found if provider.nil? }
 
     before_action :render_feedback_component, only: :show
-    before_action :set_search_params, only: :show
     before_action :set_course, only: %i[show confirm_apply]
 
     def show
@@ -38,63 +37,6 @@ module Find
         latitude: @address.latitude,
         longitude: @address.longitude,
       ).call.first.distance_to_search_location.ceil
-    end
-
-    def legacy_paramater_keys
-      %i[
-        fulltime
-        hasvacancies
-        lat
-        lng
-        parttime
-        prev_l
-        prev_lat
-        prev_lng
-        prev_loc
-        prev_lq
-        prev_query
-        prev_rad
-        qualifications
-        query
-        rad
-        senCourses
-      ]
-    end
-
-    def set_search_params
-      return if params[:search_params].blank?
-
-      session[:search_params] = ActionController::Parameters.new(
-        Rack::Utils.parse_nested_query(params[:search_params]),
-      ).permit(
-        *legacy_paramater_keys,
-        :visa_status,
-        :age_group,
-        :c,
-        :can_sponsor_visa,
-        :degree_required,
-        :engineers_teach_physics,
-        :funding,
-        :has_vacancies,
-        :university_degree_status,
-        :applications_open,
-        :l,
-        :latitude,
-        :loc,
-        :long,
-        :longitude,
-        :lq,
-        :radius,
-        :send_courses,
-        :sortby,
-        "provider.provider_name",
-        c: [],
-        qualification: [],
-        qualifications: [], # Legacy
-        study_type: [],
-        subjects: [],
-        subject_codes: [], # Legacy
-      )
     end
 
   private
