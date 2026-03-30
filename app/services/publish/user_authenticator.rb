31960eab0f7e31b4953f0ab9e75a7097e4b38b7d
Iain McNulty <iain.mcnulty@education.gov.uk>
2026-03-30 12:13:07 +0100

    Revert User authentication to use email only
      - The DfE Sign-in oatuh uid is not unique in our database
      - We can't reliably find a single user matching the oauth uid

--- app/services/publish/user_authenticator.rb
+++ app/services/publish/user_authenticator.rb
@@ -8,48 +8,23 @@ module Publish
       @oauth = oauth
     end
 
-    # 1. Find existing User via provider authentication
-    # 2. Update User details if they have changed
-    # 3. Fall back to email lookup for first-time sign-in and create authentication record
     # @return User or nil
     def call
-      if authentication.present?
-        sign_in!
-      else
-        first_sign_in!
-      end
+      user = User.find_by(email: email_address)
+      return unless user
+
+      update_user_details(user)
+      user
     end
 
   private
 
-    def sign_in!
-      authentication.authenticable.tap do |user|
-        update_user_details(user)
-      end
-    end
-
-    def first_sign_in!
-      user = User.find_by(email: email_address)
-      return unless user
-
-      ::Authentication.transaction do
-        user.authentications.create!(provider:, subject_key: oauth.uid)
-        update_user_details(user)
-      end
-      user
-    end
-
     def update_user_details(user)
       attributes = { last_login_date_utc: Time.zone.now }
-      attributes[:email] = email_address unless user.email.casecmp?(email_address)
       attributes[:first_name] = oauth.info.first_name if oauth.info.first_name.present?
       attributes[:last_name] = oauth.info.last_name if oauth.info.last_name.present?
 
       user.update!(attributes)
-    end
-
-    def authentication
-      @authentication ||= ::Authentication.find_by(subject_key: oauth.uid, provider:)
     end
 
     def provider
