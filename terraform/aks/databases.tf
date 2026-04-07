6e5f297e598b55c55979c6dea3327416d5014fa4
paul senior <paul.senior@education.gov.uk>
2026-04-07 15:00:32 +0100

    ptt - upgrade dev & review to use postgres17

--- terraform/aks/databases.tf
+++ terraform/aks/databases.tf
@@ -55,6 +55,6 @@ module "postgres" {
   azure_sku_name                 = var.postgres_flexible_server_sku
   azure_enable_high_availability = var.postgres_enable_high_availability
   azure_maintenance_window       = var.postgres_azure_maintenance_window
-  server_version                 = "14"
-  server_docker_image            = "postgis/postgis:14-3.2"
+  server_version                 = var.postgres_version
+  server_docker_image            = "postgis/postgis:17-3.5"
 }
