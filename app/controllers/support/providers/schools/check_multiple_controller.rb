7f925ad29abff0cc303ea8fe58abf5d6e1bfd9c5
Tomas D'Stefano <tomas_stefano@successoft.com>
2026-04-21 13:42:54 +0100

    Decouple Support bulk-URN controller from legacy Site data
    Mirror of the Publish bulk change: gias_schools is the source of truth,
    schools derives from it, update iterates gias_schools and passes the
    Creator-generated site_code into the legacy Site build.

--- app/controllers/support/providers/schools/check_multiple_controller.rb
+++ app/controllers/support/providers/schools/check_multiple_controller.rb
@@ -39,18 +39,19 @@ module Support
         end
 
         def save
-          schools.each do |site|
+          saved_sites = []
+
+          gias_schools.each do |gias_school|
             ActiveRecord::Base.transaction do
+              provider_school = ::ProviderSchools::Creator.call(provider:, gias_school_id: gias_school.id)
+
+              site = provider.sites.build(gias_school.school_attributes.merge(code: provider_school.site_code))
               ::ProviderSchools::LegacySiteCreator.call(site:)
-              ::ProviderSchools::Creator.call(
-                provider:,
-                gias_school_id: gias_schools_by_urn.fetch(site.urn).id,
-                site_code: site.code,
-              )
+              saved_sites << site
             end
           end
 
-          schools_added_message(schools)
+          schools_added_message(saved_sites)
         end
 
         def urn_form
@@ -61,17 +62,12 @@ module Support
           @urn_service ||= ProviderURNIdentificationService.new(provider, urn_form.values || []).call
         end
 
-        def schools
-          @schools = begin
-            gias_schools = GiasSchool.where(urn: urn_service[:new_urns])
-            gias_schools.map do |gias_school|
-              provider.sites.build(gias_school.school_attributes)
-            end
-          end
+        def gias_schools
+          @gias_schools ||= GiasSchool.where(urn: urn_service[:new_urns])
         end
 
-        def gias_schools_by_urn
-          @gias_schools_by_urn ||= GiasSchool.where(urn: schools.map(&:urn)).index_by(&:urn)
+        def schools
+          @schools ||= gias_schools.map { |gias_school| provider.sites.build(gias_school.school_attributes) }
         end
 
         def unfound_urns
