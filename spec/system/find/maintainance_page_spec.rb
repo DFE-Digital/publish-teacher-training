c7f515f5827715e0f377cf899f506c3cabda8c77
Simon L <simon.lange@education.gov.uk>
2026-04-27 16:45:36 +0100

    Update maintenance banner content and tests for maintenance on Tuesday

--- spec/system/find/maintainance_page_spec.rb
+++ spec/system/find/maintainance_page_spec.rb
@@ -15,7 +15,7 @@ RSpec.describe "Maintenance mode" do
       visit find_root_path
 
       expect(page).to have_current_path find_maintenance_path
-      expect(page).to have_no_content "Login issues"
+      expect(page).to have_no_content "Important"
     end
   end
 
