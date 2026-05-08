810e230cb80271a237cab2986492d35de8ced577
Kellie <kelliematheson@gmail.com>
2026-05-08 14:33:50 +0100

    Add school experience

---
+++ app/controllers/publish/courses/school_experience_controller.rb
@@ -0,0 +1,19 @@
+module Publish
+  module Courses
+    class SchoolExperienceController < ApplicationController
+      def edit
+        @course = find_course
+      end
+
+      def update
+        @course = find_course
+      end
+
+      private
+
+      def find_course
+        Course.find_by!(course_code: params[:code])
+      end
+    end
+  end
+end
