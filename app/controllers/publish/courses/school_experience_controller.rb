40aa6b846ad744c99a68152dc9e9c17ac4d25762
Kellie <kelliematheson@gmail.com>
2026-05-11 14:24:38 +0100

    Add branching to school experience

--- app/controllers/publish/courses/school_experience_controller.rb
+++ app/controllers/publish/courses/school_experience_controller.rb
@@ -5,9 +5,31 @@ module Publish
         @course = find_course
       end
 
-      def update
-        @course = find_course
-      end
+     def update
+  @course = find_course
+
+  school_experience_required =
+    ActiveModel::Type::Boolean.new.cast(
+      params.dig(:course, :school_experience_required)
+    )
+
+  if school_experience_required
+    # YES → school experience details page
+    redirect_to school_experience_details_publish_provider_recruitment_cycle_course_path(
+      @provider.provider_code,
+      params[:recruitment_cycle_year],
+      @course.course_code
+    )
+  else
+    # NO → course SHOW page
+    redirect_to publish_provider_recruitment_cycle_course_path(
+      @provider.provider_code,
+      params[:recruitment_cycle_year],
+      @course.course_code
+    )
+  end
+end
+
 
       def details
         @course = find_course
