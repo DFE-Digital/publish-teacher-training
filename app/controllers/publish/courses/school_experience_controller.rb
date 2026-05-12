12797c862ab6893f48569923919160e9dcb2e1b9
Kellie <kelliematheson@gmail.com>
2026-05-12 13:55:48 +0100

    Persist school experience data in change flow

--- app/controllers/publish/courses/school_experience_controller.rb
+++ app/controllers/publish/courses/school_experience_controller.rb
@@ -6,40 +6,45 @@ module Publish
       end
 
       def update
-  @course = find_course
+        @course = find_course
 
-  # ✅ CASE: Coming from the DETAILS textarea page
-  if params[:school_experience].present? && params[:course].blank?
-    redirect_to publish_provider_recruitment_cycle_course_path(
-      @provider.provider_code,
-      params[:recruitment_cycle_year],
-      @course.course_code,
-      school_experience: params[:school_experience]
-    )
-    return
-  end
+        # ✅ CASE: Coming from the DETAILS textarea page
+        if params[:school_experience].present? && params[:course].blank?
+          redirect_to publish_provider_recruitment_cycle_course_path(
+            @provider.provider_code,
+            params[:recruitment_cycle_year],
+            @course.course_code,
+            school_experience: params[:school_experience]
+          )
+          return
+        end
 
-  # ✅ CASE: Coming from the YES / NO radio page
-  school_experience_required =
-    ActiveModel::Type::Boolean.new.cast(
-      params.dig(:course, :school_experience_required)
-    )
+        # ✅ CASE: Coming from the YES / NO radio page
+        school_experience_required =
+          ActiveModel::Type::Boolean.new.cast(
+            params.dig(:course, :school_experience_required)
+          )
 
-  if school_experience_required
-    redirect_to school_experience_details_publish_provider_recruitment_cycle_course_path(
-      @provider.provider_code,
-      params[:recruitment_cycle_year],
-      @course.course_code,
-      school_experience: params[:school_experience]
-    )
-  else
-    redirect_to publish_provider_recruitment_cycle_course_path(
-      @provider.provider_code,
-      params[:recruitment_cycle_year],
-      @course.course_code
-    )
-  end
-end
+        if school_experience_required
+          school_experience =
+            params[:school_experience].presence ||
+            request.query_parameters[:school_experience]
+
+          redirect_to school_experience_details_publish_provider_recruitment_cycle_course_path(
+            @provider.provider_code,
+            params[:recruitment_cycle_year],
+            @course.course_code,
+            school_experience: school_experience
+          )
+        else
+          redirect_to publish_provider_recruitment_cycle_course_path(
+            @provider.provider_code,
+            params[:recruitment_cycle_year],
+            @course.course_code
+          )
+        end
+      end
+
       def details
         @course = find_course
       end
