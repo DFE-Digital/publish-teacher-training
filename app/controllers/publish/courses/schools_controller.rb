2d823a2665035252b8714092fff6e300513a4c38
sarahcrack <73823091+sarahcrack@users.noreply.github.com>
2026-05-12 16:45:39 +0100

    School Placements - add have you finished adding placement schools, success banner, relevant redirects

--- app/controllers/publish/courses/schools_controller.rb
+++ app/controllers/publish/courses/schools_controller.rb
@@ -27,19 +27,62 @@ module Publish
         @course_school_form = Publish::CourseSchoolForm.new(@course, params: school_params)
 
         if @course_school_form.valid?
-          Publish::Schools::UpdateCourseSchoolsService.call_or_enqueue(course: @course, params: school_params)
+          # Capture current state BEFORE update
+          previous_site_ids = @course.site_ids.sort
+          # Capture new state AFTER update
+          new_site_ids = Array(school_params[:site_ids])
+            .compact_blank
+            .map(&:to_i)
+            .sort
 
-          flash[:success] = if Array(@course_school_form.site_ids).size > Publish::Schools::UpdateCourseSchoolsService::ENQUEUE_THRESHOLD
-                              I18n.t("success.enqueued_schools")
-                            else
-                              I18n.t("success.saved", value: section_key)
-                            end
+          # Calculate counts for banner message
+          added_count   = (new_site_ids - previous_site_ids).size
+          removed_count = (previous_site_ids - new_site_ids).size
 
-          redirect_to details_publish_provider_recruitment_cycle_course_path(
-            provider.provider_code,
-            recruitment_cycle.year,
-            course.course_code,
+          # Update course schools (async if above threshold)
+          Publish::Schools::UpdateCourseSchoolsService.call_or_enqueue(
+            course: @course,
+            params: school_params,
           )
+
+          # Determine where to redirect based on whether user has finished updating schools or not
+          finished = params[:publish_course_school_form][:finished_updating_schools]
+
+          # If user has finished updating schools, show banner with counts of added/removed schools and redirect back to placement schools page
+          if finished == "no"
+            # Counts banner ONLY on schools page
+            parts = []
+            parts << "#{added_count} school#{'s' unless added_count == 1} added" if added_count.positive?
+            parts << "#{removed_count} school#{'s' unless removed_count == 1} removed" if removed_count.positive?
+
+            if parts.any?
+              flash[:success] = {
+                "title" => "Placement schools updated",
+                "body" => parts.join(" and "),
+              }
+            end
+
+            redirect_to schools_publish_provider_recruitment_cycle_course_path(
+              provider.provider_code,
+              recruitment_cycle.year,
+              course.course_code,
+            )
+          else
+            # Restore EXISTING behaviour for Basic details (no schools count included in banner (just schools updated message) - couldn't work out how to do this easily!)
+            flash[:success] =
+              if Array(@course_school_form.site_ids).size >
+                  Publish::Schools::UpdateCourseSchoolsService::ENQUEUE_THRESHOLD
+                I18n.t("success.enqueued_schools")
+              else
+                I18n.t("success.saved", value: section_key)
+              end
+
+            redirect_to details_publish_provider_recruitment_cycle_course_path(
+              provider.provider_code,
+              recruitment_cycle.year,
+              course.course_code,
+            )
+          end
         else
           render :edit
         end
@@ -70,7 +113,7 @@ module Publish
       end
 
       def school_params
-        return { site_ids: nil } if params[:publish_course_school_form][:site_ids].all?(&:empty?)
+        # return { site_ids: nil } if params[:publish_course_school_form][:site_ids].all?(&:empty?)
 
         params.expect(publish_course_school_form: [:schools_validated, { site_ids: [] }])
       end
