e20f6b93c742004dfdfaa52c77480b05c31ff931
Iain McNulty <iain.mcnulty@education.gov.uk>
2026-05-12 09:32:15 +0100

    Delete unused application open label method

--- app/decorators/course_decorator.rb
+++ app/decorators/course_decorator.rb
@@ -220,15 +220,6 @@ class CourseDecorator < ApplicationDecorator
     end
   end
 
-  def applications_open_first_label(recruitment_cycle)
-    if current_cycle?
-      "As soon as the course is published - recommended"
-    else
-      application_start_date = recruitment_cycle.application_start_date.strftime("%-d %B %Y")
-      "On #{application_start_date} when Apply opens - recommended"
-    end
-  end
-
   def selectable_subjects
     edit_course_options["subjects"].map { |subject| [subject.attributes["subject_name"], subject["id"]] }
   end
