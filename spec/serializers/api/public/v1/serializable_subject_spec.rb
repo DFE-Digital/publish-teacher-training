bf7a26400d984781e2ec3266eb5690c8b1309327
Simon L <simon.lange@education.gov.uk>
2026-05-12 08:54:49 +0100

    Adding more specs for Financial incentives

--- spec/serializers/api/public/v1/serializable_subject_spec.rb
+++ spec/serializers/api/public/v1/serializable_subject_spec.rb
@@ -35,4 +35,29 @@ RSpec.describe API::Public::V1::SerializableSubject do
     it { is_expected.to have_attribute(:bursary_amount).with_value("24000") }
     it { is_expected.to have_attribute(:scholarship).with_value("26000") }
   end
+
+  context "when every financial incentive for the subject is hidden" do
+    let(:bursary_subject) { find_or_create(:secondary_subject, :physics) }
+    let(:resource) { described_class.new(object: bursary_subject.reload) }
+
+    before do
+      bursary_subject.financial_incentive_records.find_each do |financial_incentive|
+        financial_incentive.update!(displayed: false)
+      end
+
+      create(
+        :financial_incentive,
+        :hidden,
+        subject: bursary_subject,
+        year: 2027,
+        bursary_amount: "99999",
+        early_career_payments: "88888",
+        scholarship: "77777",
+      )
+    end
+
+    it { is_expected.to have_attribute(:bursary_amount).with_value(nil) }
+    it { is_expected.to have_attribute(:early_career_payments).with_value(nil) }
+    it { is_expected.to have_attribute(:scholarship).with_value(nil) }
+  end
 end
