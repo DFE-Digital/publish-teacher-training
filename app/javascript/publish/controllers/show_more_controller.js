9f65a3d0eaea322d4b64239eab8d54c532eee9d9
gms-gs <george.schena@education.gov.uk>
2026-05-14 11:02:13 +0100

    Fix clearing schools search

--- app/javascript/publish/controllers/show_more_controller.js
+++ app/javascript/publish/controllers/show_more_controller.js
@@ -5,14 +5,25 @@ export default class extends Controller {
   static values = { pageSize: Number };
 
   connect() {
+    this.onReset = this.reset.bind(this);
+    this.element.addEventListener("show-more:reset", this.onReset);
     this.visibleCount = this.pageSizeValue;
     this.update();
+  }
+
+  disconnect() {
+    this.element.removeEventListener("show-more:reset", this.onReset);
   }
 
   showMore(event) {
     if (event) event.preventDefault();
 
     this.visibleCount += this.pageSizeValue;
+    this.update();
+  }
+
+  reset() {
+    this.visibleCount = this.pageSizeValue;
     this.update();
   }
 
