3b2bc162d2b8c79404a54386dee366060c0945a9
Tomas D'Stefano <tomas_stefano@successoft.com>
2026-05-19 14:27:01 +0100

    Register new autocomplete controllers and remove legacy JS
    Register the dfe-autocomplete and remote-autocomplete controllers in
    both bundles and delete the superseded publish autocomplete modules,
    find autocomplete controllers and request/escape utilities.

--- app/javascript/find/application.js
+++ app/javascript/find/application.js
@@ -13,11 +13,9 @@ import { initCrossServiceHeader } from 'govuk-one-login/service-header'
 import { Application } from '@hotwired/stimulus'
 import FilterSearchController from './controllers/filter_search_controller'
 import SaveCourseController from './controllers/save_course_controller.js'
-import SubjectsAutocompleteController from './controllers/subjects_autocomplete_controller'
-import ProviderAutocompleteController from './controllers/provider_autocomplete_controller'
+import DfeAutocompleteController from './controllers/dfe_autocomplete_controller'
 import RadiusQuickLinkSuggestionsController from './controllers/radius_quick_link_suggestions_controller'
-import RemoteAutocompleteController from './controllers/remote_autocomplete_controller'
-import LocationsAutocompleteController from './controllers/locations_autocomplete_controller'
+import RemoteAutocompleteController from '../shared/remote_autocomplete_controller'
 import VisibilityController from './controllers/visibility_controller'
 import DetailsComponentController from './controllers/details_component_controller'
 
@@ -25,12 +23,10 @@ window.Stimulus = Application.start()
 
 Stimulus.register('filter-search', FilterSearchController)
 Stimulus.register('save-course', SaveCourseController)
-Stimulus.register('subjects-autocomplete', SubjectsAutocompleteController)
+Stimulus.register('dfe-autocomplete', DfeAutocompleteController)
 Stimulus.register('remote-autocomplete', RemoteAutocompleteController)
-Stimulus.register('locations-autocomplete', LocationsAutocompleteController)
 Stimulus.register('visibility', VisibilityController)
 Stimulus.register('details-component', DetailsComponentController)
-Stimulus.register('provider-autocomplete', ProviderAutocompleteController)
 Stimulus.register(
   'radius-quick-link-suggestions',
   RadiusQuickLinkSuggestionsController
--- app/javascript/publish/application.js
+++ app/javascript/publish/application.js
@@ -2,13 +2,13 @@
 import jQuery from 'jquery'
 import { initAll } from 'govuk-frontend'
 
-import autocompleteSetup from './autocomplete'
 import FilterToggle from './filters'
 
 import { Application } from '@hotwired/stimulus'
 import InputPreviewController from './courses/input_preview_controller'
 import SelectAllCheckboxesController from './controllers/select_all_checkboxes_controller'
 import CopyLinkController from './controllers/copy_link_controller'
+import RemoteAutocompleteController from '../shared/remote_autocomplete_controller'
 
 window.jQuery = jQuery
 window.$ = jQuery
@@ -16,9 +16,8 @@ window.$ = jQuery
 initAll()
 FilterToggle.init()
 
-autocompleteSetup()
-
 window.Stimulus = Application.start()
 Stimulus.register('input-preview', InputPreviewController)
 Stimulus.register('select-all-checkboxes', SelectAllCheckboxesController)
 Stimulus.register('copy-link', CopyLinkController)
+Stimulus.register('remote-autocomplete', RemoteAutocompleteController)
