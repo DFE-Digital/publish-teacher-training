229aa445555f545f4c365f1f0405c22003849a46
Iain McNulty <iain.mcnulty@education.gov.uk>
2026-05-28 15:55:52 +0100

    The email alert summary card uses the same locales as the active filters

--- spec/system/find/candidates/recent_searches_spec.rb
+++ spec/system/find/candidates/recent_searches_spec.rb
@@ -363,7 +363,7 @@ RSpec.describe "Recent searches", service: :find do
     expect(page).to have_content("Courses with a salary")
     expect(page).to have_content("Full time")
     expect(page).to have_content("Qualification: QTS with PGCE or PGDE")
-    expect(page).to have_content("Degree grade: 2:1 or first")
+    expect(page).to have_content("Degree grade: 2:1 or First")
     expect(page).to have_content("Start date: September #{Find::CycleTimetable.current_year} only")
     expect(page).to have_content("Courses with a SEND specialism")
   end
