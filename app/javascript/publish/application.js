bfcc9874ec0fcc135e4db545d046c24be3ec11b3
sarahcrack <73823091+sarahcrack@users.noreply.github.com>
2026-06-01 13:03:26 +0100

    WIP: Update publish/application.js to hopefully fix deploy/merge issue
    - N.B. autocomplete not working

--- app/javascript/publish/application.js
+++ app/javascript/publish/application.js
@@ -2,13 +2,13 @@
 import jQuery from "jquery";
 import { initAll } from "govuk-frontend";
 
-import autocompleteSetup from "./autocomplete";
 import FilterToggle from "./filters";
 
 import { Application } from "@hotwired/stimulus";
 import InputPreviewController from "./courses/input_preview_controller";
 import SelectAllCheckboxesController from "./controllers/select_all_checkboxes_controller";
 import CopyLinkController from "./controllers/copy_link_controller";
+import RemoteAutocompleteController from "../shared/remote_autocomplete_controller";
 import SchoolSearchController from "./controllers/school_search_controller";
 import CourseFiltersController from "./controllers/course_filters_controller";
 import ShowMoreController from "./controllers/show_more_controller";
@@ -20,12 +20,11 @@ window.$ = jQuery;
 initAll();
 FilterToggle.init();
 
-autocompleteSetup();
-
 window.Stimulus = Application.start();
 Stimulus.register("input-preview", InputPreviewController);
 Stimulus.register("select-all-checkboxes", SelectAllCheckboxesController);
 Stimulus.register("copy-link", CopyLinkController);
+Stimulus.register("remote-autocomplete", RemoteAutocompleteController);
 Stimulus.register("school-search", SchoolSearchController);
 Stimulus.register("course-filters", CourseFiltersController);
 Stimulus.register("show-more", ShowMoreController);
