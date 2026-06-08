d260cd8d8b0c6a5b5f972a322183fbdfb618167c
Tomas D'Stefano <tomas_stefano@successoft.com>
2026-06-08 16:17:35 +0100

    Make Publish::CourseList a facade over Publish::Courses::Query

--- app/services/publish/course_list.rb
+++ app/services/publish/course_list.rb
@@ -1,16 +1,33 @@
 # frozen_string_literal: true
 
 module Publish
-  # Presentation facade over ProviderCoursesQuery for the publish course list
-  # page. Exposes the ordered course groups as an enumerable for the view.
+  # Presentation facade over Publish::Courses::Query for the publish course list
+  # page. Chunks the pre-ordered query rows into accredited-provider groups.
   class CourseList
     include Enumerable
 
-    delegate :groups, to: :query
+    # A group is self-accredited (rendered without a heading) when it has no
+    # accredited provider name.
+    Group = Data.define(:accredited_provider_name, :courses) do
+      def self_accredited?
+        accredited_provider_name.nil?
+      end
+
+      def heading
+        accredited_provider_name
+      end
+    end
+
     delegate :any?, to: :groups
 
     def initialize(provider:)
-      @query = ProviderCoursesQuery.new(provider:)
+      @provider = provider
+    end
+
+    def groups
+      @groups ||= courses
+        .chunk_while { |a, b| a[:group_name] == b[:group_name] }
+        .map { |chunk| Group.new(accredited_provider_name: chunk.first[:group_name], courses: chunk.map(&:decorate)) }
     end
 
     def each(&)
@@ -19,6 +36,10 @@ module Publish
 
   private
 
-    attr_reader :query
+    attr_reader :provider
+
+    def courses
+      @courses ||= Publish::Courses::Query.call(provider:).to_a
+    end
   end
 end
