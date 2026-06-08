292c1dab745409d7b0d65cf383e1986b919788cb
Tomas D'Stefano <tomas_stefano@successoft.com>
2026-06-08 13:36:46 +0100

    Add Publish::Courses ViewComponent tree (List/Group/Table/Row)

---
+++ app/components/publish/courses/row_component.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+module Publish
+  module Courses
+    # Renders a single course row in the publish course list table.
+    class RowComponent < ApplicationComponent
+      def initialize(course:, provider:, classes: [], html_attributes: {})
+        super(classes:, html_attributes:)
+        @course = course
+        @provider = provider
+      end
+
+      attr_reader :course, :provider
+
+      delegate :name_and_code, to: :course
+
+      def course_path
+        helpers.publish_provider_recruitment_cycle_course_path(
+          provider.provider_code,
+          course.recruitment_cycle.year,
+          course.course_code,
+        )
+      end
+
+      def on_courses_index_page?
+        helpers.current_page?(
+          helpers.publish_provider_recruitment_cycle_courses_path(
+            provider.provider_code,
+            course.recruitment_cycle.year,
+          ),
+        )
+      end
+
+      delegate :status_tag, to: :course
+    end
+  end
+end
