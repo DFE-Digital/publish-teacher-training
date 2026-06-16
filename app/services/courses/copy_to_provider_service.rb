8d0fa202ad9f2e5bdce6bfb422695043ff4a230d
Tomas D'Stefano <tomas_stefano@successoft.com>
2026-06-16 11:21:50 +0100

    Carry over publish_without_schools_allowed during rollover

--- app/services/courses/copy_to_provider_service.rb
+++ app/services/courses/copy_to_provider_service.rb
@@ -32,6 +32,7 @@ module Courses
         end
         new_course.can_sponsor_skilled_worker_visa          = course.can_sponsor_skilled_worker_visa
         new_course.can_sponsor_student_visa                 = course.can_sponsor_student_visa
+        new_course.publish_without_schools_allowed          = course.publish_without_schools_allowed
         new_course.visa_sponsorship_application_deadline_at = nil # We can't currently predict how to carry this value over. Provider must set it again
         new_course.first_published_at                       = nil
         new_course.save!(validate: false)
