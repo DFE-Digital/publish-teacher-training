fa7dac1ea76e184b3dfcf28f5eaa42ea01723f30
sarahcrack <73823091+sarahcrack@users.noreply.github.com>
2026-06-22 14:00:49 +0100

    Add course information to schools CSV download and hide first CSV download button the course list page

--- app/services/publish/data_exports/course_schools_export.rb
+++ app/services/publish/data_exports/course_schools_export.rb
@@ -16,12 +16,49 @@ module Publish
             rows << {
               "Course name" => course.name.titleize,
               "Course code" => course.course_code,
+              "Status" => status(course).titleize,
+              "Funding" => course.funding.titleize,
+              "Qualification" => qualification(course),
+              "Study mode" => course.study_mode.titleize,
+              "Start date" => format_date(course.start_date),
               "Placement school" => site.location_name || site.code,
             }
           end
         end
 
         rows
+      end
+
+    private
+
+      def format_date(date)
+        date&.strftime("%B %Y")
+      end
+
+      def qualification(course)
+        case course.qualification
+        when "qts_with_pgce" then "PGCE with QTS"
+        when "qts_only" then "QTS"
+        else course.qualification.humanize
+        end
+      end
+
+      def status(course)
+        if course.is_withdrawn?
+          "Withdrawn"
+        elsif course.scheduled?
+          "Scheduled"
+        elsif course.content_status == :draft
+          "Draft"
+        elsif course.content_status == :rolled_over
+          "Rolled over"
+        elsif course.open_for_applications?
+          "Open"
+        elsif course.only_published? && !course.open_for_applications?
+          "Closed"
+        else
+          "Unknown"
+        end
       end
     end
   end
