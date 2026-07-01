92498e206cfbc140a5940df0854e2a17be96dad9
Iain McNulty <iain.mcnulty@education.gov.uk>
2026-07-01 12:39:09 +0100

    Add see what you wrote last cycle to Schoole experience

--- spec/system/publish/courses/school_experience/editing_school_experience_spec.rb
+++ spec/system/publish/courses/school_experience/editing_school_experience_spec.rb
@@ -69,6 +69,35 @@ RSpec.describe "Editing school experience requirements", travel: mid_cycle, type
     and_the_course_has_no_school_experience_recorded
   end
 
+  scenario "provider sees the previous cycle's school experience content on the details step" do
+    given_i_am_authenticated_as_a_provider_user
+    and_i_have_a_salaried_course
+    and_the_course_had_school_experience_content_in_the_previous_cycle
+
+    when_i_visit_the_course_description_tab
+    when_i_click_to_change_school_experience
+    then_i_am_on_the_experience_required_page
+
+    when_i_choose_that_experience_is_required
+    and_i_click_continue
+    then_i_am_on_the_experience_details_page
+    and_i_can_see_the_previous_cycles_school_experience_content
+  end
+
+  scenario "provider sees no recap when there was no school experience content last cycle" do
+    given_i_am_authenticated_as_a_provider_user
+    and_i_have_a_salaried_course
+
+    when_i_visit_the_course_description_tab
+    when_i_click_to_change_school_experience
+    then_i_am_on_the_experience_required_page
+
+    when_i_choose_that_experience_is_required
+    and_i_click_continue
+    then_i_am_on_the_experience_details_page
+    and_i_do_not_see_the_last_cycle_recap
+  end
+
 private
 
   def given_i_am_authenticated_as_a_provider_user
@@ -80,6 +109,17 @@ private
 
   def and_i_have_a_salaried_course
     @course = create(:course, :with_salary, provider: @provider)
+  end
+
+  def and_the_course_had_school_experience_content_in_the_previous_cycle
+    previous_provider = create(:provider, provider_code: @provider.provider_code, recruitment_cycle: create(:recruitment_cycle))
+    create(
+      :course,
+      course_code: @course.course_code,
+      provider: previous_provider,
+      school_experience_required: true,
+      school_experience_required_content: "Spend a week observing lessons in a UK school.",
+    )
   end
 
   def when_i_visit_the_course_description_tab
@@ -152,6 +192,17 @@ private
   def and_i_see_that_experience_is_not_required
     expect(@course.reload.school_experience_required).to be(false)
     expect(page).to have_content("No, school experience is not required")
+  end
+
+  def and_i_can_see_the_previous_cycles_school_experience_content
+    expect(page).to have_content("See what you wrote last cycle")
+    page.find("span", text: "See what you wrote last cycle").click
+
+    expect(page).to have_content("Spend a week observing lessons in a UK school.")
+  end
+
+  def and_i_do_not_see_the_last_cycle_recap
+    expect(page).to have_no_content("See what you wrote last cycle")
   end
 
   def when_i_cancel
