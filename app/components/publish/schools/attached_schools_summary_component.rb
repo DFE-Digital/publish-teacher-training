44167f5a8f327b5baed9d78fb445db4713982778
Tomas D'Stefano <tomas_stefano@successoft.com>
2026-07-07 16:48:33 +0100

    Add placement schools summary component with a details toggle
    Show the schools inline for five or fewer, but collapse them into a GOV.UK
    details component with the count as the summary link once there are six or
    more, so long lists are less prominent and easier to scan.

---
+++ app/components/publish/schools/attached_schools_summary_component.rb
@@ -0,0 +1,38 @@
+module Publish
+  module Schools
+    # Renders the "Placement/Employing schools" value on a course's basic details
+    # summary list. Shows the list inline for a handful of schools, but collapses
+    # it into a GOV.UK details component (with the count as the summary link) once
+    # there are more than SCHOOLS_DETAILS_THRESHOLD, so long lists are less
+    # prominent and easier to scan.
+    class AttachedSchoolsSummaryComponent < ViewComponent::Base
+      SCHOOLS_DETAILS_THRESHOLD = 5
+
+      attr_reader :course
+
+      def initialize(course:)
+        @course = course
+
+        super()
+      end
+
+      def school_names
+        @school_names ||= course.sorted_school_names
+      end
+
+      def details?
+        school_names.size > SCHOOLS_DETAILS_THRESHOLD
+      end
+
+      def summary_text
+        t("publish.courses.schools.attached", count: school_names.size)
+      end
+
+      def school_list
+        content_tag(:ul, class: "govuk-list") do
+          safe_join(school_names.map { |name| content_tag(:li, name) })
+        end
+      end
+    end
+  end
+end
