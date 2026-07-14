54159183b164ddafe2562486889604d203f8297c
sarahcrack <73823091+sarahcrack@users.noreply.github.com>
2026-07-14 12:02:10 +0100

    Schools edit page - fix select all checkbox and add status tags

--- app/javascript/publish/controllers/select_all_checkboxes_controller.js
+++ app/javascript/publish/controllers/select_all_checkboxes_controller.js
@@ -1,31 +1,38 @@
-import { Controller } from '@hotwired/stimulus'
+import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ['selectAll', 'checkbox']
+  static targets = ["selectAll", "checkbox"];
 
-  connect () {
-    this.updateSelectAll()
+  // connect() {
+  //   this.updateSelectAll();
+  // }
+
+  connect() {
+    console.log("select-all connected");
+    this.updateSelectAll();
   }
 
-  toggleAll (event) {
-    const checked = event.target.checked
-    this.checkboxTargets.forEach(box => { box.checked = checked })
-    this.selectAllTarget.indeterminate = false
+  toggleAll(event) {
+    const checked = event.target.checked;
+    this.checkboxTargets.forEach((box) => {
+      box.checked = checked;
+    });
+    this.selectAllTarget.indeterminate = false;
   }
 
-  updateSelectAll () {
-    const total = this.checkboxTargets.length
-    const checkedCount = this.checkboxTargets.filter(cb => cb.checked).length
+  updateSelectAll() {
+    const total = this.checkboxTargets.length;
+    const checkedCount = this.checkboxTargets.filter((cb) => cb.checked).length;
 
     if (checkedCount === 0) {
-      this.selectAllTarget.checked = false
-      this.selectAllTarget.indeterminate = false
+      this.selectAllTarget.checked = false;
+      this.selectAllTarget.indeterminate = false;
     } else if (checkedCount === total) {
-      this.selectAllTarget.checked = true
-      this.selectAllTarget.indeterminate = false
+      this.selectAllTarget.checked = true;
+      this.selectAllTarget.indeterminate = false;
     } else {
-      this.selectAllTarget.checked = false
-      this.selectAllTarget.indeterminate = true
+      this.selectAllTarget.checked = false;
+      this.selectAllTarget.indeterminate = true;
     }
   }
 }
