e9937aea7cd2de5f040f8ab2e40e43b03c935b19
Simon Lange <langesi@aol.de>
2026-07-23 18:01:47 +0100

    mixed legacy Site IDs and UUIDs are explicitly rejected duplicate identifiers are deduplicated during normalisation legacy non-UUID identifiers must be numeric Site IDs

--- app/services/course_schools/identity.rb
+++ app/services/course_schools/identity.rb
@@ -1,11 +1,14 @@
 # frozen_string_literal: true
 
 module CourseSchools
-  # This service determines which model should be used when creating, removing and in some instances viewing schools.
-  # The legacy data model uses Site, while the new data model uses Course::School.
-  # During the rollover, both models are written to and their UUIDs may diverge, so we need a way to switch to the new data model.
-  # Because Publish supports both the old and new recruitment cycles during the rollover, this switch must be based on the recruitment cycle rather than a traditional feature flag.
+  # Chooses the school model for a provider's recruitment cycle.
+  # Current and rollover cycles use legacy Site records; cycles after the remodel
+  # use Provider::School records because Site and Provider::School UUIDs diverge
+  # after rollover.
   class Identity
+    LEGACY_SITE_ID_PATTERN = /\A\d+\z/
+    UUID_PATTERN = /\A[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}\z/i
+
     delegate :after_schools_remodel_cycle?, to: :provider_identity
     delegate :count, to: :available_schools, prefix: true
 
@@ -20,6 +23,8 @@ module CourseSchools
     end
 
     def current_school_uuids
+      require_course!
+
       if after_schools_remodel_cycle?
         course.schools.includes(:provider_school).map { |course_school| course_school.provider_school.uuid.to_s }
       else
@@ -27,40 +32,91 @@ module CourseSchools
       end
     end
 
-    def school_records_for(school_uuids:)
-      school_uuids = normalize_uuids(school_uuids)
+    def school_records_for(school_identifiers:)
+      school_identifiers = normalize_school_identifiers(school_identifiers)
+      records_by_identifier = school_records_by_identifier(school_identifiers)
 
-      ordered_records_for(school_uuids, school_records_by_uuid(school_uuids))
+      validate_all_identifiers_resolved!(school_identifiers, records_by_identifier)
+      ordered_records_for(school_identifiers, records_by_identifier)
     end
 
   private
 
     attr_reader :provider, :course, :provider_identity
 
-    def school_records_by_uuid(school_uuids)
+    def school_records_by_identifier(school_identifiers)
       if after_schools_remodel_cycle?
-        provider_schools_by_uuid(school_uuids)
-      elsif school_uuids.all? { |uuid| uuid?(uuid) }
-        provider.sites.where(uuid: school_uuids).index_by { |site| site.uuid.to_s }
+        validate_uuid_identifiers!(school_identifiers)
+        provider_schools_by_uuid(school_identifiers)
       else
-        provider.sites.where(id: school_uuids).index_by { |site| site.id.to_s }
+        legacy_sites_by_identifier(school_identifiers)
       end
     end
 
-    def provider_schools_by_uuid(school_uuids)
-      provider.schools.where(uuid: normalize_uuids(school_uuids)).index_by { |school| school.uuid.to_s }
+    def legacy_sites_by_identifier(school_identifiers)
+      if uuid_identifiers?(school_identifiers)
+        sites_by_uuid(site_uuids: school_identifiers)
+      elsif legacy_site_id_identifiers?(school_identifiers)
+        sites_by_id(site_ids: school_identifiers)
+      else
+        raise ArgumentError, "School identifiers must not mix legacy Site IDs and UUIDs"
+      end
     end
 
-    def ordered_records_for(school_uuids, records_by_uuid)
-      school_uuids.filter_map { |uuid| records_by_uuid[uuid] }
+    def sites_by_uuid(site_uuids:)
+      provider.sites.where(uuid: site_uuids).index_by { |site| site.uuid.to_s }
     end
 
-    def normalize_uuids(school_uuids)
-      Array(school_uuids).compact_blank.map(&:to_s)
+    def sites_by_id(site_ids:)
+      provider.sites.where(id: site_ids).index_by { |site| site.id.to_s }
+    end
+
+    def provider_schools_by_uuid(provider_school_uuids)
+      provider.schools.where(uuid: provider_school_uuids).index_by { |school| school.uuid.to_s }
+    end
+
+    def uuid_identifiers?(school_identifiers)
+      school_identifiers.all? { |identifier| uuid?(identifier) }
+    end
+
+    def legacy_site_id_identifiers?(school_identifiers)
+      school_identifiers.all? { |identifier| legacy_site_id?(identifier) }
+    end
+
+    def ordered_records_for(school_identifiers, records_by_identifier)
+      school_identifiers.map { |identifier| records_by_identifier.fetch(identifier) }
+    end
+
+    def normalize_school_identifiers(school_identifiers)
+      Array(school_identifiers).compact_blank.map(&:to_s).uniq
+    end
+
+    def require_course!
+      return if course.present?
+
+      raise ArgumentError, "CourseSchools::Identity requires a course for current_school_uuids"
+    end
+
+    def validate_uuid_identifiers!(school_identifiers)
+      invalid_identifiers = school_identifiers.reject { |identifier| uuid?(identifier) }
+      return if invalid_identifiers.empty?
+
+      raise ArgumentError, "School identifiers must be Provider::School UUIDs after the schools remodel cycle"
+    end
+
+    def validate_all_identifiers_resolved!(school_identifiers, records_by_identifier)
+      unresolved_identifiers = school_identifiers - records_by_identifier.keys
+      return if unresolved_identifiers.empty?
+
+      raise ArgumentError, "Could not resolve school identifiers for provider #{provider.id}: #{unresolved_identifiers.join(', ')}"
     end
 
     def uuid?(identifier)
-      identifier.to_s.match?(/\A[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}\z/i)
+      identifier.to_s.match?(UUID_PATTERN)
+    end
+
+    def legacy_site_id?(identifier)
+      identifier.to_s.match?(LEGACY_SITE_ID_PATTERN)
     end
   end
 end
