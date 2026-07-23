251f9c34dfb6f3eeca81205065b4ba1ec86ae56b
Simon Lange <langesi@aol.de>
2026-07-23 11:40:50 +0100

    Adding comments to the class and replaced tenary with an ifelse statement

--- app/services/provider_schools/identity.rb
+++ app/services/provider_schools/identity.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 module ProviderSchools
+  # This service determines which model should be used when creating, removing and in some instances viewing schools.
+  # The legacy data model uses Site, while the new data model uses Provider::School.
+  # During the rollover, both models are written to and their UUIDs may diverge, so we need a way to switch to the new data model.
+  # Because Publish supports both the old and new recruitment cycles during the rollover, this switch must be based on the recruitment cycle rather than a traditional feature flag.
   class Identity
     def self.ordered_school_scope(provider:)
       new(provider:).ordered_school_scope
@@ -19,7 +23,11 @@ module ProviderSchools
     end
 
     def school_for(uuid:)
-      after_schools_remodel_cycle? ? provider.schools.find_by!(uuid:) : provider.sites.find_by!(uuid:)
+      if after_schools_remodel_cycle?
+        provider.schools.find_by!(uuid:)
+      else
+        provider.sites.find_by!(uuid:)
+      end
     end
 
     def provider_school_for(site:)
