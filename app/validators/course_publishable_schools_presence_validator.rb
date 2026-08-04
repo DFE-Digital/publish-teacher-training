834777d29e6eaba304816573af82c99b809a8ae7
Iain McNulty <iain.mcnulty@education.gov.uk>
2026-08-04 09:03:58 +0100

    Prevent CoursePublishableSchoolsPresenceValidator from triggering
      - If the add course flow fails to register any of the sites we don't
        want the CoursePublishableSchoolsPresenceValidator to trigger

--- app/validators/course_publishable_schools_presence_validator.rb
+++ app/validators/course_publishable_schools_presence_validator.rb
@@ -8,7 +8,18 @@ class CoursePublishableSchoolsPresenceValidator < ActiveModel::Validator
   def validate(course)
     return if Courses::PublishRules::SchoolPresence.any?(course)
     return if Courses::PublishRules::SchoolPresenceExemption.applies?(course)
+    return if selection_made_but_unresolved?(course)
 
     course.errors.add(:sites, :blank)
+  end
+
+private
+
+  # The provider did choose schools, they just did not survive the write.
+  # CourseSchoolSelectionValidator says so properly; "Select at least one school"
+  # would send them back to re-pick what they already picked. Always false on
+  # :publish, where the accessor is nil because it is never persisted.
+  def selection_made_but_unresolved?(course)
+    course.submitted_school_uuids&.compact_blank&.any? || false
   end
 end
