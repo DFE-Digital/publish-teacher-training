72f4e2d4809a691e02170a7d15d2ff81c7cd8092
Simon Lange <langesi@aol.de>
2026-08-06 14:06:08 +0100

    Handle unsaved course schools in decorator

--- spec/factories/course_schools.rb
+++ spec/factories/course_schools.rb
@@ -22,41 +22,15 @@ FactoryBot.define do
     end
 
     trait :for_site do
-      gias_school do
-        raise ArgumentError, "site is required for :for_site" if site.blank?
-
-        if site.urn.present?
-          GiasSchool.find_or_create_by!(urn: site.urn) do |school|
-            school.name = site.location_name
-            school.address1 = site.address1
-            school.address2 = site.address2
-            school.address3 = site.address3
-            school.town = site.town
-            school.county = site.address4
-            school.postcode = site.postcode
-            school.region_code = site.region_code
-            school.status_code = GiasSchool.status_codes["open"]
-          end
-        else
-          create(
-            :gias_school,
-            name: site.location_name,
-            address1: site.address1,
-            address2: site.address2,
-            address3: site.address3,
-            town: site.town,
-            county: site.address4,
-            postcode: site.postcode,
-            region_code: site.region_code,
-          )
-        end
-      end
-
       provider_school do
         raise ArgumentError, "site is required for :for_site" if site.blank?
 
-        Provider::School.find_by(provider: course.provider, gias_school:, site_code: site.code) ||
-          association(:provider_school, :for_site, site:, provider: course.provider, gias_school:)
+        Provider::School.find_by(provider: course.provider, uuid: site.uuid) ||
+          association(:provider_school, :for_site, site:, provider: course.provider)
+      end
+
+      gias_school do
+        provider_school.gias_school
       end
     end
   end
--- spec/factories/provider_schools.rb
+++ spec/factories/provider_schools.rb
@@ -27,17 +27,20 @@ FactoryBot.define do
         raise ArgumentError, "site is required for :for_site" if site.blank?
 
         if site.urn.present?
-          GiasSchool.find_or_create_by!(urn: site.urn) do |school|
-            school.name = site.location_name
-            school.address1 = site.address1
-            school.address2 = site.address2
-            school.address3 = site.address3
-            school.town = site.town
-            school.county = site.address4
-            school.postcode = site.postcode
-            school.region_code = site.region_code
-            school.status_code = GiasSchool.status_codes["open"]
-          end
+          school = GiasSchool.find_or_initialize_by(urn: site.urn)
+          school.assign_attributes(
+            name: site.location_name,
+            address1: site.address1,
+            address2: site.address2,
+            address3: site.address3,
+            town: site.town,
+            county: site.address4,
+            postcode: site.postcode,
+            region_code: site.region_code,
+          )
+          school.status_code ||= GiasSchool.status_codes["open"]
+          school.save!
+          school
         else
           create(
             :gias_school,
@@ -55,6 +58,10 @@ FactoryBot.define do
 
       site_code { site.code }
       uuid { site.uuid }
+
+      initialize_with do
+        Provider::School.find_or_initialize_by(provider:, gias_school:, site_code:)
+      end
     end
   end
 end
