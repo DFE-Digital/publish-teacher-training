26230275f8100944d4b6d6079edcb838c0b94920
sarahcrack <73823091+sarahcrack@users.noreply.github.com>
2026-08-11 14:52:58 +0100

    Copy course content - fix linting errors

--- app/javascript/publish/controllers/copy_course_content_controller.js
+++ app/javascript/publish/controllers/copy_course_content_controller.js
@@ -1,36 +1,36 @@
-import { Controller } from "@hotwired/stimulus";
+import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
   static targets = [
-    "details",
-    "title",
-    "age",
-    "funding",
-    "qualification",
-    "studyMode",
-    "startDate",
-  ];
+    'details',
+    'title',
+    'age',
+    'funding',
+    'qualification',
+    'studyMode',
+    'startDate'
+  ]
 
-  update(event) {
-    const selected = event.target.selectedOptions[0];
+  update (event) {
+    const selected = event.target.selectedOptions[0]
 
     if (!selected || !selected.value) {
-      this.detailsTarget.hidden = true;
-      return;
+      this.detailsTarget.hidden = true
+      return
     }
 
-    this.titleTarget.textContent = `${selected.dataset.name} (${selected.dataset.code})`;
+    this.titleTarget.textContent = `${selected.dataset.name} (${selected.dataset.code})`
 
-    this.ageTarget.textContent = selected.dataset.age || "";
+    this.ageTarget.textContent = selected.dataset.age || ''
 
-    this.fundingTarget.textContent = selected.dataset.funding || "";
+    this.fundingTarget.textContent = selected.dataset.funding || ''
 
-    this.qualificationTarget.textContent = selected.dataset.qualification || "";
+    this.qualificationTarget.textContent = selected.dataset.qualification || ''
 
-    this.studyModeTarget.textContent = selected.dataset.studyMode || "";
+    this.studyModeTarget.textContent = selected.dataset.studyMode || ''
 
-    this.startDateTarget.textContent = selected.dataset.startDate || "";
+    this.startDateTarget.textContent = selected.dataset.startDate || ''
 
-    this.detailsTarget.hidden = false;
+    this.detailsTarget.hidden = false
   }
 }
