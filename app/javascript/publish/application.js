35906aaba297235ffa70fd5365ac90894eeb5c55
sarahcrack <73823091+sarahcrack@users.noreply.github.com>
2026-08-11 14:52:58 +0100

    Copy course content - fix linting errors

--- app/javascript/publish/application.js
+++ app/javascript/publish/application.js
@@ -1,8 +1,8 @@
 // Entry point for the build script in your package.json
-import jQuery from "jquery";
-import { initAll } from "govuk-frontend";
+import jQuery from 'jquery'
+import { initAll } from 'govuk-frontend'
 
-import FilterToggle from "./filters";
+import FilterToggle from './filters'
 
 import { Application } from '@hotwired/stimulus'
 import InputPreviewController from './courses/input_preview_controller'
@@ -13,11 +13,11 @@ import CopyLinkController from './controllers/copy_link_controller'
 import RemoteAutocompleteController from '../shared/remote_autocomplete_controller'
 import CopyCourseContentController from './controllers/copy_course_content_controller'
 
-window.jQuery = jQuery;
-window.$ = jQuery;
+window.jQuery = jQuery
+window.$ = jQuery
 
-initAll();
-FilterToggle.init();
+initAll()
+FilterToggle.init()
 
 window.Stimulus = Application.start()
 Stimulus.register('input-preview', InputPreviewController)
