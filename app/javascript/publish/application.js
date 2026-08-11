06d7b45fcf755a5ab97d67c4c696c1c6ff81437e
sarahcrack <73823091+sarahcrack@users.noreply.github.com>
2026-08-11 14:34:35 +0100

    Improve copy course content - display course information when option is selected

--- app/javascript/publish/application.js
+++ app/javascript/publish/application.js
@@ -1,25 +1,27 @@
 // Entry point for the build script in your package.json
-import jQuery from 'jquery'
-import { initAll } from 'govuk-frontend'
+import jQuery from "jquery";
+import { initAll } from "govuk-frontend";
 
-import FilterToggle from './filters'
+import FilterToggle from "./filters";
 
-import { Application } from '@hotwired/stimulus'
-import InputPreviewController from './courses/input_preview_controller'
-import SelectAllCheckboxesController from './controllers/select_all_checkboxes_controller'
-import ShowAllSchoolsController from './controllers/show_all_schools_controller'
-import CopyLinkController from './controllers/copy_link_controller'
-import RemoteAutocompleteController from '../shared/remote_autocomplete_controller'
+import { Application } from "@hotwired/stimulus";
+import InputPreviewController from "./courses/input_preview_controller";
+import SelectAllCheckboxesController from "./controllers/select_all_checkboxes_controller";
+import ShowAllSchoolsController from "./controllers/show_all_schools_controller";
+import CopyLinkController from "./controllers/copy_link_controller";
+import RemoteAutocompleteController from "../shared/remote_autocomplete_controller";
+import CopyCourseContentController from "./controllers/copy_course_content_controller";
 
-window.jQuery = jQuery
-window.$ = jQuery
+window.jQuery = jQuery;
+window.$ = jQuery;
 
-initAll()
-FilterToggle.init()
+initAll();
+FilterToggle.init();
 
-window.Stimulus = Application.start()
-Stimulus.register('input-preview', InputPreviewController)
-Stimulus.register('select-all-checkboxes', SelectAllCheckboxesController)
-Stimulus.register('show-all-schools', ShowAllSchoolsController)
-Stimulus.register('copy-link', CopyLinkController)
-Stimulus.register('remote-autocomplete', RemoteAutocompleteController)
+window.Stimulus = Application.start();
+Stimulus.register("input-preview", InputPreviewController);
+Stimulus.register("select-all-checkboxes", SelectAllCheckboxesController);
+Stimulus.register("show-all-schools", ShowAllSchoolsController);
+Stimulus.register("copy-link", CopyLinkController);
+Stimulus.register("remote-autocomplete", RemoteAutocompleteController);
+Stimulus.register("copy-course-content", CopyCourseContentController);
