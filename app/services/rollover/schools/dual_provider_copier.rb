610597e4b0cef6aed15e2e85030bb1db0c6f1e39
Simon Lange <langesi@aol.de>
2026-08-11 21:03:13 +0100

    Implement SyncLegacySiteUuids service and integrate UUID synchronization in DualProviderCopier

--- app/services/rollover/schools/dual_provider_copier.rb
+++ app/services/rollover/schools/dual_provider_copier.rb
@@ -3,21 +3,23 @@
 module Rollover
   module Schools
     class DualProviderCopier
-      def initialize(legacy_copier:, new_copier:)
+      def initialize(legacy_copier:, new_copier:, uuid_syncer: ProviderSchools::SyncLegacySiteUuids)
         @legacy_copier = legacy_copier
         @new_copier = new_copier
+        @uuid_syncer = uuid_syncer
       end
 
       def execute(provider:, new_provider:)
         legacy_result = legacy_copier.execute(provider:, new_provider:)
         new_copier.execute(provider:, new_provider:)
+        uuid_syncer.call(provider: new_provider)
 
         legacy_result
       end
 
     private
 
-      attr_reader :legacy_copier, :new_copier
+      attr_reader :legacy_copier, :new_copier, :uuid_syncer
     end
   end
 end
