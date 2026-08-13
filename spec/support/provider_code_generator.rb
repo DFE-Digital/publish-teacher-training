1803031c8bdae4afeacb9f7a204251bd60746f1b
Iain McNulty <iain.mcnulty@education.gov.uk>
2026-08-13 10:12:49 +0100

    Flakey test: Avoid generating GCSE grade 5 provider code by chance
      Just one provider code exists that renders a different GCSE grade
      requirement to the rest of the providers.
    
      We should avoid generating provider with code I30 by chance and
      require it to be generated with a dedicated factory.

--- spec/support/provider_code_generator.rb
+++ spec/support/provider_code_generator.rb
@@ -4,16 +4,18 @@ class ProviderCodeGenerator
   def initialize(sequence_number)
     @sequence_number = sequence_number
     @existing_codes = Provider.pluck(:provider_code).to_set
+    @unwanted_codes = @existing_codes
   end
 
-  def call
+  def call(avoid: [])
     attempt_count = 0
     possible_code = nil
+    @unwanted_codes += avoid
 
-    until possible_code && @existing_codes.exclude?(possible_code)
+    until possible_code && @unwanted_codes.exclude?(possible_code)
       possible_code = sprintf("#{('A'..'Z').to_a.sample}%02d", @sequence_number % 100)
 
-      if @existing_codes.include?(possible_code)
+      if @unwanted_codes.include?(possible_code)
         attempt_count += 1
         Rails.logger.warn("ProviderCodeGenerator: Collision detected for #{possible_code}, retrying (attempt ##{attempt_count})")
       end
--- spec/system/publish/courses/gcse_equivalency_spec.rb
+++ spec/system/publish/courses/gcse_equivalency_spec.rb
@@ -28,6 +28,7 @@ RSpec.describe "GCSE equivalency requirements" do
   scenario "a provider views course pages with course 2 GCSE requirements" do
     given_i_am_authenticated(user: user_with_courses)
     when_i_visit_the_course_page(course: course2)
+
     then_i_see_course_2_gcse_requirements
   end
 
@@ -35,7 +36,14 @@ RSpec.describe "GCSE equivalency requirements" do
     given_i_am_authenticated(user: user_with_courses)
     when_i_visit_the_course_page(course: course3)
 
-    then_i_see_course_3_gcse_requirements
+    then_i_see_course_3_gcse_requirements(grade: 4)
+  end
+
+  scenario "a provider with Grade 5 GCSE requirements" do
+    given_i_am_authenticated(user: user_with_courses(:gcse_grade_5))
+    when_i_visit_the_course_page(course: course3)
+
+    then_i_see_course_3_gcse_requirements(grade: 5)
   end
 
   scenario "a provider has completed the pending GCSE & equivalency requirements and sees their answer pre-populated on the gcse requirements page" do
@@ -48,63 +56,38 @@ RSpec.describe "GCSE equivalency requirements" do
   scenario "a provider copies gcse data from another course with all fields" do
     given_i_am_authenticated(user: user_with_courses)
     when_i_visit_the_course_publish_courses_gcse_requirements_page(course:)
-    publish_courses_gcse_requirements_page.copy_content.copy(course3)
+    and_i_copy_the_content_from(course3)
 
-    [
-      "Your changes are not yet saved",
-      "Accept pending GCSE",
-      "Accept GCSE equivalency",
-      "Accept English GCSE equivalency",
-      "Accept Maths GCSE equivalency",
-      "Additional GCSE equivalencies",
-    ].each do |name|
-      expect(publish_courses_gcse_requirements_page.copy_content_warning).to have_content(name)
-    end
-
-    expect(publish_courses_gcse_requirements_page.pending_gcse_yes_radio).to be_checked
-    expect(publish_courses_gcse_requirements_page.gcse_equivalency_yes_radio).to be_checked
-    expect(publish_courses_gcse_requirements_page.english_equivalency).to be_checked
-    expect(publish_courses_gcse_requirements_page.maths_equivalency).to be_checked
-    expect(publish_courses_gcse_requirements_page.additional_requirements.value).to eq course3.additional_gcse_equivalencies
+    then_i_see_the_copied_fields_warning
+    and_i_see_the_form_pre_populated
   end
 
   scenario "a provider copies gcse data from another course with missing fields" do
     given_i_am_authenticated(user: user_with_courses)
     when_i_visit_the_course_publish_courses_gcse_requirements_page(course:)
-    publish_courses_gcse_requirements_page.copy_content.copy(course2)
+    and_i_copy_the_content_from(course2)
 
-    expect(publish_courses_gcse_requirements_page).not_to have_copy_content_warning
-    expect(publish_courses_gcse_requirements_page.pending_gcse_no_radio).to be_checked
-    expect(publish_courses_gcse_requirements_page.gcse_equivalency_no_radio).to be_checked
-    expect(publish_courses_gcse_requirements_page.english_equivalency).not_to be_checked
-    expect(publish_courses_gcse_requirements_page.maths_equivalency).not_to be_checked
-    expect(publish_courses_gcse_requirements_page.additional_requirements.text).to eq("")
+    then_i_do_not_see_the_copied_fields_warning
+    and_i_see_the_form_is_empty
   end
 
 private
 
-  def user_with_courses
-    course = build(:course, :secondary, course_code: "XXX1", additional_gcse_equivalencies: "")
-    course2 = build(:course, :primary, course_code: "XXX2", accept_pending_gcse: false, accept_gcse_equivalency: false, additional_gcse_equivalencies: nil)
-    course3 = build(:course, :secondary,
-                    course_code: "XXX3", accept_pending_gcse: true, accept_gcse_equivalency: true,
-                    accept_english_gcse_equivalency: true, accept_maths_gcse_equivalency: true, accept_science_gcse_equivalency: nil,
-                    additional_gcse_equivalencies: "Cycling Proficiency")
+  def user_with_courses(*provider_traits)
+    provider = build(:provider, *provider_traits, courses: gcse_courses)
 
-    provider = build(
-      :provider, courses: [course, course2, course3]
-    )
-
-    create(
-      :user,
-      providers: [provider],
-    )
+    create(:user, providers: [provider])
   end
 
-  def when_i_visit_the_course_publish_courses_gcse_requirements_page(course:)
-    publish_courses_gcse_requirements_page.load(
-      provider_code: provider.provider_code, recruitment_cycle_year: provider.recruitment_cycle_year, course_code: course.course_code,
-    )
+  def gcse_courses
+    [
+      build(:course, :secondary, course_code: "XXX1", additional_gcse_equivalencies: ""),
+      build(:course, :primary, course_code: "XXX2", accept_pending_gcse: false, accept_gcse_equivalency: false, additional_gcse_equivalencies: nil),
+      build(:course, :secondary,
+            course_code: "XXX3", accept_pending_gcse: true, accept_gcse_equivalency: true,
+            accept_english_gcse_equivalency: true, accept_maths_gcse_equivalency: true, accept_science_gcse_equivalency: nil,
+            additional_gcse_equivalencies: "Cycling Proficiency"),
+    ]
   end
 
   def provider
@@ -123,17 +106,20 @@ private
     @course3 ||= provider.courses.find_by(course_code: "XXX3")
   end
 
-  def when_i_visit_the_course_page(course:)
-    publish_provider_courses_show_page.load(
+  def gcse_requirements_page
+    publish_courses_gcse_requirements_page
+  end
+
+  def when_i_visit_the_course_publish_courses_gcse_requirements_page(course:)
+    gcse_requirements_page.load(
       provider_code: provider.provider_code, recruitment_cycle_year: provider.recruitment_cycle_year, course_code: course.course_code,
     )
   end
 
-  def then_i_see_course_3_gcse_requirements
-    expect(page).to have_content("Grade 4 (C) or above in English and maths")
-    expect(page).to have_content("Candidates with pending GCSEs will be considered")
-    expect(page).to have_content("Equivalency tests will be accepted in English or maths")
-    expect(page).to have_content("Cycling Proficiency")
+  def when_i_visit_the_course_page(course:)
+    publish_provider_courses_show_page.load(
+      provider_code: provider.provider_code, recruitment_cycle_year: provider.recruitment_cycle_year, course_code: course.course_code,
+    )
   end
 
   def then_i_see_course_2_gcse_requirements
@@ -142,16 +128,51 @@ private
     expect(page).to have_content("Equivalency tests will not be accepted")
   end
 
+  def then_i_see_course_3_gcse_requirements(grade:)
+    expect(page).to have_content("Grade #{grade} (C) or above in English and maths")
+    expect(page).to have_content("Candidates with pending GCSEs will be considered")
+    expect(page).to have_content("Equivalency tests will be accepted in English or maths")
+    expect(page).to have_content("Cycling Proficiency")
+  end
+
   def then_i_see_the_form_pre_populated
-    expect(publish_courses_gcse_requirements_page.pending_gcse_yes_radio).to be_checked
-    expect(publish_courses_gcse_requirements_page.gcse_equivalency_yes_radio).to be_checked
-    expect(publish_courses_gcse_requirements_page.english_equivalency).to be_checked
-    expect(publish_courses_gcse_requirements_page.maths_equivalency).to be_checked
-    expect(publish_courses_gcse_requirements_page.additional_requirements).to have_content("Cycling Proficiency")
+    expect(gcse_requirements_page.pending_gcse_yes_radio).to be_checked
+    expect(gcse_requirements_page.gcse_equivalency_yes_radio).to be_checked
+    expect(gcse_requirements_page.english_equivalency).to be_checked
+    expect(gcse_requirements_page.maths_equivalency).to be_checked
+    expect(gcse_requirements_page.additional_requirements.value).to eq("Cycling Proficiency")
+  end
+  alias_method :and_i_see_the_form_pre_populated, :then_i_see_the_form_pre_populated
+
+  def and_i_see_the_form_is_empty
+    expect(gcse_requirements_page.pending_gcse_no_radio).to be_checked
+    expect(gcse_requirements_page.gcse_equivalency_no_radio).to be_checked
+    expect(gcse_requirements_page.english_equivalency).not_to be_checked
+    expect(gcse_requirements_page.maths_equivalency).not_to be_checked
+    expect(gcse_requirements_page.additional_requirements.value).to eq("")
+  end
+
+  def and_i_copy_the_content_from(course)
+    gcse_requirements_page.copy_content.copy(course)
+  end
+
+  def then_i_see_the_copied_fields_warning
+    warning = gcse_requirements_page.copy_content_warning
+
+    expect(warning).to have_content("Your changes are not yet saved")
+    expect(warning).to have_content("Accept pending GCSE")
+    expect(warning).to have_content("Accept GCSE equivalency")
+    expect(warning).to have_content("Accept English GCSE equivalency")
+    expect(warning).to have_content("Accept Maths GCSE equivalency")
+    expect(warning).to have_content("Additional GCSE equivalencies")
+  end
+
+  def then_i_do_not_see_the_copied_fields_warning
+    expect(gcse_requirements_page).not_to have_copy_content_warning
   end
 
   def and_i_click_save
-    publish_courses_gcse_requirements_page.save.click
+    gcse_requirements_page.save.click
   end
 
   def and_i_see_pending_gcse_and_equivalency_tests_errors
@@ -160,12 +181,12 @@ private
   end
 
   def and_i_set_the_gcse_requirements
-    publish_courses_gcse_requirements_page.pending_gcse_yes_radio.click
-    publish_courses_gcse_requirements_page.gcse_equivalency_yes_radio.click
+    gcse_requirements_page.pending_gcse_yes_radio.click
+    gcse_requirements_page.gcse_equivalency_yes_radio.click
   end
 
   def and_there_is_no_science_equivalency
-    expect(publish_courses_gcse_requirements_page).not_to have_science_equivalency
+    expect(gcse_requirements_page).not_to have_science_equivalency
   end
 
   def and_i_see_equivalency_errors
@@ -180,22 +201,20 @@ private
   end
 
   def when_i_fill_the_equivalency_requirements
-    publish_courses_gcse_requirements_page.english_equivalency.check
-    publish_courses_gcse_requirements_page.maths_equivalency.check
-    publish_courses_gcse_requirements_page.additional_requirements.set("Cycling Proficiency")
+    gcse_requirements_page.english_equivalency.check
+    gcse_requirements_page.maths_equivalency.check
+    gcse_requirements_page.additional_requirements.set("Cycling Proficiency")
   end
 
-  def and_i_am_on_the_course_page
+  def then_i_am_on_the_course_page
     expect(page).to have_current_path publish_provider_recruitment_cycle_course_path(
       provider.provider_code,
       course.recruitment_cycle.year,
       course.course_code,
     )
   end
-  alias_method :then_i_am_on_the_course_page, :and_i_am_on_the_course_page
 
   def and_i_see_the_success_summary
-    expect(publish_provider_courses_index_page.success_summary).to have_content(I18n.t("success.saved", value: "GCSE requirements"))
+    expect(publish_provider_courses_index_page.success_summary).to have_content("GCSE requirements updated")
   end
-  alias_method :then_i_see_the_success_summary, :and_i_see_the_success_summary
 end
