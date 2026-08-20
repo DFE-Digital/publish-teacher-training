3b18f27de7416714c3f94935c8175ea075f43c83
Sam Johnson <sam.johnson@tecknuovo.com>
2026-08-20 22:49:52 +0100

    Move error reporting rate limiter onto Rails.cache

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -4,8 +4,4 @@ class RedisClient
   def self.current
     @current ||= Redis.new(url: ENV.fetch("REDIS_WORKER_URL", nil))
   end
-
-  def self.cache
-    @cache ||= Redis.new(url: ENV.fetch("REDIS_CACHE_URL") { ENV.fetch("REDIS_WORKER_URL", nil) })
-  end
 end
