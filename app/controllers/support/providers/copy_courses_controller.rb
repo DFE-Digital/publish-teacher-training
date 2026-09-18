07e43c2e93d1195721534224d9669ec098deddbe
Tomas D'Stefano <tomas_stefano@successoft.com>
2026-09-18 10:48:29 +0100

    Preload course subjects when copying a provider's courses
    Courses::CopyToProviderService copies each course's subjects, reading
    course_subjects once per course.
    
    Clears the baseline line
      Course => [:course_subjects] @ app/models/course.rb

--- app/controllers/support/providers/copy_courses_controller.rb
+++ app/controllers/support/providers/copy_courses_controller.rb
@@ -23,7 +23,7 @@ module Support
           )
 
           Provider.transaction do
-            @copy_courses_form.provider.courses.includes(:latest_enrichment, provider: :recruitment_cycle).map do |course|
+            @copy_courses_form.provider.courses.includes(:latest_enrichment, :course_subjects, provider: :recruitment_cycle).map do |course|
               copier.execute(course:, new_provider: @copy_courses_form.target_provider)
             end
           end
