8088f5f7cbe1d9dc5672f52b23c23d126f9f462d
Keith McDonnell <keith@dancingtext.com>
2026-09-21 14:58:39 +0100

    Add 'date last signed in' and 'remove user' to the Users landing page

--- spec/support/page_objects/sections/user.rb
+++ spec/support/page_objects/sections/user.rb
@@ -5,7 +5,10 @@ require_relative "base"
 module PageObjects
   module Sections
     class User < PageObjects::Sections::Base
-      element :full_name, ".govuk-link"
+      element :full_name, ".govuk-table__cell:first-child .govuk-link"
+      element :email, ".govuk-table__cell:nth-child(2)"
+      element :last_signed_in, ".last-signed-in"
+      element :remove_user_link, ".remove-user .govuk-link"
     end
   end
 end
