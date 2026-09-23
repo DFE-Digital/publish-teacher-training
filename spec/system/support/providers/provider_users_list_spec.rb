2c020ba750913f69c1f9b90e076a7d9ff0fc9aa4
Keith McDonnell <keith@dancingtext.com>
2026-09-23 10:06:42 +0100

    Address review comments on the users list

--- spec/system/support/providers/provider_users_list_spec.rb
+++ spec/system/support/providers/provider_users_list_spec.rb
@@ -12,6 +12,7 @@ RSpec.describe "View provider users" do
     and_click_on_the_users_tab
     then_i_should_see_a_table_of_users
     and_i_see_the_month_and_year_they_last_signed_in
+    and_i_see_that_a_user_who_never_signed_in_says_so
   end
 
   scenario "i can remove a user from the list" do
@@ -26,6 +27,7 @@ RSpec.describe "View provider users" do
   def and_there_is_a_provider
     @provider = create(:provider, :with_users)
     @signed_in_user = create(:user, first_name: "Ada", last_name: "Aardvark", last_login_date_utc: Time.zone.local(2024, 10, 15, 9, 30), providers: [@provider])
+    @never_signed_in_user = create(:user, first_name: "Bob", last_name: "Bobson", providers: [@provider])
   end
 
   def when_i_visit_the_support_provider_show_page
@@ -37,15 +39,19 @@ RSpec.describe "View provider users" do
   end
 
   def then_i_should_see_a_table_of_users
-    expect(support_provider_users_index_page.users.size).to eq(5)
+    expect(support_provider_users_index_page.users.size).to eq(6)
   end
 
   def and_i_see_the_month_and_year_they_last_signed_in
-    expect(signed_in_user_row.last_signed_in.text).to eq("October 2024")
+    expect(page).to have_table(with_rows: [{ "Name" => @signed_in_user.full_name, "Last signed in" => "October 2024" }])
+  end
+
+  def and_i_see_that_a_user_who_never_signed_in_says_so
+    expect(page).to have_table(with_rows: [{ "Name" => @never_signed_in_user.full_name, "Last signed in" => "Never signed in" }])
   end
 
   def and_i_click_remove_user_for_the_user_who_has_signed_in
-    signed_in_user_row.remove_user_link.click
+    click_link_or_button "Remove user for #{@signed_in_user.full_name}"
   end
 
   def then_i_am_taken_to_the_support_provider_user_delete_page
@@ -54,9 +60,5 @@ RSpec.describe "View provider users" do
       provider_id: @provider.id,
       id: @signed_in_user.id,
     )
-  end
-
-  def signed_in_user_row
-    support_provider_users_index_page.users.find { |row| row.full_name.text == @signed_in_user.full_name }
   end
 end
