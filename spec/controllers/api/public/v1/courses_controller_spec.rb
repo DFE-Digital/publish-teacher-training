9e91805903b8a9380746991d3c904572a9437842
gms-gs <george.schena@education.gov.uk>
2026-09-25 13:47:49 +0100

    6.3.2 School Remodel: Remove findable filtering from API queries

--- spec/controllers/api/public/v1/courses_controller_spec.rb
+++ spec/controllers/api/public/v1/courses_controller_spec.rb
@@ -55,6 +55,20 @@ RSpec.describe API::Public::V1::CoursesController do
         expect(findable_by_id[published_course.id.to_s]).to be(true)
         expect(findable_by_id[draft_course.id.to_s]).to be(false)
       end
+
+      it "ignores filter[findable] when querying courses" do
+        published_course = create(:course, :published, provider:)
+        draft_course = create(:course, :draft_enrichment, provider:)
+
+        get :index, params: {
+          recruitment_cycle_year: recruitment_cycle.year,
+          filter: { findable: true },
+        }
+
+        ids = json_response["data"].map { |course| course["id"] }
+
+        expect(ids).to include(published_course.id.to_s, draft_course.id.to_s)
+      end
     end
 
     context "when the recruitment cycle is not after the remodel cutover year" do
@@ -79,6 +93,18 @@ RSpec.describe API::Public::V1::CoursesController do
 
         expect(course.site_statuses).to be_empty
         expect(json_response["data"].sole["attributes"]["findable"]).to be(true)
+      end
+
+      it "ignores filter[findable] and does not apply Course.findable" do
+        course = create(:course, provider:)
+
+        get :index, params: {
+          recruitment_cycle_year: recruitment_cycle.year,
+          filter: { findable: true },
+        }
+
+        expect(course.site_statuses).to be_empty
+        expect(json_response["data"].map { |returned_course| returned_course["id"] }).to include(course.id.to_s)
       end
     end
 
