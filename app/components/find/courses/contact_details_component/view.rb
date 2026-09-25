2b668c39b4b31ddf056932e6f4fc6475853a5783
Sam Johnson <sam.johnson@tecknuovo.com>
2026-09-25 14:42:00 +0100

    Link provider website directly in Publish course preview

--- app/components/find/courses/contact_details_component/view.rb
+++ app/components/find/courses/contact_details_component/view.rb
@@ -4,18 +4,27 @@ module Find
   module Courses
     module ContactDetailsComponent
       class View < ViewComponent::Base
-        attr_reader :course
+        attr_reader :course, :preview
 
         delegate :provider, to: :course
         delegate :contact_form?, :contact_form, :show_address?, to: :course_information_config
 
-        def initialize(course)
+        def initialize(course, preview: false)
           super()
           @course = course
+          @preview = preview
         end
 
         def show_contact_form_instead_of_email?
           contact_form?
+        end
+
+        def website_url
+          if preview
+            provider.decorate.website
+          else
+            find_track_click_path(url: find_provider_website_path(course.provider_code, course.course_code))
+          end
         end
 
       private
